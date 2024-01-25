07d2c225832a7f28318b2afa1941af750bec9070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:28 +0000

    Add package ikpls/1.0.1/ikpls-1.0.1.tar.gz

---
+++ packages/ikpls/ikpls-1.0.1.tar.gz/ikpls-1.0.1/ikpls/jax_ikpls_alg_1.py
@@ -0,0 +1,372 @@
+from functools import partial
+from typing import Tuple
+
+import jax
+import jax.numpy as jnp
+from jax.experimental import host_callback
+
+from ikpls.jax_ikpls_base import PLSBase
+
+
+class PLS(PLSBase):
+    """
+    Implements partial least-squares regression using Improved Kernel PLS Algorithm #1 by Dayal and MacGregor: https://doi.org/10.1002/(SICI)1099-128X(199701)11:1%3C73::AID-CEM435%3E3.0.CO;2-%23.
+
+    Parameters
+    ----------
+    reverse_differentiable: bool, optional (default=False). Whether to make the implementation end-to-end differentiable. The differentiable version is slightly slower. Results among the two versions are identical.
+
+    verbose : bool, optional (default=False). If True, each sub-function will print when it will be JIT compiled. This can be useful to track if recompilation is triggered due to passing inputs with different shapes.
+    """
+
+    def __init__(
+        self, reverse_differentiable: bool = False, verbose: bool = False
+    ) -> None:
+        name = "Improved Kernel PLS Algorithm #1"
+        super().__init__(
+            name=name, reverse_differentiable=reverse_differentiable, verbose=verbose
+        )
+
+    @partial(jax.jit, static_argnums=(0, 1, 2, 3, 4))
+    def _get_initial_matrices(
+        self, A: int, K: int, M: int, N: int
+    ) -> Tuple[
+        jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray
+    ]:
+        """
+        Initialize the matrices and arrays needed for the PLS algorithm. This method is part of the PLS fitting process.
+
+        Parameters
+        ----------
+        A : int
+            Number of components in the PLS model.
+
+        K : int
+            Number of predictor variables.
+
+        M : int
+            Number of response variables.
+
+        N : int
+            Number of samples.
+
+        Returns
+        -------
+        B : Array of shape (A, K, M)
+            PLS regression coefficients tensor.
+
+        W : Array of shape (A, K)
+            PLS weights matrix for X.
+
+        P : Array of shape (A, K)
+            PLS loadings matrix for X.
+
+        Q : Array of shape (A, M)
+            PLS Loadings matrix for Y.
+
+        R : Array of shape (A, K)
+            PLS weights matrix to compute scores T directly from original X.
+
+        T : Array of shape (A, N)
+            PLS scores matrix of X.
+        """
+        if self.verbose:
+            print(f"_get_initial_matrices for {self.name} will be JIT compiled...")
+        B, W, P, Q, R = super()._get_initial_matrices(A, K, M)
+        T = jnp.empty(shape=(A, N), dtype=jnp.float64)
+        return B, W, P, Q, R, T
+
+    @partial(jax.jit, static_argnums=(0,))
+    def _step_1(self, X: jnp.ndarray, Y: jnp.ndarray) -> jnp.ndarray:
+        """
+        Perform the first step of Improved Kernel PLS Algorithm #1.
+
+        Parameters
+        ----------
+        X : Array of shape (N, K)
+            Predictor variables.
+
+        Y : Array of shape (N, M)
+            Response variables.
+
+        Returns
+        -------
+        XTY : Array of shape (K, M)
+            Intermediate result used in the PLS algorithm.
+        """
+        if self.verbose:
+            print(f"_step_1 for {self.name} will be JIT compiled...")
+        return self._compute_initial_XTY(X.T, Y)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def _step_4(self, X: jnp.ndarray, XTY: jnp.ndarray, r: jnp.ndarray):
+        """
+        Perform the fourth step of Improved Kernel PLS Algorithm #1.
+
+        Parameters
+        ----------
+        X : Array of shape (N, K)
+            Predictor variables.
+
+        XTY : Array of shape (K, M)
+            Intermediate result used in the PLS algorithm.
+
+        r : Array of shape (K, 1)
+            Intermediate result used in the PLS algorithm.
+
+        Returns
+        -------
+        tTt : float
+            Intermediate result used in the PLS algorithm.
+
+        p : Array of shape (K, 1)
+            Intermediate result used in the PLS algorithm.
+
+        q : Array of shape (M, 1)
+            Intermediate result used in the PLS algorithm.
+
+        t : Array of shape (N, 1)
+            Intermediate result used in the PLS algorithm.
+
+        See Also
+        --------
+        _step_1 : Computes the initial intermediate result in the PLS algorithm.
+        """
+        if self.verbose:
+            print(f"_step_4 for {self.name} will be JIT compiled...")
+        t = X @ r
+        tT = t.T
+        tTt = tT @ t
+        p = (tT @ X).T / tTt
+        q = (r.T @ XTY).T / tTt
+        return tTt, p, q, t
+
+    @partial(jax.jit, static_argnums=(0, 1, 5, 6, 9))
+    def _main_loop_body(
+        self,
+        A: int,
+        i: int,
+        X: jnp.ndarray,
+        XTY: jnp.ndarray,
+        M: int,
+        K: int,
+        P: jnp.ndarray,
+        R: jnp.ndarray,
+        reverse_differentiable: bool,
+    ) -> Tuple[
+        jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray
+    ]:
+        """
+        Execute the main loop body of Improved Kernel PLS Algorithm #1. This function performs various steps of the PLS algorithm for each component.
+
+        Parameters
+        ----------
+        A : int
+            Number of components in the PLS model.
+
+        i : int
+            Current component index.
+
+        X : Array of shape (N, K)
+            Predictor variables.
+
+        XTY : Array of shape (K, M)
+            Intermediate result used in the PLS algorithm.
+
+        M : int
+            Number of response variables.
+
+        K : int
+            Number of predictor variables.
+
+        P : Array of shape (K, A)
+            PLS loadings matrix for X.
+
+        R : Array of shape (K, A)
+            PLS weights matrix to compute scores T directly from original X.
+
+        reverse_differentiable : bool
+            Whether to use a reverse_differentiable version of the algorithm.
+
+        Returns
+        -------
+        XTY : Array of shape (K, M)
+            Updated intermediate result used in the PLS algorithm.
+
+        w : Array of shape (K, 1)
+            Updated intermediate result used in the PLS algorithm.
+
+        p : Array of shape (K, 1)
+            Updated intermediate result used in the PLS algorithm.
+
+        q : Array of shape (M, 1)
+            Updated intermediate result used in the PLS algorithm.
+
+        r : Array of shape (K, 1)
+            Updated intermediate result used in the PLS algorithm.
+
+        t : Array of shape (N, 1)
+            Updated intermediate result used in the PLS algorithm.
+
+
+        Warns
+        -----
+        UserWarning.
+            If at any point during iteration over the number of components `A`, the residual goes below machine precision for jnp.float64.
+        """
+        if self.verbose:
+            print(f"_main_loop_body for {self.name} will be JIT compiled...")
+        # step 2
+        w, norm = self._step_2(XTY, M, K)
+        host_callback.id_tap(self._weight_warning, [i, norm])
+        # step 3
+        if reverse_differentiable:
+            r = self._step_3(A, w, P, R)
+        else:
+            r = self._step_3(i, w, P, R)
+        # step 4
+        tTt, p, q, t = self._step_4(X, XTY, r)
+        # step 5
+        XTY = self._step_5(XTY, p, q, tTt)
+        return XTY, w, p, q, r, t
+
+    def fit(self, X: jnp.ndarray, Y: jnp.ndarray, A: int) -> None:
+        """
+        Fits Improved Kernel PLS Algorithm #1 on `X` and `Y` using `A` components.
+
+        Parameters
+        ----------
+        X : Array of shape (N, K)
+            Predictor variables. Its dtype will be converted to float64 for reliable results.
+
+        Y : Array of shape (N, M)
+            Response variables. Its dtype will be converted to float64 for reliable results.
+
+        A : int
+            Number of components in the PLS model.
+
+        Attributes
+        ----------
+        B : Array of shape (A, K, M)
+            PLS regression coefficients tensor.
+
+        W : Array of shape (K, A)
+            PLS weights matrix for X.
+
+        P : Array of shape (K, A)
+            PLS loadings matrix for X.
+
+        Q : Array of shape (M, A)
+            PLS Loadings matrix for Y.
+
+        R : Array of shape (K, A)
+            PLS weights matrix to compute scores T directly from original X.
+
+        T : Array of shape (N, A)
+            PLS scores matrix of X.
+
+        Returns
+        -------
+        None.
+
+        Warns
+        -----
+        UserWarning.
+            If at any point during iteration over the number of components `A`, the residual goes below machine precision for jnp.float64.
+
+        See Also
+        --------
+        stateless_fit : Performs the same operation but returns the output matrices instead of storing them in the class instance.
+        """
+        X = jnp.asarray(X, dtype=jnp.float64)  # Ensure float64 precision
+        Y = jnp.asarray(Y, dtype=jnp.float64)  # Ensure float64 precision
+        self.B, W, P, Q, R, T = self.stateless_fit(X, Y, A)
+        self.W = W.T
+        self.P = P.T
+        self.Q = Q.T
+        self.R = R.T
+        self.T = T.T
+
+    @partial(jax.jit, static_argnums=(0, 3))
+    def stateless_fit(
+        self, X: jnp.ndarray, Y: jnp.ndarray, A: int
+    ) -> Tuple[
+        jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray
+    ]:
+        """
+        Fits Improved Kernel PLS Algorithm #1 on `X` and `Y` using `A` components. Returns the internal matrices instead of storing them in the class instance.
+
+        Parameters
+        ----------
+        X : Array of shape (N, K)
+            Predictor variables. Its dtype will be converted to float64 for reliable results.
+
+        Y : Array of shape (N, M)
+            Response variables. Its dtype will be converted to float64 for reliable results.
+
+        A : int
+            Number of components in the PLS model.
+
+        Returns
+        -------
+        B : Array of shape (A, K, M)
+            PLS regression coefficients tensor.
+
+        W : Array of shape (A, K)
+            PLS weights matrix for X.
+
+        P : Array of shape (A, K)
+            PLS loadings matrix for X.
+
+        Q : Array of shape (A, M)
+            PLS Loadings matrix for Y.
+
+        R : Array of shape (A, K)
+            PLS weights matrix to compute scores T directly from original X.
+
+        T : Array of shape (A, N)
+            PLS scores matrix of X.
+
+        Warns
+        -----
+        UserWarning.
+            If at any point during iteration over the number of components `A`, the residual goes below machine precision for jnp.float64.
+
+        See Also
+        --------
+        fit : Performs the same operation but stores the output matrices in the class instance instead of returning them.
+
+        Notes
+        -----
+        For optimization purposes, the internal representation of all matrices (except B) is transposed from the usual representation.
+        """
+
+        if self.verbose:
+            print(f"stateless_fit for {self.name} will be JIT compiled...")
+
+        X = jnp.asarray(X, dtype=jnp.float64)  # Ensure float64 precision
+        Y = jnp.asarray(Y, dtype=jnp.float64)  # Ensure float64 precision
+
+        # Get shapes
+        N, K = X.shape
+        M = Y.shape[1]
+
+        # Initialize matrices
+        B, W, P, Q, R, T = self._get_initial_matrices(A, K, M, N)
+
+        # step 1
+        XTY = self._step_1(X, Y)
+
+        for i in range(A):
+            XTY, w, p, q, r, t = self._main_loop_body(
+                A, i, X, XTY, M, K, P, R, self.reverse_differentiable
+            )
+            W = W.at[i].set(w.squeeze())
+            P = P.at[i].set(p.squeeze())
+            Q = Q.at[i].set(q.squeeze())
+            R = R.at[i].set(r.squeeze())
+            T = T.at[i].set(t.squeeze())
+            b = self._compute_regression_coefficients(B[i - 1], r, q)
+            B = B.at[i].set(b)
+
+        return B, W, P, Q, R, T
---
+++ packages/ikpls/ikpls-1.0.1.tar.gz/ikpls-1.0.1/ikpls/numpy_ikpls.py
@@ -0,0 +1,194 @@
+import warnings
+from typing import Union
+
+import numpy as np
+import numpy.linalg as la
+import numpy.typing as npt
+from sklearn.base import BaseEstimator
+
+
+class PLS(BaseEstimator):
+    """
+    Implements partial least-squares regression using Improved Kernel PLS by Dayal and MacGregor: https://doi.org/10.1002/(SICI)1099-128X(199701)11:1%3C73::AID-CEM435%3E3.0.CO;2-%23
+
+    Parameters
+    ----------
+    algorithm : int
+        Whether to use Improved Kernel PLS Algorithm #1 or #2. Defaults to 1.
+
+    dtype : numpy.float, default=numpy.float64
+        The float datatype to use in computation of the PLS algorithm. Using a lower precision than float64 will yield significantly worse results when using an increasing number of components due to propagation of numerical errors.
+
+    Raises
+    ------
+    ValueError
+        If `algorithm` is not 1 or 2.
+    """
+
+    def __init__(self, algorithm: int = 1, dtype: np.float_ = np.float64) -> None:
+        self.algorithm = algorithm
+        self.dtype = dtype
+        self.name = f"Improved Kernel PLS Algorithm #{algorithm}"
+        if self.algorithm not in [1, 2]:
+            raise ValueError(
+                f"Invalid algorithm: {self.algorithm}. Algorithm must be 1 or 2."
+            )
+
+    def fit(self, X: npt.ArrayLike, Y: npt.ArrayLike, A: int) -> None:
+        """
+        Fits Improved Kernel PLS Algorithm #1 on `X` and `Y` using `A` components.
+
+        Parameters
+        ----------
+        X : Array of shape (N, K)
+            Predictor variables.
+
+        Y : Array of shape (N, M)
+            Response variables.
+
+        A : int
+            Number of components in the PLS model.
+
+        Attributes
+        ----------
+        B : Array of shape (A, K, M)
+            PLS regression coefficients tensor.
+
+        W : Array of shape (K, A)
+            PLS weights matrix for X.
+
+        P : Array of shape (K, A)
+            PLS loadings matrix for X.
+
+        Q : Array of shape (M, A)
+            PLS Loadings matrix for Y.
+
+        R : Array of shape (K, A)
+            PLS weights matrix to compute scores T directly from original X.
+
+        T : Array of shape (N, A)
+            PLS scores matrix of X. Only assigned for Improved Kernel PLS Algorithm #1.
+
+        Returns
+        -------
+        None.
+
+        Warns
+        -----
+        UserWarning.
+            If at any point during iteration over the number of components `A`, the residual goes below machine precision for jnp.float64.
+        """
+        X = np.asarray(X, dtype=self.dtype)
+        Y = np.asarray(Y, dtype=self.dtype)
+
+        N, K = X.shape
+        M = Y.shape[1]
+
+        self.B = np.zeros(shape=(A, K, M), dtype=self.dtype)
+        W = np.zeros(shape=(A, K), dtype=self.dtype)
+        P = np.zeros(shape=(A, K), dtype=self.dtype)
+        Q = np.zeros(shape=(A, M), dtype=self.dtype)
+        R = np.zeros(shape=(A, K), dtype=self.dtype)
+        self.W = W.T
+        self.P = P.T
+        self.Q = Q.T
+        self.R = R.T
+        if self.algorithm == 1:
+            T = np.zeros(shape=(A, N), dtype=self.dtype)
+            self.T = T.T
+        self.A = A
+        self.N = N
+        self.K = K
+        self.M = M
+
+        # Step 1
+        XTY = X.T @ Y
+
+        # Used for algorithm #2
+        if self.algorithm == 2:
+            XTX = X.T @ X
+
+        for i in range(A):
+            # Step 2
+            if M == 1:
+                norm = la.norm(XTY, ord=2)
+                if np.isclose(norm, 0, atol=np.finfo(np.float64).eps, rtol=0):
+                    warnings.warn(
+                        f"Weight is close to zero. Stopping fitting after A = {i} component(s)."
+                    )
+                    break
+                w = XTY / norm
+            else:
+                if M < K:
+                    XTYTXTY = XTY.T @ XTY
+                    eig_vals, eig_vecs = la.eigh(XTYTXTY)
+                    q = eig_vecs[:, -1:]
+                    w = XTY @ q
+                    norm = la.norm(w)
+                    if np.isclose(norm, 0, atol=np.finfo(np.float64).eps, rtol=0):
+                        warnings.warn(
+                            f"Weight is close to zero. Stopping fitting after A = {i} component(s)."
+                        )
+                        break
+                    w = w / norm
+                else:
+                    XTYYTX = XTY @ XTY.T
+                    eig_vals, eig_vecs = la.eigh(XTYYTX)
+                    norm = eig_vals[-1]
+                    if np.isclose(norm, 0, atol=np.finfo(np.float64).eps, rtol=0):
+                        warnings.warn(
+                            f"Weight is close to zero. Stopping fitting after A = {i} component(s)."
+                        )
+                        break
+                    w = eig_vecs[:, -1:]
+            W[i] = w.squeeze()
+
+            # Step 3
+            r = np.copy(w)
+            for j in range(i):
+                r = r - P[j].reshape(-1, 1).T @ w * R[j].reshape(-1, 1)
+            R[i] = r.squeeze()
+
+            # Step 4
+            if self.algorithm == 1:
+                t = X @ r
+                T[i] = t.squeeze()
+                tTt = t.T @ t
+                p = (t.T @ X).T / tTt
+            elif self.algorithm == 2:
+                rXTX = r.T @ XTX
+                tTt = rXTX @ r
+                p = rXTX.T / tTt
+            q = (r.T @ XTY).T / tTt
+            P[i] = p.squeeze()
+            Q[i] = q.squeeze()
+
+            # Step 5
+            XTY = XTY - (p @ q.T) * tTt
+
+            # Compute regression coefficients
+            self.B[i] = self.B[i - 1] + r @ q.T
+
+    def predict(
+        self, X: npt.ArrayLike, n_components: Union[None, int] = None
+    ) -> npt.NDArray[np.float_]:
+        """
+        Predicts with Improved Kernel PLS Algorithm #1 on `X` with `B` using `n_components` components. If `n_components` is None, then predictions are returned for all number of components.
+
+        Parameters
+        ----------
+        X : Array of shape (N, K)
+            Predictor variables.
+
+        n_components : int or None, optional
+            Number of components in the PLS model. If None, then all number of components are used.
+
+        Returns
+        -------
+        Y_pred : Array of shape (N, M) or (A, N, M)
+            If `n_components` is an int, then an array of shape (N, M) with the predictions for that specific number of components is used. If `n_components` is None, returns a prediction for each number of components up to `A`.
+        """
+        X = np.asarray(X, dtype=self.dtype)
+        if n_components is None:
+            return X @ self.B
+        return X @ self.B[n_components - 1]
