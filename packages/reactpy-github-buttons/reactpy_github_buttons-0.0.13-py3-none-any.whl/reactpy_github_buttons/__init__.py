13b67f9896dcb7d11c4f7bfcb4d55f531c8c97dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:09 +0000

    Add package reactpy-github-buttons/0.0.13/reactpy_github_buttons-0.0.13-py3-none-any.whl

---
+++ packages/reactpy-github-buttons/reactpy_github_buttons-0.0.13-py3-none-any.whl/reactpy_github_buttons/__init__.py
@@ -0,0 +1,7 @@
+# the version is statically loaded by setup.py
+__version__ = "0.0.13"
+
+from .button_wrapper import (
+    FollowButton,StarButton,SponsorButton, WatchButton, ForkButton,
+    IssueButton, DiscussButton, DownloadButton, InstallPackageButton,
+    UseTemplateButton, UseThisGitHubActionButton, make_color_scheme, ColorScheme)
