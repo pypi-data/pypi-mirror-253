f2f77ade7c10ca09fc705ac20a5aaad25dae398c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:40 +0000

    Add package v-stream/0.0.4/v-stream-0.0.4.tar.gz

---
+++ packages/v-stream/v-stream-0.0.4.tar.gz/v-stream-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='v-stream',
+    version='0.0.4',
+    description='STREAM: Spatio-TempoRal Evaluation and Analysis Metric for Video Generative Models',
+    author='proinit',
+    author_email='seojun.kim@unist.ac.kr',
+    url='https://github.com/pro2nit/STREAM',
+    install_requires=[
+        'torch',
+        'tqdm',
+        'numpy',
+        'top-pr'],
+    packages=find_packages(exclude=[]),
+    keywords=['pypi', 'video', 'evaluation', 'stream', 'v-stream'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
