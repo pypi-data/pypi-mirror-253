dafa059c20dd80420a19cc4254d7b14e5977ce07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:12 +0000

    Add package lichungtsaiTest/0.1.1/lichungtsaiTest-0.1.1.tar.gz

---
+++ packages/lichungtsaiTest/lichungtsaiTest-0.1.1.tar.gz/lichungtsaiTest-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lichungtsaiTest',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A simple test library for demonstration',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Li-Chung Tsai',
+    author_email='lichungtsai611@gmail.com',
+    url='https://github.com/lichungtsai611/lichungtsaiTest',
+    license='MIT'
+)
+
