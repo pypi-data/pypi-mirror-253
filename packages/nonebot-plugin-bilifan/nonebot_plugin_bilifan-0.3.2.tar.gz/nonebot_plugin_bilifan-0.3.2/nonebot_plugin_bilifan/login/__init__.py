544e0b3fb15bffbd35513a1aac6f5bd56546bd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:12 +0000

    Add package nonebot-plugin-bilifan/0.3.2/nonebot_plugin_bilifan-0.3.2.tar.gz

---
+++ packages/nonebot-plugin-bilifan/nonebot_plugin_bilifan-0.3.2.tar.gz/nonebot_plugin_bilifan-0.3.2/nonebot_plugin_bilifan/login/__init__.py
@@ -0,0 +1,153 @@
+import asyncio
+import hashlib
+import shutil
+import time
+import urllib.parse as urlparse
+from io import BytesIO
+from pathlib import Path
+
+import aiohttp
+
+# import qrcode_terminal
+import qrcode
+import yaml
+from nonebot.log import logger
+
+# Cookies = cookiejar.CookieJar()
+# session = requests.Session()
+# session.cookies = Cookies
+
+csrf = ""
+access_key = ""
+base_path = Path().joinpath("data/bilifan")
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+}
+
+
+async def is_login(session, cookies):
+    api = "https://api.bilibili.com/x/web-interface/nav"
+
+    async with session.get(api, headers=headers, cookies=cookies) as resp:
+        data = await resp.json()
+        return data["code"] == 0, data["data"]["uname"]
+
+
+async def get_tv_qrcode_url_and_auth_code():
+    api = "https://passport.bilibili.com/x/passport-login/web/qrcode/generate"
+    data = {
+        "local_id": "0",
+        "ts": str(int(time.time())),
+    }
+    await signature(data)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            api,
+            data=await map_to_string(data),
+            headers=headers,
+            # headers={"Content-Type": "application/x-www-form-urlencoded"},
+        ) as resp:
+            print(resp)
+            resp_data = await resp.json()
+            code = resp_data["code"]
+            if code == 0:
+                login_url = resp_data["data"]["url"]
+                login_key = resp_data["data"]["qrcode_key"]
+                return login_url, login_key
+            raise Exception("get_tv_qrcode_url_and_auth_code error")
+
+
+async def verify_login(auth_code, data_path):
+    api = "https://passport.bilibili.com/x/passport-login/web/qrcode/poll"
+    data = {
+        "auth_code": auth_code,
+        "local_id": "0",
+        "ts": str(int(time.time())),
+    }
+    await signature(data)
+    data_string = await map_to_string(data)
+    while True:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api, headers=headers) as resp:
+                if resp.status != 200:
+                    print(resp)
+                    raise Exception("Failed to connect to server")
+                response_dict = await resp.json()
+                code = response_dict["code"]
+                try:
+                    access_key = response_dict["data"]["access_token"]
+                except Exception:
+                    access_key = ""
+                    await asyncio.sleep(3)
+
+            if code == 0:
+                logger.success("登录成功")
+                with Path(data_path / "login_info.txt").open("w") as f:
+                    f.write(access_key)
+                if not Path(data_path / "users.yaml").is_file():
+                    logger.info("初始化配置文件")
+                    shutil.copy2(
+                        Path().joinpath("data/bilifan/users.yaml"),
+                        data_path / "users.yaml",
+                    )
+                with Path(data_path / "users.yaml").open("r", encoding="utf-8") as f:
+                    config = yaml.safe_load(f)
+                config["USERS"][0]["access_key"] = access_key
+                with Path(data_path / "users.yaml").open("w", encoding="utf-8") as f:
+                    yaml.dump(config, f, allow_unicode=True, default_flow_style=False)
+                return "access_key已保存"
+            await asyncio.sleep(3)
+
+
+appkey = "4409e2ce8ffd12b8"
+appsec = "59b43e04ad6965f34319062b478f83dd"
+
+
+async def signature(params: dict):
+    keys = list(params.keys())
+    params["appkey"] = appkey
+    keys.append("appkey")
+    keys.sort()
+    query = "&".join([k + "=" + urlparse.quote(params[k]) for k in keys])
+    query += appsec
+    hash_ = hashlib.md5(query.encode("utf-8"))
+    params["sign"] = hash_.hexdigest()
+
+
+async def map_to_string(params: dict) -> str:
+    return "&".join([k + "=" + v for k, v in params.items()])
+
+
+async def draw_QR(login_url: str):  # noqa: N802
+    "绘制二维码"
+    qr = qrcode.QRCode(version=1, box_size=10, border=4)  # type: ignore
+    qr.add_data(login_url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    return buffered.getvalue()
+    # img.save("qrcode.png")
+
+
+# async def loginBili():
+
+
+#     login_url, auth_code = await get_tv_qrcode_url_and_auth_code()
+#     qrcode_terminal.draw(login_url)
+#     print("或将此链接复制到手机B站打开:", login_url)
+#     while True:
+#         if await verify_login(auth_code):
+#             print("登录成功！")
+#             break
+#         else:
+#             time.sleep(3)
+#             print("等待扫码登录中...")
+
+
+# async def main():
+#     loginBili()
+#     input()
+
+# if __name__ == "__main__":
+#     main()
---
+++ packages/nonebot-plugin-bilifan/nonebot_plugin_bilifan-0.3.2.tar.gz/nonebot_plugin_bilifan-0.3.2/nonebot_plugin_bilifan/utils.py
@@ -0,0 +1,95 @@
+import asyncio
+import shutil
+from pathlib import Path
+
+import yaml
+from nonebot import get_bot
+
+# from nonebot.adapters import Bot
+from nonebot.log import logger
+
+from .main import mains
+
+config_dir = Path("data/bilifan")
+config_dir.mkdir(parents=True, exist_ok=True)
+CONFIG_PATH = config_dir / "config.yaml"
+if not Path().joinpath("data/bilifan/users.yaml").is_file():
+    logger.info("初始化配置文件")
+    shutil.copy2(
+        Path(__file__).parent.joinpath("users.yaml"),
+        Path().joinpath("data/bilifan/users.yaml"),
+    )
+if not CONFIG_PATH.exists():
+    # 创建一个空YAML文件
+    with CONFIG_PATH.open("w") as f:
+        yaml.dump({}, f)
+
+
+def load_config():
+    if not CONFIG_PATH.exists():
+        CONFIG_PATH.touch()
+        return {}
+
+    with CONFIG_PATH.open("r", encoding="utf-8") as f:
+        return yaml.safe_load(f) or {}
+
+
+def save_config(data):
+    with CONFIG_PATH.open("w", encoding="utf-8") as f:
+        yaml.safe_dump(data, f, allow_unicode=True, default_flow_style=False)
+
+
+async def auto_cup():
+    config = load_config()
+    count = {}
+    tasks = []
+
+    for user_id, group_id in config.items():  # noqa: B007
+        msg_path = Path().joinpath(f"data/bilifan/{user_id}/login_info.txt")
+        if msg_path.is_file:
+            pass
+        else:
+            logger.warning("usr_id尚未登录，已忽略")
+            logger.warning("usr_id尚未登录，已忽略")
+            continue
+
+        task = asyncio.create_task(mains(msg_path.parent))
+        tasks.append(task)
+
+    messageList = []
+    for task in asyncio.as_completed(tasks):
+        messageList.extend(await task)
+
+    message_str = "\n".join(messageList)
+    message_str = "\n".join(messageList)
+
+    if count:
+        for group_id, num in count.items():
+            await get_bot().send_group_msg(
+                group_id=group_id,
+                message=f"今日已完成{num}个自动刷牌子任务",
+            )
+    if message_str:
+        for user_id, group_id in config.items():
+            if user_id == group_id:
+                await get_bot().send_private_msg(user_id=user_id, message=message_str)
+                continue
+            count_value = count.get(group_id, 0)
+            count[group_id] = count_value + 1
+
+
+def render_forward_msg(msg_list: list, uid=2711142767, name="宁宁"):
+    try:
+        uid = get_bot().self_id
+        name = next(iter(get_bot().config.nickname))
+    except Exception as e:
+        logger.warning(f"获取bot信息错误\n{e}")
+    forward_msg = []
+    for msg in msg_list:
+        forward_msg.append(
+            {
+                "type": "node",
+                "data": {"name": str(name), "uin": str(uid), "content": msg},
+            },
+        )
+    return forward_msg
