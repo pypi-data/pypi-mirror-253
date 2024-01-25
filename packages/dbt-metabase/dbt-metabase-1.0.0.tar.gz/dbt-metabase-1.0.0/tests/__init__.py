c327427d16e690cb5b188c5d70d6a42d8061ae05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:06 +0000

    Add package dbt-metabase/1.0.0/dbt-metabase-1.0.0.tar.gz

---
+++ packages/dbt-metabase/dbt-metabase-1.0.0.tar.gz/dbt-metabase-1.0.0/tests/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+from dbtmetabase.format import setup_logging
+
+from .test_exposures import *
+from .test_format import *
+from .test_models import *
+
+setup_logging(level=logging.DEBUG, path=None)
