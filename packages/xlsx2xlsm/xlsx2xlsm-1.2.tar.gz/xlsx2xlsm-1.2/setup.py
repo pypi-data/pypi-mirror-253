20946d8e467716571410bbc517e592ff39ed7650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:05:16 +0000

    Add package xlsx2xlsm/1.2/xlsx2xlsm-1.2.tar.gz

---
+++ packages/xlsx2xlsm/xlsx2xlsm-1.2.tar.gz/xlsx2xlsm-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xlsx2xlsm',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        # 
+    ],
+    author='Zhui Cao',
+    author_email='857330385@qq.com',
+    description='A brief description of your package',
+    license='',
+    keywords='sample setuptools development',
+    url='https://github.com/yourusername/mypackage'
+)
