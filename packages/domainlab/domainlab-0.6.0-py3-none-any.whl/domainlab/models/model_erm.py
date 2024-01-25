a2895e5d637a1d462dace2208489448e2ec63c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:11 +0000

    Add package domainlab/0.6.0/domainlab-0.6.0-py3-none-any.whl

---
+++ packages/domainlab/domainlab-0.6.0-py3-none-any.whl/domainlab/models/model_erm.py
@@ -0,0 +1,44 @@
+"""
+Emperical risk minimization
+"""
+from domainlab.compos.nn_zoo.nn import LayerId
+from domainlab.models.a_model_classif import AModelClassif
+from domainlab.utils.override_interface import override_interface
+
+
+def mk_erm(parent_class=AModelClassif, **kwargs):
+    """
+    Instantiate a Deepall (ERM) model
+
+    Details:
+        Creates a model, which trains a neural network via standard empirical risk minimization
+        (ERM). The fact that the training data stems from different domains is neglected, as all
+        domains are pooled together during training.
+
+    Args:
+        parent_class (AModel, optional):
+            Class object determining the task type. Defaults to AModelClassif.
+
+    Returns:
+        ModelERM: model inheriting from parent class
+
+    Input Parameters:
+        custom neural network, the output dimension must be the number of labels
+
+    Usage:
+        For a concrete example, see:
+        https://github.com/marrlab/DomainLab/blob/tests/test_mk_exp_erm.py
+    """
+
+    class ModelERM(parent_class):
+        """
+        anonymous
+        """
+
+        def __init__(self, net=None, net_feat=None):
+            if net is not None:
+                net_feat = net
+                kwargs["net_classifier"] = LayerId()
+            super().__init__(**kwargs)
+            self._net_invar_feat = net_feat
+    return ModelERM
