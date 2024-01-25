3274313cfc2a517e960f3df7329156e71c5802c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:05:05 +0000

    Add package pydantic-base-aiogram/1.0.135/pydantic_base_aiogram-1.0.135.tar.gz

---
+++ packages/pydantic-base-aiogram/pydantic_base_aiogram-1.0.135.tar.gz/pydantic_base_aiogram-1.0.135/pydantic_base_aiogram/controller/file.py
@@ -0,0 +1,30 @@
+from aiogram import Router
+from aiogram.filters.state import StateFilter
+
+from pydantic_base_aiogram.types import Event
+from pydantic_base_aiogram.field_factory.base import logger
+from pydantic_base_aiogram.exceptions import DataValidationError
+from pydantic_base_aiogram.utils.proxy.album_message import ProxyAlbumMessage
+
+from .base import BaseController
+
+
+class FileController(BaseController):
+    async def format_data(self, self_, event: Event[ProxyAlbumMessage], _):
+        if not hasattr(event._event, 'album'):
+            raise NotImplementedError(f'`{self.__class__.__name__}` requires `AlbumMiddleware` for usage')
+
+        if not self._is_list:
+            if len(event._event.album) > 1:
+                raise DataValidationError(self.dialects.REQUIRED_ONLY_ONE_FILE)
+
+            return event._event.album[-1]
+
+        return event._event.album 
+
+    def register2router(self, router: Router) -> Router:
+        router.message(StateFilter(self.state))(self.__call__)
+
+        logger.debug(f"[{self.__class__.__name__}][register2router]: {locals()=};")
+        return router
+
