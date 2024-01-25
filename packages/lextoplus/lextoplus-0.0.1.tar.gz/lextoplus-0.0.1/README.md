99a0916288817e1ac86201686e91b974d9ae9a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:05:20 +0000

    Add package lextoplus/0.0.1/lextoplus-0.0.1.tar.gz

---
+++ packages/lextoplus/lextoplus-0.0.1.tar.gz/lextoplus-0.0.1/README.md
@@ -0,0 +1,13 @@
+LexToPlus Python version<br/>
+Require:<br/>
+
+-marisa_trie<br/>
+```
+  pip install marisa_trie
+```
+Example:
+```
+from LexToPlus import LexToPlus
+lexto = LexToPlus()
+print(lexto.wordInstance('ทดสอบตัดคำภาษาไทย'))
+```
---
+++ packages/lextoplus/lextoplus-0.0.1.tar.gz/lextoplus-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+#The directory containing this file
+HERE = pathlib.Path(__file__).parent
+#The text of the README file
+README = (HERE / "README.md").read_text()
+#This call to setup() does all the work
+setup(
+    name="lextoplus", # package name
+    version="0.0.1", # package version
+    author="c-tawayip", # creator username
+    author_email="piyawatchuangkrud@gmail.com", # email creator
+    description="lextoplus package", # description
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.nectec.or.th/9meo/pyLexToPlus", #directory ที่เก็บ file code
+    # url="", #directory ที่เก็บ file code
+    # license="MIT",
+     classifiers=[
+        #  "License :: OSI Approved :: MIT License",
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.8",
+     ],
+     packages=["lextoplus"], # folder ที่เก็บ package
+     include_package_data=True,
+     install_requires=['marisa_trie'], # requirement
+ )
