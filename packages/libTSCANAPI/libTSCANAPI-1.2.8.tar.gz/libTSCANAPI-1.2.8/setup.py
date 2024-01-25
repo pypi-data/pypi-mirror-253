f808fc1eaecc6de09b7598256a0fa273363b9325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:11 +0000

    Add package libTSCANAPI/1.2.8/libTSCANAPI-1.2.8.tar.gz

---
+++ packages/libTSCANAPI/libTSCANAPI-1.2.8.tar.gz/libTSCANAPI-1.2.8/setup.py
@@ -0,0 +1,45 @@
+'''
+Author: seven 865762826@qq.com
+Date: 2023-03-24 09:26:29
+LastEditors: seven 865762826@qq.com
+LastEditTime: 2023-11-24 11:46:54
+FilePath: \VSCode_Pro\Python_Pro\TSMasterApi\setup.py
+'''
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r",encoding="utf-8") as f:
+  long_description = f.read()
+
+# 
+setup(name='libTSCANAPI',  # 包名
+      version='1.2.8',  # 版本号
+      description='Use TSMaster hardware',
+      long_description=long_description,
+      author='seven',
+      author_email='865762826@qq.com',
+      install_requires=[],
+      license='MIT License',
+      packages=find_packages(),
+      data_files=[
+        ('.\\linux', ['.\\libTSCANAPI\\linux\\libTSCANApiOnLinux.so','.\\libTSCANAPI\\linux\\libTSH.so']),
+        ('.\\windows\\x64', ['.\\libTSCANAPI\\windows\\x64\\libTSCAN.dll','.\\libTSCANAPI\\windows\\x64\\libTSH.dll']),
+        ('.\\windows\\x86', ['.\\libTSCANAPI\\windows\\x86\\libTSCAN.dll','.\\libTSCANAPI\\windows\\x86\\libTSH.dll','.\\libTSCANAPI\\windows\\x86\\binlog.dll','.\\libTSCANAPI\\windows\\x86\\libLog.dll']),
+              ],
+      platforms=["WINDOWS"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
