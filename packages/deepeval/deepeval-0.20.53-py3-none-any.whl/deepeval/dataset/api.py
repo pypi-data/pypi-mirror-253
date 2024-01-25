d3253da96104e11c6b36bb1f8ddc2f52d6ca3239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:05:20 +0000

    Add package deepeval/0.20.53/deepeval-0.20.53-py3-none-any.whl

---
+++ packages/deepeval/deepeval-0.20.53-py3-none-any.whl/deepeval/dataset/api.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List, Dict
+
+
+class Golden(BaseModel):
+    input: str
+    actual_output: Optional[str] = Field(None, alias="actualOutput")
+    expected_output: Optional[str] = Field(None, alias="expectedOutput")
+    context: Optional[list] = Field(None)
+    retrieval_context: Optional[list] = Field(None)
+    additional_metadata: Optional[Dict] = Field(
+        None, alias="additionalMetadata"
+    )
+
+
+class APIDataset(BaseModel):
+    alias: str
+    overwrite: bool
+    goldens: Optional[List[Golden]] = Field(default=None)
+
+
+class CreateDatasetHttpResponse(BaseModel):
+    link: str
+
+
+class DatasetHttpResponse(BaseModel):
+    goldens: List[Golden]
