0f656208edff6920030c92e169ae7839e0f3db4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:04:06 +0000

    Add package rossum/3.17.1/rossum-3.17.1.tar.gz

---
+++ packages/rossum/rossum-3.17.1.tar.gz/rossum-3.17.1/tests/test_main.py
@@ -0,0 +1,10 @@
+from traceback import print_tb
+
+from rossum.main import entry_point
+
+
+class TestEntryPoint:
+    def test_shell(self, cli_runner):
+        result = cli_runner.invoke(entry_point)
+        assert not result.exit_code, print_tb(result.exc_info[2])
+        assert result.output.rstrip("\n").endswith("rossum> ")
