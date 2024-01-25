4c6bb8ba79af8f64a2b1f888f3fa053e091ae961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-25 14:05:22 +0000

    Add package openhexa.sdk/0.1.35/openhexa.sdk-0.1.35.tar.gz

---
+++ packages/openhexa.sdk/openhexa.sdk-0.1.35.tar.gz/openhexa.sdk-0.1.35/openhexa/sdk/__init__.py
@@ -0,0 +1,25 @@
+"""SDK package."""
+
+from .pipelines import current_run, parameter, pipeline
+from .workspaces import workspace
+from .workspaces.connection import (
+    CustomConnection,
+    DHIS2Connection,
+    GCSConnection,
+    IASOConnection,
+    PostgreSQLConnection,
+    S3Connection,
+)
+
+__all__ = [
+    "workspace",
+    "pipeline",
+    "parameter",
+    "current_run",
+    "DHIS2Connection",
+    "IASOConnection",
+    "PostgreSQLConnection",
+    "GCSConnection",
+    "S3Connection",
+    "CustomConnection",
+]
