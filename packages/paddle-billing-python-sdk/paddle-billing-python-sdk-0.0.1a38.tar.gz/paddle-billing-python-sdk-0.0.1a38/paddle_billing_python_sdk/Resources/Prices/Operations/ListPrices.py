b6558805c2684791b0d8ebba8ae9404c68536c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:04:05 +0000

    Add package paddle-billing-python-sdk/0.0.1a38/paddle-billing-python-sdk-0.0.1a38.tar.gz

---
+++ packages/paddle-billing-python-sdk/paddle-billing-python-sdk-0.0.1a38.tar.gz/paddle-billing-python-sdk-0.0.1a38/paddle_billing_python_sdk/Resources/Prices/Operations/ListPrices.py
@@ -0,0 +1,60 @@
+from paddle_billing_python_sdk.Entities.Shared.CatalogType import CatalogType
+from paddle_billing_python_sdk.Entities.Shared.Status      import Status
+
+from paddle_billing_python_sdk.Exceptions.SdkExceptions.InvalidArgumentException import InvalidArgumentException
+
+from paddle_billing_python_sdk.Resources.Prices.Operations.List.Includes import Includes
+
+
+class ListPrices:
+    def __init__(
+        self, 
+        pager             = None,
+        includes:    dict = None,
+        ids:         dict = None,
+        types:       dict = None,
+        product_ids: dict = None,
+        statuses:    dict = None,
+        recurring:   dict = None,
+    ):
+        self.pager       = pager
+        self.includes    = includes    if includes    is not None else []
+        self.ids         = ids         if ids         is not None else []
+        self.types       = types       if types       is not None else []
+        self.product_ids = product_ids if product_ids is not None else []
+        self.statuses    = statuses    if statuses    is not None else []
+        self.recurring   = recurring
+
+        # Validation
+        if any(not isinstance(pid, str) for pid in self.ids):
+            raise InvalidArgumentException('ids', 'string')
+        if any(not isinstance(include, Includes) for include in self.includes):
+            raise InvalidArgumentException('includes', Includes.__name__)
+        if any(not isinstance(pid, str) for pid in self.product_ids):
+            raise InvalidArgumentException('product_ids', 'string')
+        if any(not isinstance(status, Status) for status in self.statuses):
+            raise InvalidArgumentException('statuses', Status.__name__)
+        if any(not isinstance(ctype, CatalogType) for ctype in self.types):
+            raise InvalidArgumentException('types', CatalogType.__name__)
+
+
+    def get_parameters(self) -> dict:
+        enum_stringify = lambda enum: enum.value  # noqa E731
+
+        parameters = {}
+        if self.pager:
+            parameters.update(self.pager.get_parameters())
+        if self.ids:
+            parameters['id'] = ','.join(self.ids)
+        if self.includes:
+            parameters['include'] = ','.join(map(enum_stringify, self.includes))
+        if self.product_ids:
+            parameters['product_id'] = ','.join(self.product_ids)
+        if self.recurring is not None:
+            parameters['recurring'] = 'true' if self.recurring else 'false'
+        if self.statuses:
+            parameters['status'] = ','.join(map(enum_stringify, self.statuses))
+        if self.types:
+            parameters['type'] = ','.join(map(enum_stringify, self.types))
+
+        return parameters
