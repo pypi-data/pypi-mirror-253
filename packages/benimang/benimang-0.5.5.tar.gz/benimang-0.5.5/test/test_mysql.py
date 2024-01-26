9b0ec1d24450098a71e9523ca57671171fdca676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:04:10 +0000

    Add package benimang/0.5.5/benimang-0.5.5.tar.gz

---
+++ packages/benimang/benimang-0.5.5.tar.gz/benimang-0.5.5/test/test_mysql.py
@@ -0,0 +1,15 @@
+import pytest
+from beni.bmysql import MysqlDb
+
+
+@pytest.mark.asyncio
+async def test():
+    DB = MysqlDb(
+        host='114.132.242.93',
+        port=3306,
+        user='admin',
+        password='mydb@@709394',
+        db='test',
+    )
+    result = await DB.getValue(int, 'SELECT COUNT(*) FROM product WHERE (product_id, change_date) IN (%s, %s)', [(1, '1970-01-01'), (2, '1970-01-01')])
+    assert result == 2
