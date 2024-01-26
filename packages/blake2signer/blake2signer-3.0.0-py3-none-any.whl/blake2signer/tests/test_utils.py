cf9263ff131948e4fded70a746af91192836530d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 07:03:13 +0000

    Add package blake2signer/3.0.0/blake2signer-3.0.0-py3-none-any.whl

---
+++ packages/blake2signer/blake2signer-3.0.0-py3-none-any.whl/blake2signer/tests/test_utils.py
@@ -0,0 +1,157 @@
+"""Utils module tests."""
+
+import io
+import typing
+from datetime import datetime
+from datetime import timedelta
+
+import pytest
+
+from .. import utils
+
+
+def test_b64encode_strips_padding() -> None:
+    """Test b64encode strips padding correctly."""
+    encoded = utils.b64encode(b'abcd')
+    assert encoded == b'YWJjZA'
+
+    encoded = utils.b64encode(b'abc')
+    assert encoded == b'YWJj'
+
+
+def test_b64decode_stripped_padding_works() -> None:
+    """Test b64decode works without padding."""
+    decoded = utils.b64decode(b'YWJjZA')
+    assert decoded == b'abcd'
+
+    decoded = utils.b64decode(b'YWJj')
+    assert decoded == b'abc'
+
+
+def test_b64decode_works_with_padding() -> None:
+    """Test b64decode accepts padded value correctly."""
+    decoded = utils.b64decode(b'YWJj==')
+    assert decoded == b'abc'
+
+
+def test_b32encode_strips_padding() -> None:
+    """Test b32encode strips padding correctly."""
+    encoded = utils.b32encode(b'abcd')
+    assert encoded == b'MFRGGZA'
+
+    encoded = utils.b32encode(b'abc')
+    assert encoded == b'MFRGG'
+
+
+def test_b32decode_stripped_padding_works() -> None:
+    """Test b32decode works without padding."""
+    decoded = utils.b32decode(b'MFRGGZA')
+    assert decoded == b'abcd'
+
+    decoded = utils.b32decode(b'MFRGG')
+    assert decoded == b'abc'
+
+
+def test_b32decode_works_with_padding() -> None:
+    """Test b32decode accepts padded value correctly."""
+    decoded = utils.b32decode(b'MFRGG===')
+    assert decoded == b'abc'
+
+
+def test_hexdecode_accepts_bytes() -> None:
+    """Test hexdecode accepts bytes correctly."""
+    decoded = utils.hexdecode(b'616263')
+    assert decoded == b'abc'
+
+
+def test_hexencode_accepts_bytes() -> None:
+    """Test hexencode accepts bytes correctly."""
+    encoded = utils.hexencode(b'abc')
+    assert encoded == b'616263'
+
+
+def test_timestamp_to_aware_datetime_accepts_int() -> None:
+    """Test timestamp_to_aware_datetime accepts int timestamps."""
+    timestamp = 1619064799
+    converted_timestamp = utils.timestamp_to_aware_datetime(timestamp)
+
+    assert isinstance(converted_timestamp, datetime)
+    assert timedelta() == converted_timestamp.utcoffset()  # Aware in UTC
+    # Technically, Python does some coercing, as in (1 == 1.0) is True, but just in case:
+    assert timestamp == int(converted_timestamp.timestamp())
+    assert float(timestamp) == converted_timestamp.timestamp()
+    assert timestamp == converted_timestamp.timestamp()
+
+
+def test_timestamp_to_aware_datetime_accepts_float() -> None:
+    """Test timestamp_to_aware_datetime accepts float timestamps."""
+    timestamp = 1619064799.965
+    converted_timestamp = utils.timestamp_to_aware_datetime(timestamp)
+
+    assert isinstance(converted_timestamp, datetime)
+    assert timedelta() == converted_timestamp.utcoffset()  # Aware in UTC
+    assert timestamp == converted_timestamp.timestamp()
+
+
+def test_file_mode_is_text() -> None:
+    """Test file_mode_is_text works correctly."""
+    text_file = io.StringIO()
+    assert utils.file_mode_is_text(text_file)
+
+    bin_file = io.BytesIO()
+    assert not utils.file_mode_is_text(bin_file)
+
+
+def test_ordinal() -> None:
+    """Test that the ordinal function works correctly."""
+    assert '0th' == utils.ordinal(0)
+    assert '1st' == utils.ordinal(1)
+    assert '2nd' == utils.ordinal(2)
+    assert '3rd' == utils.ordinal(3)
+    assert '4th' == utils.ordinal(4)
+    assert '12th' == utils.ordinal(12)
+    assert '100th' == utils.ordinal(100)
+    assert '101st' == utils.ordinal(101)
+    assert '1003rd' == utils.ordinal(1003)
+
+
+@pytest.mark.parametrize(
+    ('value', 'expected'),
+    (
+        (b'abc', b'abc'),
+        ('abc', b'abc'),
+    ),
+)
+def test_force_bytes(value: typing.Any, expected: str) -> None:
+    """Test that force_bytes works correctly."""
+    forced = utils.force_bytes(value)
+
+    assert isinstance(forced, bytes)
+    assert expected == forced
+
+
+def test_force_bytes_wrong_type() -> None:
+    """Test that force_bytes raises exception on a wrong type."""
+    with pytest.raises(TypeError, match='value must be bytes or str'):
+        utils.force_bytes(1)  # type: ignore
+
+
+@pytest.mark.parametrize(
+    ('value', 'expected'),
+    (
+        (b'abc', 'abc'),
+        ('abc', 'abc'),
+    ),
+)
+def test_force_string(value: typing.Any, expected: str) -> None:
+    """Test that force_string works correctly."""
+    forced = utils.force_string(value)
+
+    assert isinstance(forced, str)
+    assert expected == forced
+
+
+def test_force_string_wrong_type() -> None:
+    """Test that force_string raises exception on a wrong type."""
+    with pytest.raises(TypeError, match='value must be bytes or str'):
+        utils.force_string(1)  # type: ignore
---
+++ packages/blake2signer/blake2signer-3.0.0-py3-none-any.whl/blake2signer/utils.py
@@ -0,0 +1,171 @@
+"""Miscellaneous utilities."""
+
+import base64
+import io
+import typing
+from datetime import datetime
+from datetime import timezone
+
+
+def force_bytes(value: typing.Union[str, bytes]) -> bytes:
+    """Force a given value into bytes.
+
+    Args:
+        value: Value to convert to bytes.
+
+    Returns:
+        Converted value into bytes.
+
+    Raises:
+        TypeError: Value is neither bytes nor string.
+    """
+    if isinstance(value, bytes):
+        return value
+
+    if isinstance(value, str):
+        return value.encode('utf-8', errors='strict')
+
+    raise TypeError('value must be bytes or str')
+
+
+def force_string(value: typing.Union[str, bytes]) -> str:
+    """Force a given value into string.
+
+    Args:
+        value: Value to convert to string.
+
+    Returns:
+        Converted value into string.
+
+    Raises:
+        TypeError: Value is neither bytes nor string.
+    """
+    if isinstance(value, str):
+        return value
+
+    if isinstance(value, bytes):
+        return value.decode('utf-8', errors='strict')
+
+    raise TypeError('value must be bytes or str')
+
+
+def b64encode(data: bytes) -> bytes:
+    """Encode data as Base 64 URL-safe, stripping padding.
+
+    Args:
+        data: Data to encode.
+
+    Returns:
+        Encoded data.
+    """
+    return base64.urlsafe_b64encode(data).rstrip(b'=')
+
+
+def b64decode(data: bytes) -> bytes:
+    """Decode data encoded as Base 64 URL-safe without padding.
+
+    Args:
+        data: Data to decode.
+
+    Returns:
+        Original data.
+    """
+    return base64.urlsafe_b64decode(data + (b'=' * (len(data) % 4)))
+
+
+def b32encode(data: bytes) -> bytes:
+    """Encode data as Base 32, stripping padding.
+
+    Args:
+        data: Data to encode.
+
+    Returns:
+        Encoded data.
+    """
+    return base64.b32encode(data).rstrip(b'=')
+
+
+def b32decode(data: bytes) -> bytes:
+    """Decode data encoded as Base 32 without padding.
+
+    Args:
+        data: Data to decode.
+
+    Returns:
+        Original data.
+    """
+    return base64.b32decode(data + (b'=' * ((8 - (len(data) % 8)) % 8)))
+
+
+def hexencode(data: bytes) -> bytes:
+    """Encode data as hexadecimal (uppercase).
+
+    Args:
+        data: Data to encode.
+
+    Returns:
+        Encoded data.
+    """
+    return base64.b16encode(data)
+
+
+def hexdecode(data: bytes) -> bytes:
+    """Decode data encoded as hexadecimal (uppercase).
+
+    Args:
+        data: Data to decode.
+
+    Returns:
+        Original data.
+    """
+    return base64.b16decode(data)
+
+
+def timestamp_to_aware_datetime(timestamp: typing.Union[int, float]) -> datetime:
+    """Convert a UNIX timestamp into an aware datetime in UTC.
+
+    Args:
+        timestamp: UNIX timestamp to convert.
+
+    Returns:
+        Converted timestamp into an aware datetime in UTC.
+    """
+    return datetime.fromtimestamp(timestamp, tz=timezone.utc)
+
+
+def file_mode_is_text(file: typing.IO[typing.AnyStr]) -> bool:
+    """Check if a given file is opened in text mode, or otherwise in binary mode.
+
+    Args:
+        file: File to check its mode.
+
+    Returns:
+        True if file is opened in text mode, False otherwise.
+    """
+    return isinstance(file, io.TextIOBase)
+
+
+def ordinal(number: int) -> str:
+    """Convert an integer into its ordinal representation.
+
+    Args:
+        number: Integer number to get its ordinal representation.
+
+    Returns:
+        The ordinal string representation of the number as the number + ordinal suffix.
+
+    Examples:
+        >>> ordinal(0)
+        '0th'
+        >>> ordinal(3)
+        '3rd'
+    """
+    # From https://stackoverflow.com/a/50992575
+    if 11 <= (number % 100) <= 13:
+        suffix = 'th'
+    else:
+        suffixes = ('th', 'st', 'nd', 'rd', 'th')
+        idx = min(number % 10, 4)
+        suffix = suffixes[idx]
+
+    return f'{number}{suffix}'
