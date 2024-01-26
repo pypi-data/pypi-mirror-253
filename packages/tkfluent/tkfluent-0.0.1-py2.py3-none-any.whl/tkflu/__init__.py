d7d44f87a2532f2b57330929f1650c4ca5ee03a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:54 +0000

    Add package tkfluent/0.0.1/tkfluent-0.0.1-py2.py3-none-any.whl

---
+++ packages/tkfluent/tkfluent-0.0.1-py2.py3-none-any.whl/tkflu/__init__.py
@@ -0,0 +1,24 @@
+"""、
+
+Fluent设计的tkinter组件库（模板）
+
+-------------
+作者：XiangQinxi
+-------------
+"""
+
+from .badge import FluBadge
+from .button import FluButton
+from .entry import FluEntry
+from .frame import FluFrame
+from .label import FluLabel
+from .text import FluText
+from .window import FluWindow
+
+FluChip = FluBadge
+FluPushButton = FluButton
+FluTextInput = FluEntry
+FluTextBox = FluText
+FluPanel = FluFrame
+
+# 
