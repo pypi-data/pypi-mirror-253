d3a671176710731a375b602555809491103cdaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:03:38 +0000

    Add package ploomes-api-client/0.2.96/ploomes-api-client-0.2.96.tar.gz

---
+++ packages/ploomes-api-client/ploomes-api-client-0.2.96.tar.gz/ploomes-api-client-0.2.96/ploomes_client/__init__.py
@@ -0,0 +1,16 @@
+from .core.ploomes_client import PloomesClient
+from .collections import (
+    Account,
+    Cities,
+    Contacts,
+    Deals,
+    Fields,
+    InteractionRecords,
+    Filters,
+    Tables,
+    Users,
+    Comments,
+    Products,
+    Tasks,
+    Emails
+)
