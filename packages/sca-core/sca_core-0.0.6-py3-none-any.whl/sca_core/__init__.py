fca5980b63b3b61db2b84473f7f2e9d83105e24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:20 +0000

    Add package sca-core/0.0.6/sca_core-0.0.6-py3-none-any.whl

---
+++ packages/sca-core/sca_core-0.0.6-py3-none-any.whl/sca_core/__init__.py
@@ -0,0 +1,5 @@
+from .log import *
+from .config import sca_config
+from .multi_process import execute_multi_core
+from .utils import *
+from .mq import RabbitConsumer, RabbitProducer
---
+++ packages/sca-core/sca_core-0.0.6-py3-none-any.whl/sca_core/utils.py
@@ -0,0 +1,4 @@
+import time
+
+def get_current_second():
+    return int(round(time.time()))
