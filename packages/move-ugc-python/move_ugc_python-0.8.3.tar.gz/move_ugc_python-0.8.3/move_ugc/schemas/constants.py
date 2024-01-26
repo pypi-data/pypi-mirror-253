a0384769017a496ad5d2a63f59d342c0cdaa887d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:03:38 +0000

    Add package move-ugc-python/0.8.3/move_ugc_python-0.8.3.tar.gz

---
+++ packages/move-ugc-python/move_ugc_python-0.8.3.tar.gz/move_ugc_python-0.8.3/move_ugc/schemas/constants.py
@@ -0,0 +1,15 @@
+"""Constant values for the client."""
+from typing import Literal
+
+AUTHORIZATION_HEADER = "authorization"
+MINIMUM_API_KEY_LENGTH = 12
+ALLOWED_EXPAND_ATTRS = Literal[
+    "client",
+    "video_file",
+    "additional_files",
+    "outputs",
+    "take",
+]
+CLIENT_LITERAL = "client"
+VIDEO_FILE_LITERAL = "video_file"
+ADDITIONAL_FILE_LITERAL = "additional_files"
