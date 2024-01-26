18788b417486095170abd1acacf230c72b5c699c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:04:45 +0000

    Add package Feluda/0.0.4/Feluda-0.0.4.tar.gz

---
+++ packages/Feluda/Feluda-0.0.4.tar.gz/Feluda-0.0.4/Feluda/main.py
@@ -0,0 +1,9 @@
+class FeluMittir:
+    def __init__(self, username) -> None:
+        self.user = username
+
+    def sayHello(self):
+        print(f"Hello {self.user} from FeluMittir!")
+
+    def goodMorning(self):
+        print(f"Good Morning {self.user} From FeluMittir!")
---
+++ packages/Feluda/Feluda-0.0.4.tar.gz/Feluda-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+# Get the long description from the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = 'feluda'
+
+# Setting up
+setup(
+    name="Feluda",
+    version=VERSION,
+    author="Soumyajit Pan",
+    author_email="soumyajitpan29@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['excel', 'excelreader', 'excelwriter', 'python', 'excel reader and writer', 'read excel data using python', 'write data in excel using python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
