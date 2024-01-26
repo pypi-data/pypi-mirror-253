739dd74bc2a02f2d71edcba389108cf5dd136fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:04:07 +0000

    Add package compas-model/0.1.0/compas_model-0.1.0.tar.gz

---
+++ packages/compas-model/compas_model-0.1.0.tar.gz/compas_model-0.1.0/src/compas_model/model/__init__.py
@@ -0,0 +1,7 @@
+from .element_node import ElementNode
+from .group_node import GroupNode
+from .element_tree import ElementTree
+from .model import Model
+
+
+__all__ = ["ElementNode", "GroupNode", "ElementTree", "Model"]
---
+++ packages/compas-model/compas_model-0.1.0.tar.gz/compas_model-0.1.0/src/compas_model/model/element_node.py
@@ -0,0 +1,99 @@
+from compas_model.elements.element import Element
+from compas.datastructures import TreeNode
+
+
+class ElementNode(TreeNode):
+    """A leaf node that stores Element object.
+
+    Parameters
+    ----------
+    name : str, optional
+        If element is not given, it defaults to str(``uuid.uuid4()``) of this class.
+    element : :class:`compas_model.elements.Element`, optional
+        :class:`compas_model.elements.Element` or any classes that inherits from it.
+    parent : :class:`compas_model.model.GroupNode`, optional
+        The parent node of this node.
+        This input is required when the node is created separately (not by my_model.add_element(...))
+        After creation, the parent becomes the branch or sub-branch of the node.
+
+    Attributes
+    ----------
+    name : str
+        Name of the node, default: :class:`compas_model.elements.Element` str(``uuid.uuid4()``), otherwise user defined.
+    element : :class:`compas_model.elements.Element`, read-only
+        Element object stored in the node or any classes that inherits from it.
+
+    """
+
+    DATASCHEMA = None
+
+    @property
+    def __data__(self):
+        return {
+            "name": self.name,
+            "element": self.element,
+        }
+
+    @classmethod
+    def __from_data__(cls, data):
+        element = data["element"]
+        node = cls(name=data["name"], element=element)
+        return node
+
+    def __init__(self, name=None, element=None, parent=None):
+        super(ElementNode, self).__init__(name=name)
+
+        # --------------------------------------------------------------------------
+        # The node stores Element object in the attributes dictionary.
+        # --------------------------------------------------------------------------
+        if isinstance(element, Element) is False:
+            raise Exception("ElementNode should have an element input.")
+
+        element.node = (
+            self  # reference the current node to the element once it is added
+        )
+        self._element = element  # node stores the Element object
+
+        # --------------------------------------------------------------------------
+        # Make the node into a leaf, it has no children.
+        # --------------------------------------------------------------------------
+        self._children = None  # make the leaf
+
+        # --------------------------------------------------------------------------
+        # When a node is created separately, a user must define the parent node:
+        # --------------------------------------------------------------------------
+        self._parent = parent
+        if parent is not None:
+            self._tree = parent._tree
+
+        # --------------------------------------------------------------------------
+        # For debugging, the default name is the guid of an ElementNode
+        # --------------------------------------------------------------------------
+        self._name = None
+        self.name = name
+
+    def __repr__(self):
+        return "<{}> {}, <element> {}".format(
+            self.__class__.__name__, self.name, self.element
+        )
+
+    def __str__(self):
+        return self.__repr__()
+
+    # ==========================================================================
+    # Attributes
+    # ==========================================================================
+
+    @property
+    def name(self):
+        if not self._name:
+            return str(self.guid)
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def element(self):
+        return self._element
