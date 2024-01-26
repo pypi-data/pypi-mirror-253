b71e3d218784efa0388cf48c263c9bf72f82174a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:28 +0000

    Add package datashaper/0.0.23/datashaper-0.0.23-py3-none-any.whl

---
+++ packages/datashaper/datashaper-0.0.23-py3-none-any.whl/datashaper/execution/__init__.py
@@ -0,0 +1,7 @@
+from .derive_from_rows import derive_from_rows
+from .execution_node import ExecutionNode
+from .types import VerbDefinitions
+from .utils import parallelize
+
+
+__all__ = ["derive_from_rows", "ExecutionNode", "VerbDefinitions", "parallelize"]
---
+++ packages/datashaper/datashaper-0.0.23-py3-none-any.whl/datashaper/progress/types.py
@@ -0,0 +1,23 @@
+from typing import Any, Callable, Optional
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ProgressStatus:
+    progress: float | None = None
+    """0 - 1 progress"""
+
+    description: str | None = None
+    """Description of the progress"""
+
+    total_items: int | None = None
+    """Total number of items"""
+
+    completed_items: int | None = None
+    """Number of items completed"""
+
+
+ProgressTicker = Callable[[Optional[int]], None]
+StatusReportHandler = Callable[[ProgressStatus], Any]
+"""A progress reporter function."""
