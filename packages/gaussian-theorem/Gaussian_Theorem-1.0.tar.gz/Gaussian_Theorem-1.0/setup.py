87c4a221b5677ec4fd9f267a705b5101721bcba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:03:18 +0000

    Add package gaussian-theorem/1.0/Gaussian_Theorem-1.0.tar.gz

---
+++ packages/gaussian-theorem/Gaussian_Theorem-1.0.tar.gz/Gaussian_Theorem-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="Gaussian_Theorem",
+      version="1.0",
+      description="Gaussian and Binomial distributions", 
+      packages=['Gaussian_Theorem'],
+      author = 'Makoha Dharren Pius',
+      author_email = 'dharrenpius@outlook.com',
+    zip_safe=False)
