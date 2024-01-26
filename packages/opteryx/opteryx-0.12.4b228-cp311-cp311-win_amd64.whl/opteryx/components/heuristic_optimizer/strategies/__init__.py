43e1e2c7f1b556bf6219c2d927dadb35e4b2da82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:16 +0000

    Add package opteryx/0.12.4b228/opteryx-0.12.4b228-cp311-cp311-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.12.4b228-cp311-cp311-win_amd64.whl/opteryx/components/heuristic_optimizer/strategies/__init__.py
@@ -0,0 +1,17 @@
+from .defragment_morsels import DefragmentMorselsStrategy
+from .split_conjunctive_predicates import SplitConjunctivePredicatesStrategy
+
+__all__ = [
+    "DefragmentMorselsStrategy",
+    "SplitConjunctivePredicatesStrategy",
+]
+
+
+# predicate rewriter (negatives, demorgans, constants)
+# aggregate pushown (into SQL reader)
+# subquery flattening
+# CTE elimination
+# rewrite LIMIT and SORT to head sort
+# replace DISTINCT LIMIT with a pass-thru limiter
+# replace ORDER BY LIMIT with a heap sort
+# IN (<subquery>) to join
