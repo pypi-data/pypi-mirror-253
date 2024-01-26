626d43e167febd9ce4fe26984dfab1e37d79c80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:03:15 +0000

    Add package mattermost-notify/24.1.0/mattermost_notify-24.1.0.tar.gz

---
+++ packages/mattermost-notify/mattermost_notify-24.1.0.tar.gz/mattermost_notify-24.1.0/mattermost_notify/status.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022-2023 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from enum import Enum
+
+
+class Status(Enum):
+    SUCCESS = ":white_check_mark: success"
+    FAILURE = ":x: failure"
+    UNKNOWN = ":grey_question: unknown"
+    CANCELLED = ":no_entry_sign: canceled"
+
+    def __str__(self):
+        return self.name
