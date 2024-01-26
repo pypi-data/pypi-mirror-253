b04a17d06d594382201f78d2016b8fdbdfb17943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:04:03 +0000

    Add package pypsa/0.26.3/pypsa-0.26.3.tar.gz

---
+++ packages/pypsa/pypsa-0.26.3.tar.gz/pypsa-0.26.3/pypsa/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Python for Power Systems Analysis (PyPSA)
+
+Energy system modelling library.
+"""
+
+from pypsa import (
+    clustering,
+    components,
+    contingency,
+    descriptors,
+    examples,
+    geo,
+    io,
+    linopf,
+    linopt,
+    opf,
+    opt,
+    optimization,
+    pf,
+    plot,
+    statistics,
+)
+from pypsa.components import Network, SubNetwork
+
+__version__ = "0.26.3"
+
+__author__ = (
+    "PyPSA Developers, see https://pypsa.readthedocs.io/en/latest/developers.html"
+)
+__copyright__ = (
+    "Copyright 2015-2023 PyPSA Developers, see https://pypsa.readthedocs.io/en/latest/developers.html, "
+    "MIT License"
+)
