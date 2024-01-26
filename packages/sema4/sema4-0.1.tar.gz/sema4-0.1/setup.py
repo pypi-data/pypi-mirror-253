17b88f2f377c772128a8602b9b7d4e11da724ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:03:31 +0000

    Add package sema4/0.1/sema4-0.1.tar.gz

---
+++ packages/sema4/sema4-0.1.tar.gz/sema4-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='sema4',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple hello world package',
+    long_description='This is a simple hello world package',
+    author='Mikko Korpela',
+    author_email='mikko@robocorp.com',
+    url='https://github.com/robocorp/robocorp',
+)
