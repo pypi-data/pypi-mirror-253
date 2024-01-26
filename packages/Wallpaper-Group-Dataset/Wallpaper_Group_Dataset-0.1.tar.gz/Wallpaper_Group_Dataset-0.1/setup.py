fdf02cd74a97d9ed5fd800194c2954f723891838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 06:03:41 +0000

    Add package Wallpaper-Group-Dataset/0.1/Wallpaper_Group_Dataset-0.1.tar.gz

---
+++ packages/Wallpaper-Group-Dataset/Wallpaper_Group_Dataset-0.1.tar.gz/Wallpaper_Group_Dataset-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Wallpaper_Group_Dataset',
+    version='0.1',
+    author='Yichen Guo, Joshua Agar',
+    url='https://github.com/yig319/Wallpaper_Group_Dataset.git',
+    packages=find_packages(),
+)
