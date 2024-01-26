91843b6a829e75fd313454269afc4b892ab3124a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:39 +0000

    Add package powerbot-client/2.14.5/powerbot_client-2.14.5.tar.gz

---
+++ packages/powerbot-client/powerbot_client-2.14.5.tar.gz/powerbot_client-2.14.5/powerbot_client/api/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from powerbot_client.api.algorithms_api import AlgorithmsApi
+from powerbot_client.api.authentication_api import AuthenticationApi
+from powerbot_client.api.capacities_api import CapacitiesApi
+from powerbot_client.api.contract_api import ContractApi
+from powerbot_client.api.historic_data_api import HistoricDataApi
+from powerbot_client.api.logs_api import LogsApi
+from powerbot_client.api.market_api import MarketApi
+from powerbot_client.api.messages_api import MessagesApi
+from powerbot_client.api.orders_api import OrdersApi
+from powerbot_client.api.portfolios_api import PortfoliosApi
+from powerbot_client.api.report_api import ReportApi
+from powerbot_client.api.schema_api import SchemaApi
+from powerbot_client.api.settings_api import SettingsApi
+from powerbot_client.api.signals_api import SignalsApi
+from powerbot_client.api.subscriptions_api import SubscriptionsApi
+from powerbot_client.api.tenants_api import TenantsApi
+from powerbot_client.api.trades_api import TradesApi
