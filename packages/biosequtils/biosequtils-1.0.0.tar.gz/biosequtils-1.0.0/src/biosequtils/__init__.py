1a4d0ed47ee50081269fa9354f26619b703b62a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:03:35 +0000

    Add package biosequtils/1.0.0/biosequtils-1.0.0.tar.gz

---
+++ packages/biosequtils/biosequtils-1.0.0.tar.gz/biosequtils-1.0.0/src/biosequtils/__init__.py
@@ -0,0 +1,10 @@
+from .commons import Commons
+from .dir import Dir
+from .file import File
+
+from .handle_json import HandleJson
+from .jtxt import Jtxt
+from .key_value import KeyValue
+
+from .utils import Utils
+
