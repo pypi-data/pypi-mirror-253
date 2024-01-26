24bb17eed116b6f82bf2aa4bee5166d15ebbe1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:03:53 +0000

    Add package dsn39-probability/1.2/dsn39_probability-1.2.tar.gz

---
+++ packages/dsn39-probability/dsn39_probability-1.2.tar.gz/dsn39_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsn39_probability',
+      version='1.2',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['dsn39_probability'],
+      author = 'Joshua Agba',
+      author_email = 'joshua.agba.aj@gmail.com',
+      zip_safe=False)
