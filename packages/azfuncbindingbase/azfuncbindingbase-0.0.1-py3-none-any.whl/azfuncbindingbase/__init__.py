311441cc04fad034904bd66d7518b050c7a9bdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:03:04 +0000

    Add package azfuncbindingbase/0.0.1/azfuncbindingbase-0.0.1-py3-none-any.whl

---
+++ packages/azfuncbindingbase/azfuncbindingbase-0.0.1-py3-none-any.whl/azfuncbindingbase/__init__.py
@@ -0,0 +1,4 @@
+from .meta import Datum, _ConverterMeta, _BaseConverter, InConverter, OutConverter
+from .type import SdkType
+
+__all__ = ['Datum', '_ConverterMeta', '_BaseConverter', 'InConverter', 'OutConverter', 'SdkType']
