9c86ad5582f20c0c719bd27d4f0c00a0843f400d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:08 +0000

    Add package clipped/0.7.8/clipped-0.7.8-py3-none-any.whl

---
+++ packages/clipped/clipped-0.7.8-py3-none-any.whl/clipped/config/exceptions.py
@@ -0,0 +1,13 @@
+from click import ClickException
+
+
+class ClippedException(ClickException):
+    def __init__(self, message=None):
+        super().__init__(message)
+
+    def __repr__(self):
+        return self.message
+
+
+class SchemaError(ClippedException):
+    pass
