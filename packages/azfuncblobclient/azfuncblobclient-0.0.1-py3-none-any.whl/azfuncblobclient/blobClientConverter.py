9f11826c841724998acdafe7e5ae57f2d575449c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:02:55 +0000

    Add package azfuncblobclient/0.0.1/azfuncblobclient-0.0.1-py3-none-any.whl

---
+++ packages/azfuncblobclient/azfuncblobclient-0.0.1-py3-none-any.whl/azfuncblobclient/blobClientConverter.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+from typing import Any, Optional
+
+from az.func.binding.base import Datum, InConverter, OutConverter, SdkType
+from .blobClient import BlobClient
+
+class BlobClientConverter(InConverter,
+                          OutConverter,
+                          binding='blob',
+                          trigger='blobTrigger',):
+    
+    @classmethod
+    def check_input_type_annotation(cls, pytype: type) -> bool:
+        return issubclass(pytype, (BlobClient, bytes, str))
+
+    @classmethod
+    # NEED TO ADD PYTYPE AS ADDITIONAL PARAMETER
+    # do I need to parse the trigger_metadata?
+    def decode(cls, data: Datum, *, trigger_metadata) -> Any:
+        if data is None or data.type is None:
+            return None
+
+        data_type = data.type
+
+        if data_type == 'model_binding_data':
+            data = data.value
+        else:
+            raise ValueError(
+                f'unexpected type of data received for the "blob" binding '
+                f': {data_type!r}'
+            )
+
+        # SWITCH STATEMENT HERE ON SPECIFIC PY TYPE
+
+        return BlobClient(data=data).get_sdk_type()
