ea99d02019594c821e6eced7f889105b1de1060b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:53 +0000

    Add package realtime-trains/0.0.3/realtime-trains-0.0.3.tar.gz

---
+++ packages/realtime-trains/realtime-trains-0.0.3.tar.gz/realtime-trains-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setup(
+    name="realtime-trains",
+    version="0.0.3",
+    description="Unofficial Python library for Realtime Trains API",
+    package_dir={"": "realtime_trains"},
+	packages=find_packages(where="realtime_trains"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mbukovac/realtime-trains",
+    author="Marko B.",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["httpx", "pydantic"]
+)
