a1affa6ddee643b47027dfbc87e77224fe6d640d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:04:29 +0000

    Add package s3torchconnector/1.1.3/s3torchconnector-1.1.3.tar.gz

---
+++ packages/s3torchconnector/s3torchconnector-1.1.3.tar.gz/s3torchconnector-1.1.3/src/s3torchconnector/s3iterable_dataset.py
@@ -0,0 +1,103 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  // SPDX-License-Identifier: BSD
+from functools import partial
+from typing import Iterator, Any, Union, Iterable, Callable
+
+import torch.utils.data
+
+from . import S3Reader
+from ._s3bucket_key import S3BucketKey
+from ._s3client import S3Client
+from ._s3dataset_common import (
+    identity,
+    get_objects_from_uris,
+    get_objects_from_prefix,
+)
+
+
+class S3IterableDataset(torch.utils.data.IterableDataset):
+    """An IterableStyle dataset created from S3 objects.
+
+    To create an instance of S3IterableDataset, you need to use
+    `from_prefix` or `from_objects` methods.
+    """
+
+    def __init__(
+        self,
+        region: str,
+        get_dataset_objects: Callable[[S3Client], Iterable[S3BucketKey]],
+        transform: Callable[[S3Reader], Any] = identity,
+    ):
+        self._get_dataset_objects = get_dataset_objects
+        self._transform = transform
+        self._region = region
+        self._client = None
+
+    @property
+    def region(self):
+        return self._region
+
+    @classmethod
+    def from_objects(
+        cls,
+        object_uris: Union[str, Iterable[str]],
+        *,
+        region: str,
+        transform: Callable[[S3Reader], Any] = identity,
+    ):
+        """Returns an instance of S3IterableDataset using the S3 URI(s) provided.
+
+        Args:
+          object_uris(str | Iterable[str]): S3 URI of the object(s) desired.
+          region(str): AWS region of the S3 bucket where the objects are stored.
+          transform: Optional callable which is used to transform an S3Reader into the desired type.
+
+        Returns:
+            S3IterableDataset: An IterableStyle dataset created from S3 objects.
+
+        Raises:
+            S3Exception: An error occurred accessing S3.
+        """
+        return cls(
+            region, partial(get_objects_from_uris, object_uris), transform=transform
+        )
+
+    @classmethod
+    def from_prefix(
+        cls,
+        s3_uri: str,
+        *,
+        region: str,
+        transform: Callable[[S3Reader], Any] = identity,
+    ):
+        """Returns an instance of S3IterableDataset using the S3 URI provided.
+
+        Args:
+          s3_uri(str): An S3 URI (prefix) of the object(s) desired. Objects matching the prefix will be included in the returned dataset.
+          region(str): AWS region of the S3 bucket where the objects are stored.
+          transform: Optional callable which is used to transform an S3Reader into the desired type.
+
+        Returns:
+            S3IterableDataset: An IterableStyle dataset created from S3 objects.
+
+        Raises:
+            S3Exception: An error occurred accessing S3.
+        """
+        return cls(
+            region, partial(get_objects_from_prefix, s3_uri), transform=transform
+        )
+
+    def _get_client(self):
+        if self._client is None:
+            self._client = S3Client(self.region)
+        return self._client
+
+    def _get_transformed_object(self, bucket_key: S3BucketKey) -> Any:
+        return self._transform(
+            self._get_client().get_object(bucket_key.bucket, bucket_key.key)
+        )
+
+    def __iter__(self) -> Iterator[Any]:
+        return map(
+            self._get_transformed_object, self._get_dataset_objects(self._get_client())
+        )
---
+++ packages/s3torchconnector/s3torchconnector-1.1.3.tar.gz/s3torchconnector-1.1.3/src/s3torchconnector/s3map_dataset.py
@@ -0,0 +1,112 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  // SPDX-License-Identifier: BSD
+from functools import partial
+from typing import List, Any, Callable, Iterable, Union
+
+import torch.utils.data
+from s3torchconnector._s3bucket_key import S3BucketKey
+
+from ._s3client import S3Client
+from . import S3Reader
+
+from ._s3dataset_common import (
+    get_objects_from_uris,
+    get_objects_from_prefix,
+    identity,
+)
+
+
+class S3MapDataset(torch.utils.data.Dataset):
+    """A Map-Style dataset created from S3 objects.
+
+    To create an instance of S3MapDataset, you need to use
+    `from_prefix` or `from_objects` methods.
+    """
+
+    def __init__(
+        self,
+        region: str,
+        get_dataset_objects: Callable[[S3Client], Iterable[S3BucketKey]],
+        transform: Callable[[S3Reader], Any] = identity,
+    ):
+        self._get_dataset_objects = get_dataset_objects
+        self._transform = transform
+        self._region = region
+        self._client = None
+        self._bucket_key_pairs = None
+
+    @property
+    def region(self):
+        return self._region
+
+    @property
+    def _dataset_bucket_key_pairs(self) -> List[S3BucketKey]:
+        if self._bucket_key_pairs is None:
+            self._bucket_key_pairs = list(self._get_dataset_objects(self._get_client()))
+        return self._bucket_key_pairs
+
+    @classmethod
+    def from_objects(
+        cls,
+        object_uris: Union[str, Iterable[str]],
+        *,
+        region: str,
+        transform: Callable[[S3Reader], Any] = identity,
+    ):
+        """Returns an instance of S3MapDataset using the S3 URI(s) provided.
+
+        Args:
+          object_uris(str | Iterable[str]): S3 URI of the object(s) desired.
+          region(str): AWS region of the S3 bucket where the objects are stored.
+          transform: Optional callable which is used to transform an S3Reader into the desired type.
+
+        Returns:
+            S3MapDataset: A Map-Style dataset created from S3 objects.
+
+        Raises:
+            S3Exception: An error occurred accessing S3.
+        """
+
+        return cls(
+            region, partial(get_objects_from_uris, object_uris), transform=transform
+        )
+
+    @classmethod
+    def from_prefix(
+        cls,
+        s3_uri: str,
+        *,
+        region: str,
+        transform: Callable[[S3Reader], Any] = identity,
+    ):
+        """Returns an instance of S3MapDataset using the S3 URI provided.
+
+        Args:
+          s3_uri(str): An S3 URI (prefix) of the object(s) desired. Objects matching the prefix will be included in the returned dataset.
+          region(str): AWS region of the S3 bucket where the objects are stored.
+          transform: Optional callable which is used to transform an S3Reader into the desired type.
+
+        Returns:
+            S3MapDataset: A Map-Style dataset created from S3 objects.
+
+        Raises:
+            S3Exception: An error occurred accessing S3.
+        """
+        return cls(
+            region, partial(get_objects_from_prefix, s3_uri), transform=transform
+        )
+
+    def _get_client(self):
+        if self._client is None:
+            self._client = S3Client(self.region)
+        return self._client
+
+    def _get_object(self, i: int) -> S3Reader:
+        bucket_key = self._dataset_bucket_key_pairs[i]
+        return self._get_client().get_object(bucket_key.bucket, bucket_key.key)
+
+    def __getitem__(self, i: int) -> Any:
+        return self._transform(self._get_object(i))
+
+    def __len__(self):
+        return len(self._dataset_bucket_key_pairs)
