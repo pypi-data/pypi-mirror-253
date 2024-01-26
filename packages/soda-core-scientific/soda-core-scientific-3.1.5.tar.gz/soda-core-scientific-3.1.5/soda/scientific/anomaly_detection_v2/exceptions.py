c70eb0387e82d5d36aee9376115c376195405e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:04:01 +0000

    Add package soda-core-scientific/3.1.5/soda-core-scientific-3.1.5.tar.gz

---
+++ packages/soda-core-scientific/soda-core-scientific-3.1.5.tar.gz/soda-core-scientific-3.1.5/soda/scientific/anomaly_detection_v2/exceptions.py
@@ -0,0 +1,26 @@
+class PreprocessError(Exception):
+    """Thrown in case of pre-processing.
+
+    To be raised and passed as a result error message down the line.
+    """
+
+
+class AggregationValueError(Exception):
+    """Thrown in case of wrong frequency aggregation.
+
+    To be raised and passed as a result error message down the line.
+    """
+
+
+class FreqDetectionResultError(Exception):
+    """Thrown in case of wrong frequency values.
+
+    To be raised and passed as a result error message down the line.
+    """
+
+
+class NotSupportedHolidayCountryError(Exception):
+    """Thrown in case of wrong holiday country.
+
+    To be raised and passed as a result error message down the line.
+    """
