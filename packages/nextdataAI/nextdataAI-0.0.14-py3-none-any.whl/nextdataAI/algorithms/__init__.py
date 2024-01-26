b5ce9a7009f43726b5282a7b9a6fc83c1037219d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 06:03:50 +0000

    Add package nextdataAI/0.0.14/nextdataAI-0.0.14-py3-none-any.whl

---
+++ packages/nextdataAI/nextdataAI-0.0.14-py3-none-any.whl/nextdataAI/algorithms/__init__.py
@@ -0,0 +1,9 @@
+from .AStar import AStar
+from .Algorithm import Algorithm
+from .Dijkstra import Dijkstra
+from .FS import DFS, BFS
+from .Genetic import Genetic
+from .Greedy import Greedy
+from .QLSTM import QLSTM
+from .QTable import QTable
+from .QNN import QNN
