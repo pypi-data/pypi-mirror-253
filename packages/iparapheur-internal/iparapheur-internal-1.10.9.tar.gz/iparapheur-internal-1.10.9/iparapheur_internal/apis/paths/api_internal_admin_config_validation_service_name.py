164130b03239a54a424ef60673aa947b0f05766a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:02:51 +0000

    Add package iparapheur-internal/1.10.9/iparapheur-internal-1.10.9.tar.gz

---
+++ packages/iparapheur-internal/iparapheur-internal-1.10.9.tar.gz/iparapheur-internal-1.10.9/iparapheur_internal/apis/paths/api_internal_admin_config_validation_service_name.py
@@ -0,0 +1,7 @@
+from iparapheur_internal.paths.api_internal_admin_config_validation_service_name.get import ApiForget
+
+
+class ApiInternalAdminConfigValidationServiceName(
+    ApiForget,
+):
+    pass
