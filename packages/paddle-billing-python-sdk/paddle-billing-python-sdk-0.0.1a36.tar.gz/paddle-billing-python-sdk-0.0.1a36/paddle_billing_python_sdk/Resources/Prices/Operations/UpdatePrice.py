27d545eb4ff4e8a92ae2e256f46046dde773cb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:53 +0000

    Add package paddle-billing-python-sdk/0.0.1a36/paddle-billing-python-sdk-0.0.1a36.tar.gz

---
+++ packages/paddle-billing-python-sdk/paddle-billing-python-sdk-0.0.1a36.tar.gz/paddle-billing-python-sdk-0.0.1a36/paddle_billing_python_sdk/Resources/Prices/Operations/UpdatePrice.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from paddle_billing_python_sdk.Entities.Shared.CatalogType       import CatalogType
+from paddle_billing_python_sdk.Entities.Shared.CustomData        import CustomData
+from paddle_billing_python_sdk.Entities.Shared.Money             import Money
+from paddle_billing_python_sdk.Entities.Shared.PriceQuantity     import PriceQuantity
+from paddle_billing_python_sdk.Entities.Shared.Status            import Status
+from paddle_billing_python_sdk.Entities.Shared.TaxMode           import TaxMode
+from paddle_billing_python_sdk.Entities.Shared.TimePeriod        import TimePeriod
+from paddle_billing_python_sdk.Entities.Shared.UnitPriceOverride import UnitPriceOverride
+
+
+@dataclass
+class UpdatePrice:
+    description:          str | None                     = None
+    name:                 str | None                     = None
+    type:                 CatalogType | None             = None
+    billing_cycle:        TimePeriod | None              = None
+    tax_mode:             TaxMode | None                 = None
+    unit_price:           Money | None                   = None
+    unit_price_overrides: list[UnitPriceOverride] | None = None
+    quantity:             PriceQuantity | None           = None
+    status:               Status | None                  = None
+    custom_data:          CustomData | None              = None
---
+++ packages/paddle-billing-python-sdk/paddle-billing-python-sdk-0.0.1a36.tar.gz/paddle-billing-python-sdk-0.0.1a36/paddle_billing_python_sdk/Resources/Prices/PricesClient.py
@@ -0,0 +1,65 @@
+from dataclasses import asdict
+from typing      import TYPE_CHECKING
+
+from paddle_billing_python_sdk.ResponseParser import ResponseParser
+
+from paddle_billing_python_sdk.Entities.PriceWithIncludes import PriceWithIncludes
+
+from paddle_billing_python_sdk.Entities.Collections.Paginator                   import Paginator
+from paddle_billing_python_sdk.Entities.Collections.PriceWithIncludesCollection import PriceWithIncludesCollection
+
+from paddle_billing_python_sdk.Entities.Shared.Status import Status
+
+from paddle_billing_python_sdk.Resources.Prices.Operations.CreatePrice import CreatePrice
+from paddle_billing_python_sdk.Resources.Prices.Operations.ListPrices  import ListPrices
+from paddle_billing_python_sdk.Resources.Prices.Operations.UpdatePrice import UpdatePrice
+
+
+if TYPE_CHECKING:
+    from paddle_billing_python_sdk.Client import Client
+
+
+class PricesClient:
+    def __init__(self, client: 'Client'):
+        self.client = client
+
+
+    def list(self, operation: ListPrices = None) -> PriceWithIncludesCollection:
+        if operation is None:
+            operation = ListPrices()
+
+        response = self.client.get_raw('/prices', operation.get_parameters())
+        parser   = ResponseParser(response)
+        return PriceWithIncludesCollection.from_list(parser.get_data(), Paginator(self.client, parser.get_pagination(), PriceWithIncludesCollection))
+
+
+    def get(self, price_id: str, includes = None) -> PriceWithIncludes:
+        if includes is None:
+            includes = []
+
+        # Validate 'includes' items and build parameters
+        parameters = {'include': ','.join(include.value for include in includes)} if includes else {}
+        response   = self.client.get_raw(f"/prices/{price_id}", parameters)
+        parser     = ResponseParser(response)
+
+        return PriceWithIncludes.from_dict(parser.get_data())
+
+
+    def create(self, operation: CreatePrice) -> PriceWithIncludes:
+        response = self.client.post_raw('/prices', asdict(operation))
+        parser   = ResponseParser(response)
+
+        return PriceWithIncludes.from_dict(parser.get_data())
+
+
+    def update(self, price_id: str, operation: UpdatePrice) -> PriceWithIncludes:
+        response = self.client.patch_raw(f"/prices/{price_id}", asdict(operation))
+        parser   = ResponseParser(response)
+
+        return PriceWithIncludes.from_dict(parser.get_data())
+
+
+    def archive(self, price_id: str) -> PriceWithIncludes:
+        operation = UpdatePrice(status=Status.Archived)
+
+        return self.update(price_id, operation)
