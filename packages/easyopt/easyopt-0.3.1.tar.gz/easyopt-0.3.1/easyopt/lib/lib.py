11e21bcaf6fe5c66d2b50f243926af81ac03b69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:03:47 +0000

    Add package easyopt/0.3.1/easyopt-0.3.1.tar.gz

---
+++ packages/easyopt/easyopt-0.3.1.tar.gz/easyopt-0.3.1/easyopt/lib/lib.py
@@ -0,0 +1,72 @@
+import os
+import socket
+import threading
+import time
+
+from easyopt.utils import recv_object, send_object, log
+
+_easyopt_socket = None
+
+_easyopt_socket_lock = threading.Lock()
+_easyopt_heartbeat_lock = threading.Lock()
+
+_heartbeat_thread = None
+_heartbeat_thread_running = True
+
+def init():
+    init_heartbeat()
+
+def init_socket():
+    global _easyopt_socket
+    global _easyopt_socket_lock
+    with _easyopt_socket_lock:
+        if _easyopt_socket is None and "EASYOPT_SOCKET" in os.environ:
+            log("[lib] initializing socket")
+            _easyopt_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            _easyopt_socket.connect(os.environ["EASYOPT_SOCKET"])
+
+
+def init_heartbeat():
+    if "EASYOPT_SOCKET" in os.environ:
+        def heartbeat():
+            global _heartbeat_thread_running
+            while _heartbeat_thread_running:
+                init_socket()
+                time.sleep(1)
+                log(f"[lib] sending hearbeat _heartbeat_thread_running={_heartbeat_thread_running}")
+                send_object(dict(command="heartbeat"), _easyopt_socket)
+            log("[lib] heartbeat_thread terminated")
+            return
+
+        global _heartbeat_thread
+        global _easyopt_heartbeat_lock
+        with _easyopt_heartbeat_lock:
+            if _heartbeat_thread is None:
+                _heartbeat_thread = threading.Thread(target=heartbeat, daemon=True)
+                _heartbeat_thread.start()
+                log("[lib] heartbeat_thread started")
+
+def objective(value):
+    if "EASYOPT_SOCKET" not in os.environ:
+        return
+    global _easyopt_socket
+    global _heartbeat_thread_running
+
+    init_socket()
+    send_object(dict(command="objective", value=value), _easyopt_socket)
+
+def report(value):
+    if "EASYOPT_SOCKET" not in os.environ:
+        return
+    global _easyopt_socket
+    init_socket()
+    send_object(dict(command="report", value=value), _easyopt_socket)
+
+def should_prune():
+    if "EASYOPT_SOCKET" not in os.environ:
+        return
+    global _easyopt_socket
+    init_socket()
+    send_object(dict(command="should_prune"), _easyopt_socket)
+    reply = recv_object(_easyopt_socket)
+    return reply["reply"]
---
+++ packages/easyopt/easyopt-0.3.1.tar.gz/easyopt-0.3.1/easyopt/src/optimize.py
@@ -0,0 +1,102 @@
+import os
+import json
+import inspect
+import uuid
+import socket
+import subprocess
+import threading
+import queue
+import optuna
+
+from easyopt.utils import log
+from easyopt.src.heartbeat import HeartbeatMonitor, HeartbeatException
+from easyopt.src.socketserver import SocketServer
+
+def sample_parameters(trial, config):
+    parameters = dict()
+    for parameter, parameter_args in config["parameters"].items():
+        distribution = parameter_args.pop("distribution")
+        optuna_func = "suggest_"+distribution
+        
+        args = dict()
+        for parameter_name, parameter_value in parameter_args.items():
+            if isinstance(parameter_value, str): parameter_value = float(parameter_value)
+            args[parameter_name] = parameter_value
+        
+        parameters[parameter] = getattr(trial, optuna_func)(parameter, **args)
+        
+    return parameters
+
+def build_parameters_strings(parameters):
+    args = " ".join([f"--{k}={v}" for k, v in parameters.items()])
+
+    return dict(
+        args = args
+    )
+
+def optimize(study):
+    def objective(trial):
+        config = study.user_attrs["config"]
+        parameters = sample_parameters(trial, config)
+        command_variables = build_parameters_strings(parameters)
+        env_variables = dict(
+            name=study.study_name
+        )
+        
+        command = config["command"].format(**command_variables, **env_variables)
+        socket_file = f"/tmp/{uuid.uuid4()}"
+        log(f"[optimize] socket file {socket_file}")
+        
+        q = queue.Queue()
+        server = SocketServer(q, socket_file)
+        heartbeat_monitor = HeartbeatMonitor(q)
+
+        server.listen()
+
+        env = os.environ.copy()
+        env["EASYOPT_SOCKET"] = socket_file
+
+        process = subprocess.Popen(command.split(" "), env=env)
+        heartbeat_monitor.beat()
+        log(f"[optimize] running process {command}")
+        global_step = 1
+        results = []
+        while True:
+            data = q.get()
+            log(f"[optimize] received {data}")
+            if data["command"] == "objective":
+                results.append(data["value"])
+                log(f"[optimize] result {data['value']} added, len(results)={len(results)}")
+                process.wait()
+                log(f"[optimize] process terminated")
+                if len(results) >= config["replicas"]:
+                    server.stop()
+                    heartbeat_monitor.stop()
+                    os.remove(socket_file)
+                    log(f"[optimize] finished runs")
+                    return sum(results)/len(results)
+                else:
+                    log(f"[optimize] running process {command}")
+                    process = subprocess.Popen(command.split(" "), env=env)
+                    heartbeat_monitor.beat()
+            
+            elif data["command"] == "report":
+                trial.report(data["value"], step=global_step)
+                global_step += 1
+            
+            elif data["command"] == "should_prune":
+                reply = int(trial.should_prune())
+                server.send(dict(reply=reply))
+            
+            elif data["command"] == "heartbeat":
+                heartbeat_monitor.beat()
+            
+            elif data["command"] == "heartbeat_fail":
+                server.stop()
+                heartbeat_monitor.stop()
+                os.remove(socket_file)
+                raise HeartbeatException
+
+                
+
+    study.optimize(objective, n_trials=1)
