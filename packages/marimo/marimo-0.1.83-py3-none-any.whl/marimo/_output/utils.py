fc934521759038a914a378cd29387831b3083240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:30 +0000

    Add package marimo/0.1.83/marimo-0.1.83-py3-none-any.whl

---
+++ packages/marimo/marimo-0.1.83-py3-none-any.whl/marimo/_output/utils.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from typing import Optional, Union
+
+
+def build_data_url(mimetype: str, data: bytes) -> str:
+    str_repr = data.decode("utf-8").replace("\n", "")
+    return f"data:{mimetype};base64,{str_repr}"
+
+
+def flatten_string(text: str) -> str:
+    return "".join([line.strip() for line in text.split("\n")])
+
+
+def create_style(
+    pairs: dict[str, Union[str, int, float, None]]
+) -> Optional[str]:
+    if not pairs:
+        return None
+
+    return ";".join([f"{k}: {v}" for k, v in pairs.items() if v is not None])
