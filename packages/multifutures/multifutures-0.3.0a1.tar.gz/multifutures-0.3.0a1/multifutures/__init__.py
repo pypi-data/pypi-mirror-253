14055c0ba3353b37dd01bfab209e1288d0573d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:53 +0000

    Add package multifutures/0.3.0a1/multifutures-0.3.0a1.tar.gz

---
+++ packages/multifutures/multifutures-0.3.0a1.tar.gz/multifutures-0.3.0a1/multifutures/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import importlib.metadata
+
+from ._multi import check_results
+from ._multi import FutureResult
+from ._multi import multiprocess
+from ._multi import multithread
+from ._rate_limit import RateLimit
+from ._rate_limit import wait
+
+__version__ = importlib.metadata.version("multifutures")
+
+__all__: list[str] = [
+    "__version__",
+    # multi
+    "check_results",
+    "FutureResult",
+    "multiprocess",
+    "multithread",
+    # rate_limit
+    "RateLimit",
+    "wait",
+]
