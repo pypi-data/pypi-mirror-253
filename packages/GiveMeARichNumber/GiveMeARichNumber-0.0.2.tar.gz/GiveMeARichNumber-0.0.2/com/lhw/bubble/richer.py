ce7619f67dffef954889e43268628fb414708c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:16 +0000

    Add package GiveMeARichNumber/0.0.2/GiveMeARichNumber-0.0.2.tar.gz

---
+++ packages/GiveMeARichNumber/GiveMeARichNumber-0.0.2.tar.gz/GiveMeARichNumber-0.0.2/com/lhw/bubble/richer.py
@@ -0,0 +1,24 @@
+import random
+
+targetNum = 666
+
+def getRichNum():
+    # 定义布尔变量 a
+    a = False
+    # 创建一个空列表 b
+    b = []
+    c = []
+    # 循环从 1 到 35，将每个数字添加到 b 列表中
+    for i in range(1, 37):
+        b.append(i)
+    for i in range(0,5):
+        x = random.choice(b)
+        print(x, end=" ")
+        b.remove(x)
+    for i in range(1, 13):
+        c.append(i)
+
+    for i in range(0,2):
+        x = random.choice(c)
+        print(x, end=" ")
+        c.remove(x)
