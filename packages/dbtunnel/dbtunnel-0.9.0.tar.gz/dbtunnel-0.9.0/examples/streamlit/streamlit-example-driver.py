3380e5dff58d3b6f37861f07952b6dfd46b8510d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:02:53 +0000

    Add package dbtunnel/0.9.0/dbtunnel-0.9.0.tar.gz

---
+++ packages/dbtunnel/dbtunnel-0.9.0.tar.gz/dbtunnel-0.9.0/examples/streamlit/streamlit-example-driver.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %pip install dbtunnel[streamlit]
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import os
+
+current_directory = os.getcwd()
+script_path = current_directory + "/streamlit_example.py"
+
+# COMMAND ----------
+
+from dbtunnel import dbtunnel
+
+dbtunnel.streamlit(script_path).run()
