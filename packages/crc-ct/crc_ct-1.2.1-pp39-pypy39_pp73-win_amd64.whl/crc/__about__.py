fe12865206dbda6f7b4b9772e812c1b2efa9990d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 06:03:34 +0000

    Add package crc-ct/1.2.1/crc_ct-1.2.1-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/crc-ct/crc_ct-1.2.1-pp39-pypy39_pp73-win_amd64.whl/crc/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 1994 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/license/zlib
+
+__import__("pkg_about").about("crc-ct")
+__copyright__ = f"Copyright (c) 1994-2024 {__author__}"  # noqa
