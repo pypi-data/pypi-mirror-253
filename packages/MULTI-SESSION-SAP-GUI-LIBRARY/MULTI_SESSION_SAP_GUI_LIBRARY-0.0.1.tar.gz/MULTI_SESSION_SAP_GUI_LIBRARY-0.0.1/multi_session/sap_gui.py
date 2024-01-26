00ba3372216b98832e00ecd5a7952842990c46a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:40 +0000

    Add package MULTI-SESSION-SAP-GUI-LIBRARY/0.0.1/MULTI_SESSION_SAP_GUI_LIBRARY-0.0.1.tar.gz

---
+++ packages/MULTI-SESSION-SAP-GUI-LIBRARY/MULTI_SESSION_SAP_GUI_LIBRARY-0.0.1.tar.gz/MULTI_SESSION_SAP_GUI_LIBRARY-0.0.1/multi_session/sap_gui.py
@@ -0,0 +1,7 @@
+class sap_gui:
+
+    def __init__(self):
+        self.gui = ''
+
+    def helloworld(self, message):
+        print (message)
---
+++ packages/MULTI-SESSION-SAP-GUI-LIBRARY/MULTI_SESSION_SAP_GUI_LIBRARY-0.0.1.tar.gz/MULTI_SESSION_SAP_GUI_LIBRARY-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='MULTI_SESSION_SAP_GUI_LIBRARY',
+    version='0.0.1',
+    license='MIT License',
+    author='Jose luis and Guillerme Manhaes',
+    keywords='sap',
+    description=u'Lib teste',
+    packages=['multi_session'])
