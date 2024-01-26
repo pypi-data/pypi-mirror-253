6065f6b1cbde263605ddbe50103b4f2ec3128dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:27 +0000

    Add package benimang/0.5.6/benimang-0.5.6.tar.gz

---
+++ packages/benimang/benimang-0.5.6.tar.gz/benimang-0.5.6/beni/bmysql.py
@@ -0,0 +1,164 @@
+from __future__ import annotations
+
+import logging
+import re
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
+from typing import Any, AsyncGenerator, Sequence, Type, TypeVar, cast
+
+import aiomysql
+from pydantic import BaseModel
+
+from beni.bfunc import toAny
+from beni.btype import Null
+
+
+class BModel(BaseModel):
+
+    _tableName: str = ''
+
+    @classmethod
+    @property
+    def tableName(cls):
+        if type(cls._tableName) is not str:
+            className = cls.__name__
+            result = [className[0].lower()]
+            for char in className[1:]:
+                if char.isupper():
+                    result.extend(['_', char.lower()])
+                else:
+                    result.append(char)
+            cls._tableName = ''.join(result)
+        return cls._tableName
+
+
+_BModel = TypeVar('_BModel', bound=BModel)
+_T = TypeVar('_T')
+
+
+_sqlFormatRe = re.compile(r'\s*\n\s*')
+
+
+def _sqlFormat(sql: str):
+    return _sqlFormatRe.sub(' ', sql).strip()
+
+
+class MysqlDbCursor:
+
+    def __init__(self, cursor: aiomysql.Cursor):
+        self.cursor = cursor
+
+    @property
+    def connection(self) -> aiomysql.Connection:
+        return toAny(self.cursor.connection)
+
+    @property
+    def lastrowid(self):
+        return self.cursor.lastrowid
+
+    @property
+    def rowcount(self):
+        return self.cursor.rowcount
+
+    async def commit(self):
+        await self.connection.commit()
+
+    async def execute(self, query: str, args: Sequence[Any] = ()):
+        return await self.cursor.execute(query, args)
+
+    async def getOne(self, modelClass: Type[_BModel], sql: str, args: Sequence[Any] = ()) -> _BModel | None:
+        await self.cursor.execute(_sqlFormat(sql), args)
+        result: Sequence[Any] = await self.cursor.fetchone()
+        if not result:
+            return None
+        columns = self._getColumns()
+        data = {v: result[i] for i, v in enumerate(columns)}
+        if modelClass is Any:
+            return cast(_BModel, data)
+        else:
+            return modelClass(**data)
+
+    async def getList(self, modelClass: Type[_BModel], sql: str, args: Sequence[Any] = ()) -> list[_BModel]:
+        await self.cursor.execute(_sqlFormat(sql), args)
+        result: Sequence[Sequence[Any]] = await self.cursor.fetchall()
+        if not result:
+            return []
+        columns = self._getColumns()
+        datas = [{v: row[i] for i, v in enumerate(columns)} for row in result]
+        if modelClass is Any:
+            return cast(list[_BModel], datas)
+        else:
+            return [modelClass(**x) for x in datas]
+
+    async def getValue(self, valueClass: Type[_T], sql: str, args: Sequence[Any] = ()) -> _T | None:
+        await self.cursor.execute(sql, args)
+        result: Sequence[Any] = await self.cursor.fetchone()
+        if not result:
+            return None
+        return result[0]
+
+    async def getValueList(self, valueClass: Type[_T], sql: str, args: Sequence[Any] = ()) -> list[_T] | None:
+        await self.cursor.execute(sql, args)
+        result: Sequence[Sequence[Any]] = await self.cursor.fetchall()
+        if not result:
+            return None
+        return [x[0] for x in result]
+
+    async def saveOne(self, model: BModel, *, tableName: str = ''):
+        columns: list[str] = []
+        values: list[Any] = []
+        for k, v in model.model_dump(exclude_unset=True).items():
+            columns.append(f'`{k}`')
+            values.append(v)
+        tableName = tableName or model.__class__.tableName
+        updateSql = ','.join([f'{x} = VALUES( {x} )' for x in columns])
+        sql = f'''INSERT INTO `{tableName}` ( {','.join(columns)} ) VALUES %s ON DUPLICATE KEY UPDATE {updateSql}'''
+        await self.cursor.execute(sql, [values])
+
+    async def saveList(self, modelList: Sequence[BModel], *, tableName: str = ''):
+        assert modelList, 'modelList 必须至少有一个元素'
+        columns: list[str] = []
+        values: list[Sequence[Any]] = []
+        for k in modelList[0].model_dump().keys():
+            columns.append(f'`{k}`')
+        for model in modelList:
+            values.append(tuple(model.model_dump().values()))
+        tableName = tableName or modelList[0].__class__.tableName
+        updateSql = ','.join([f'{x} = VALUES( {x} )' for x in columns])
+        sql = f'''INSERT INTO `{tableName}` ( {','.join(columns)} ) VALUES %s, %s ON DUPLICATE KEY UPDATE {updateSql}'''
+        await self.cursor.execute(sql, values)
+
+    def _getColumns(self) -> tuple[Any, ...]:
+        return tuple(x[0] for x in toAny(self.cursor.description))
+
+
+@dataclass
+class MysqlDb:
+    host: str
+    port: int
+    user: str
+    password: str
+    db: str = ''
+    _pool: aiomysql.Pool = Null
+
+    @asynccontextmanager
+    async def getCursor(self) -> AsyncGenerator[MysqlDbCursor, None]:
+        isEcho = logging.getLogger().level is logging.DEBUG
+        if not self._pool:
+            self._pool = await aiomysql.create_pool(  # type: ignore
+                host=self.host,
+                port=self.port,
+                user=self.user,
+                password=self.password,
+                db=self.db or None,
+                echo=isEcho,
+            )
+        async with cast(aiomysql.Connection, self._pool.acquire()) as connection:
+            async with cast(aiomysql.Cursor, connection.cursor()) as cursor:
+                try:
+                    yield MysqlDbCursor(cursor)
+                finally:
+                    await connection.rollback()
+
+    def makeByDb(self, db: str):
+        return MysqlDb(self.host, self.port, self.user, self.password, db=db)
