eaea4cb93e3df8cb21e4c25722683b116db72641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:55 +0000

    Add package kytest/0.0.28/kytest-0.0.28-py3-none-any.whl

---
+++ packages/kytest/kytest-0.0.28-py3-none-any.whl/kytest/running/conf.py
@@ -0,0 +1,30 @@
+"""
+@Author: kang.yang
+@Date: 2024/1/25 11:22
+"""
+
+
+class App:
+    auto_start = False
+    bundle_id = ""
+    package = ""
+    serial = ""
+    udid = ""
+    ocr_service = ""
+
+
+class Common:
+    base_url = ""
+    headers = ""
+    ocr_service = ""
+    web_base_url = ""
+
+
+class Web:
+    browser_name = ""
+    cookies = ""
+    headless = False
+    maximized = False
+    state_file = ""
+    window_size = ""
+
