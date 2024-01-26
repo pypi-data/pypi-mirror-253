2e65d5bb99408d74945a472a44a5abb1db14e2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:03:23 +0000

    Add package ex-liangwangkuai/0.0.1/ex-liangwangkuai-0.0.1.tar.gz

---
+++ packages/ex-liangwangkuai/ex-liangwangkuai-0.0.1.tar.gz/ex-liangwangkuai-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='ex-liangwangkuai',
+    version='0.0.1',
+    author='linwk',
+    author_email='2259214992@qq.com',
+    description='my first try',
+    long_description='text/markdown',
+    packages=setuptools.find_packages(),
+    url="http://github.com/", ## 项目主页
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
