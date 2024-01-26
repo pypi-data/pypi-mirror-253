d0e5b82492552c4163b335741a0162df103f817e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:04:06 +0000

    Add package edc-adverse-event/0.3.71/edc-adverse-event-0.3.71.tar.gz

---
+++ packages/edc-adverse-event/edc-adverse-event-0.3.71.tar.gz/edc-adverse-event-0.3.71/edc_adverse_event/model_mixins/death_report/__init__.py
@@ -0,0 +1,11 @@
+from .death_report_extra_fields_model_mixin import DeathReportExtraFieldsModelMixin
+from .death_report_model_mixin import DeathReportModelMixin
+from .death_report_tmg_model_mixin import (
+    DeathReportTmgManager,
+    DeathReportTmgModelMixin,
+    DeathReportTmgSecondManager,
+    DeathReportTmgSecondModelMixin,
+    DeathReportTmgSecondSiteManager,
+    DeathReportTmgSiteManager,
+)
+from .simple_death_report_model_mixin import SimpleDeathReportModelMixin
