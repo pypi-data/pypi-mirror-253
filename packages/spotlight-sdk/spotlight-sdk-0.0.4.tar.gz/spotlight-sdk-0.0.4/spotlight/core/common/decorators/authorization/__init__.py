62e83c44b8b587007f270462182bb27aebc07d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:51 +0000

    Add package spotlight-sdk/0.0.4/spotlight-sdk-0.0.4.tar.gz

---
+++ packages/spotlight-sdk/spotlight-sdk-0.0.4.tar.gz/spotlight-sdk-0.0.4/spotlight/core/common/decorators/authorization/__init__.py
@@ -0,0 +1,9 @@
+"""
+Authenticated request decorators.
+"""
+from spotlight.core.common.decorators.authorization.asynchronous import (
+    async_authenticated_request,
+)
+from spotlight.core.common.decorators.authorization.synchronous import (
+    authenticated_request,
+)
---
+++ packages/spotlight-sdk/spotlight-sdk-0.0.4.tar.gz/spotlight-sdk-0.0.4/spotlight/core/common/decorators/data/__init__.py
@@ -0,0 +1,5 @@
+"""
+Data request decorators.
+"""
+from spotlight.core.common.decorators.data.asynchronous import async_data_request
+from spotlight.core.common.decorators.data.synchronous import data_request
