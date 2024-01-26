e52a59fab900d3cc0218242c809cb35500345a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:24 +0000

    Add package granloader/2.3.2/granloader-2.3.2.tar.gz

---
+++ packages/granloader/granloader-2.3.2.tar.gz/granloader-2.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='granloader',
+	version='2.3.2',
+	packages=find_packages(),
+	install_requires=[
+		'yt-dlp',
+		'ffmpeg-python'
+	]
+)
