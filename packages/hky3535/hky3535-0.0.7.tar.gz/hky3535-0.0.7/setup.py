98512d5faa737734d05fede9851a0cdbe1d114f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:24 +0000

    Add package hky3535/0.0.7/hky3535-0.0.7.tar.gz

---
+++ packages/hky3535/hky3535-0.0.7.tar.gz/hky3535-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import os 
+from setuptools import setup, find_packages
+
+setup(
+	name = "hky3535",
+	version = "0.0.7",
+    author ="hky3535",
+    author_email = "hky3535@163.com",
+	url = 'https://github.com/hky3535/hky3535.git',
+    long_description_content_type="text/markdown",
+	long_description = open('README.md',encoding="utf-8").read(),
+    install_requires=[
+        "opencv-python", 
+        "numpy"
+    ], 
+	packages = find_packages(),
+ 	license = 'Apache',
+   	classifiers = [
+       'License :: OSI Approved :: Apache Software License'
+    ],
+    package_data={'': ['*.csv', '*.txt','.toml']}, 
+    include_package_data=True
+)
