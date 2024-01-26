216d7050d9fbb1a145e3674654014d3f76e7399b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:02 +0000

    Add package Topsis-Sachin-102103575/0.2/Topsis_Sachin_102103575-0.2.tar.gz

---
+++ packages/Topsis-Sachin-102103575/Topsis_Sachin_102103575-0.2.tar.gz/Topsis_Sachin_102103575-0.2/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE=pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='Topsis_Sachin_102103575',
+    version='0.2',
+    description='It does a topsis analysis of a given csv file',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Liquidator420/Topsis-Sachin-102103575",
+    author="Sachin Sushil Singh",
+    author_email="sachinsushilsingh@gmail.com",
+    license="MIT",
+    packages=["analysis"],
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'numpy',
+    ],
+    entry_points={
+        "console_scripts": [
+            "analysis=analysis.__main__:main",
+        ]
+    },
+)
