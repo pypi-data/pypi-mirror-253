93d0ca0fd9c8e6bb27d06e90205671e93f212dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:51 +0000

    Add package vanilla-knn/0.1.0/vanilla-knn-0.1.0.tar.gz

---
+++ packages/vanilla-knn/vanilla-knn-0.1.0.tar.gz/vanilla-knn-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='vanilla-knn',
+    version='0.1.0',
+    packages=['vanilla_knn'],
+    author='RayVil',
+    description='Uma implementação simples do algoritmo KNN em Python.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/RayVilaca/vanilla-knn',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
