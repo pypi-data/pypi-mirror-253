a212ccd7ff4ac3a43cd3c6ea72aca8d70d73f10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:03:29 +0000

    Add package openaoe/0.0.3/openaoe-0.0.3.tar.gz

---
+++ packages/openaoe/openaoe-0.0.3.tar.gz/openaoe-0.0.3/openaoe/backend/service/service_google.py
@@ -0,0 +1,59 @@
+import json
+
+import requests
+
+from openaoe.backend.config.biz_config import get_api_key, get_base_url
+from openaoe.backend.config.constant import *
+from openaoe.backend.model.aoe_response import AOEResponse
+from openaoe.backend.model.google import GooglePalmChatBody
+from openaoe.backend.util.log import log
+
+logger = log(__name__)
+
+
+def palm_chat_svc(body: GooglePalmChatBody):
+    """
+    chat logic for google PaLM model
+    """
+    api_key = get_api_key(PROVIDER_GOOGLE, body.model)
+    url = get_base_url(PROVIDER_GOOGLE, body.model)
+    url = f"{url}/google/v1beta2/models/{body.model}:generateMessage?key={api_key}"
+    messages = [
+        {"content": msg.content, "author": msg.author}
+        for msg in body.prompt.messages or []
+    ]
+    body = {
+        "prompt": {
+            "messages": messages
+        },
+        "temperature": body.temperature,
+        "candidate_count": body.candidate_count,
+        "model": body.model
+    }
+    try:
+        response_json = requests.post(
+            url=url,
+            data=json.dumps(body)
+        ).json()
+        if response_json.get('error') is not None:
+            err_msg = response_json.get('error').get("message")
+            return AOEResponse(
+                msg="error",
+                msgCode="-1",
+                data=err_msg
+            )
+
+        # remove messages
+        if response_json and response_json.get("messages"):
+            response_json.pop("messages")
+        base = AOEResponse(
+            data=response_json
+        )
+    except Exception as e:
+        logger.error(f"{e}")
+        base = AOEResponse(
+            msg="error",
+            msgCode="-1",
+            data=str(e)
+        )
+    return base
