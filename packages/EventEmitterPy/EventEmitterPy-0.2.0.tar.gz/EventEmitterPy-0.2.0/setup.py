84f2ebd54d5f60959111dadb638087b503103f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:03:59 +0000

    Add package EventEmitterPy/0.2.0/EventEmitterPy-0.2.0.tar.gz

---
+++ packages/EventEmitterPy/EventEmitterPy-0.2.0.tar.gz/EventEmitterPy-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='EventEmitterPy',
+    version='0.2.0',
+    description="A super fast, memory effcient events system for Python",
+    author="Ahmed Rakan",
+    author_email="ar.aldhafeeri11@gmail.com",
+    packages=['EventEmitterPy'],
+    install_requires=[
+        # Add any dependencies here
+    ],
+    test_suite='tests',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
