4e1db5eff247d47f7713754c89c74f66884e2f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:13 +0000

    Add package inventory-monitor/7.1.0/inventory-monitor-7.1.0.tar.gz

---
+++ packages/inventory-monitor/inventory-monitor-7.1.0.tar.gz/inventory-monitor-7.1.0/inventory_monitor/__init__.py
@@ -0,0 +1,12 @@
+from extras.plugins import PluginConfig
+
+
+class NetBoxInventoryMonitorConfig(PluginConfig):
+    name = "inventory_monitor"
+    verbose_name = " Inventory Monitor"
+    description = "Manage inventory discovered by SNMP"
+    version = "7.1.0"
+    base_url = "inventory-monitor"
+
+
+config = NetBoxInventoryMonitorConfig
