ac591f9dbd7235439bfe389d942db4654e5f7949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:45 +0000

    Add package pyecotrend-ista/3.0.0/pyecotrend-ista-3.0.0.tar.gz

---
+++ packages/pyecotrend-ista/pyecotrend-ista-3.0.0.tar.gz/pyecotrend-ista-3.0.0/src/pyecotrend_ista/const.py
@@ -0,0 +1,20 @@
+VERSION = "3.0.0"
+
+BASE_URL = "https://api.prod.eed.ista.com/"
+ACCOUNT_URL = BASE_URL + "account"
+CONSUMPTIONS_URL = BASE_URL + "consumptions?consumptionUnitUuid="
+
+PROVIDER_URL = "https://keycloak.ista.com/realms/eed-prod/protocol/openid-connect/"
+REDIRECT_URI = "https://ecotrend.ista.de/login-redirect"
+CLIENT_ID = "ecotrend"
+SCOPE = "openid"
+RESPONSE_MODE = "fragment"
+RESPONSE_TPYE = "code"
+CODE_CHALLENGE_METHODE = "S256"
+GRANT_TYPE_REFRESH_TOKEN = "refresh_token"
+GRANT_TYPE_AUTHORIZATION_CODE = "authorization_code"
+
+MAX_RETRIES = 3
+RETRY_DELAY = 2
+
+TIMEOUT = 10
