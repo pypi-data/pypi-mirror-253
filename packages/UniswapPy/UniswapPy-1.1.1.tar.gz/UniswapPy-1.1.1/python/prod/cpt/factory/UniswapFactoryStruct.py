8da319969b8c7f36427e6e6ca5c96c292a0c5c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:04:00 +0000

    Add package UniswapPy/1.1.1/UniswapPy-1.1.1.tar.gz

---
+++ packages/UniswapPy/UniswapPy-1.1.1.tar.gz/UniswapPy-1.1.1/python/prod/cpt/factory/UniswapFactoryStruct.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class UniswapFactoryStruct:
+    exchange_to_tokens: {}
+    parent_lp: None
+    name: str = None
+    address: str = None
+    
+   
