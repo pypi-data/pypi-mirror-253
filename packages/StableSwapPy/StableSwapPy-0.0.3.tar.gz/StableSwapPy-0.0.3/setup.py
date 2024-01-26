2b81ad32fe788b1e680754c5b7ae68f994613a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:55 +0000

    Add package StableSwapPy/0.0.3/StableSwapPy-0.0.3.tar.gz

---
+++ packages/StableSwapPy/StableSwapPy-0.0.3.tar.gz/StableSwapPy-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='StableSwapPy',
+      version='0.0.3',
+      description='StableSwap for Python',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/icmoore/stableswappy',
+      author = "icmoore",
+      author_email = "utiliwire@gmail.com",
+      license='MIT',
+      package_dir = {"stableswappy": "python/prod"},
+      packages=[
+          'stableswappy',
+          'stableswappy.cst.exchg',
+          'stableswappy.cst.factory',
+          'stableswappy.erc',
+          'stableswappy.vault'
+      ],
+      zip_safe=False)
