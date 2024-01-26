c69244abc4fe31cb0a6905a060bdde2e25b979f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 06:04:05 +0000

    Add package bauplan/0.0.3a54/bauplan-0.0.3a54-py3-none-win_amd64.whl

---
+++ packages/bauplan/bauplan-0.0.3a54-py3-none-win_amd64.whl/bauplan/cli.py
@@ -0,0 +1,34 @@
+import os
+import signal
+import subprocess  # nosec
+import sys
+
+
+class ProcessWrapper:
+    def __init__(self, command):
+        self.command = command
+        self.child_process = None
+
+    def forward_signal_to_child(self, signum, frame):
+        if self.child_process is not None:
+            os.kill(self.child_process.pid, signum)
+
+    def run(self):
+        signal.signal(signal.SIGINT, self.forward_signal_to_child)
+        signal.signal(signal.SIGTERM, self.forward_signal_to_child)
+        self.child_process = subprocess.Popen(self.command)  # nosec
+        exit_code = self.child_process.wait()
+        return exit_code
+
+
+def run():
+    command = [
+        os.path.join(os.path.dirname(__file__), 'bauplan-cli'),
+        *sys.argv[1:],
+    ]
+    wrapper = ProcessWrapper(command)
+    sys.exit(wrapper.run())
+
+
+if __name__ == '__main__':
+    run()
