bd471ef5ba61c993409d45228d92b588586358f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:31 +0000

    Add package novelai-python/0.1.1/novelai_python-0.1.1.tar.gz

---
+++ packages/novelai-python/novelai_python-0.1.1.tar.gz/novelai_python-0.1.1/src/novelai_python/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/11/18 上午12:18
+# @Author  : sudoskys
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from .credential import *  # noqa: F401, F403
+from .sdk import *  # noqa: F401, F403
