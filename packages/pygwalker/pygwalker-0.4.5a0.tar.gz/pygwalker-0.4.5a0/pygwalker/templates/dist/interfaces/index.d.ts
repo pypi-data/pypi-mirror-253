d2e5be624c70322130d596fb856a92d70dc5ff6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:05:04 +0000

    Add package pygwalker/0.4.5a0/pygwalker-0.4.5a0.tar.gz

---
+++ packages/pygwalker/pygwalker-0.4.5a0.tar.gz/pygwalker-0.4.5a0/pygwalker/templates/dist/interfaces/index.d.ts
@@ -0,0 +1,37 @@
+import type { IRow, IMutField } from '@kanaries/graphic-walker/dist/interfaces';
+export interface IAppProps {
+    hideDataSourceConfig: boolean;
+    fieldkeyGuard: boolean;
+    themeKey: string;
+    dark: string;
+    dataSource: IRow[];
+    rawFields: IMutField[];
+    id: string;
+    dataSourceProps: IDataSourceProps;
+    version?: string;
+    hashcode?: string;
+    visSpec: any;
+    userConfig?: IUserConfig;
+    env?: string;
+    needLoadDatas?: boolean;
+    specType: string;
+    showCloudTool: boolean;
+    needInitChart: boolean;
+    useKernelCalc: boolean;
+    useSaveTool: boolean;
+    parseDslType: "server" | "client";
+    communicationUrl: string;
+    gwMode: "explore" | "renderer";
+    needLoadLastSpec: boolean;
+    extraConfig?: any;
+    fieldMetas: any;
+    isExportDataFrame: boolean;
+}
+export interface IDataSourceProps {
+    tunnelId: string;
+    dataSourceId: string;
+}
+export interface IUserConfig {
+    [key: string]: any;
+    privacy: 'events' | 'update-only' | 'offline';
+}
