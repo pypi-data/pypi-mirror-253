f309531f32f2125ba231431ee1b8979a1d16af31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:01 +0000

    Add package inventory-monitor/7.0.9/inventory-monitor-7.0.9.tar.gz

---
+++ packages/inventory-monitor/inventory-monitor-7.0.9.tar.gz/inventory-monitor-7.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'inventory-monitor',
+    version='7.0.9',
+    description = 'Asset Management with semi-auto discovery processes',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
