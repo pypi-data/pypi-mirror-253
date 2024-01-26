62fa435ef3a0bf5bad43efd498be21b19d2e845a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:05:04 +0000

    Add package flowpyter-task/1.1.2.post0.dev17/flowpyter-task-1.1.2.post0.dev17.tar.gz

---
+++ packages/flowpyter-task/flowpyter-task-1.1.2.post0.dev17.tar.gz/flowpyter-task-1.1.2.post0.dev17/src/flowpytertask/__init__.py
@@ -0,0 +1,17 @@
+from . import _version
+__version__ = _version.get_versions()['version']
+
+from . import flowpytertask
+
+from .flowpytertask import \
+    NotebookExtensionError, \
+    ReservedParameterError, \
+    ReservedMountError, \
+    RelativePathError, \
+    BadParameterError, \
+    FlowkitEnvError, \
+    ParameterFileError, \
+    PapermillOperator, \
+    FlowpyterOperator, \
+    TemplateOperator, \
+    is_running_on_docker
