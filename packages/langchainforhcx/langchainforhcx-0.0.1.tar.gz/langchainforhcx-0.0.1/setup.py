8f38f26fd6a9b3c6fe89f0a297f01c4b0c549168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:32 +0000

    Add package langchainforhcx/0.0.1/langchainforhcx-0.0.1.tar.gz

---
+++ packages/langchainforhcx/langchainforhcx-0.0.1.tar.gz/langchainforhcx-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchainforhcx',
+    version='0.0.1',
+    description='langchain for hyperclovax written by xeexu',
+    author='xeexu',
+    author_email='bhs379@gmail.com',
+    url='',
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=['xeexu', 'pypi'],
+    python_requires='>=3.11',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+    ],
+)
