10609904cec48e4076fbbdd1fdc14ea76a9320b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:04:26 +0000

    Add package g4f/0.2.0.6/g4f-0.2.0.6-py3-none-any.whl

---
+++ packages/g4f/g4f-0.2.0.6-py3-none-any.whl/g4f/gui/__init__.py
@@ -0,0 +1,34 @@
+try:
+    from .server.app     import app
+    from .server.website import Website
+    from .server.backend import Backend_Api
+except ImportError:
+    from g4f.errors import MissingRequirementsError
+    raise MissingRequirementsError('Install "flask" and "werkzeug" package for gui')
+
+def run_gui(host: str = '0.0.0.0', port: int = 80, debug: bool = False) -> None:
+    config = {
+        'host' : host,
+        'port' : port,
+        'debug': debug
+    }
+    
+    site = Website(app)
+    for route in site.routes:
+        app.add_url_rule(
+            route,
+            view_func = site.routes[route]['function'],
+            methods   = site.routes[route]['methods'],
+        )
+    
+    backend_api  = Backend_Api(app)
+    for route in backend_api.routes:
+        app.add_url_rule(
+            route,
+            view_func = backend_api.routes[route]['function'],
+            methods   = backend_api.routes[route]['methods'],
+        )
+    
+    print(f"Running on port {config['port']}")
+    app.run(**config)
+    print(f"Closing port {config['port']}")
