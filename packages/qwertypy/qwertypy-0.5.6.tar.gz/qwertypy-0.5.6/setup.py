0b9ea500d36451142946042e2196333dba3f78e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:29 +0000

    Add package qwertypy/0.5.6/qwertypy-0.5.6.tar.gz

---
+++ packages/qwertypy/qwertypy-0.5.6.tar.gz/qwertypy-0.5.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='qwertypy',
+    version='0.5.6',
+    author='Vipul Sharma | @qwertyvipul',
+    description='My personal python utilities library.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qwertyvipul/qwertypy",
+    project_urls={
+        "Repository": "https://github.com/qwertyvipul/qwertypy",
+        "Changelog": "https://github.com/qwertyvipul/qwertypy/blob/main/CHANGELOG.md"
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires='>=3.6',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=install_requires,
+)
