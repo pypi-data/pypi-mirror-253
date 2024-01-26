d199d41ca37650a2e67386e2bd983d57107b387f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:04:46 +0000

    Add package Feluda/0.0.5/Feluda-0.0.5.tar.gz

---
+++ packages/Feluda/Feluda-0.0.5.tar.gz/Feluda-0.0.5/setup.py
@@ -0,0 +1,33 @@
+import codecs
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.5'
+DESCRIPTION = 'feluda'
+
+# Setting up
+setup(
+    name="Feluda",
+    version=VERSION,
+    author="Soumyajit Pan",
+    author_email="soumyajitpan29@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['excel', 'excelreader', 'excelwriter', 'python', 'excel reader and writer', 'read excel data using python', 'write data in excel using python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
