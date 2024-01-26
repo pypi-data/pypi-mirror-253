770f8a9fa2cb9c4d537cd4317c494ce4af22b218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:07 +0000

    Add package dr22-probability/0.1/dr22_probability-0.1.tar.gz

---
+++ packages/dr22-probability/dr22_probability-0.1.tar.gz/dr22_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dr22_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dr22_probability'],
+      zip_safe=False)
