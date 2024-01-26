8db524a64ffdaa7b78af5de91f89b567bd4c2be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 04:04:51 +0000

    Add package fake-statement-detection/0.1/fake-statement-detection-0.1.tar.gz

---
+++ packages/fake-statement-detection/fake-statement-detection-0.1.tar.gz/fake-statement-detection-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fake-statement-detection',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'spacy', 'langchain', 'sentence_transformers', 'xgboost'],
+    entry_points={
+        'console_scripts': [
+            'fake-classifier-init = FakeStatementDetection.Satalia_Assignment:Classifier.init',
+            'fake-classifier-load-preprocess = FakeStatementDetection.Satalia_Assignment:Classifier.load_and_preprocess_dataset_from_csv',
+            'fake-classifier-fit = FakeStatementDetection.Satalia_Assignment:Classifier.fit_fake_classifier',
+            'fake-classifier-predict-dict = FakeStatementDetection.Satalia_Assignment:Classifier.predict_dict',
+            'fake-classifier-predict-json = FakeStatementDetection.Satalia_Assignment:Classifier.predict_json_by_path',
+        ],
+    },
+)
