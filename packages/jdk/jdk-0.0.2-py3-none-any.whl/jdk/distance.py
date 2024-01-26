2af0a2f1c101cfe3fe767b33a8e43fda58351a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:41 +0000

    Add package jdk/0.0.2/jdk-0.0.2-py3-none-any.whl

---
+++ packages/jdk/jdk-0.0.2-py3-none-any.whl/jdk/distance.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def Euclidean(a,b):
+    a_array=df[a].values
+    b_array=df[b].values
+    distance = np.linalg.norm(a-b)
+    
+    return distance
+
+def test():
+    return 11
+
+
