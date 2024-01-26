69983fde683228cda0e144bc4ed5a0f2bbb5e2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 06:03:41 +0000

    Add package RikPy/0.2.14/RikPy-0.2.14.tar.gz

---
+++ packages/RikPy/RikPy-0.2.14.tar.gz/RikPy-0.2.14/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RikPy",
+    version="0.2.14",
+    packages=find_packages(),
+    description="A personal Python package by Rik",
+    author="Rik",
+    author_email="rfornp@gmail.com",
+    url="https://github.com/rfp-byte/RikPy",
+    # Add more options as needed
+)
