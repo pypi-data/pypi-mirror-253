f3d46c9cf15466ba06f06f92602d95ea18aef69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:04:18 +0000

    Add package hlatypingtools/1.0.7.3.1/hlatypingtools-1.0.7.3.1-py3-none-any.whl

---
+++ packages/hlatypingtools/hlatypingtools-1.0.7.3.1-py3-none-any.whl/hlatypingtools/decrypt_file.py
@@ -0,0 +1,28 @@
+# Only needs to be run once
+import logging
+import pandas as pd
+import os
+import pickle
+import pyAesCrypt  # type: ignore
+
+PATH_THIS_FILE: str = os.path.dirname(os.path.abspath(__file__))
+
+
+def decrypt_file(password: str) -> None:
+    try:
+        pyAesCrypt.decryptFile(
+            PATH_THIS_FILE + "/data/data.pickle.aes",
+            PATH_THIS_FILE + "/data/data.pickle",
+            password
+        )
+        logging.info("File decrypted successfully. Restart Kernel / Python.")
+    except ValueError:
+        logging.error(
+            "Error 0: Wrong password, please contact lhotteromain@gmail.com to request access to the product."
+        )
+
+
+def open_pickle_file() -> dict[str, pd.DataFrame]:
+    with open(PATH_THIS_FILE + "/data/data.pickle", "rb") as f:
+        data = pickle.load(f)
+    return data
