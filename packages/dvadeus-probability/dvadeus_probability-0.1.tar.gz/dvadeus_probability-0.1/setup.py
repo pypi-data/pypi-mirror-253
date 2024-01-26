58823ab53855f89f87233c68f912891bd14e68d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 16:03:31 +0000

    Add package dvadeus-probability/0.1/dvadeus_probability-0.1.tar.gz

---
+++ packages/dvadeus-probability/dvadeus_probability-0.1.tar.gz/dvadeus_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dvadeus_probability',
+      version='0.1',
+      description='Gaussian snd Binomial distributions',
+      packages=['dvadeus_probability'],
+      autor = 'David Duarte',
+      author_email = 'email@email.com',
+      zip_safe=False)
