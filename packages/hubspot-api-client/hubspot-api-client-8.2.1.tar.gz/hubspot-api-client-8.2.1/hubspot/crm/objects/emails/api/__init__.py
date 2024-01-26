110ca014c88b7568cca536a91834ab03471c2969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:33 +0000

    Add package hubspot-api-client/8.2.1/hubspot-api-client-8.2.1.tar.gz

---
+++ packages/hubspot-api-client/hubspot-api-client-8.2.1.tar.gz/hubspot-api-client-8.2.1/hubspot/crm/objects/emails/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from hubspot.crm.objects.emails.api.basic_api import BasicApi
+from hubspot.crm.objects.emails.api.batch_api import BatchApi
+from hubspot.crm.objects.emails.api.public_object_api import PublicObjectApi
+from hubspot.crm.objects.emails.api.search_api import SearchApi
---
+++ packages/hubspot-api-client/hubspot-api-client-8.2.1.tar.gz/hubspot-api-client-8.2.1/hubspot/discovery/marketing/events/discovery.py
@@ -0,0 +1,20 @@
+import hubspot.marketing.events as api_client
+from ...discovery_base import DiscoveryBase
+
+
+class Discovery(DiscoveryBase):
+    @property
+    def attendance_subscriber_state_changes_api(self) -> api_client.AttendanceSubscriberStateChangesApi:
+        return self._configure_api_client(api_client, "AttendanceSubscriberStateChangesApi")
+
+    @property
+    def marketing_events_external_api(self) -> api_client.MarketingEventsExternalApi:
+        return self._configure_api_client(api_client, "MarketingEventsExternalApi")
+
+    @property
+    def search_api(self) -> api_client.SearchApi:
+        return self._configure_api_client(api_client, "SearchApi")
+
+    @property
+    def settings_external_api(self) -> api_client.SettingsExternalApi:
+        return self._configure_api_client(api_client, "SettingsExternalApi")
