cd72f68aa84f2e48032824678e20328ff51dc15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 15:03:51 +0000

    Add package datamachine/0.1.4/datamachine-0.1.4-py3-none-any.whl

---
+++ packages/datamachine/datamachine-0.1.4-py3-none-any.whl/datamachine/__init__.py
@@ -0,0 +1,6 @@
+""" happy now? """
+from ._modular import import_notebook
+from ._modular import set_home
+from ._modular import set_index
+from ._modular import set_library
+from ._modular import show
