479db1be4bdee2ef7704751923bb9bf89b61ee52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:04:08 +0000

    Add package aind-data-schema/0.25.0/aind-data-schema-0.25.0.tar.gz

---
+++ packages/aind-data-schema/aind-data-schema-0.25.0.tar.gz/aind-data-schema-0.25.0/examples/subject.py
@@ -0,0 +1,20 @@
+""" example subject """
+
+import datetime
+
+from aind_data_schema.core.subject import Housing, Sex, Subject
+from aind_data_schema.models.species import Species
+
+t = datetime.datetime(2022, 11, 22, 8, 43, 00)
+
+s = Subject(
+    species=Species.MUS_MUSCULUS,
+    subject_id="12345",
+    sex=Sex.MALE,
+    date_of_birth=t.date(),
+    genotype="Emx1-IRES-Cre;Camk2a-tTA;Ai93(TITL-GCaMP6f)",
+    housing=Housing(home_cage_enrichment=["Running wheel"], cage_id="123"),
+    background_strain="C57BL/6J",
+)
+
+s.write_standard_file()
