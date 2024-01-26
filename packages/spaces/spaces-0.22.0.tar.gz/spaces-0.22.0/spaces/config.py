9b7879fd4d231949cee202c9465550be349f7c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 03:04:38 +0000

    Add package spaces/0.22.0/spaces-0.22.0.tar.gz

---
+++ packages/spaces/spaces-0.22.0.tar.gz/spaces-0.22.0/spaces/config.py
@@ -0,0 +1,27 @@
+"""
+"""
+from __future__ import annotations
+
+import os
+
+from .utils import boolean
+
+
+class Settings:
+    def __init__(self):
+        self.zero_gpu = boolean(
+            os.getenv('SPACES_ZERO_GPU'))
+        self.zero_device_api_url = (
+            os.getenv('SPACES_ZERO_DEVICE_API_URL'))
+        self.gradio_auto_wrap = boolean(
+            os.getenv('SPACES_GRADIO_AUTO_WRAP'))
+
+
+Config = Settings()
+
+
+if Config.zero_gpu:
+    assert Config.zero_device_api_url is not None, (
+        'SPACES_ZERO_DEVICE_API_URL env must be set '
+        'on ZeroGPU Spaces (identified by SPACES_ZERO_GPU=true)'
+    )
