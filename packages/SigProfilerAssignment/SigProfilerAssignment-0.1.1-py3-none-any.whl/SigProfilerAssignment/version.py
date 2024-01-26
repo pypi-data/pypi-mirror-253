1eaa7b7635e9ad777f7aa21cb5445b4d74b3011e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 05:03:51 +0000

    Add package SigProfilerAssignment/0.1.1/SigProfilerAssignment-0.1.1-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.1.1-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.1.1'
+version = '0.1.1'
+Update = 'v0.1.1: Update cosmic fit and decompose fit to use SV COSMIC signatures.'
+
+    
+    
