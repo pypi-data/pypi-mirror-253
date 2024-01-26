cd7fb8d7d45d367af5458736b6064840f4d91d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-26 14:03:52 +0000

    Add package cellmaps-imagedownloader/0.1.0a18/cellmaps_imagedownloader-0.1.0a18-py2.py3-none-any.whl

---
+++ packages/cellmaps-imagedownloader/cellmaps_imagedownloader-0.1.0a18-py2.py3-none-any.whl/cellmaps_imagedownloader/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cellmaps_imagedownloader."""
+
+__author__ = 'Cell Maps team'
+__email__ = 'tools@cm4ai.org'
+__version__ = '0.1.0a18'
+__repo_url__ = 'https://github.com/idekerlab/cellmaps_imagedownloader'
+__description__ = 'A tool to download specified image data from the Human Protein Atlas website using CSV file'
+__computation_name__ = 'IF Image Loader'
