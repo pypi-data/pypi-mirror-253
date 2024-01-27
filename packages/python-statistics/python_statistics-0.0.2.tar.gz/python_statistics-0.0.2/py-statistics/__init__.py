14652341ebd7befb05a091a17bbc7df53a2e6148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:25 +0000

    Add package python-statistics/0.0.2/python_statistics-0.0.2.tar.gz

---
+++ packages/python-statistics/python_statistics-0.0.2.tar.gz/python_statistics-0.0.2/py-statistics/__init__.py
@@ -0,0 +1,1 @@
+from .statisticsmodule import Z_Score
---
+++ packages/python-statistics/python_statistics-0.0.2.tar.gz/python_statistics-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_statistics",
+    version="0.0.2",
+    author="Meteey (Mustafa Konur)",
+    author_email="<konurmustafa17@gmail.com>",
+    description="Module name confusion fixed",
+    packages=find_packages(),
+    keywords=["statistics", "z-score", "normalization"],
+    requires=[],
+    
+)
