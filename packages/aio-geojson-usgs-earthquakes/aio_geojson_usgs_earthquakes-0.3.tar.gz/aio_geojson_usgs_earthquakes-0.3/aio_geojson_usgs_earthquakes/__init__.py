343c0398128685d23c655e6300d3ed4bb71cf8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:04:24 +0000

    Add package aio-geojson-usgs-earthquakes/0.3/aio_geojson_usgs_earthquakes-0.3.tar.gz

---
+++ packages/aio-geojson-usgs-earthquakes/aio_geojson_usgs_earthquakes-0.3.tar.gz/aio_geojson_usgs_earthquakes-0.3/aio_geojson_usgs_earthquakes/__init__.py
@@ -0,0 +1,3 @@
+"""USGS Earthquake Hazards Program library."""
+from .feed import UsgsEarthquakeHazardsProgramFeed  # noqa: F401
+from .feed_manager import UsgsEarthquakeHazardsProgramFeedManager  # noqa: F401
