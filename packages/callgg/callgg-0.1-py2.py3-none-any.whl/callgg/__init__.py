689910ac5140799c4e1c9ad9001ba7f867b9034c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 14:04:28 +0000

    Add package callgg/0.1/callgg-0.1-py2.py3-none-any.whl

---
+++ packages/callgg/callgg-0.1-py2.py3-none-any.whl/callgg/__init__.py
@@ -0,0 +1,21 @@
+
+__version__ = "0.1"
+
+# if __name__=="__init__":
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("connector", help="choose a connector: c, cpp, g, gpp")
+#     args = parser.parse_args()
+
+#     if args.connector=="c":
+#         from .c import *
+#     elif args.connector=="cpp":
+#         from .cpp import *
+#     elif args.connector=="g":
+#         from .g import *
+#     elif args.connector=="gpp":
+#         from .gpp import *
+#     else:
+#         print("Not a valid connector.")
+
+# else: print("imported as a module")
