8bfc4a2b086e6d3c200b832347528321f70d2318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 14:04:32 +0000

    Add package tox-backtick/0.4.4/tox-backtick-0.4.4.zip

---
+++ packages/tox-backtick/tox-backtick-0.4.4.zip/tox-backtick-0.4.4/tests/test_main.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2024 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/license/zlib
+
+import unittest
+from pathlib import Path
+
+import tox_backtick
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
