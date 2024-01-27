4cc312a0291b9210d43b8a82fc031eea1f80eedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:07:10 +0000

    Add package sparse_autoencoder/1.10.0/sparse_autoencoder-1.10.0.tar.gz

---
+++ packages/sparse_autoencoder/sparse_autoencoder-1.10.0.tar.gz/sparse_autoencoder-1.10.0/sparse_autoencoder/train/utils/get_model_device.py
@@ -0,0 +1,29 @@
+"""Get the device that the model is on."""
+import torch
+from torch.nn import Module
+from torch.nn.parallel import DataParallel
+
+
+def get_model_device(model: Module | DataParallel) -> torch.device:
+    """Get the device on which a PyTorch model is on.
+
+    Args:
+        model: The PyTorch model.
+
+    Returns:
+        The device ('cuda' or 'cpu') where the model is located.
+
+    Raises:
+        ValueError: If the model has no parameters.
+    """
+    # Deepspeed models already have a device property, so just return that
+    if hasattr(model, "device"):
+        return model.device
+
+    # Check if the model has parameters
+    if len(list(model.parameters())) == 0:
+        exception_message = "The model has no parameters."
+        raise ValueError(exception_message)
+
+    # Return the device of the first parameter
+    return next(model.parameters()).device
