20952174262f399332dae281547c896a6f45feaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:06:08 +0000

    Add package pygpt-net/2.0.120/pygpt-net-2.0.120.tar.gz

---
+++ packages/pygpt-net/pygpt-net-2.0.120.tar.gz/pygpt-net-2.0.120/src/pygpt_net/core/chain/completion.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.23 21:00:00                  #
+# ================================================== #
+
+class Completion:
+    def __init__(self, window=None):
+        """
+        Langchain Wrapper
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.input_tokens = 0
+
+    def send(
+            self,
+            input_prompt: str,
+            stream_mode: bool = False,
+            system_prompt: str = None,
+            ai_name: str = None,
+            user_name: str = None
+    ):
+        """
+        Chat with LLM
+
+        :param input_prompt: prompt
+        :param stream_mode: stream mode
+        :param system_prompt: system prompt (optional)
+        :param ai_name: AI name (optional)
+        :param user_name: username (optional)
+        :return: LLM response
+        """
+        llm = None
+        model = self.window.core.models.get(self.window.core.config.get('model'))
+        if 'provider' in model.langchain:
+            provider = model.langchain['provider']
+            if provider in self.window.core.llm.llms:
+                try:
+                    # init
+                    self.window.core.llm.llms[provider].init(
+                        self.window, model, "langchain", "completion")
+                    # get LLM provider instance
+                    llm = self.window.core.llm.llms[provider].completion(
+                        self.window, model, stream_mode)
+                except Exception as e:
+                    self.window.core.debug.log(e)
+                    raise e
+        if llm is None:
+            raise Exception("Invalid LLM")
+
+        message = self.build(
+            input_prompt,
+            system_prompt=system_prompt,
+            ai_name=ai_name,
+            user_name=user_name
+        )
+        if stream_mode:
+            return llm.stream(message)
+        else:
+            return llm.invoke(message)
+
+    def build(
+            self,
+            input_prompt: str,
+            system_prompt: str = None,
+            ai_name: str = None,
+            user_name: str = None
+    ) -> str:
+        """
+        Build completion string
+
+        :param input_prompt: prompt (current)
+        :param system_prompt: system prompt (optional)
+        :param ai_name: AI name (optional)
+        :param user_name: username (optional)
+        :return: message string (parsed with context)
+        """
+        message = ""
+
+        # tokens config
+        model = self.window.core.config.get('model')
+        model_id = self.window.core.models.get_id(model)
+        mode = self.window.core.config.get('mode')
+
+        used_tokens = self.window.core.tokens.from_user(input_prompt, system_prompt)
+        max_tokens = self.window.core.config.get('max_total_tokens')
+        model_ctx = self.window.core.models.get_num_ctx(model_id)
+
+        # fit to max model ctx tokens
+        if max_tokens > model_ctx:
+            max_tokens = model_ctx
+
+        # input tokens: reset
+        self.reset_tokens()
+
+        if system_prompt is not None and system_prompt != "":
+            message += system_prompt
+
+        if self.window.core.config.get('use_context'):
+            items = self.window.core.ctx.get_prompt_items(
+                model_id,
+                mode,
+                used_tokens,
+                max_tokens
+            )
+            for item in items:
+                if item.input_name is not None \
+                        and item.output_name is not None \
+                        and item.input_name != "" \
+                        and item.output_name != "":
+                    if item.input is not None and item.input != "":
+                        message += "\n" + item.input_name + ": " + item.input
+                    if item.output is not None and item.output != "":
+                        message += "\n" + item.output_name + ": " + item.output
+                else:
+                    if item.input is not None and item.input != "":
+                        message += "\n" + item.input
+                    if item.output is not None and item.output != "":
+                        message += "\n" + item.output
+
+        # append names
+        if user_name is not None \
+                and ai_name is not None \
+                and user_name != "" \
+                and ai_name != "":
+            message += "\n" + user_name + ": " + str(input_prompt)
+            message += "\n" + ai_name + ":"
+        else:
+            message += "\n" + str(input_prompt)
+
+        # input tokens: update
+        self.input_tokens += self.window.core.tokens.from_text(
+            message,
+            model_id
+        )
+
+        return message
+
+    def reset_tokens(self):
+        """Reset input tokens counter"""
+        self.input_tokens = 0
+
+    def get_used_tokens(self) -> int:
+        """
+        Get input tokens counter
+
+        :return: input tokens counter
+        """
+        return self.input_tokens
---
+++ packages/pygpt-net/pygpt-net-2.0.120.tar.gz/pygpt-net-2.0.120/src/pygpt_net/core/idx/chat.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.23 19:00:00                  #
+# ================================================== #
+
+from llama_index.llms import ChatMessage, MessageRole
+from llama_index.prompts import ChatPromptTemplate
+from llama_index.memory import ChatMemoryBuffer
+
+from pygpt_net.item.ctx import CtxItem
+from pygpt_net.item.model import ModelItem
+from .context import Context
+
+
+class Chat:
+    def __init__(self, window=None, storage=None):
+        """
+        Chat with index core
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.storage = storage
+        self.context = Context(window)
+
+    def call(
+            self,
+            ctx: CtxItem,
+            idx: str,
+            model: ModelItem,
+            sys_prompt: str = None,
+            stream: bool = False) -> bool:
+        """
+        Call chat or query mode
+
+        :param ctx: context item
+        :param idx: index name
+        :param model: model item
+        :param sys_prompt: system prompt
+        :param stream: stream mode
+        :return: True if success
+        """
+        # check if model provided
+        if model is None:
+            raise Exception("Model config not provided")
+
+        # check if chat mode is available
+        if "chat" in model.llama_index['mode']:
+            return self.chat(ctx, idx, model, sys_prompt, stream)
+        else:
+            # if not, use query mode
+            return self.query(ctx, idx, model, sys_prompt, stream)
+
+    def raw_query(
+            self,
+            ctx: CtxItem,
+            idx: str,
+            model: ModelItem,
+            sys_prompt: str = None,
+            stream: bool = False) -> bool:
+        """
+        Raw query mode
+
+        :param ctx: context item
+        :param idx: index name
+        :param model: model item
+        :param sys_prompt: system prompt
+        :param stream: stream mode
+        :return: True if success
+        """
+        return self.query(ctx, idx, model, sys_prompt, stream)
+
+    def query(
+            self,
+            ctx: CtxItem,
+            idx: str,
+            model: ModelItem,
+            sys_prompt: str = None,
+            stream: bool = False) -> bool:
+        """
+        Query index
+
+        :param ctx: CtxItem
+        :param idx: index name
+        :param model: model item
+        :param sys_prompt: system prompt
+        :param stream: stream response
+        :return: True if success
+        """
+        query = ctx.input
+
+        # log query
+        is_log = False
+        if self.window.core.config.has("llama.log") \
+                and self.window.core.config.get("llama.log"):
+            is_log = True
+
+        if model is None:
+            raise Exception("Model config not provided")
+
+        if is_log:
+            print("[LLAMA-INDEX] Query index...")
+            print("[LLAMA-INDEX] Idx: {}, query: {}, model: {}".format(idx, query, model.id))
+
+        # check if index exists
+        if not self.storage.exists(idx):
+            raise Exception("Index not prepared")
+
+        context = self.window.core.idx.llm.get_service_context(model=model)
+        index = self.storage.get(idx, service_context=context)  # get index
+        input_tokens = self.window.core.tokens.from_llama_messages(
+            query, [], model.id)
+
+        # query index
+        tpl = self.get_custom_prompt(sys_prompt)
+        if tpl is not None:
+            if is_log:
+                print("[LLAMA-INDEX] Query index with custom prompt: {}...".format(sys_prompt))
+            response = index.as_query_engine(
+                streaming=stream,
+                text_qa_template=tpl,
+            ).query(query)  # query with custom sys prompt
+        else:
+            response = index.as_query_engine(
+                streaming=stream,
+            ).query(query)  # query with default prompt
+
+        if stream:
+            ctx.stream = response.response_gen
+            ctx.input_tokens = input_tokens
+            ctx.set_output("", "")
+            return True
+
+        ctx.input_tokens = input_tokens
+        ctx.output_tokens = self.window.core.tokens.from_llama_messages(
+            response, [], model.id)  # calc from response
+        ctx.set_output(str(response), "")
+        return True
+
+    def chat(
+            self,
+            ctx: CtxItem,
+            idx: str,
+            model: ModelItem,
+            sys_prompt: str = None,
+            stream: bool = False) -> bool:
+        """
+        Chat using index
+
+        :param ctx: context item
+        :param idx: index name
+        :param model: model item
+        :param sys_prompt: system prompt
+        :param stream: stream response
+        :return: True if success
+        """
+        query = ctx.input
+
+        # log query
+        is_log = False
+        if self.window.core.config.has("llama.log") \
+                and self.window.core.config.get("llama.log"):
+            is_log = True
+
+        if model is None:
+            raise Exception("Model config not provided")
+
+        if is_log:
+            print("[LLAMA-INDEX] Chat with index...")
+            print("[LLAMA-INDEX] Idx: {}, query: {}, model: {}".format(idx, query, model.id))
+
+        # check if index exists
+        if not self.storage.exists(idx):
+            raise Exception("Index not prepared")
+
+        context = self.window.core.idx.llm.get_service_context(model=model)
+        index = self.storage.get(idx, service_context=context)  # get index
+
+        # append context from DB
+        history = self.context.get_messages(ctx.input, sys_prompt)
+        memory = self.get_memory_buffer(history)
+        input_tokens = self.window.core.tokens.from_llama_messages(
+            query, history, model.id)
+        chat_engine = index.as_chat_engine(
+            chat_mode="context",
+            memory=memory,
+            system_prompt=sys_prompt,
+        )
+        if stream:
+            response = chat_engine.stream_chat(query)
+            ctx.stream = response.response_gen
+            ctx.input_tokens = input_tokens
+            ctx.set_output("", "")
+            return True
+        else:
+            response = chat_engine.chat(query)
+
+        ctx.input_tokens = input_tokens
+        ctx.output_tokens = self.window.core.tokens.from_llama_messages(
+            response, [], model.id)  # calc from response
+        ctx.set_output(str(response), "")
+
+        return True
+
+    def get_memory_buffer(self, history: list) -> ChatMemoryBuffer:
+        """
+        Get memory buffer
+
+        :param history: Memory with chat history
+        """
+        return ChatMemoryBuffer.from_defaults(chat_history=history)
+
+    def get_custom_prompt(self, prompt: str = None) -> ChatPromptTemplate or None:
+        """
+        Get custom prompt template if sys prompt is not empty
+
+        :param prompt: system prompt
+        :return: ChatPromptTemplate or None if prompt is empty
+        """
+        if prompt is None or prompt.strip() == "":
+            return None
+
+        qa_msgs = [
+            ChatMessage(
+                role=MessageRole.SYSTEM,
+                content=prompt,
+            ),
+            ChatMessage(
+                role=MessageRole.USER,
+                content=(
+                    "Context information is below.\n"
+                    "---------------------\n"
+                    "{context_str}\n"
+                    "---------------------\n"
+                    "Given the context information and not prior knowledge, "
+                    "answer the question: {query_str}\n"
+                ),
+            ),
+        ]
+        return ChatPromptTemplate(qa_msgs)
