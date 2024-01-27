7bc66c1ad37a43f3274ac202e49e69c3c34ff0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:03:44 +0000

    Add package nextline/0.7.0/nextline-0.7.0-py3-none-any.whl

---
+++ packages/nextline/nextline-0.7.0-py3-none-any.whl/nextline/plugin/hook.py
@@ -0,0 +1,20 @@
+from logging import getLogger
+
+from apluggy import PluginManager
+
+from . import plugins, spec
+
+
+def build_hook() -> PluginManager:
+    hook = PluginManager(spec.PROJECT_NAME)
+    hook.add_hookspecs(spec)
+
+    plugins.register(hook)
+
+    logger = getLogger(__name__)
+
+    plugin_names = [n for n, p in hook.list_name_plugin() if p]
+    logger.info(f'Pluggy project name: {hook.project_name!r}')
+    logger.info(f'Loaded plugins: {plugin_names}')
+
+    return hook
