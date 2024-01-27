a223de2b0beb0b7aa6a59d5ed8005b08a633a640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:04:19 +0000

    Add package vistutils/0.1.5/vistutils-0.1.5.tar.gz

---
+++ packages/vistutils/vistutils-0.1.5.tar.gz/vistutils-0.1.5/src/vistutils/metas/_type_names.py
@@ -0,0 +1,7 @@
+"""This file provides names for types commonly used in pyros. """
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+Bases = tuple[type, ...]
+Namespace = dict
