87d9a87ca485803d23fd926a1eec480d25b13d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 05:05:53 +0000

    Add package COMPAS/2.0.0b4/COMPAS-2.0.0b4-py2.py3-none-any.whl

---
+++ packages/COMPAS/COMPAS-2.0.0b4-py2.py3-none-any.whl/compas/datastructures/datastructure.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from compas.data import Data
+
+
+class Datastructure(Data):
+    """Base class for all data structures."""
+
+    def __init__(self, attributes=None, name=None):
+        super(Datastructure, self).__init__(name=name)
+        self.attributes = attributes or {}
+
+    def transform(self, transformation):
+        """Transforms the data structure.
+
+        Parameters
+        ----------
+        transformation : :class:`Transformation`
+            The transformation used to transform the data structure.
+
+        Returns
+        -------
+        None
+
+        """
+        raise NotImplementedError
+
+    def transformed(self, transformation):
+        """Returns a transformed copy of this data structure.
+
+        Parameters
+        ----------
+        transformation : :class:`Transformation`
+            The transformation used to transform the copy.
+
+        Returns
+        -------
+        Datastructure
+            The transformed copy.
+
+        """
+        datastructure = self.copy()
+        datastructure.transform(transformation)
+        return datastructure
+
+    def transform_numpy(self, transformation):
+        """Transforms the data structure.
+
+        Parameters
+        ----------
+        transformation : :class:`Transformation`
+            The transformation used to transform the data structure.
+
+        Returns
+        -------
+        None
+
+        """
+        raise NotImplementedError
+
+    def transformed_numpy(self, transformation):
+        """Returns a transformed copy of this data structure.
+
+        Parameters
+        ----------
+        transformation : :class:`Transformation`
+            The transformation used to transform the copy.
+
+        Returns
+        -------
+        Datastructure
+            The transformed copy.
+
+        """
+        datastructure = self.copy()
+        datastructure.transform_numpy(transformation)
+        return datastructure
---
+++ packages/COMPAS/COMPAS-2.0.0b4-py2.py3-none-any.whl/compas_ghpython/components/Compas_FromJson/code.py
@@ -0,0 +1,17 @@
+"""
+Deserializes JSON into COMPAS objects.
+"""
+from ghpythonlib.componentbase import executingcomponent as component
+
+import compas
+
+
+class CompasInfo(component):
+    def RunScript(self, json):
+        if not json:
+            return None
+
+        try:
+            return compas.json_load(json)
+        except:  # noqa: E722
+            return compas.json_loads(json)
