3d9455413f676f1a50b923acbbcc0566e14a7aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:56 +0000

    Add package ttxt/0.0.3.5/ttxt-0.0.3.5.tar.gz

---
+++ packages/ttxt/ttxt-0.0.3.5.tar.gz/ttxt-0.0.3.5/ttxt/__init__.py
@@ -0,0 +1,21 @@
+from ttxt.base import baseFuturesExchange, baseSpotExchange
+from ttxt.exchanges.gateFutures import gateFutures
+from ttxt.exchanges.bybitFutures import bybitFutures
+from ttxt.exchanges.bitgetFutures import bitgetFutures
+from ttxt.exchanges.bingx import bingx
+from ttxt.exchanges.biconomy import biconomy
+
+exchanges = [
+    "gateFutures",
+    "bybitFutures",
+    "bitgetFutures"
+    "bingx",
+    "biconomy"
+]
+
+base = [
+    "baseFuturesExchange",
+    "baseSpotExchange"
+]
+
+_all__ =  exchanges + base
