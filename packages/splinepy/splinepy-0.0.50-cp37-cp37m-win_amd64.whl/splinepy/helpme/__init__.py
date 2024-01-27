25435b25a2e6aadf4209dd8efbe5e38ee2a815f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:04 +0000

    Add package splinepy/0.0.50/splinepy-0.0.50-cp37-cp37m-win_amd64.whl

---
+++ packages/splinepy/splinepy-0.0.50-cp37-cp37m-win_amd64.whl/splinepy/helpme/__init__.py
@@ -0,0 +1,19 @@
+from splinepy.helpme import (
+    create,
+    extract,
+    integrate,
+    mapper,
+    multi_index,
+    permute,
+    visualize,
+)
+
+__all__ = [
+    "create",
+    "extract",
+    "integrate",
+    "mapper",
+    "multi_index",
+    "permute",
+    "visualize",
+]
