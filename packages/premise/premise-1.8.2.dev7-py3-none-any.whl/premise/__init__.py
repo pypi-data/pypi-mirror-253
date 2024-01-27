3b9fd81638a1100016e27d4c942c43a083e16aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:07:05 +0000

    Add package premise/1.8.2.dev7/premise-1.8.2.dev7-py3-none-any.whl

---
+++ packages/premise/premise-1.8.2.dev7-py3-none-any.whl/premise/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ("NewDatabase", "clear_cache", "get_regions_definition")
+__version__ = (1, 8, 2, "dev7")
+
+
+from .ecoinvent_modification import NewDatabase
+from .utils import clear_cache, get_regions_definition
