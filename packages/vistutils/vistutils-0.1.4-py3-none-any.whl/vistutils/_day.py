1c0c0c30e34e126827797caa76f9fd9cc4833b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:05:08 +0000

    Add package vistutils/0.1.4/vistutils-0.1.4-py3-none-any.whl

---
+++ packages/vistutils/vistutils-0.1.4-py3-none-any.whl/vistutils/_day.py
@@ -0,0 +1,13 @@
+"""Day represents a date format for the purpose of demonstrating the Field
+descriptor class. """
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from vistutils.fields import Field
+
+
+class Day:
+  """Day represents a date format for the purpose of demonstrating the Field
+  descriptor class. """
+  
