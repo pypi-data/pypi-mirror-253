b5ef3b414b7de9d7e0d79feb73610b6f94dec5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:53 +0000

    Add package python-statistics/0.0.1/python_statistics-0.0.1.tar.gz

---
+++ packages/python-statistics/python_statistics-0.0.1.tar.gz/python_statistics-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_statistics",
+    version="0.0.1",
+    author="Meteey (Mustafa Konur)",
+    author_email="<konurmustafa17@gmail.com>",
+    description="First publishing library try",
+    packages=find_packages(),
+    keywords=["statistics", "z-score", "normalization"],
+    requires=[],
+    
+)
