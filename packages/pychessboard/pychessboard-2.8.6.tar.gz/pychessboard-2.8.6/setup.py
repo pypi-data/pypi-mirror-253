f13374ae3eedcb1267589bbabdf0c16e9b45b9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:55 +0000

    Add package pychessboard/2.8.6/pychessboard-2.8.6.tar.gz

---
+++ packages/pychessboard/pychessboard-2.8.6.tar.gz/pychessboard-2.8.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pychessboard',
+    version='2.8.6',
+    packages=find_packages(),
+    install_requires=[
+        'pygame'
+    ],
+    author='James gonzalez',
+    author_email='nadernmds@gmail.com',
+    description='a chessboard game',
+    package_data={'pychessboard': ['assets/images/*']},
+    long_description=open('README').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nadernmds/pychessboard',
+)
