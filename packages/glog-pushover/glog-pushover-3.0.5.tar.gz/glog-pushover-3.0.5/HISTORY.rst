b5e3dc79ab0ecae6ffcc8adc16d6dc8638a0325e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:05:31 +0000

    Add package glog-pushover/3.0.5/glog-pushover-3.0.5.tar.gz

---
+++ packages/glog-pushover/glog-pushover-3.0.5.tar.gz/glog-pushover-3.0.5/HISTORY.rst
@@ -0,0 +1,15 @@
+=======
+History
+=======
+
+1.0.0 (2022-09-24)
+------------------
+
+* Official release.
+* Add pydantic support
+* New structure
+
+0.1.0 (2022-09-24)
+------------------
+
+* First release on PyPI.
---
+++ packages/glog-pushover/glog-pushover-3.0.5.tar.gz/glog-pushover-3.0.5/glog/__init__.py
@@ -0,0 +1,36 @@
+"""This is the main script of the project"""
+import logging
+import logging.handlers
+from glog.handler import GLogHandler
+import structlog
+
+__author__ = """Greg Rabago"""
+__email__ = 'greg.rabago@gmail.com'
+__version__ = '3.0.5'
+
+
+class GLog():
+    """This is the main class of the project"""
+    def __init__(self, logger_name, config_dict):
+        self.logger_name = logger_name
+        self.config_dict = config_dict
+        self.logger = self.configure_logger()
+
+    def configure_logger(self):
+        """Configure python's standard logging"""
+        logger = logging.getLogger(self.logger_name)
+        logger.addHandler(GLogHandler(self.config_dict))
+        logger.level = logging.DEBUG
+        return structlog.wrap_logger(logger)
+
+    def info(self, msg):
+        """Log info messages"""
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        """Log warning messages"""
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        """Log error messages"""
+        self.logger.error(msg)
