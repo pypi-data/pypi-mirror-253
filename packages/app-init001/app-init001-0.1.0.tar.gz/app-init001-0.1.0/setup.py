d5fb0d1cc241371505262c7d244cd02c5bad97a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:03:53 +0000

    Add package app-init001/0.1.0/app-init001-0.1.0.tar.gz

---
+++ packages/app-init001/app-init001-0.1.0.tar.gz/app-init001-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="app-init001",
+    description='a basic code to host a simple flask app',
+    packages=find_packages(),
+    version='0.1.0'
+)
