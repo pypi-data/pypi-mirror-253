a3a6beecb321c1d8bcc0ebc374c54d4ab2b43698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 15:04:08 +0000

    Add package PyEDCR/0.1.5/PyEDCR-0.1.5.tar.gz

---
+++ packages/PyEDCR/PyEDCR-0.1.5.tar.gz/PyEDCR-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+  description = f.read()
+
+
+setup(
+  name='PyEDCR',
+  version='0.1.5',
+  author='Joshua Shay Kricheli, Paulo Shakarian, Spencer Ozgur, Aniruddha Datta, Khoa Vo',
+  author_email='name@example.com',
+  description='A short description of your package',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.6',
+  long_description=description,
+  long_description_content_type = "text/markdown",
+)
