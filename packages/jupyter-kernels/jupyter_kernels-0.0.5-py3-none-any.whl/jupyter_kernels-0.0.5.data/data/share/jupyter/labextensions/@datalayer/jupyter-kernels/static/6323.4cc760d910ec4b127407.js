d660358ee6b0c07dd5c7adcbad57f68d12180196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 16:04:13 +0000

    Add package jupyter-kernels/0.0.5/jupyter_kernels-0.0.5-py3-none-any.whl

---
+++ packages/jupyter-kernels/jupyter_kernels-0.0.5-py3-none-any.whl/jupyter_kernels-0.0.5.data/data/share/jupyter/labextensions/@datalayer/jupyter-kernels/static/6323.4cc760d910ec4b127407.js
@@ -0,0 +1,6 @@
+"use strict";(self.webpackChunk_datalayer_jupyter_kernels=self.webpackChunk_datalayer_jupyter_kernels||[]).push([[6323],{16323:(e,r,t)=>{t.r(r),t.d(r,{KernelsJupyterLab:()=>p,KernelsJupyterLabHeadless:()=>c,KernelsRoutes:()=>j.r});var s=t(74512),a=t(18321),l=t(86497),n=t(89010),u=t(19036);const o=()=>(0,s.jsx)(a.JupyterLabApp,{extensions:[l,n,u],disabledExtensions:[],position:"absolute",height:"100vh"}),p=()=>(0,s.jsx)(a.Jupyter,{jupyterServerHttpUrl:"https://dev1-io.datalayer.run/jupyter/server/eric/default",jupyterServerWsUrl:"wss://dev1-io.datalayer.run/jupyter/server/eric/default",jupyterToken:"test",startDefaultKernel:!1,disableCssLoading:!0,collaborative:!0,children:(0,s.jsx)(o,{})});var d=t(96560),i=t(29482);const y=d.createGlobalStyle`
+  body {
+    background-color: white !important;
+    overflow: auto;
+  }
+`,c=()=>(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(y,{}),(0,s.jsx)(i.Z,{})]});var j=t(24548)}}]);
