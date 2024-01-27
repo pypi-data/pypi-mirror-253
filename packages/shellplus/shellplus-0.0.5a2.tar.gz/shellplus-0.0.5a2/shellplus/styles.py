a474dc0e2c9998028c1062c6a775dd74041419c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:30 +0000

    Add package shellplus/0.0.5a2/shellplus-0.0.5a2.tar.gz

---
+++ packages/shellplus/shellplus-0.0.5a2.tar.gz/shellplus-0.0.5a2/shellplus/styles.py
@@ -0,0 +1,21 @@
+from rich import style
+from rich.console import Console
+
+styles = {
+    'info': 'bold blue',
+    'warn': 'italic yellow',
+    'err': 'bold red',
+    'serr': style(color='#FA6E69', style='bold'),
+    'note': 'dim italic',
+    'worry': style(color='#fdff8e'),
+    'invalid': style(color='#ff3d3d', style='underline italic'),
+    'marked1': 'reverse yellow',
+    'marked2': 'reverse green',
+    'no_intr': 'strikethrough dim',
+    'comment': style(color='#186218', style='dim'),
+}
+
+def print(message, style):
+    shell = Console()
+    stil = styles.get(style, '')
+    shell.print(f'[{stil}]{message}[/{stil}]')
