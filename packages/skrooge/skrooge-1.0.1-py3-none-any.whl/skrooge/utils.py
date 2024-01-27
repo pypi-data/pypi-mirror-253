f35ccaf9c04da10f6ab2352be2379752c0cbdfd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:55 +0000

    Add package skrooge/1.0.1/skrooge-1.0.1-py3-none-any.whl

---
+++ packages/skrooge/skrooge-1.0.1-py3-none-any.whl/skrooge/utils.py
@@ -0,0 +1,43 @@
+import logging
+import math
+
+
+def determine_constrained_resource(instance_data, cpu, mem):
+    logging.info(
+        f"Calculating constrained resource: {instance_data['specs']=}, {cpu=}, {mem=}"
+    )
+    if (
+        instance_data["specs"]["cores"] * 1000 / cpu
+        > instance_data["specs"]["memory"] * 1024 / mem
+    ):
+        return "memory"
+    else:
+        return "cores"
+
+
+def determine_instance_count_required(
+    instance_data, constrained_resource, resource_requirement
+):
+    if constrained_resource == "cores":
+        normalization_factor = 1000
+    else:
+        normalization_factor = 1024
+
+    resource_per_instance = (
+        instance_data["specs"][constrained_resource] * normalization_factor
+    )
+    logging.info(f"{resource_per_instance=}, {resource_requirement=}")
+    logging.info(f"{math.ceil(resource_requirement / resource_per_instance)=}")
+    return math.ceil(resource_requirement / resource_per_instance)
+
+
+def calculate_cost(instance_data, instance_count, cost_class, region):
+    hourly_cost = instance_data["regions"][region][cost_class] * instance_count
+    costs = {
+        "hourly": round(hourly_cost, 2),
+        "monthly": round(hourly_cost * 24 * 365 / 12),
+        "yearly": round(hourly_cost * 24 * 365),
+        "class": cost_class,
+        "region": region,
+    }
+    return costs
