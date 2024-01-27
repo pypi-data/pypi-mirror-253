614787aaf5684970ada2da4f408f965e793e09ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:06:00 +0000

    Add package data-fetcher/0.1.8.1/data_fetcher-0.1.8.1.tar.gz

---
+++ packages/data-fetcher/data_fetcher-0.1.8.1.tar.gz/data_fetcher-0.1.8.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_fetcher',
+    version='0.1.8.1',
+    packages=find_packages(),
+    install_requires=[
+        'ccxt',
+        'pandas',
+        'numpy',
+        'alpaca-trade-api',
+        'alpaca-py',
+        'python-dateutil',
+
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'your_package_name=your_package_name:main',
+        ],
+    },
+)
