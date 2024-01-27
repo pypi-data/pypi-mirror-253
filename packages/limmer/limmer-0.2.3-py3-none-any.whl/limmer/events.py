5dc5b5c88c25f7a74876db451203479249b2cfb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 05:04:56 +0000

    Add package limmer/0.2.3/limmer-0.2.3-py3-none-any.whl

---
+++ packages/limmer/limmer-0.2.3-py3-none-any.whl/limmer/events.py
@@ -0,0 +1,98 @@
+from .basics import Cursor
+import threading
+import time
+from typing import Tuple, Optional
+
+
+class Event:
+    length = 0
+
+    def __init__(self, position: Optional[Tuple[int]]=(1, 1)):
+        self.position = position
+        self.current_cursor = None
+        self.loop_running = False
+        self.loop_thread = None
+
+    def update(self, cursor: Cursor):
+        #print(self.position, cursor.position)
+        self.current_cursor = cursor
+
+    def doUpdate(self, cursor: Cursor):
+        cursor.go_to(*self.position)
+        # ...
+        cursor.finishCMD()
+
+    def startEventLoop(self):
+        self.loop_running = True
+        self.loop_thread = threading.Thread(target=self.eventLoop)
+        self.loop_thread.start()
+
+    def eventLoop(self):
+        for i in range(100):
+            time.sleep(0.1)  # Do stuff
+
+            # Update
+            if self.current_cursor:
+                self.doUpdate(self.current_cursor)
+                self.current_cursor = None
+
+    def stopEventLoop(self):
+        self.loop_running = False
+        self.loop_thread.join()  # Wait for the event loop thread to finish
+
+
+class SpinningEvent(Event):
+    length = 1
+
+    def __init__(self, position: Optional[Tuple[int]]=(0, 0)):
+        super().__init__(position)
+        self.spinner = spinning_cursor()
+        
+    def doUpdate(self, cursor: Cursor):
+        update_pos = self.position
+        update_pos[0] += 1
+        cursor.go_to(*update_pos)
+        cursor.appendCMD("\b")
+        #cursor.go_left()
+        cursor.appendCMD(next(self.spinner))
+        cursor.finishCMD()
+
+
+def spinning_cursor():
+    while True:
+        for cursor in '\\|/-':
+            yield cursor
+
+
+class PointingsEvent(Event):
+    length = 3
+
+    def __init__(self, position: Optional[Tuple[int]]=(0, 0)):
+        super().__init__(position)
+        self.spinner = pointings_cursor()
+        
+    def doUpdate(self, cursor: Cursor):
+        cursor.go_to(*self.position)
+        cursor.go_left(3)
+        cursor.appendCMD(next(self.spinner))
+        cursor.finishCMD()
+
+
+def pointings_cursor():
+    while True:
+        for cursor in ["   ", ".  ", ".. ", "..."]:
+            yield cursor
+
+
+class BetterPointingsEvent(PointingsEvent):
+    def doUpdate(self, cursor: Cursor):
+        cursor.go_to(*self.position)
+        cursor.go_left(3)
+        
+        pointing = next(self.spinner).strip()
+        
+        difference = self.length - len(pointing)
+        
+        cursor.appendCMD(pointing + (" " * difference))
+        cursor.go_left(difference)
+        cursor.finishCMD()
---
+++ packages/limmer/limmer-0.2.3-py3-none-any.whl/limmer/styles.py
@@ -0,0 +1,54 @@
+from typing import Union
+
+class InLineStyleAttr:
+    def __init__(self, value: str):
+        self.value: str = value
+
+
+class InlineStyle:
+    @staticmethod
+    def addAttr(value: str) -> InLineStyleAttr:
+        return InLineStyleAttr(value)
+        
+    CLEAR = addAttr("0;0;0")
+
+
+class Color(InlineStyle):
+    BLACK = InlineStyle.addAttr(";30;")
+    RED = InlineStyle.addAttr(";31;")
+    GREEN = InlineStyle.addAttr(";32;")
+    YELLOW = InlineStyle.addAttr(";33;")
+    BLUE = InlineStyle.addAttr(";34;")
+    MAGENTA = InlineStyle.addAttr(";35;")
+    CYAN = InlineStyle.addAttr(";36;")
+    WHITE = InlineStyle.addAttr(";37;")
+    CLEAR = InlineStyle.addAttr(";0;")
+
+    @staticmethod
+    def RGB(r: Union[str, int], g: Union[str, int], b: Union[str, int]) -> InLineStyleAttr:
+        return InLineStyleAttr(f";38:2:{r}:{g}:{b};")
+
+class Formatting(InlineStyle):
+    BOLD = InlineStyle.addAttr("1;;")
+    ITALIC = InlineStyle.addAttr("3;;")
+    STRIKETROUGH = InlineStyle.addAttr("9;;")
+    UNDERLINE = InlineStyle.addAttr("4;;")
+    SUBSCRIPT = InlineStyle.addAttr("8B;;")  # Source: http://www.inwap.com/pdp10/ansicode.txt
+    SUPERSCRIPT = InlineStyle.addAttr("8C;;")  # Partial Line Down (subscript) ; Partial Line Up (superscript)
+    CLEAR = InlineStyle.addAttr("0;;")
+
+
+class Background(InlineStyle):
+    BLACK = InlineStyle.addAttr(";;40")
+    RED = InlineStyle.addAttr(";;41")
+    GREEN = InlineStyle.addAttr(";;42")
+    YELLOW = InlineStyle.addAttr(";;43")
+    BLUE = InlineStyle.addAttr(";;44")
+    MAGENTA = InlineStyle.addAttr(";;45")
+    CYAN = InlineStyle.addAttr(";;46")
+    WHITE = InlineStyle.addAttr(";;47")
+    CLEAR = InlineStyle.addAttr(";;0")
+
+    @staticmethod
+    def RGB(r: Union[str, int], g: Union[str, int], b: Union[str, int]) -> InLineStyleAttr:
+        return InLineStyleAttr(f";;48:2:{r}:{g}:{b}")
