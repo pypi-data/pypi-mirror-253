a80deaa25ad1a503a70517af263f0734747a6065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:54 +0000

    Add package skatooniespackage/0.1/skatooniespackage-0.1.tar.gz

---
+++ packages/skatooniespackage/skatooniespackage-0.1.tar.gz/skatooniespackage-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='skatooniespackage',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['skatooniespackage'],
+      zip_safe=False)
