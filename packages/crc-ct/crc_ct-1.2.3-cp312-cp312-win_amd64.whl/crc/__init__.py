1fcedf4e8daa40154639e008308c0e9aafe012d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 15:03:54 +0000

    Add package crc-ct/1.2.3/crc_ct-1.2.3-cp312-cp312-win_amd64.whl

---
+++ packages/crc-ct/crc_ct-1.2.3-cp312-cp312-win_amd64.whl/crc/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 1994 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/license/zlib
+
+"""Public Python API of CRC package."""
+
+from .__about__ import * ; del __about__  # noqa
+
+from ._crc import *  # noqa
