2407043c6e31fc3b132d3431b1937265e2f0e788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:03:54 +0000

    Add package trial-initRelated/0.1.0/trial-initRelated-0.1.0.tar.gz

---
+++ packages/trial-initRelated/trial-initRelated-0.1.0.tar.gz/trial-initRelated-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="trial-initRelated",
+    description='a basic code to host a simple flask app',
+    packages=find_packages(),
+    version='0.1.0'
+)
