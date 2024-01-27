32e2c6d32bc6a6aaa2f056f0e79add3b24d76cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 16:04:38 +0000

    Add package terminalai/0.0.5/terminalai-0.0.5.tar.gz

---
+++ packages/terminalai/terminalai-0.0.5.tar.gz/terminalai-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="terminalai",
+    version="0.0.5",
+    license="GNU General Public License v2.0",
+    author="Talaviya Bhavik",
+    author_email="talaviyabhavik@proton.me",
+    description="AI Terminal assistant",
+    packages=find_packages("terminalai"),
+    package_dir={"": "terminalai"},
+    url="https://github.com/imnotdev25/Term-ai",
+    project_urls={
+        "Bug Report": "https://github.com/imnotdev25/Term-ai/issues/new",
+    },
+    install_requires=[
+        "google-generativeai",
+        "httpx",
+    ],
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["term-ai"],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
