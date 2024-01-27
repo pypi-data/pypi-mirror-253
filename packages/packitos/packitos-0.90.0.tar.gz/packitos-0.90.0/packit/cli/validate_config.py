29896e3d989352f08f0a17fa8d3329004ecaf972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:24 +0000

    Add package packitos/0.90.0/packitos-0.90.0.tar.gz

---
+++ packages/packitos/packitos-0.90.0.tar.gz/packitos-0.90.0/packit/cli/validate_config.py
@@ -0,0 +1,37 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+"""
+Validate PackageConfig
+"""
+
+import logging
+import os
+
+import click
+
+from packit.api import PackitAPI
+from packit.cli.types import LocalProjectParameter
+from packit.cli.utils import cover_packit_exception
+from packit.config import get_context_settings
+from packit.local_project import LocalProject
+
+logger = logging.getLogger(__name__)
+
+
+@click.command("validate-config", context_settings=get_context_settings())
+@click.argument("path_or_url", type=LocalProjectParameter(), default=os.path.curdir)
+@cover_packit_exception
+def validate_config(path_or_url: LocalProject):
+    """
+    Validate PackageConfig.
+
+    \b
+    - checks missing values
+    - checks incorrect types
+
+    PATH_OR_URL argument is a local path or a URL to a git repository with packit configuration file
+    """
+    output = PackitAPI.validate_package_config(path_or_url.working_dir)
+    logger.info(output)
+    # TODO: print more if config.debug
