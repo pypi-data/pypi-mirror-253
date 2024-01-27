fe01fa97a5b151aab0abaefdd66e1b253fdb5dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:47 +0000

    Add package pynnkit/1.0.1/pynnkit-1.0.1.tar.gz

---
+++ packages/pynnkit/pynnkit-1.0.1.tar.gz/pynnkit-1.0.1/setup.py
@@ -0,0 +1,61 @@
+"""
+Setup file for PYPI
+"""
+from setuptools import setup, find_packages
+import os.path
+
+setup(
+    name='pynnkit',
+    version='1.0.1',
+    author='Paul Walcher',
+    author_email='paulwalcher12@gmail.com',
+    description='NeuralNetwork Kit',
+    packages=["NeuralNet", "NeuralNet.Layers", "NeuralNet.MNIST",
+                "NeuralNet.Networks",
+                "NeuralNet.Layers.Activation",
+                "NeuralNet.Layers.Loss",
+                "NeuralNet.Layers.Initializers",
+             ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy>=1.26.2"
+    ]
+)
+
+"""data_files=[
+            
+                            (os.path.join("NeuralNet","MNIST", "archive"), 
+                                        [
+                                        os.path.join("NeuralNet","MNIST", "archive", "t10k-images.idx3-ubyte"),
+                                        os.path.join("NeuralNet","MNIST", "archive", "t10k-labels.idx1-ubyte"),
+                                        os.path.join("NeuralNet","MNIST", "archive", "train-images.idx3-ubyte"),
+                                        os.path.join("NeuralNet","MNIST", "archive", "train-labels.idx1-ubyte"),
+                                        ]
+                            ),
+                            (os.path.join("NeuralNet","MNIST", "archive", "t10k-images-idx3-ubyte"), 
+                                        [
+                                        os.path.join("NeuralNet","MNIST", "archive", "t10k-images-idx3-ubyte", "t10k-images-idx3-ubyte")
+                                        ]
+                            ),
+                            (os.path.join("NeuralNet","MNIST", "archive", "t10k-labels-idx1-ubyte"), 
+                                        [
+                                        os.path.join("NeuralNet","MNIST", "archive", "t10k-labels-idx1-ubyte", "t10k-labels-idx1-ubyte") 
+                                        ]
+                            ),
+                            (os.path.join("NeuralNet","MNIST", "archive", "train-images-idx3-ubyte"), 
+                                        [
+                                            os.path.join("NeuralNet","MNIST", "archive", "train-images-idx3-ubyte", "train-images-idx3-ubyte"),
+                                        ]
+                            ),
+                            (os.path.join("NeuralNet","MNIST", "archive", "train-labels-idx1-ubyte"), 
+                                        [
+                                        os.path.join("NeuralNet","MNIST", "archive", "train-labels-idx1-ubyte", "train-labels-idx1-ubyte")
+                                        ]
+                            ),
+                ],"""
