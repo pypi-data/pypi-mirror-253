2f8bb1071ff24283c44469a5e45d054f7a9b405e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 05:04:55 +0000

    Add package PreProPackage/0.0.2/PreProPackage-0.0.2.tar.gz

---
+++ packages/PreProPackage/PreProPackage-0.0.2.tar.gz/PreProPackage-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup (
+    name='PreProPackage',
+    version='0.0.2',
+    license='MIT',
+    description='code for generate preprocessing in gpr',
+    long_description='code generate preprocessing in gpr data, include data syntetic, data field, data in laboratory',
+    author='Aplatag',
+    packages=find_packages(),
+    install_requires = ['numpy'],
+
+    url='https://github.com/aplatag/testPackagepip'
+)
