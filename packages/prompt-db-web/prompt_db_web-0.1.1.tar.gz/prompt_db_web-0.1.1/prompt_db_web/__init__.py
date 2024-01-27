2a23b9ebc4dc604d5d8632a4f9fac7297d22114e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 05:05:08 +0000

    Add package prompt-db-web/0.1.1/prompt_db_web-0.1.1.tar.gz

---
+++ packages/prompt-db-web/prompt_db_web-0.1.1.tar.gz/prompt_db_web-0.1.1/prompt_db_web/__init__.py
@@ -0,0 +1,3 @@
+from .prompt_db_web import timer
+
+__version__ = "0.1.1"
---
+++ packages/prompt-db-web/prompt_db_web-0.1.1.tar.gz/prompt_db_web-0.1.1/prompt_db_web/prompt_db_web.py
@@ -0,0 +1,20 @@
+# prompt_db_web.py
+
+from functools import wraps
+
+import pendulum
+
+
+def timer(function):
+    """
+    Simple Decorator to measure a function execution time.
+    """
+
+    @wraps(function)
+    def function_wrapper():
+        start = pendulum.now()
+        function()
+        elapsed_time = pendulum.now() - start
+        print(f"Execution Time: {elapsed_time.microseconds} ms.")
+
+    return function_wrapper
