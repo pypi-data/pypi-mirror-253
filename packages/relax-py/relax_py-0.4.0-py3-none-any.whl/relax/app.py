2d4b82df6f1f2e53db010a551a0474d157b4bcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 14:03:50 +0000

    Add package relax-py/0.4.0/relax_py-0.4.0-py3-none-any.whl

---
+++ packages/relax-py/relax_py-0.4.0-py3-none-any.whl/relax/app.py
@@ -0,0 +1,229 @@
+import inspect
+from enum import StrEnum, auto
+from functools import wraps
+from inspect import Parameter, signature
+from typing import (
+    Annotated,
+    Any,
+    Awaitable,
+    Callable,
+    Concatenate,
+    Generic,
+    Literal,
+    Mapping,
+    Protocol,
+    TypedDict,
+    TypeVar,
+    get_args,
+    get_origin,
+)
+from starlette.datastructures import URL
+
+import starlette.requests
+import starlette.types
+from starlette.applications import Starlette
+from starlette.authentication import requires
+import starlette.responses
+from starlette.routing import Route
+from typing_extensions import ParamSpec
+
+import relax.html
+import relax.injection
+
+QueryStr = Annotated[str, "query_param"]
+QueryInt = Annotated[int, "query_param"]
+PathInt = Annotated[int, "path_param"]
+PathStr = Annotated[str, "path_param"]
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+Method = Literal["GET", "POST", "PUT", "DELETE", "PATCH"]
+
+
+class HTMLResponse(starlette.responses.HTMLResponse):
+    def __init__(
+        self,
+        content: relax.html.Element,
+        status_code: int = 200,
+        headers: Mapping[str, str] | None = None,
+    ) -> None:
+        super().__init__(content.render(), status_code, headers)
+
+
+class AuthScope(StrEnum):
+    Authenticated = auto()
+
+
+class Scope(TypedDict):
+    from_htmx: bool
+
+
+class Request(starlette.requests.Request, Generic[T]):
+    user: T
+    scope: Scope
+
+    def __init__(self, base_request: starlette.requests.Request):
+        super().__init__(base_request.scope, base_request._receive, base_request._send)
+
+    def url_of(
+        self,
+        func: Callable[Concatenate["Request", P], Awaitable[Any]],
+        *_: P.args,
+        **kwargs: P.kwargs,
+    ) -> URL:
+        return self.url_for(func.__name__, **kwargs)
+
+    def url_wrapper(
+        self,
+        func: Callable[Concatenate["Request", P], Awaitable[Any]],
+    ) -> Callable[P, URL]:
+        def inner(*_: P.args, **kwargs: P.kwargs) -> URL:
+            return self.url_for(func.__name__, **kwargs)
+
+        return inner
+
+
+class UserType(Protocol):
+    username: str
+    display_name: str
+    is_authenticated: bool
+
+
+def get_annotated(param: Parameter) -> Any:
+    if get_origin(param.annotation) is Annotated:
+        return get_args(param.annotation)
+
+    try:
+        if get_origin(get_args(param.annotation)[0]) is Annotated:
+            return get_args(get_args(param.annotation)[0])
+    except IndexError:
+        return None
+
+    return None
+
+
+class App(Starlette):
+    # TODO: method should be enum maybe?
+    def path_function(
+        self,
+        method: Method,
+        endpoint: str,
+        auth_scopes: list[AuthScope] | None = None,
+    ):
+        if auth_scopes is None:
+            auth_scopes = []
+
+        def decorator(func):
+            @wraps(func)
+            @requires(auth_scopes)
+            async def inner(request: starlette.requests.Request):
+                request = Request(request)
+                params: dict[str, Any] = {}
+                request.scope["from_htmx"] = (
+                    request.headers.get("HX-Request", False) == "true"
+                )
+                for param_name, param in signature(func).parameters.items():
+                    if (args := get_annotated(param)) and args[1] == "query_param":
+                        if (
+                            param_value := request.query_params.get(param_name)
+                        ) is None:
+                            if param.default is inspect._empty:
+                                msg = (
+                                    f"parameter {param_name} from function "
+                                    f"{func.__name__} has no default value "
+                                    "and was not provided in the request"
+                                )
+                                raise TypeError(msg)
+                            params[param_name] = param.default
+                        else:
+                            # TODO: also allow something like
+                            # \ Annotated[Path | None, "query_param"] = Path("/")
+                            params[param_name] = args[0](param_value)
+                    elif (args := get_annotated(param)) and args[1] == "path_param":
+                        if (param_value := request.path_params.get(param_name)) is None:
+                            if param.default is inspect._empty:
+                                msg = (
+                                    f"parameter {param_name} from function "
+                                    f"{func.__name__} has no default value "
+                                    "and was not provided in the request"
+                                )
+                                raise TypeError(msg)
+                            params[param_name] = param.default
+                        else:
+                            params[param_name] = args[0](param_value)
+                return await func(request, **params)
+
+            # TODO: maybe make the name file + fn_name?
+            # TODO: also, error out when finding a duplicate name
+            self.routes.append(
+                Route(endpoint, inner, methods=[method], name=func.__name__),
+            )
+
+            return inner
+
+        return decorator
+
+
+class Router:
+    def __init__(self) -> None:
+        self.routes: list[Route] = []
+
+    def path_function(
+        self,
+        method: Method,
+        endpoint: str,
+        auth_scopes: list[AuthScope] | None = None,
+    ):
+        if auth_scopes is None:
+            auth_scopes = []
+
+        def decorator(func):
+            @wraps(func)
+            @requires(auth_scopes)
+            async def inner(request: starlette.requests.Request):
+                request = Request(request)
+                params: dict[str, Any] = {}
+                request.scope["from_htmx"] = (
+                    request.headers.get("HX-Request", False) == "true"
+                )
+                for param_name, param in signature(func).parameters.items():
+                    if (args := get_annotated(param)) and args[1] == "query_param":
+                        if (
+                            param_value := request.query_params.get(param_name)
+                        ) is None:
+                            if param.default is inspect._empty:
+                                msg = (
+                                    f"parameter {param_name} from function "
+                                    f"{func.__name__} has no default value "
+                                    "and was not provided in the request"
+                                )
+                                raise TypeError(msg)
+                            params[param_name] = param.default
+                        else:
+                            # TODO: also allow something like
+                            # \ Annotated[Path | None, "query_param"] = Path("/")
+                            params[param_name] = args[0](param_value)
+                    elif (args := get_annotated(param)) and args[1] == "path_param":
+                        if (param_value := request.path_params.get(param_name)) is None:
+                            if param.default is inspect._empty:
+                                msg = (
+                                    f"parameter {param_name} from function "
+                                    f"{func.__name__} has no default value "
+                                    "and was not provided in the request"
+                                )
+                                raise TypeError(msg)
+                            params[param_name] = param.default
+                        else:
+                            params[param_name] = args[0](param_value)
+                return await relax.injection.injectable(func)(request, **params)
+
+            # TODO: maybe make the name file + fn_name?
+            # TODO: also, error out when finding a duplicate name
+            self.routes.append(
+                Route(endpoint, inner, methods=[method], name=func.__name__),
+            )
+
+            return inner
+
+        return decorator
