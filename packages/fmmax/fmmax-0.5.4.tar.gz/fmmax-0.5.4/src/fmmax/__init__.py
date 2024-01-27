b710d4ac4e76f2bbd75a106172744d03794a3eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 05:05:20 +0000

    Add package fmmax/0.5.4/fmmax-0.5.4.tar.gz

---
+++ packages/fmmax/fmmax-0.5.4.tar.gz/fmmax-0.5.4/src/fmmax/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+__version__ = "v0.5.4"
+
+from . import (
+    basis,
+    beams,
+    farfield,
+    fft,
+    fields,
+    fmm,
+    fmm_matrices,
+    scattering,
+    sources,
+    utils,
+    vector,
+)
