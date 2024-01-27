38a47ecc788b22a054f85ee5109da2e93f41ed0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:14 +0000

    Add package django-component-kit/0.4.0/django_component_kit-0.4.0-py3-none-any.whl

---
+++ packages/django-component-kit/django_component_kit-0.4.0-py3-none-any.whl/django_component_kit/assets.py
@@ -0,0 +1,33 @@
+"""
+Assets management for Django Component Kit.
+"""
+from typing import Iterable
+from django.templatetags.static import static
+
+
+class AssetRegistry:
+    """Registry for assets"""
+
+    def __init__(self):
+        self.js = set()
+        self.css = set()
+        self._string = ""
+
+    def add_js(self, js_list: Iterable[str] | None):
+        """Add JS files"""
+        js_list = js_list or []
+        self.js.update([f'<script src="{static(js)}" defer></script>' for js in js_list])
+
+    def add_css(self, css_list: Iterable[str] | None):
+        """Add CSS files"""
+        css_list = css_list or []
+        self.css.update([f'<link crossorigin="anonymous" href="{static(css)}" rel="stylesheet">' for css in css_list])
+
+    def as_string(self, refresh: bool = False) -> str:
+        """Return cached assets as string"""
+        if not self._string or refresh:
+            self._string = "\n".join(self.js) + "\n" + "\n".join(self.css)
+        return self._string
+
+
+assets = AssetRegistry()
