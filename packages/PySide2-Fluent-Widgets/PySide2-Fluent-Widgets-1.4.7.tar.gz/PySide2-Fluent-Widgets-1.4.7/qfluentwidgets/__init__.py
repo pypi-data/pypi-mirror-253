407f4ffa0abe3763d883f41dc02542cac3e52560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:05:41 +0000

    Add package PySide2-Fluent-Widgets/1.4.7/PySide2-Fluent-Widgets-1.4.7.tar.gz

---
+++ packages/PySide2-Fluent-Widgets/PySide2-Fluent-Widgets-1.4.7.tar.gz/PySide2-Fluent-Widgets-1.4.7/qfluentwidgets/__init__.py
@@ -0,0 +1,21 @@
+"""
+PySide2-Fluent-Widgets
+======================
+A fluent design widgets library based on PySide2.
+
+Documentation is available in the docstrings and
+online at https://qfluentwidgets.com.
+
+Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/PySide2/examples.
+
+:copyright: (c) 2023 by zhiyiYo.
+:license: GPLv3, see LICENSE for more details.
+"""
+
+__version__ = "1.4.7"
+__author__ = "zhiyiYo"
+
+from .components import *
+from .common import *
+from .window import *
+from ._rc import resource
