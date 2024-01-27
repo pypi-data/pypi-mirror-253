6e9966bb50c5d7edc5f104cbf345bf447c1ab521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 16:04:45 +0000

    Add package randrythm/1.0.0/randrythm-1.0.0.tar.gz

---
+++ packages/randrythm/randrythm-1.0.0.tar.gz/randrythm-1.0.0/README.md
@@ -0,0 +1,3 @@
+# Randrythm
+
+This is module that provides a randomly chosen rhythm game for rhythm game gamers!
---
+++ packages/randrythm/randrythm-1.0.0.tar.gz/randrythm-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", 'r') as f:
+    desc = f.read()
+
+
+setup(
+    name="randrythm",
+    version='1.0.0',
+    long_description=desc,
+    long_description_content_type='text/markdown',
+    author="Torrez",
+)
