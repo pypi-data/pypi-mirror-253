38c67cce9b0f848ceb2ac7b7d4268afd093caa0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:26 +0000

    Add package CustomDiagramIcons/1.0.0.dev11/CustomDiagramIcons-1.0.0.dev11.tar.gz

---
+++ packages/CustomDiagramIcons/CustomDiagramIcons-1.0.0.dev11.tar.gz/CustomDiagramIcons-1.0.0.dev11/CustomDiagramIcons/generic.py
@@ -0,0 +1,16 @@
+from .common import CustomNode
+
+class softwareEngineer(CustomNode):
+    _icon = "software_engineer_icon.png"
+
+class JIRA(CustomNode):
+    _icon = "jira_icon.png"
+
+class DealerTrackCanada(CustomNode):
+    _icon = "dealertrack_canada.png"
+
+class GitHub(CustomNode):
+    _icon = "github_icon.png"
+
+class ReportPortal(CustomNode):
+    _icon = "report_portal_icon.png"
