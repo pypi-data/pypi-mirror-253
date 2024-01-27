d4cf7781826058e86d7d9c5325aa470be2e66abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:44 +0000

    Add package python-statistics/0.0.4/python_statistics-0.0.4.tar.gz

---
+++ packages/python-statistics/python_statistics-0.0.4.tar.gz/python_statistics-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_statistics",
+    version="0.0.4",
+    author="Meteey (Mustafa Konur)",
+    author_email="<konurmustafa17@gmail.com>",
+    description="Module name confusion fixed",
+    packages=find_packages(),
+    keywords=["statistics", "z-score", "normalization"],
+    
+)
