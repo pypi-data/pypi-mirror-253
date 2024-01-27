d08ac38c5a6caa2293c22f6c9697e697d51137c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 14:04:04 +0000

    Add package megamock/0.1.0b10/megamock-0.1.0b10-py3-none-any.whl

---
+++ packages/megamock/megamock-0.1.0b10-py3-none-any.whl/megamock/type_util.py
@@ -0,0 +1,21 @@
+from typing import TypeVar, Union
+from unittest.mock import _Call  # noqa
+
+
+class MISSING_TYPE:
+    """
+    Class to indicate a missing value
+    """
+
+
+_T = TypeVar("_T")
+
+
+Opt = Union[_T, MISSING_TYPE]
+
+MISSING = MISSING_TYPE()
+
+Call = _Call
+call = Call(from_kall=False)
+
+__all__ = ["MISSING", "call"]
