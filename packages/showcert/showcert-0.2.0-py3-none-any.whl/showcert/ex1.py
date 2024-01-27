fbafb8e70da3184992b265f023e50c4a40d7f57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:37 +0000

    Add package showcert/0.2.0/showcert-0.2.0-py3-none-any.whl

---
+++ packages/showcert/showcert-0.2.0-py3-none-any.whl/showcert/ex1.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+# https://stackoverflow.com/questions/56285000/python-cryptography-create-a-certificate-signed-by-an-existing-ca-and-export
+
+import datetime
+
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+
+root_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Texas"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"Austin"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"My CA ex1"),
+])
+root_cert = x509.CertificateBuilder().subject_name(
+    subject
+).issuer_name(
+    issuer
+).public_key(
+    root_key.public_key()
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.datetime.utcnow()
+).not_valid_after(
+    datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+).add_extension(
+     x509.BasicConstraints(ca=True, path_length=None), True
+).sign(root_key, hashes.SHA256(), default_backend())
+
+
+with open("/tmp/cacert.pem", "wb") as fh:
+    fh.write(root_cert.public_bytes(encoding=serialization.Encoding.PEM))
+
+with open("/tmp/cakey.pem", "wb") as fh:
+    fh.write(root_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    ))
+
+
+# Now we want to generate a cert from that root
+cert_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+new_subject = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Texas"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"Austin"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"New Org Name!"),
+])
+cert = x509.CertificateBuilder().subject_name(
+    new_subject
+).issuer_name(
+    root_cert.issuer
+).public_key(
+    cert_key.public_key()
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.datetime.utcnow()
+).not_valid_after(
+datetime.datetime.utcnow() + datetime.timedelta(days=30)
+).add_extension(
+    x509.SubjectAlternativeName([x509.DNSName(u"somedomain.com")]),
+    critical=False,
+).sign(root_key, hashes.SHA256(), default_backend())
+
+
+with open("/tmp/xcert.pem", "wb") as fh:
+    fh.write(cert.public_bytes(encoding=serialization.Encoding.PEM))
+
+with open("/tmp/xkey.pem", "wb") as fh:
+    fh.write(cert_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    ))
