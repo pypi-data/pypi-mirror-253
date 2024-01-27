8b782cac443319c03b027d6d3d82f451655707b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 16:04:26 +0000

    Add package lazy-serve/0.2.post2/lazy_serve-0.2.post2.tar.gz

---
+++ packages/lazy-serve/lazy_serve-0.2.post2.tar.gz/lazy_serve-0.2.post2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lazy_serve",
+    version="0.2post2",
+    description="A simple package for effortlessly starting HTTP servers.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/lazy_serve",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
