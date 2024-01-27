1153f71fb101f7bb240aae660a73ad85c90ec7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:03:46 +0000

    Add package trapi-predict-kit/0.2.3/trapi_predict_kit-0.2.3.tar.gz

---
+++ packages/trapi-predict-kit/trapi_predict_kit-0.2.3.tar.gz/trapi_predict_kit-0.2.3/src/trapi_predict_kit/config.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    BIOLINK_VERSION: str = "3.1.0"
+    TRAPI_VERSION: str = "1.4.0"
+
+    TIMEOUT: int = 30
+    LOG_LEVEL: str = "ERROR"
+
+    VIRTUAL_HOST: Optional[str] = None
+
+
+settings = Settings()
