453c98c068e5378e8420db5bd64ff971da8a4ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 16:04:17 +0000

    Add package lazy-serve/0.2.post5/lazy_serve-0.2.post5.tar.gz

---
+++ packages/lazy-serve/lazy_serve-0.2.post5.tar.gz/lazy_serve-0.2.post5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lazy_serve",
+    version="0.2post5",
+    description="A simple package for effortlessly starting multiple threaded HTTP servers.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Michael Pilosov",
+    author_email="mm@clfx.cc",
+    url="https://git.mlden.com/mm/lazy_serve.git",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
