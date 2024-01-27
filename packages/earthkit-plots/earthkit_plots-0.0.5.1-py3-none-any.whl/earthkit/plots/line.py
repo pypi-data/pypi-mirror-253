a0faeedf2694fc80734a46c44bf7bd8727d5af4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 03:05:01 +0000

    Add package earthkit-plots/0.0.5.1/earthkit_plots-0.0.5.1-py3-none-any.whl

---
+++ packages/earthkit-plots/earthkit_plots-0.0.5.1-py3-none-any.whl/earthkit/plots/line.py
@@ -0,0 +1,11 @@
+import plotly.graph_objects as go
+
+from earthkit.plots import inputs
+from earthkit.plots.schemas import schema
+
+
+@schema.line.apply()
+@inputs.sanitise()
+def line(*args, **kwargs):
+    trace = go.Scatter(*args, **kwargs)
+    return trace
