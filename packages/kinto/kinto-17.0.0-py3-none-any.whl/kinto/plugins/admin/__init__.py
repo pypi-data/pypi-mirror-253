276d530bba281f5bdc979b49dc1eac86cff04d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:30 +0000

    Add package kinto/17.0.0/kinto-17.0.0-py3-none-any.whl

---
+++ packages/kinto/kinto-17.0.0-py3-none-any.whl/kinto/plugins/admin/__init__.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from pyramid.httpexceptions import HTTPTemporaryRedirect
+from pyramid.static import static_view
+
+from .views import admin_home_view
+
+
+VERSION_FILE_PATH = Path(__file__).parent / "VERSION"
+
+
+def includeme(config):
+    admin_version = VERSION_FILE_PATH.read_text().strip()
+
+    # Expose capability.
+    config.add_api_capability(
+        "admin",
+        version=admin_version,
+        description="Serves the admin console.",
+        url="https://github.com/Kinto/kinto-admin/",
+    )
+
+    config.add_route("admin_home", "/admin/")
+    config.add_view(admin_home_view, route_name="admin_home")
+
+    admin_assets_path = (
+        config.registry.settings["admin_assets_path"] or "kinto.plugins.admin:build"
+    )
+    build_dir = static_view(admin_assets_path, use_subpath=True)
+    config.add_route("catchall_static", "/admin/*subpath")
+    config.add_view(build_dir, route_name="catchall_static")
+
+    # Setup redirect without trailing slash.
+    def admin_redirect_view(request):
+        raise HTTPTemporaryRedirect(request.path + "/")
+
+    config.add_route("admin_redirect", "/admin")
+    config.add_view(admin_redirect_view, route_name="admin_redirect")
