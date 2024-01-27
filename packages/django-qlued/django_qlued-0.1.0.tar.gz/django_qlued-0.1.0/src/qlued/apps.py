aca40d5903494dfba9e84d41cb84c531cb51be38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 06:04:33 +0000

    Add package django-qlued/0.1.0/django_qlued-0.1.0.tar.gz

---
+++ packages/django-qlued/django_qlued-0.1.0.tar.gz/django_qlued-0.1.0/src/qlued/apps.py
@@ -0,0 +1,14 @@
+"""
+Module that configures the app.
+"""
+from django.apps import AppConfig
+
+
+class BackendsConfig(AppConfig):
+    """
+    Class that defines some basic configuration settings.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "qlued"
+    label = "qlued"
