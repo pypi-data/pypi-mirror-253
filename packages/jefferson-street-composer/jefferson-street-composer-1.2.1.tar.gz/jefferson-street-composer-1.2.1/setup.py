4fb045eb4e5fc9b94286b2966087ffbc31430aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 16:04:27 +0000

    Add package jefferson-street-composer/1.2.1/jefferson-street-composer-1.2.1.tar.gz

---
+++ packages/jefferson-street-composer/jefferson-street-composer-1.2.1.tar.gz/jefferson-street-composer-1.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jefferson_street_composer',
+ 'jefferson_street_composer.services',
+ 'jefferson_street_composer.services.specifications']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['apache-airflow-providers-cncf-kubernetes==5.0.0',
+ 'apache-airflow==2.7.1',
+ 'google-cloud-orchestration-airflow==1.4.1']
+
+setup_kwargs = {
+    'name': 'jefferson-street-composer',
+    'version': '1.2.1',
+    'description': 'Library for Jefferson Street Technologies Composer Configuration',
+    'long_description': '',
+    'author': 'Nikolai McFall',
+    'author_email': 'nikolai@jeffersonst.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.9',
+}
+
+
+setup(**setup_kwargs)
