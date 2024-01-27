c1541f7585aa7e1ac04af0f2415ad5ba5680e563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:26 +0000

    Add package STEM-Vibrations/0.1.5/STEM_Vibrations-0.1.5-py3-none-any.whl

---
+++ packages/STEM-Vibrations/STEM_Vibrations-0.1.5-py3-none-any.whl/stem/__init__.py
@@ -0,0 +1,41 @@
+import subprocess
+import pkg_resources
+import subprocess
+from .__version__ import __version__
+
+
+def install_external_dependency():
+    """
+    Install external dependencies for STEM
+    """
+
+    # check if gmsh utils is installed and it is version 1.0
+    if not check_package_version('gmsh_utils', '1.0'):
+        subprocess.check_call(['pip', 'install', "git+https://github.com/StemVibrations/gmsh_utils@v1.0"])
+    # check if RandFields is installed and it is version 1.0
+    if not check_package_version('random_fields', '1.0'):
+        subprocess.check_call(['pip', 'install', "git+https://github.com/StemVibrations/RandomFields@v1.0"])
+
+
+def check_package_version(package_name: str, target_version: str):
+    """
+    Check if a package is installed and if the installed version matches the target version
+
+    Args:
+        package_name (str): Name of the package to check
+        target_version (str): Version of the package to check
+    """
+    try:
+        # Use `require` to check if the package is installed and get its version
+        package_version = pkg_resources.require(package_name)[0].version
+
+        # Compare the installed version with the target version
+        if package_version == target_version:
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
+
+install_external_dependency()
