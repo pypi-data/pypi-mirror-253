841d6035d0ac7a7de93f821f2b0873d7653ee77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:18 +0000

    Add package uda-tolu-distributions/0.1/uda_tolu_distributions-0.1.tar.gz

---
+++ packages/uda-tolu-distributions/uda_tolu_distributions-0.1.tar.gz/uda_tolu_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='uda_tolu_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['uda_tolu_distributions'],
+      zip_safe=False)
