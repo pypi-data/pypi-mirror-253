3a2ad4d1d2b54a8850fe35fca8b39f57b6491fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-27 04:04:06 +0000

    Add package azcam/24.0/azcam-24.0-py2.py3-none-any.whl

---
+++ packages/azcam/azcam-24.0-py2.py3-none-any.whl/azcam/__init__.py
@@ -0,0 +1,34 @@
+"""
+AzCam is a software framework for the acquisition and analysis of image data
+from scientific imaging systems as well as the control of instrumentation.
+It is intended to be customized for specific hardware, observational,
+and data reduction requirements.
+"""
+
+from importlib import metadata
+
+__version__ = metadata.version(__package__)
+__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
+
+import typing
+
+# import here so future importing is not required
+from azcam import fits
+from azcam import utils
+from azcam.exceptions import AzcamError, AzcamWarning
+from azcam.logger import Logger
+from azcam.database import AzcamDatabase
+
+# logger object
+logger: Logger = Logger()
+
+# initially azcam.log() is print(), will usually be overwritten
+log: typing.Callable = print
+
+# database placeholder
+db: AzcamDatabase = AzcamDatabase()
+
+# clean namespace
+del metadata
+del typing
+# del Database
