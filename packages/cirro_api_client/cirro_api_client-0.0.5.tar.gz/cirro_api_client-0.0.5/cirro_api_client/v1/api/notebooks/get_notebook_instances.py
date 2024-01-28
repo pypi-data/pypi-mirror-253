ea4a57fb10d073aab17e04ea59eddd689974b344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:25 +0000

    Add package cirro_api_client/0.0.5/cirro_api_client-0.0.5.tar.gz

---
+++ packages/cirro_api_client/cirro_api_client-0.0.5.tar.gz/cirro_api_client-0.0.5/cirro_api_client/v1/api/notebooks/get_notebook_instances.py
@@ -0,0 +1,163 @@
+from http import HTTPStatus
+from typing import Any, Dict, List, Optional, Union
+
+import httpx
+
+from ... import errors
+from ...client import AuthenticatedClient, Client
+from ...models.notebook_instance import NotebookInstance
+from ...types import Response
+
+
+def _get_kwargs(
+    project_id: str,
+) -> Dict[str, Any]:
+    _kwargs: Dict[str, Any] = {
+        "method": "get",
+        "url": f"/projects/{project_id}/notebook-instances",
+    }
+
+    return _kwargs
+
+
+def _parse_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Optional[List["NotebookInstance"]]:
+    if response.status_code == HTTPStatus.OK:
+        response_200 = []
+        _response_200 = response.json()
+        for response_200_item_data in _response_200:
+            response_200_item = NotebookInstance.from_dict(response_200_item_data)
+
+            response_200.append(response_200_item)
+
+        return response_200
+    if client.raise_on_unexpected_status:
+        raise errors.UnexpectedStatus(response.status_code, response.content)
+    else:
+        return None
+
+
+def _build_response(
+    *, client: Union[AuthenticatedClient, Client], response: httpx.Response
+) -> Response[List["NotebookInstance"]]:
+    return Response(
+        status_code=HTTPStatus(response.status_code),
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(client=client, response=response),
+    )
+
+
+def sync_detailed(
+    project_id: str,
+    *,
+    client: Client,
+) -> Response[List["NotebookInstance"]]:
+    """Get notebook instances
+
+     Retrieves a list of notebook instances that the user has access to
+
+    Args:
+        project_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[List['NotebookInstance']]
+    """
+
+    kwargs = _get_kwargs(
+        project_id=project_id,
+    )
+
+    response = client.get_httpx_client().request(
+        **kwargs,
+    )
+
+    return _build_response(client=client, response=response)
+
+
+def sync(
+    project_id: str,
+    *,
+    client: Client,
+) -> Optional[List["NotebookInstance"]]:
+    """Get notebook instances
+
+     Retrieves a list of notebook instances that the user has access to
+
+    Args:
+        project_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        List['NotebookInstance']
+    """
+
+    return sync_detailed(
+        project_id=project_id,
+        client=client,
+    ).parsed
+
+
+async def asyncio_detailed(
+    project_id: str,
+    *,
+    client: Client,
+) -> Response[List["NotebookInstance"]]:
+    """Get notebook instances
+
+     Retrieves a list of notebook instances that the user has access to
+
+    Args:
+        project_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        Response[List['NotebookInstance']]
+    """
+
+    kwargs = _get_kwargs(
+        project_id=project_id,
+    )
+
+    response = await client.get_async_httpx_client().request(**kwargs)
+
+    return _build_response(client=client, response=response)
+
+
+async def asyncio(
+    project_id: str,
+    *,
+    client: Client,
+) -> Optional[List["NotebookInstance"]]:
+    """Get notebook instances
+
+     Retrieves a list of notebook instances that the user has access to
+
+    Args:
+        project_id (str):
+
+    Raises:
+        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+        httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+    Returns:
+        List['NotebookInstance']
+    """
+
+    return (
+        await asyncio_detailed(
+            project_id=project_id,
+            client=client,
+        )
+    ).parsed
