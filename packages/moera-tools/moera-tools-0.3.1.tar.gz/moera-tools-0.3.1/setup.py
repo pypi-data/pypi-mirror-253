1324a3b21e95885f3c0941af6c70c7c2d9cf23fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:27 +0000

    Add package moera-tools/0.3.1/moera-tools-0.3.1.tar.gz

---
+++ packages/moera-tools/moera-tools-0.3.1.tar.gz/moera-tools-0.3.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='moera-tools',
+    version='0.3.1',
+    install_requires=[
+        'moeralib~=0.14.5',
+        'python-dateutil',
+        'PyYAML',
+        'first',
+    ],
+)
