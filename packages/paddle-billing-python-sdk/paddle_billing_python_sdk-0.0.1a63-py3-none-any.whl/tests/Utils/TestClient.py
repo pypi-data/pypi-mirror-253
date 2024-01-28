c39b0b6292f9aa1f9fb4906470afbc74f2a09dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 15:04:19 +0000

    Add package paddle-billing-python-sdk/0.0.1a63/paddle_billing_python_sdk-0.0.1a63-py3-none-any.whl

---
+++ packages/paddle-billing-python-sdk/paddle_billing_python_sdk-0.0.1a63-py3-none-any.whl/tests/Utils/TestClient.py
@@ -0,0 +1,46 @@
+from os            import environ
+from pytest        import fixture
+from requests_mock import Mocker
+
+from paddle_billing_python_sdk.Client      import Client
+from paddle_billing_python_sdk.Environment import Environment
+from paddle_billing_python_sdk.Options     import Options
+
+
+class TestClient:
+    def __init__(
+        self,
+        client:         Client | None = None,
+        api_secret_key: str    | None = None,
+        base_url:       str           = Environment.SANDBOX.base_url,
+     ):
+        self._client   = client or self.create_client(api_secret_key)
+        self._base_url = base_url
+
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def base_url(self):
+        return self._base_url
+
+
+    @staticmethod
+    def create_client(api_secret_key: str | None = None):
+        return Client(
+            api_key = environ.get('PADDLE_API_SECRET_KEY') if api_secret_key is None else api_secret_key,
+            options = Options(Environment.SANDBOX),
+        )
+
+
+@fixture(autouse=True)
+def test_client():
+    return TestClient()
+
+
+@fixture
+def mock_requests():
+    with Mocker() as m:
+        yield m
