f67e56404723afb57d4256d48590ba8247a03d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:26 +0000

    Add package tspart/0.6.14/tspart-0.6.14.tar.gz

---
+++ packages/tspart/tspart-0.6.14.tar.gz/tspart-0.6.14/src/tspart/__init__.py
@@ -0,0 +1,20 @@
+"""TSP Art Module for Python"""
+
+__version__ = "0.6.14"
+
+
+import tspart.voronoi
+import tspart.tsp
+import tspart.neos
+import tspart.studio
+
+from tspart._draw import draw_route, draw_cmyk_routes, draw_rgb_routes
+from tspart._image import split_rgb, split_cmyk, rgb_to_grayscale, image_to_base64, base64_to_image
+from tspart._helpers import (
+    image_to_array, array_to_image, map_points_to_tour, map_points_to_tour_multi, image_array_size, factors_from_image,
+    factors_from_image_multi, filter_white_points, filter_white_points_multi
+)
+from tspart._files import (
+    save_array_as_image, load_image_as_array, make_tsplib, decode_tsplib, save_tsplib, load_tsplib, save_cyc_tour,
+    load_cyc_tour
+)
