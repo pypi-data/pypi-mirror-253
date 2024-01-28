112bcd6e731ccb57c91c77711f8c28f0f6f02e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:12 +0000

    Add package pfc-geometry/0.2.2/pfc_geometry-0.2.2.tar.gz

---
+++ packages/pfc-geometry/pfc_geometry-0.2.2.tar.gz/pfc_geometry-0.2.2/geometry/time.py
@@ -0,0 +1,36 @@
+from geometry import Base
+from numbers import Number
+from typing import Self
+import numpy as np
+from time import time
+
+
+class Time(Base):
+    cols=["t", "dt"]
+    
+    @staticmethod
+    def from_t(t: np.ndarray) -> Self:
+        if isinstance(t, Number):
+            return Time(t, 1/30)
+        else:
+            dt = np.array([1/30]) if len(t) == 1 else np.gradient(t)
+            return Time(t, dt)
+
+    def scale(self, duration) -> Self:
+        old_duration = self.t[-1] - self.t[0]
+        sfac = duration / old_duration
+        return Time(
+            self.t[0] + (self.t - self.t[0]) * sfac,
+            self.dt * sfac
+        )
+
+    def reset_zero(self):
+        return Time(self.t - self.t[0], self.dt)
+
+    @staticmethod
+    def now():
+        return Time.from_t(time())
+
+    def extend(self):
+        return Time.concatenate([self, Time(self.t[-1] + self.dt[-1], self.dt[-1])])
+
