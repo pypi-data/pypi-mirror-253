55e3ed4197351b5eb79f6827ceff82b945aac11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:05 +0000

    Add package com-ana/1.1.3/com_ana-1.1.3.tar.gz

---
+++ packages/com-ana/com_ana-1.1.3.tar.gz/com_ana-1.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='com_ana',
+version='1.1.3',
+author='Guangpeng Xue',
+author_email='athonyxue@gmail.com',
+description='md_protein_process is a Python package for protein process',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
