4156f9cde6d81b823f0540a091aa53fc70d7938c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:03 +0000

    Add package ionic-langchain/0.2.3/ionic_langchain-0.2.3-py3-none-any.whl

---
+++ packages/ionic-langchain/ionic_langchain-0.2.3-py3-none-any.whl/ionic_langchain/prompt.py
@@ -0,0 +1,13 @@
+TOOL_PROMPT = str("""
+Ionic is an e-commerce shopping tool. Assistant uses the Ionic Commerce Shopping Tool to find, discover, and compare products from thousands of online retailers. Assistant should use the tool when the user is looking for a product recommendation or trying to find a specific product. 
+
+The user may specify the number of results, minimum price, and maximum price for which they want to see results.
+Ionic Tool input is a comma-separated string of values:
+  - query string (required, must not include commas)
+  - number of results (default to 4, no more than 10)
+  - minimum price in cents ($5 becomes 500)
+  - maximum price in cents
+For example, if looking for coffee beans between 5 and 10 dollars, the tool input would be `coffee beans, 5, 500, 1000`.
+
+Present product results as a list, always including the name of the product with the provided link to purchase. Other provided information can be included as relevant to the request, including price, merchant name, etc.
+""")
