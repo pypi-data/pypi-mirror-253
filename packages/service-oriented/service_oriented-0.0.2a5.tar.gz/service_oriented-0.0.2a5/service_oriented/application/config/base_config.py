f0b9f96f490a79bcd1d5e2002155040aeac5e518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:02 +0000

    Add package service-oriented/0.0.2a5/service_oriented-0.0.2a5.tar.gz

---
+++ packages/service-oriented/service_oriented-0.0.2a5.tar.gz/service_oriented-0.0.2a5/service_oriented/application/config/base_config.py
@@ -0,0 +1,50 @@
+from typing import Any, Optional, Tuple, Type
+
+from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
+
+from service_oriented.application.config.yaml_config_settings_source import (
+    YamlConfigSettingsSource,
+)
+from service_oriented.deployment_environment import DeploymentEnvironment
+
+
+class BaseConfig(BaseSettings):
+    deployment_environment: DeploymentEnvironment
+    entry_point: str
+    yaml_config_path: Optional[str] = None
+
+    def __init__(self, *args: Any, **kwargs: Any):
+        if not self.model_config.get("env_nested_delimiter"):
+            raise RuntimeError("env_nested_delimiter model config is required")
+
+        if not self.model_config.get("env_prefix"):
+            raise RuntimeError("env_prefix model config is required")
+
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        yaml_config_settings = YamlConfigSettingsSource.from_sources(
+            settings_cls=settings_cls,
+            sources=[
+                init_settings,
+                env_settings,
+                file_secret_settings,
+                dotenv_settings,
+            ],
+        )
+
+        return (
+            init_settings,
+            env_settings,
+            file_secret_settings,
+            yaml_config_settings,
+            dotenv_settings,
+        )
