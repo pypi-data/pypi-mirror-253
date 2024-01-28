53903fb88604f55bd46507c166e56eec84b9679d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:36 +0000

    Add package weak-annotators/0.0.1/weak_annotators-0.0.1-py3-none-any.whl

---
+++ packages/weak-annotators/weak_annotators-0.0.1-py3-none-any.whl/weak_annotators/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from weak_annotators.flair_extractor import FlairExtractor
+from weak_annotators.medalpaca_extractor import MedAlpacaExtractor
+from weak_annotators.universal_ner_extractor import UniversalNerExtractor
+from weak_annotators.version import __version__
