5d307098fdfbb5b86e172bd0972a7d718d1af3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 15:04:03 +0000

    Add package bosch-thermostat-client/0.24.6/bosch_thermostat_client-0.24.6-py3-none-any.whl

---
+++ packages/bosch-thermostat-client/bosch_thermostat_client-0.24.6-py3-none-any.whl/bosch_thermostat_client/connectors/easycontrol.py
@@ -0,0 +1,62 @@
+"""XMPP Connector to talk to bosch."""
+
+import aioxmpp
+from bosch_thermostat_client.const import (
+    PUT,
+    GET,
+    USER_AGENT,
+    CONTENT_TYPE,
+    APP_JSON,
+    ACCESS_KEY,
+)
+from bosch_thermostat_client.const.easycontrol import EASYCONTROL
+from .xmpp import XMPPBaseConnector
+
+USERAGENT = "rrc2"
+
+
+class EasycontrolConnector(XMPPBaseConnector):
+    xmpp_host = "xmpp.rrcng.ticx.boschtt.net"
+    _accesskey_prefix = "C42i9NNp_"
+    _rrc_contact_prefix = "rrc2contact_"
+    _rrc_gateway_prefix = "rrc2gateway_"
+    no_verify = True
+    device_type = EASYCONTROL
+
+    def __init__(self, host, encryption, **kwargs):
+        self._seqno = 0
+        super().__init__(
+            host=host,
+            encryption=encryption,
+            access_key=kwargs.get(ACCESS_KEY),
+        )
+
+    def _build_message(self, method, path, data=None):
+        msg = aioxmpp.stanza.Message(
+            to=self._to,
+            type_=aioxmpp.MessageType.CHAT,
+        )
+        if method == GET:
+            body = "\n".join(
+                [
+                    f"GET {path} HTTP/1.1",
+                    f"{USER_AGENT}: {USERAGENT}",
+                    f"Seq-No: {self._seqno}",
+                    "\n",
+                ]
+            )
+        elif method == PUT and data:
+            body = "\r".join(
+                [
+                    f"PUT {path} HTTP/1.1",
+                    f"{USER_AGENT}: {USERAGENT}",
+                    f"{CONTENT_TYPE}: {APP_JSON}",
+                    f"Content-Length: {len(data)}",
+                    "",
+                    data.decode("utf-8"),
+                    "\r",
+                ]
+            )
+        msg.body[None] = body
+        self._seqno += 1
+        return msg
