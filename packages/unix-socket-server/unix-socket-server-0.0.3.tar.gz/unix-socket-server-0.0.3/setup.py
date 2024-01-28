5a11c8e08b247abe9993a98bb0a4d051052f3d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:04:38 +0000

    Add package unix-socket-server/0.0.3/unix-socket-server-0.0.3.tar.gz

---
+++ packages/unix-socket-server/unix-socket-server-0.0.3.tar.gz/unix-socket-server-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="unix-socket-server",
+    version="0.0.3",
+    author="Erfan Shekari",
+    author_email="erfan.dp.co@gmail.com",
+    description="A single-threaded server for serving in-memory files via Unix sockets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/erfanshekari/unix-socket-server",
+    project_urls={
+        "Bug Tracker": "https://github.com/erfanshekari/unix-socket-server",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+    packages=find_packages(
+        where='.',
+        include=['unix_socket_server*'],
+    ),
+    include_package_data=True,
+    install_requires=[],
+)
