f43e1b85539f2eaf81dc76ed68708cd26610c418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:03:58 +0000

    Add package hqdata-api-wrapper/0.0.1/hqdata_api_wrapper-0.0.1.tar.gz

---
+++ packages/hqdata-api-wrapper/hqdata_api_wrapper-0.0.1.tar.gz/hqdata_api_wrapper-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='hqdata_api_wrapper',
+    version='0.0.1',
+    packages=['HQDataAPIWrapper'],
+    install_requires=[
+        'requests'
+    ],
+)
