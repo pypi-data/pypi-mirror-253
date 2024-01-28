b11ec5d75fb5baa246cdaab604e6bf41c07d6d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:11 +0000

    Add package memmpy/0.1.8/memmpy-0.1.8-py3-none-any.whl

---
+++ packages/memmpy/memmpy-0.1.8-py3-none-any.whl/memmpy/__init__.py
@@ -0,0 +1,31 @@
+from ._labels import hash_64bit, safe_load
+from ._loader import (
+    Batched,
+    Dict,
+    Indexed,
+    Shuffled,
+    Sliced,
+    split,
+    unwrap,
+)
+from ._subset import compute_cut_batched
+from ._vector import WriteVector, WriteVectorDict, read_vector, read_vectors
+
+__all__ = [
+    "hash_64bit",
+    "safe_load",
+    "Batched",
+    "Shuffled",
+    "Dict",
+    "Indexed",
+    "Sliced",
+    "split",
+    "unwrap",
+    "compute_cut_batched",
+    "WriteVector",
+    "WriteVectorDict",
+    "read_vector",
+    "read_vectors",
+]
+
+__version__ = "0.1.8"
