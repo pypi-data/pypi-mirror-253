42c6941962a360e1f0bcf8413e5de2666ccd0235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:08 +0000

    Add package scratchapi-python/1.0.1/scratchapi-python-1.0.1.tar.gz

---
+++ packages/scratchapi-python/scratchapi-python-1.0.1.tar.gz/scratchapi-python-1.0.1/README.md
@@ -0,0 +1,64 @@
+# scratchapi-python
+
+[![PyPI - Version](https://img.shields.io/pypi/v/scratchapi-python)](https://pypi.org/project/scratchapi-python/)
+[![GitHub Release](https://img.shields.io/github/v/release/Artcas2/scratchapi-python)](https://github.com/Artcas2/scratchapi-python/releases/latest)
+[![GitHub License](https://img.shields.io/github/license/Artcas2/scratchapi-python)](https://github.com/Artcas2/scratchapi-python/blob/main/LICENSE)
+
+A scratch API wrapper for Python, created by [Artcas2](https://scratch.mit.edu/users/Artcas2/).
+
+## Installation
+
+To install the library, run the following command:
+```console
+pip install -U scratchapi-python
+```
+
+## Example
+
+```python
+import scratchapi
+
+session = scratchapi.login("username", "password")
+
+user = session.get_current_user()
+print("Follower count:", user.get_follower_count())
+print("Following count:", user.get_following_count())
+print("Project cout:", user.get_project_count())
+
+followers = user.get_followers(limit=1)
+print("\nLast follower:")
+for follower in followers:
+    print(follower)
+
+project = scratchapi.get_project("731849091")
+print(f"\nProject stats: {project.loves} loves and {project.favorites} favs.")
+```
+
+## Usage
+
+#### Logging in with username and password
+
+```python
+import scratchapi
+
+session = scratchapi.login("username", "password")
+```
+
+#### Logging in with a session id
+
+```python
+import scratchapi
+
+session = scratchapi.Session("username", "session_id")
+```
+
+#### Session attributes
+
+```
+session.username
+session.session_id
+session.email
+session.xtoken
+session.new_scratcher
+session.banned
+```
---
+++ packages/scratchapi-python/scratchapi-python-1.0.1.tar.gz/scratchapi-python-1.0.1/src/scratchapi/user.py
@@ -0,0 +1,120 @@
+import requests
+
+from . import exceptions
+
+
+class User:
+    def __init__(self, username, session=None):
+        self.username = username
+        self.session = session
+        self.id = None
+        self.country = None
+        self.join_date = None
+        self.about_me = None
+        self.wiwo = None
+        if self.session:
+            self._headers = self.session.headers
+            self._cookies = self.session.cookies
+        else:
+            self._headers = {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/75.0.3770.142 Safari/537.36",
+                "x-csrftoken": "a",
+                "x-requested-with": "XMLHttpRequest",
+                "referer": "https://scratch.mit.edu"
+            }
+            self._cookies = {}
+
+    def __str__(self):
+        return str(self.username)
+
+    def update(self):
+        response = requests.get(f"https://api.scratch.mit.edu/users/{self.username}").json()
+        try:
+            self.id = response["id"]
+        except KeyError:
+            raise exceptions.UserNotFound()
+        self.country = response["profile"]["country"]
+        self.join_date = response["history"]["joined"]
+        self.about_me = response["profile"]["bio"]
+        self.wiwo = response["profile"]["status"]
+
+    def get_follower_count(self):
+        count = requests.get(
+            f"https://scratch.mit.edu/users/{self.username}/followers/",
+            headers={
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/75.0.3770.142 Safari/537.36",
+                "x-csrftoken": "a",
+                "x-requested-with": "XMLHttpRequest",
+                "referer": "https://scratch.mit.edu",
+                "Cookie": "scratchcsrftoken=a;scratchlanguage=en;"
+            }
+        ).text
+        count = count.split("Followers (")[1]
+        count = count.split(")")[0]
+        return int(count)
+
+    def get_following_count(self):
+        count = requests.get(
+            f"https://scratch.mit.edu/users/{self.username}/following/",
+            headers={
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/75.0.3770.142 Safari/537.36",
+                "x-csrftoken": "a",
+                "x-requested-with": "XMLHttpRequest",
+                "referer": "https://scratch.mit.edu",
+                "Cookie": "scratchcsrftoken=a;scratchlanguage=en;"
+            }
+        ).text
+        count = count.split("Following (")[1]
+        count = count.split(")")[0]
+        return int(count)
+
+    def get_followers(self, limit=40, offset=0):
+        if limit > 40:
+            limit = 40
+        response = requests.get(
+            f"https://api.scratch.mit.edu/users/{self.username}/followers?limit={limit}&offset={offset}").json()
+        followers = []
+        for user in response:
+            try:
+                followers.append(User(username=user["username"]))
+            except Exception:
+                raise exceptions.ParseError("Failed to parse", user)
+        return followers
+
+    def get_following(self, limit=40, offset=0):
+        if limit > 40:
+            limit = 40
+        response = requests.get(
+            f"https://api.scratch.mit.edu/users/{self.username}/following?limit={limit}&offset={offset}").json()
+        following = []
+        for user in response:
+            try:
+                following.append(User(username=user["username"]))
+            except Exception:
+                raise exceptions.ParseError("Failed to parse", user)
+        return following
+
+    def get_project_count(self):
+        text = requests.get(
+            f"https://scratch.mit.edu/users/{self.username}/projects/",
+            headers={
+                "x-csrftoken": "a",
+                "x-requested-with": "XMLHttpRequest",
+                "Cookie": "scratchcsrftoken=a;scratchlanguage=en;",
+                "referer": "https://scratch.mit.edu",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                              "Chrome/75.0.3770.142 Safari/537.36"
+            },
+        ).text
+        text = text.split("Shared Projects (")[1]
+        text = text.split(")")[0]
+        return int(text)
+
+
+def get_user(username):
+    user = User(username)
+    user.update()
+    return user
