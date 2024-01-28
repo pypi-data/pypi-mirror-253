cb6b8ac9eb0d9c403b989402859e2ed7c69befcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 15:04:19 +0000

    Add package python-dd/0.0.12/python_dd-0.0.12.tar.gz

---
+++ packages/python-dd/python_dd-0.0.12.tar.gz/python_dd-0.0.12/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_dd",
+    version="0.0.12",
+    author="Bedram Tamang",
+    author_email="tmgbedu@gmail.com",
+    description="The missing `dd` function in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bedus-creation/python_dd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
