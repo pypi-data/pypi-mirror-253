72b955e9e82564d27fda902977cee5bcb4b542b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:19 +0000

    Add package ENGR131-Util-2024/0.0.1/ENGR131-Util-2024-0.0.1.tar.gz

---
+++ packages/ENGR131-Util-2024/ENGR131-Util-2024-0.0.1.tar.gz/ENGR131-Util-2024-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="ENGR131-Util-2024",
+    version="0.0.1",
+    author="Joshua C. Agar",
+    description="Drexel Jupyter Logger Package",
+    packages=setuptools.find_packages(),
+    install_requires=["cryptography", "drexel_jupyter_logger", "ipywidgets"],
+)
