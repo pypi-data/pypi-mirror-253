5ce03af6c4e6d7ed77fb7c4e3cdf0a44c09c2b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:11 +0000

    Add package process-protein/1.1.2/process_protein-1.1.2.tar.gz

---
+++ packages/process-protein/process_protein-1.1.2.tar.gz/process_protein-1.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='process_protein',
+version='1.1.2',
+author='Guangpeng Xue',
+author_email='athonyxue@gmail.com',
+description='md_protein_process is a Python package for protein process',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
