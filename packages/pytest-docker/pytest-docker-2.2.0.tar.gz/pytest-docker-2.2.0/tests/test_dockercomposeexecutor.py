83d7c14721a5a6a7c96748fbec0ebebe42e956c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:09 +0000

    Add package pytest-docker/2.2.0/pytest-docker-2.2.0.tar.gz

---
+++ packages/pytest-docker/pytest-docker-2.2.0.tar.gz/pytest-docker-2.2.0/tests/test_dockercomposeexecutor.py
@@ -0,0 +1,65 @@
+import subprocess
+from unittest import mock
+
+import py
+from pytest_docker.plugin import DockerComposeExecutor
+
+
+def test_execute():
+    docker_compose = DockerComposeExecutor(
+        "docker compose", "docker-compose.yml", "pytest123"
+    )
+    with mock.patch("subprocess.check_output") as check_output:
+        docker_compose.execute("up")
+        assert check_output.call_args_list == [
+            mock.call(
+                'docker compose -f "docker-compose.yml" -p "pytest123" up',
+                shell=True,
+                stderr=subprocess.STDOUT,
+            )
+        ]
+
+
+def test_execute_docker_compose_v2():
+    docker_compose = DockerComposeExecutor(
+        "docker compose", "docker-compose.yml", "pytest123"
+    )
+    with mock.patch("subprocess.check_output") as check_output:
+        docker_compose.execute("up")
+        assert check_output.call_args_list == [
+            mock.call(
+                'docker compose -f "docker-compose.yml" -p "pytest123" up',
+                shell=True,
+                stderr=subprocess.STDOUT,
+            )
+        ]
+
+
+def test_pypath_compose_files():
+    compose_file = py.path.local("/tmp/docker-compose.yml")
+    docker_compose = DockerComposeExecutor("docker compose", compose_file, "pytest123")
+    with mock.patch("subprocess.check_output") as check_output:
+        docker_compose.execute("up")
+        assert check_output.call_args_list == [
+            mock.call(
+                'docker compose -f "/tmp/docker-compose.yml"' ' -p "pytest123" up',
+                shell=True,
+                stderr=subprocess.STDOUT,
+            )
+        ]
+
+
+def test_multiple_compose_files():
+    docker_compose = DockerComposeExecutor(
+        "docker compose", ["docker-compose.yml", "other-compose.yml"], "pytest123"
+    )
+    with mock.patch("subprocess.check_output") as check_output:
+        docker_compose.execute("up")
+        assert check_output.call_args_list == [
+            mock.call(
+                'docker compose -f "docker-compose.yml" -f "other-compose.yml"'
+                ' -p "pytest123" up',
+                shell=True,
+                stderr=subprocess.STDOUT,
+            )
+        ]
