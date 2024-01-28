a6437d618e8c1ef21c032c1eb968685e0046a7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:55 +0000

    Add package mspect/0.0.1/mspect-0.0.1.tar.gz

---
+++ packages/mspect/mspect-0.0.1.tar.gz/mspect-0.0.1/mspect/__init__.py
@@ -0,0 +1,39 @@
+#!python
+
+
+__project__ = "mspect"
+__version__ = "0.0.1"
+__license__ = "Apache"
+__description__ = "TODO"
+__author__ = "Wen-Feng Zeng"
+__author_email__ = "jalew188@gmail.com"
+__github__ = "https://github.com/FennOmix/mspect"
+__keywords__ = [
+    "computational proteomics",
+    "bioinformatics",
+    "software",
+]
+__python_version__ = ">=3.8"
+__classifiers__ = [
+    "Development Status :: 1 - Planning",
+    # "Development Status :: 2 - Pre-Alpha",
+    # "Development Status :: 3 - Alpha",
+    # "Development Status :: 4 - Beta",
+    # "Development Status :: 5 - Production/Stable",
+    # "Development Status :: 6 - Mature",
+    # "Development Status :: 7 - Inactive"
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+__console_scripts__ = []
+__urls__ = {
+    "GitHub": __github__,
+    "Docs": "https://mspect.readthedocs.io/en/latest/",
+    "PyPi": "https://pypi.org/project/mspect/",
+}
+__extra_requirements__ = {
+    "development": "extra_requirements/development.txt",
+}
