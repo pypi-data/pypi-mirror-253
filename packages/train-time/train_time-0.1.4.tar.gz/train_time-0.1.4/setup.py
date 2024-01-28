e2cfcaf838d1783a377bc91b866834d69a1d4196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:40 +0000

    Add package train-time/0.1.4/train_time-0.1.4.tar.gz

---
+++ packages/train-time/train_time-0.1.4.tar.gz/train_time-0.1.4/setup.py
@@ -0,0 +1,19 @@
+import pathlib
+
+import setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name='train_time',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        'keras',
+    ],
+    author='Akinyemi Arabambi',
+    description='A Keras callback to estimate remaining training time',
+    long_description= pathlib.Path('README.md').read_text(),
+    long_description_content_type = 'text/markdown',
+    url='https://github.com/2abet/TrainTime',
+)
