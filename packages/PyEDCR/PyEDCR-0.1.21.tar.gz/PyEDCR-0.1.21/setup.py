5c5e0a53058b5ef9c45e174ab45ca22020534a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:29 +0000

    Add package PyEDCR/0.1.21/PyEDCR-0.1.21.tar.gz

---
+++ packages/PyEDCR/PyEDCR-0.1.21.tar.gz/PyEDCR-0.1.21/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+  description = f.read()
+
+
+setuptools.setup(
+  name='PyEDCR',
+  version='0.1.21',
+  author='Joshua Shay Kricheli, Paulo Shakarian, Spencer Ozgur, Aniruddha Datta, Khoa Vo',
+  author_email='name@example.com',
+  description='A short description of your package',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.7',
+  long_description=description,
+  long_description_content_type = "text/markdown",
+  packages=setuptools.find_packages(where="."),
+  install_requires=[           
+    'beautifulsoup4',
+    'LTNtorch',
+    'matplotlib',
+    'numpy',
+    'opencv_python',
+    'pandas',
+    'Pillow',
+    'protobuf',
+    'Requests',
+    'scikit_learn',
+    'timm',
+    'torch',
+    'torchsummary',
+    'torchvision',
+    'tqdm'
+  ],
+  package_data={
+    'PyEDCR': ['metacognitive_error_detection_and_correction_v2/combined_results/vit_b_16_BCE_test_coarse_pred_lr0.0001_e19.npy', 
+               'metacognitive_error_detection_and_correction_v2/combined_results/vit_b_16_BCE_test_fine_pred_lr0.0001_e19.npy',
+               "metacognitive_error_detection_and_correction_v2/test_fine/test_true_fine.npy",
+               "metacognitive_error_detection_and_correction_v2/test_coarse/test_true_coarse.npy",],
+  },
+)
