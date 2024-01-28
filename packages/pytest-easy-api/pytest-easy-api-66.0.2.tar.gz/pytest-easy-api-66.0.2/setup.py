4a171220ea3da31818e5e3f0520ecc5440bca864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:05 +0000

    Add package pytest-easy-api/66.0.2/pytest-easy-api-66.0.2.tar.gz

---
+++ packages/pytest-easy-api/pytest-easy-api-66.0.2.tar.gz/pytest-easy-api-66.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='pytest-easy-api',
+   version='66.0.2',
+   author='Yandex',
+   author_email='security@yandex-team.ru',
+   url='https://ya.ru',
+   readme="README.md",
+   long_description="""This is a security placeholder package.
+If you want to claim this name for legitimate purposes,
+please contact us at security@yandex-team.ru or pypi-security@yandex-team.ru""",
+   long_description_content_type='text/markdown',
+   description='A package to prevent Dependency Confusion attacks against Yandex.',
+   )
