b29aaf4587e79614dbe99a2d449f0d785629f4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:05:12 +0000

    Add package pygpt-net/2.0.122/pygpt_net-2.0.122-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.0.122-py3-none-any.whl/pygpt_net/ui/menu/plugins.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.27 11:00:00                  #
+# ================================================== #
+
+from PySide6.QtGui import QAction, QIcon
+
+from pygpt_net.utils import trans
+
+
+class Plugins:
+    def __init__(self, window=None):
+        """
+        Menu setup
+
+        :param window: Window instance
+        """
+        self.window = window
+
+    def setup(self):
+        """Setup plugins menu"""
+        self.window.ui.menu['plugins.settings'] = QAction(QIcon.fromTheme("preferences-other"),
+                                                          trans("menu.plugins.settings"), self.window)
+
+        self.window.ui.menu['plugins.settings'].triggered.connect(
+            lambda: self.window.controller.plugins.settings.toggle_editor())
+
+        self.window.ui.menu['plugins'] = {}
+        self.window.ui.menu['menu.plugins'] = self.window.menuBar().addMenu(trans("menu.plugins"))
+        self.window.ui.menu['menu.plugins'].addAction(self.window.ui.menu['plugins.settings'])
+        self.window.ui.menu['menu.plugins'].setToolTipsVisible(True)
