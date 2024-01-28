51a6267af15be63dd0f94ff1d4490a35908fefd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:34 +0000

    Add package train-time/0.1.2/train_time-0.1.2.tar.gz

---
+++ packages/train-time/train_time-0.1.2.tar.gz/train_time-0.1.2/train_time/traintime.py
@@ -0,0 +1,20 @@
+import time
+
+class train_time(Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+        self.total_time = 0.0
+
+    def on_epoch_begin(self, epoch, logs={}):
+        self.epoch_start_time = time.time()
+
+    def on_epoch_end(self, epoch, logs={}):
+        epoch_time = time.time() - self.epoch_start_time
+        self.times.append(epoch_time)
+        self.total_time += epoch_time
+        average_time_per_epoch = self.total_time / (epoch + 1)
+        remaining_time = average_time_per_epoch * (self.params['epochs'] - epoch - 1)
+        print("\nEpoch: {}/{}. Estimated time remaining: {:.2f} seconds.".format(
+            epoch + 1, self.params['epochs'], remaining_time))
+
+
