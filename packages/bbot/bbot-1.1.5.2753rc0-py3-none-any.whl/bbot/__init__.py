061a2a5aae3617a54ed9eac0c6995e51d51c4d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:14 +0000

    Add package bbot/1.1.5.2753rc0/bbot-1.1.5.2753rc0-py3-none-any.whl

---
+++ packages/bbot/bbot-1.1.5.2753rc0-py3-none-any.whl/bbot/__init__.py
@@ -0,0 +1,10 @@
+# version placeholder (replaced by poetry-dynamic-versioning)
+__version__ = "v1.1.5.2753rc"
+
+# global app config
+from .core import configurator
+
+config = configurator.config
+
+# helpers
+from .core import helpers
