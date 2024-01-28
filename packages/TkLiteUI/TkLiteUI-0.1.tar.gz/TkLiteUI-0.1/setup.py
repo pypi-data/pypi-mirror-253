5cfe29fc62be6a04c10a1ccc0c9f4b008b2a9f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:34 +0000

    Add package TkLiteUI/0.1/TkLiteUI-0.1.tar.gz

---
+++ packages/TkLiteUI/TkLiteUI-0.1.tar.gz/TkLiteUI-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TkLiteUI",
+    version="0.1",
+    description="Basit ve hafif bir Tkinter kullanıcı arayüzü kütüphanesi",
+    author="Adem Ulker",
+    author_email="ademulker055@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
