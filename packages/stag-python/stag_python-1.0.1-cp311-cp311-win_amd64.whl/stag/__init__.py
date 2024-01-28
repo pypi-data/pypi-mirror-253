28eb8dc53cc7b5eb0dc9c1dc831e9639426a086f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:04:21 +0000

    Add package stag-python/1.0.1/stag_python-1.0.1-cp311-cp311-win_amd64.whl

---
+++ packages/stag-python/stag_python-1.0.1-cp311-cp311-win_amd64.whl/stag/__init__.py
@@ -0,0 +1,13 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_5_2():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'stag_python.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_5_2()
+del _delvewheel_patch_1_5_2
+# end delvewheel patch
+
+from stag._core import *
