ea64565786064d3778216965c8abb89c43788dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 15:04:04 +0000

    Add package Topsis-Chhavi-102103605/0.1/Topsis_Chhavi_102103605-0.1.tar.gz

---
+++ packages/Topsis-Chhavi-102103605/Topsis_Chhavi_102103605-0.1.tar.gz/Topsis_Chhavi_102103605-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Chhavi_102103605',         
+  packages = ['Topsis_Chhavi_102103605'],  
+  version = '0.1',     
+  license='MIT',    
+  description = 'This library will help you apply TOPSIS to your datasets for multiple criteria decision making',   
+  author = 'Chhavi Dhankhar',                   
+  author_email = 'chhavidhankhar07@gmail.com',      
+  url = 'https://github.com/Chdh11/Topsis-Chhavi-102103605',   
+  download_url = 'https://github.com/Chdh11/Topsis-Chhavi-102103605/archive/refs/tags/v_01.tar.gz',  
+  keywords = ['TOPSIS', 'DECISION-MAKING', 'RANK'],  
+  install_requires=[           
+          'pandas'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
