5b827e522b9f5c80fbdd8b1e35b57c7f516ab3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:36 +0000

    Add package Renewable-Suitability-Predictor/0.0.3/Renewable_Suitability_Predictor-0.0.3.tar.gz

---
+++ packages/Renewable-Suitability-Predictor/Renewable_Suitability_Predictor-0.0.3.tar.gz/Renewable_Suitability_Predictor-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A quick and simple module for predicting a location\'s solar or wind renewable suitability using the NREL PSM API'
+
+# Setting up
+setup(
+    name="Renewable_Suitability_Predictor",
+    version=VERSION,
+    author="Phoenix Sheppard",
+    author_email="phoenixsheppard28@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pandas', 'numpy', 'requests', 'scikit-learn'],
+    keywords=['Machine Learning', 'Renewable Energy', 'Solar Energy', 'Wind Energy'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
