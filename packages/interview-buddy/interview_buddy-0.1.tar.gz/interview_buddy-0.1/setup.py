6230e3be06444a390bd90af059bcdd27dd3eaf4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:04 +0000

    Add package interview-buddy/0.1/interview_buddy-0.1.tar.gz

---
+++ packages/interview-buddy/interview_buddy-0.1.tar.gz/interview_buddy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='interview_buddy',
+    version='0.1',
+    packages=find_packages(),
+    author='R Jeyaraman',
+    author_email='jeyaramanjr7@gmail.com',
+    description='This package is made for beginners who are appearing for interview preparation and to crack coding interviews',
+)
