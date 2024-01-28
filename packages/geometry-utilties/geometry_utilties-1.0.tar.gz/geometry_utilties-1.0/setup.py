00a7a9783ea2add7942fb9404482cc22fe8331d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:14 +0000

    Add package geometry-utilties/1.0/geometry_utilties-1.0.tar.gz

---
+++ packages/geometry-utilties/geometry_utilties-1.0.tar.gz/geometry_utilties-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geometry_utilties',
+    version='1.0',
+    description='O GeometryUtils é uma biblioteca simples e precisa para cálculos eficientes de áreas, perímetros e volumes de formas geométricas 2D e 3D',
+    author='João Felipe do Nascimento Lopes',
+    packages=find_packages(),
+)
