35674de7dcce02f889d829c27acc3c5eebe35559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:24 +0000

    Add package obscure_stats/0.2.1/obscure_stats-0.2.1.tar.gz

---
+++ packages/obscure_stats/obscure_stats-0.2.1.tar.gz/obscure_stats-0.2.1/src/obscure_stats/association/__init__.py
@@ -0,0 +1,27 @@
+"""Association module."""
+
+from .association import (
+    blomqvistbeta,
+    chatterjeexi,
+    concordance_corrcoef,
+    concordance_rate,
+    rank_minrelation_coefficient,
+    symmetric_chatterjeexi,
+    tanimoto_similarity,
+    tukey_correlation,
+    winsorized_correlation,
+    zhangi,
+)
+
+__all__ = [
+    "blomqvistbeta",
+    "chatterjeexi",
+    "concordance_corrcoef",
+    "concordance_rate",
+    "rank_minrelation_coefficient",
+    "symmetric_chatterjeexi",
+    "tanimoto_similarity",
+    "tukey_correlation",
+    "winsorized_correlation",
+    "zhangi",
+]
