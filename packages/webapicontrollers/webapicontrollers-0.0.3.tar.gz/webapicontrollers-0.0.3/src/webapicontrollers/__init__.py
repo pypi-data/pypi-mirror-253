b1be3498b1adbc5173cd102ca655e5c8c7816c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:11 +0000

    Add package webapicontrollers/0.0.3/webapicontrollers-0.0.3.tar.gz

---
+++ packages/webapicontrollers/webapicontrollers-0.0.3.tar.gz/webapicontrollers-0.0.3/src/webapicontrollers/__init__.py
@@ -0,0 +1,7 @@
+from .controllers import APIController
+from .routing.get import Get
+from .routing.post import Post
+from .routing.put import Put
+from .routing.delete import Delete
+from .routing.patch import Patch
+from .routing.route import Route
