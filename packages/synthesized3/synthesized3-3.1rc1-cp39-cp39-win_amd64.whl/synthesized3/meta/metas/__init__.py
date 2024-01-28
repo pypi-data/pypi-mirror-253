d2f4e681935f33ee71fedacb13bfd410ccce58e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:03:56 +0000

    Add package synthesized3/3.1rc1/synthesized3-3.1rc1-cp39-cp39-win_amd64.whl

---
+++ packages/synthesized3/synthesized3-3.1rc1-cp39-cp39-win_amd64.whl/synthesized3/meta/metas/__init__.py
@@ -0,0 +1,18 @@
+"""Meta classes for different data types."""
+from .affine_meta import AffineMeta
+from .boolean_meta import BooleanMeta
+from .categorical_meta import CategoricalMeta
+from .constant_meta import ConstantMeta
+from .datetime_meta import DatetimeMeta
+from .meta_schema import MetaSchema
+from .missing_value_meta import MissingValueMeta
+
+__all__ = [
+    "AffineMeta",
+    "BooleanMeta",
+    "CategoricalMeta",
+    "ConstantMeta",
+    "DatetimeMeta",
+    "MetaSchema",
+    "MissingValueMeta",
+]
