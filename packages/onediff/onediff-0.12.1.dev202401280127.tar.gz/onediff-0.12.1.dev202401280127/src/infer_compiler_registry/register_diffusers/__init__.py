8303b6dfeae5c0967931e7d19b4056d5541ed265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:46 +0000

    Add package onediff/0.12.1.dev202401280127/onediff-0.12.1.dev202401280127.tar.gz

---
+++ packages/onediff/onediff-0.12.1.dev202401280127.tar.gz/onediff-0.12.1.dev202401280127/src/infer_compiler_registry/register_diffusers/__init__.py
@@ -0,0 +1,61 @@
+from onediff.infer_compiler.transform import register
+
+from packaging import version
+import importlib.metadata
+
+diffusers_version = version.parse(importlib.metadata.version("diffusers"))
+
+# register(package_names=["diffusers"])
+from diffusers.models.attention_processor import Attention, AttnProcessor, AttnProcessor2_0
+from diffusers.models.attention_processor import LoRAAttnProcessor2_0
+from diffusers.models.unet_2d_condition import UNet2DConditionModel
+from diffusers.models.transformer_2d import Transformer2DModel
+if diffusers_version >= version.parse("0.24.00"):
+    from diffusers.models.resnet import SpatioTemporalResBlock
+    from diffusers.models.transformer_temporal import TransformerSpatioTemporalModel
+    from diffusers.models.attention import TemporalBasicTransformerBlock 
+    from diffusers.models.unet_spatio_temporal_condition import UNetSpatioTemporalConditionModel
+    
+    if diffusers_version >= version.parse("0.25.00"):
+        from diffusers.models.autoencoders.autoencoder_kl_temporal_decoder import TemporalDecoder
+    else:
+        from diffusers.models.autoencoder_kl_temporal_decoder import TemporalDecoder
+
+from .attention_processor_oflow import Attention as AttentionOflow
+from .attention_processor_oflow import AttnProcessor as AttnProcessorOflow
+from .attention_processor_oflow import LoRAAttnProcessor2_0 as LoRAAttnProcessorOflow
+from .unet_2d_condition_oflow import UNet2DConditionModel as UNet2DConditionModelOflow
+from .transformer_2d_oflow import Transformer2DModel as Transformer2DModelOflow
+from .spatio_temporal_oflow import (
+    SpatioTemporalResBlock as SpatioTemporalResBlockOflow,
+)
+from .spatio_temporal_oflow import TemporalDecoder as TemporalDecoderOflow
+from .spatio_temporal_oflow import TransformerSpatioTemporalModel as TransformerSpatioTemporalModelOflow
+from .spatio_temporal_oflow import TemporalBasicTransformerBlock as TemporalBasicTransformerBlockOflow
+from .spatio_temporal_oflow import UNetSpatioTemporalConditionModel as UNetSpatioTemporalConditionModelOflow
+
+# For CI
+if diffusers_version >= version.parse("0.24.00"):
+    torch2oflow_class_map = {
+        Attention: AttentionOflow,
+        AttnProcessor: AttnProcessorOflow,
+        AttnProcessor2_0: AttnProcessorOflow,
+        LoRAAttnProcessor2_0: LoRAAttnProcessorOflow,
+        SpatioTemporalResBlock: SpatioTemporalResBlockOflow,
+        TemporalDecoder: TemporalDecoderOflow,
+        TransformerSpatioTemporalModel: TransformerSpatioTemporalModelOflow, 
+        TemporalBasicTransformerBlock: TemporalBasicTransformerBlockOflow,
+        UNetSpatioTemporalConditionModel: UNetSpatioTemporalConditionModelOflow,
+    }
+else:
+    torch2oflow_class_map = {
+        Attention: AttentionOflow,
+        AttnProcessor: AttnProcessorOflow,
+        AttnProcessor2_0: AttnProcessorOflow,
+        LoRAAttnProcessor2_0: LoRAAttnProcessorOflow,
+    }
+
+torch2oflow_class_map.update({Transformer2DModel: Transformer2DModelOflow})
+torch2oflow_class_map.update({UNet2DConditionModel: UNet2DConditionModelOflow})
+
+register(torch2oflow_class_map=torch2oflow_class_map)
