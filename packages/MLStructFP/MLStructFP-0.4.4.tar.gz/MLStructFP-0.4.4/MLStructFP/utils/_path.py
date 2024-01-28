257526fcf96deb1ec76897f8dec63aaab0899b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:49 +0000

    Add package MLStructFP/0.4.4/MLStructFP-0.4.4.tar.gz

---
+++ packages/MLStructFP/MLStructFP-0.4.4.tar.gz/MLStructFP-0.4.4/MLStructFP/utils/_path.py
@@ -0,0 +1,24 @@
+"""
+MLSTRUCTFP - UTILS - PATH
+
+Path utils.
+"""
+
+__all__ = ['make_dirs']
+
+import os
+
+
+def make_dirs(f: str) -> str:
+    """
+    Create dir if not exists.
+
+    :param f: Filename
+    :returns: File
+    """
+    if f != '':
+        if f[-1] != '/':
+            f += '/'
+        fdir = os.path.dirname(f)
+        os.makedirs(fdir, exist_ok=True)
+    return f
