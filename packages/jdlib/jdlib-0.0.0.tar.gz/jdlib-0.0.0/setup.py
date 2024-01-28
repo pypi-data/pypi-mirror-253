11c88399a29952ba99a0fe732cbda2316b5dd882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:32 +0000

    Add package jdlib/0.0.0/jdlib-0.0.0.tar.gz

---
+++ packages/jdlib/jdlib-0.0.0.tar.gz/jdlib-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jdlib',
+    version='0.0.0',
+    author='Janus Digital LLC',
+    author_email='justice@janusdigital.io',
+    packages=find_packages()
+)
