1cbeef851a59f66169187f90cf6d828fd890b791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:07:49 +0000

    Add package clickhouse-s3-etl-tools/0.2.8/clickhouse_s3_etl_tools-0.2.8-py3-none-any.whl

---
+++ packages/clickhouse-s3-etl-tools/clickhouse_s3_etl_tools-0.2.8-py3-none-any.whl/clickhouse_s3_etl_tools/schema/schema_table_metadata.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class MetadataConfig(BaseModel):
+    """
+    Pydantic model for metadata configuration.
+
+    Attributes:
+        create_table_query (str): SQL query for table creation.
+        engine (str): Database engine.
+        partition_key (str): Key used for partitioning.
+        total_rows (int): Total number of rows.
+        total_bytes (int): Total size in bytes.
+    """
+
+    create_table_query: str
+    engine: str
+    partition_key: str
+    total_rows: int
+    total_bytes: int
