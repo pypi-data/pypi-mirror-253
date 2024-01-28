c1912fa9125d557cc559236aab67891e45009450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:46 +0000

    Add package ex4test/1.0.9/ex4test-1.0.9.tar.gz

---
+++ packages/ex4test/ex4test-1.0.9.tar.gz/ex4test-1.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ex4test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nicegui>=1.4.12,<2.0.0']
+
+setup_kwargs = {
+    'name': 'ex4test',
+    'version': '1.0.9',
+    'description': '',
+    'long_description': '# my-libs',
+    'author': 'CrystalWindSnake',
+    'author_email': '568166495@qq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
