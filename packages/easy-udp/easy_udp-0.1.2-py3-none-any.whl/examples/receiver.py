2f8c2ab0d50dd5469d18abdb64c83a4a3c750d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:51 +0000

    Add package easy-udp/0.1.2/easy_udp-0.1.2-py3-none-any.whl

---
+++ packages/easy-udp/easy_udp-0.1.2-py3-none-any.whl/examples/receiver.py
@@ -0,0 +1,10 @@
+from easy_udp import UDPReceiver
+
+# Create UDP receiver instance
+udp_receiver = UDPReceiver(host="localhost", port=12345, timeout=1.0)
+
+# receive data
+while True:
+    received_data = udp_receiver.receive()
+    if received_data is not None:
+        print(received_data)
