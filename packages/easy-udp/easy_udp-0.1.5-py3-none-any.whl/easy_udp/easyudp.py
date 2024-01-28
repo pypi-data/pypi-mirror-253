ad6a0f2cf93a8c10727f91afb078ad924ee395df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 15:04:09 +0000

    Add package easy-udp/0.1.5/easy_udp-0.1.5-py3-none-any.whl

---
+++ packages/easy-udp/easy_udp-0.1.5-py3-none-any.whl/easy_udp/easyudp.py
@@ -0,0 +1,45 @@
+"""
+easyudp module provides the EasyUDP abstract class for handling UDP communication.
+
+Classes:
+    EasyUDP: An abstract class for creating UDP communication.
+
+"""
+
+import socket
+from abc import ABC, abstractmethod
+
+
+class EasyUDP(ABC):
+    def __init__(self) -> None:
+        """
+        Initialize a new EasyUDP instance.
+
+        """
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2 * 1024 * 1024)
+
+    @abstractmethod
+    def send(self, message) -> None:
+        """
+        Abstract method for sending data through UDP.
+
+        Raises:
+            NotImplementedError: This method must be implemented in the derived class.
+
+        """
+
+        raise NotImplementedError
+
+    @abstractmethod
+    def receive(self):
+        """
+        Abstract method for receiving data through UDP.
+
+
+        Raises:
+            NotImplementedError: This method must be implemented in the derived class.
+
+        """
+        raise NotImplementedError
