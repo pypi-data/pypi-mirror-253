f0de09370ba660de2f4104be8bc2416b5f36ff66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:04:04 +0000

    Add package binomial-normal-prob/0.1/binomial_normal_prob-0.1.tar.gz

---
+++ packages/binomial-normal-prob/binomial_normal_prob-0.1.tar.gz/binomial_normal_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial_normal_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['binomial_normal_prob'],
+      zip_safe=False)
