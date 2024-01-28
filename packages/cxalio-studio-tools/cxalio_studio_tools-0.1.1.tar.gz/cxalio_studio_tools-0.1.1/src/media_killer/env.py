bb0eb656cc6e75c4267fbc6d05d2a689e0d4a288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 03:05:38 +0000

    Add package cxalio-studio-tools/0.1.1/cxalio_studio_tools-0.1.1.tar.gz

---
+++ packages/cxalio-studio-tools/cxalio_studio_tools-0.1.1.tar.gz/cxalio_studio_tools-0.1.1/src/media_killer/env.py
@@ -0,0 +1,52 @@
+import signal
+
+from rich.progress import Progress, SpinnerColumn, MofNCompleteColumn
+
+from cx_core import AppLogger, AbstractEnv, LogLevel, DataPackage
+
+
+class Env(AbstractEnv):
+    def __init__(self):
+        super().__init__()
+        self._progress = Progress(
+            SpinnerColumn(),
+            # MofNCompleteColumn(),
+            *Progress.get_default_columns(),
+            # expand=True,
+            transient=True)
+
+        self._logger = AppLogger(console=self._progress.console)
+        self._logger.level = LogLevel.WARNING
+
+        self.args = None
+        self.profile = DataPackage()
+
+        self.wanna_quit = False
+
+    def start(self):
+        self._progress.start()
+        self.info('Started ...')
+
+    def stop(self):
+        self._progress.stop()
+        self.info('Bye~')
+
+    @property
+    def progress(self):
+        return self._progress
+
+    @property
+    def console(self):
+        return self._progress.console
+
+
+env = Env()
+
+
+def signal_handler(signal, frame):
+    env.info('接收到 SIGINT')
+    env.print("收到终止信号，准备退出...")
+    env.wanna_quit = True
+
+
+signal.signal(signal.SIGINT, signal_handler)
