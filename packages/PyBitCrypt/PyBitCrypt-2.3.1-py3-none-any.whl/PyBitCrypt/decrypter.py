325d22a75e4f683d85c336bff5e5f4a965f7f9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 14:04:32 +0000

    Add package PyBitCrypt/2.3.1/PyBitCrypt-2.3.1-py3-none-any.whl

---
+++ packages/PyBitCrypt/PyBitCrypt-2.3.1-py3-none-any.whl/PyBitCrypt/decrypter.py
@@ -0,0 +1,25 @@
+def decrypt(key:str, text:str):
+    KeyList = []
+    for w in key:
+        num = CharList.index(w)
+        KeyList.append(num)
+
+    TextList = []
+    for e in text:
+        num = CharList.index(e)
+        TextList.append(num)
+
+    OutList = []
+    count = 0
+    for r in TextList:
+        t = KeyList[count%len(KeyList)]
+        OutList.append(r-t)
+        count += 1
+
+    output = ''
+    for y in OutList:
+        u = y%len(CharList)
+        output = output + CharList[u]
+
+    return output
+
