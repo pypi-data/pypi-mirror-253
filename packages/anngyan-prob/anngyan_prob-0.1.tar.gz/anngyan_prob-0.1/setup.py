32315347a446094adfce0f92ce5dd1ecee003e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-28 04:03:58 +0000

    Add package anngyan-prob/0.1/anngyan_prob-0.1.tar.gz

---
+++ packages/anngyan-prob/anngyan_prob-0.1.tar.gz/anngyan_prob-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='anngyan_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['anngyan_prob'],
+      author = 'Anna Gasparyan',
+      author_email = 'gasparyan.2003.ag@gmail.com',
+      zip_safe=False)
