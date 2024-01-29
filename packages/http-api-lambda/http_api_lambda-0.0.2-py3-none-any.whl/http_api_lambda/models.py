6d3d102b0763647276976b3550fd8e49d0b4c66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:18 +0000

    Add package http-api-lambda/0.0.2/http_api_lambda-0.0.2-py3-none-any.whl

---
+++ packages/http-api-lambda/http_api_lambda-0.0.2-py3-none-any.whl/http_api_lambda/models.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+class ApiGatewayEvent(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    class RequestContext(BaseModel):
+        model_config = ConfigDict(populate_by_name=True)
+        class Http(BaseModel):
+            model_config = ConfigDict(populate_by_name=True)
+            method: str
+            path: str
+            protocol: str
+            source_ip: str = Field(alias='sourceIp')
+            user_agent: str = Field(alias='userAgent')
+            
+        account_id: str = Field(alias='accountId')
+        api_id: str = Field(alias='apiId')
+        domain_name: str = Field(alias='domainName')
+        domain_prefix: str = Field(alias='domainPrefix')        
+        http: Http
+        request_id: str = Field(alias='requestId')
+        route_key: str = Field(alias='routeKey')
+        stage: str
+        time: str
+        time_epoch: int = Field(alias='timeEpoch')
+        
+    version: str
+    route_key: str = Field(alias='routeKey')
+    raw_path: str = Field(alias='rawPath')
+    raw_query_string: str = Field(alias='rawQueryString')
+    headers: dict[str, str]
+    request_context: RequestContext = Field(alias='requestContext')
+    body: str | None = None
+    is_base64_encoded: bool = Field(alias='isBase64Encoded')
+    
+    
+class LambdaContext(BaseModel):
+    function_name: str
+    function_version: str
+    invoked_function_arn: str
+    memory_limit_in_mb: int
+    aws_request_id: str
+    log_group_name: str
+    log_stream_name: str
+
+
+class Response(BaseModel):
+    is_base64_encoded: bool = Field(alias='isBase64Encoded', default=False)
+    status_code: int = Field(alias='statusCode', default=200)
+    body: str = ''
+    headers: dict = {}
