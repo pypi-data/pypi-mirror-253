6b8ddbe220769756d90ef4081245ae1f7a90e24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:52 +0000

    Add package MRCpy/2.0/MRCpy-2.0-py3-none-any.whl

---
+++ packages/MRCpy/MRCpy-2.0-py3-none-any.whl/MRCpy/datasets/__init__.py
@@ -0,0 +1,56 @@
+from .load import \
+    load_adult, \
+    load_catsvsdogs_features_resnet18, \
+    load_credit, \
+    load_diabetes, \
+    load_ecoli, \
+    load_forestcov, \
+    load_glass, \
+    load_haberman, \
+    load_indian_liver, \
+    load_iris, \
+    load_letterrecog, \
+    load_mammographic, \
+    load_mnist_features_resnet18, \
+    load_optdigits, \
+    load_redwine, \
+    load_satellite, \
+    load_segment, \
+    load_usenet2, \
+    load_vehicle, \
+    load_yearbook_features_resnet18, \
+    load_yearbook_path, \
+    load_comp_vs_sci, \
+    load_comp_vs_talk, \
+    load_rec_vs_sci, \
+    load_rec_vs_talk, \
+    load_sci_vs_talk, \
+    load_comp_vs_sci_short
+
+__all__ = ['load_adult',
+           'load_catsvsdogs_features_resnet18',
+           'load_credit',
+           'load_diabetes',
+           'load_ecoli',
+           'load_forestcov',
+           'load_glass',
+           'load_haberman',
+           'load_indian_liver',
+           'load_iris',
+           'load_letterrecog',
+           'load_mammographic',
+           'load_mnist_features_resnet18',
+           'load_optdigits',
+           'load_redwine',
+           'load_satellite',
+           'load_segment',
+           'load_usenet2',
+           'load_vehicle',
+           'load_yearbook_features_resnet18',
+           'load_yearbook_path',
+           'load_comp_vs_sci',
+           'load_comp_vs_talk',
+           'load_rec_vs_sci',
+           'load_rec_vs_talk',
+           'load_sci_vs_talk',
+           'load_comp_vs_sci_short']
