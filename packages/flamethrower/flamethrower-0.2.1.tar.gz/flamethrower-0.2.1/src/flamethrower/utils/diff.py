6447f7388489b237a75367d82f298fdc85488632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:29 +0000

    Add package flamethrower/0.2.1/flamethrower-0.2.1.tar.gz

---
+++ packages/flamethrower/flamethrower-0.2.1.tar.gz/flamethrower-0.2.1/src/flamethrower/utils/diff.py
@@ -0,0 +1,21 @@
+import os
+import git
+from pydantic import BaseModel, ConfigDict
+from flamethrower.shell.printer import Printer
+
+class Diff(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
+    base_path: str = os.getcwd()
+    repo: git.Repo = None
+    printer: Printer = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.repo = git.Repo(self.base_path)
+
+    def get_diffs(self) -> list:
+        if not self.repo.is_dirty(untracked_files=True):
+            return []
+        
+        return self.repo.git.diff(None).split('\n')
