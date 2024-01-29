64ffa87000fa1a33a16c27474ee753512df49977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:36 +0000

    Add package stone-color/0.1.1/stone-color-0.1.1.tar.gz

---
+++ packages/stone-color/stone-color-0.1.1.tar.gz/stone-color-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='stone-color',
+    packages=find_packages(include=["stone_color"]),
+    version='0.1.1',
+    description='Text formatting, coloring and tables library',
+    keywords=["color", "cli", "table", "terminal-colors", "ansi", "rgb"],
+    author='aniko33',
+)
