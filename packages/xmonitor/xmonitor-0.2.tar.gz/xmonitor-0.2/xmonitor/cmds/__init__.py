cd8f531be474b53246653b81046cb570a54d132a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:48 +0000

    Add package xmonitor/0.2/xmonitor-0.2.tar.gz

---
+++ packages/xmonitor/xmonitor-0.2.tar.gz/xmonitor-0.2/xmonitor/cmds/__init__.py
@@ -0,0 +1,45 @@
+# coding:utf-8
+
+from typing import List
+from typing import Optional
+from typing import Sequence
+
+from xarg import add_command
+from xarg import argp
+from xarg import commands
+from xarg import run_command
+
+from ..utils import __description__
+from ..utils import __name__
+from ..utils import __url_home__
+from ..utils import __version__
+from .glances import add_cmd_glances
+
+subs: List[add_command] = list()
+subs.append(add_cmd_glances)
+
+try:
+    from .logrotate import add_cmd_logrotate
+    subs.append(add_cmd_logrotate)
+except Exception:
+    pass
+
+
+@add_command(__name__)
+def add_cmd(_arg: argp):
+    pass
+
+
+@run_command(add_cmd, *subs)
+def run_cmd(cmds: commands) -> int:
+    return 0
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    cmds = commands()
+    cmds.version = __version__
+    return cmds.run(
+        root=add_cmd,
+        argv=argv,
+        description=__description__,
+        epilog=f"For more, please visit {__url_home__}.")
---
+++ packages/xmonitor/xmonitor-0.2.tar.gz/xmonitor-0.2/xmonitor/utils/__init__.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+from .attribute import __author__
+from .attribute import __author_email__
+from .attribute import __description__
+from .attribute import __name__
+from .attribute import __url_bugs__
+from .attribute import __url_code__
+from .attribute import __url_docs__
+from .attribute import __url_home__
+from .attribute import __version__
+from .glances import glances_sd_service as glances_systemd_service
+
+try:
+    from .logrotate import lr_files as logrotate_files
+    from .logrotate import lr_state as logrotate_state
+    from .logrotate import lr_status as logrotate_status
+except Exception:
+    pass
