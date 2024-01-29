921280b9a629fe34dffea052e2028c149eca88ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:51 +0000

    Add package query-exporter/2.10.0/query-exporter-2.10.0.tar.gz

---
+++ packages/query-exporter/query-exporter-2.10.0.tar.gz/query-exporter-2.10.0/CHANGES.rst
@@ -0,0 +1,336 @@
+v2.10.0 - 2024-01-28
+====================
+
+- Fix columns names in InvalidResultColumnNames being reported the wrong way
+  round (#185).
+- Add a metric to track query execution timestamp (#178).
+- [docker] Define a volume containing the config file (#158).
+- [docker] Add support for ODBC version 17, support alternative versions.
+- Switch to ruff for formatting.
+
+
+v2.9.2 - 2023-10-28
+===================
+
+- Fix main script (#171).
+- Typos fixes in documentation.
+
+
+v2.9.1 - 2023-10-28
+===================
+
+- Update dependency to ``prometheus-aioexporter`` 2.0.
+- [snap] Add support for ClickHouse.
+- [docker] Add support for ClickHouse.
+
+
+v2.9.0 - 2023-08-18
+===================
+
+- Add ``increment`` flag for counter metrics (#124).
+- Rework project setup.
+- [docker] Add ``pymssql`` package (#133).
+- [docker] Fix setup for Microsoft repository (#159).
+
+
+v2.8.3 - 2022-07-16
+===================
+
+- Test with Python 3.10 in GitHub actions
+- Fix tests
+
+
+v2.8.2 - 2022-07-16
+===================
+
+- Require Python 3.10.
+- [snap] Change base to core22.
+- [docker] Use Python 3.10.
+- [docker] Base on Debian 11.
+
+
+v2.8.1 - 2022-02-18
+===================
+
+- Require ``sqlalchemy_aio`` 0.17.0, drop workaround for previous versions
+  (#105).
+
+
+v2.8.0 - 2022-01-18
+===================
+
+- Add support for parameters matrix in queries.
+- Allow freetext name for databases in config (#99).
+
+
+v2.7.1 - 2021-12-01
+===================
+
+- Require Python3.8
+- Move creation of async locks and queues after loop setup (#90, #96).
+- Update Python project config.
+- Document action performed for each query type (#86).
+
+
+v2.7.0 - 2021-03-28
+===================
+
+- Support optional ``expiration`` parameter for metrics to clear stale series
+  (#26).
+- Correctly close the database connection when ``keep-connected: false`` is
+  used (#81).
+- Don't connect to all databases at startup, only when the first query is run.
+
+
+v2.6.2 - 2021-03-13
+===================
+
+- Workaround missing attributes from AsyncioEngine (#62 and #76).
+- Log app version at startup.
+
+
+v2.6.1 - 2020-12-20
+===================
+
+- Fix schedule times iterator for scheduled queries (#76).
+- [docker] - Fix build.
+
+
+v2.6.0 - 2020-12-06
+===================
+
+- Add support reading database DSN from file.
+- Add support for specifying database connection details as separate elements.
+- [docker] Fix MSSQL support in.
+
+
+v2.5.1 - 2020-11-26
+===================
+
+- Add tracebacks for database errors in debug log.
+
+
+v2.5.0 - 2020-11-19
+===================
+
+- Add ``query`` name label for builtin metrics.
+- Add optional ``timeout`` option for queries.
+- [snap] Add bash completion.
+- [snap] Switch to core20 base.
+- [docker] Include support for Oracle database.
+
+
+v2.4.0 - 2020-06-20
+===================
+
+- Add a ``query_latency`` metric to track query execution times. This is
+  labeled by database and query name (#46).
+
+
+v2.3.0 - 2020-06-04
+===================
+
+- Add support for query schedule (#29).
+- [docker] Pass the config file in the command line.
+
+
+v2.2.1 - 2020-05-08
+===================
+
+- [snap] Enable IBM DB2 support only on supported architectures (x86_64,
+  ppc64le and s390x).
+- [docker] Fix python library paths.
+- Add support for disabling query autocommit in database configuration.
+
+
+v2.2.0 - 2020-03-16
+===================
+
+- Validate that metric names don't collide with builtin ones.
+- Perform database connect/disconnect under lock (#28).
+- Add support for queries to run at connection (#31).
+- [snap,docker] Support IBM DB2 (#14).
+
+
+v2.1.0 - 2020-02-29
+===================
+
+- When validating config, warn about database and metrics that are not used in
+  any query.
+- Support extra per-database labels for metrics. All databases must define the
+  same set of labels (#27).
+
+
+v2.0.2 - 2020-02-15
+===================
+
+- Don't disable queries failing because of ``OperationalError`` as it might not
+  be a fatal error (#25).
+
+
+v2.0.1 - 2020-02-07
+===================
+
+- Fix validation for entries in the ``queries`` section for config file.
+- [snap,docker] Add MSSQL support.
+
+
+v2.0.0 - 2020-02-02
+===================
+
+- Support only named parameters (e.g.: ``:param``) in queries (#21, #24).
+- Add JSON-schema validation for config file (#23).
+- Validate at startup if database engines from DSNs are supported and
+  corresponding modules are available.
+- Check that names for queries ``parameters`` match the ones in queries SQL.
+- Add ``--check-only`` command line option to just validate configuration.
+- Drop support for matching query columns positionally, only support name
+  match. This is to avoid confusing behavior with positional match, and make
+  queries more explicit.
+
+**NOTE**:
+ some of the changes above for query definitions are backwards incompatible,
+ thus queries might need updating. Specifically:
+
+ - Only named parameters with the ``:param`` style are now supported, queries
+   using positional parameters or other styles of named parameters need to be
+   updated.
+ - Literal ``:`` at the beginning of a word need to be escaped (with backslash)
+   to avoid confusion with parameter markers. Colons that appear inside words
+   don't need to be escaped.
+ - Column names for query results must now always match metric and label names
+   involved in the query. Position-based match for queries without labels is no
+   longer supported. Queries can be updated adding ``AS
+   <metric_name|label_name>`` expressions.
+
+
+v1.9.3 - 2019-12-29
+===================
+
+- Convert ``Decimal`` query results to float (#19).
+
+
+v1.9.2 - 2019-12-24
+===================
+
+- Fix failure when multiple query columns have the same name (#18).
+- [docker] Add Dockerfile (#17).
+
+
+v1.9.1 - 2019-11-26
+===================
+
+- Track doomed queries on a per-database basis (#16).
+- Add ``--version`` option.
+
+
+v1.9.0 - 2019-11-03
+===================
+
+- Support passing sets of parameters for queries.
+
+
+v1.8.1 - 2019-07-14
+===================
+
+- Enable autocommit on connection (#10).
+
+
+v1.8.0 - 2019-05-25
+===================
+
+- Support custom labels in metrics, setting values from queries result (#7).
+- Suport matching metrics by query result column name instead of order.
+- Disable queries that will certainly always fail (e.g. because of invalid.
+  returned column names/number) (#6).
+- Support disconnecting from after each query (#8).
+- Rework tests to use actually SQLite in-memory databases instead of fakes.
+
+
+v1.7.0 - 2019-04-07
+===================
+
+- Add a ``queries`` and ``database_errors`` metrics labeled by database (#1).
+- Support database DSNs defined as ``env:<VARNAME>`` to supply the dns from the
+  environment (#5).
+
+
+v1.6.0 - 2019-03-26
+===================
+
+- Change default port to 9560 (to make it unique).
+
+
+v1.5.0 - 2018-12-28
+===================
+
+- Drop support for Python 3.5.
+- Add support for ``enum`` metrics.
+- [snap] Add initial snap support.
+- Rework project setup and use pytest.
+
+
+v1.4.0 - 2018-06-08
+===================
+
+- Support for python3.7.
+- Use asynctest for asynchronous tests.
+- Updated toolrack dependency.
+
+
+v1.3.0 - 2018-02-20
+===================
+
+- Support aperiodic queries, which are run at every request for the metrics
+  endpoint.
+
+
+v1.2.2 - 2017-10-25
+===================
+
+- Fix tests for latest prometheus_aioexporter.
+
+
+v1.2.1 - 2017-10-25
+===================
+
+- Documentation cleanups (and conversion to reST).
+
+
+v1.2.0 - 2017-06-30
+===================
+
+- Switch to SQLAlchemy. Multiple database engines are now supported.
+- Needed database libraries must now be installed separately, as there is no
+  explicit dependency in SQLAlchemy.
+
+
+v1.1.0 - 2017-05-21
+===================
+
+- Use connection pools for queries.
+
+
+v1.0.0 - 2017-05-13
+===================
+
+- Replace aiopg with asyncpg. The database dsn string is now specified as a
+  ``postgres://`` URI.
+
+
+v0.1.2 - 2017-05-07
+===================
+
+- Replace Makefile with tox.
+
+
+v0.1.1 - 2017-03-07
+===================
+
+- Fix setup.py issues.
+
+
+v0.1.0 - 2017-03-07
+===================
+
+- First release.
---
+++ packages/query-exporter/query-exporter-2.10.0.tar.gz/query-exporter-2.10.0/query_exporter/db.py
@@ -0,0 +1,430 @@
+"""Database wrapper."""
+
+import asyncio
+from collections.abc import Iterable
+from itertools import chain
+import logging
+import sys
+from time import perf_counter, time
+from traceback import format_tb
+from typing import (
+    Any,
+    NamedTuple,
+    cast,
+)
+
+from croniter import croniter
+from sqlalchemy import (
+    create_engine,
+    event,
+    text,
+)
+from sqlalchemy.exc import (
+    ArgumentError,
+    NoSuchModuleError,
+)
+from sqlalchemy_aio import ASYNCIO_STRATEGY
+from sqlalchemy_aio.asyncio import AsyncioEngine
+from sqlalchemy_aio.base import (
+    AsyncConnection,
+    AsyncResultProxy,
+)
+
+#: Timeout for a query
+QueryTimeout = int | float
+
+
+#: Label used to tag metrics by database
+DATABASE_LABEL = "database"
+
+
+class DataBaseError(Exception):
+    """A databease error.
+
+    if `fatal` is True, it means the Query will never succeed.
+    """
+
+    def __init__(self, message: str, fatal: bool = False):
+        super().__init__(message)
+        self.fatal = fatal
+
+
+class DataBaseConnectError(DataBaseError):
+    """Database connection error."""
+
+
+class DataBaseQueryError(DataBaseError):
+    """Database query error."""
+
+
+class QueryTimeoutExpired(Exception):
+    """Query execution timeout expired."""
+
+    def __init__(self, query_name: str, timeout: QueryTimeout):
+        super().__init__(
+            f'Execution for query "{query_name}" expired after {timeout} seconds'
+        )
+
+
+class InvalidResultCount(Exception):
+    """Number of results from a query don't match metrics count."""
+
+    def __init__(self, expected: int, got: int):
+        super().__init__(
+            f"Wrong result count from query: expected {expected}, got {got}"
+        )
+
+
+class InvalidResultColumnNames(Exception):
+    """Invalid column names in query results."""
+
+    def __init__(self, expected: list[str], got: list[str]) -> None:
+        super().__init__(
+            "Wrong column names from query: "
+            f"expected {self._names(expected)}, got {self._names(got)}"
+        )
+
+    def _names(self, names: list[str]) -> str:
+        names_list = ", ".join(names)
+        return f"({names_list})"
+
+
+class InvalidQueryParameters(Exception):
+    """Query parameter names don't match those in query SQL."""
+
+    def __init__(self, query_name: str):
+        super().__init__(
+            f'Parameters for query "{query_name}" don\'t match those from SQL'
+        )
+
+
+class InvalidQuerySchedule(Exception):
+    """Query schedule is wrong or both schedule and interval specified."""
+
+    def __init__(self, query_name: str, message: str):
+        super().__init__(
+            f'Invalid schedule for query "{query_name}": {message}'
+        )
+
+
+# database errors that mean the query won't ever succeed.  Not all possible
+# fatal errors are tracked here, because some DBAPI errors can happen in
+# circumstances which can be fatal or not.  Since there doesn't seem to be a
+# reliable way to know, there might be cases when a query will never succeed
+# but will end up being retried.
+FATAL_ERRORS = (InvalidResultCount, InvalidResultColumnNames)
+
+
+def create_db_engine(dsn: str, **kwargs) -> AsyncioEngine:
+    """Create the database engine, validating the DSN"""
+    try:
+        return create_engine(dsn, **kwargs)
+    except ImportError as error:
+        raise DataBaseError(f'module "{error.name}" not found')
+    except (ArgumentError, ValueError, NoSuchModuleError):
+        raise DataBaseError(f'Invalid database DSN: "{dsn}"')
+
+
+class QueryMetric(NamedTuple):
+    """Metric details for a Query."""
+
+    name: str
+    labels: Iterable[str]
+
+
+class QueryResults(NamedTuple):
+    """Results of a database query."""
+
+    keys: list[str]
+    rows: list[tuple]
+    timestamp: float | None = None
+    latency: float | None = None
+
+    @classmethod
+    async def from_results(cls, results: AsyncResultProxy):
+        """Return a QueryResults from results for a query."""
+        timestamp = time()
+        conn_info = results._result_proxy.connection.info
+        latency = conn_info.get("query_latency", None)
+        return cls(
+            await results.keys(),
+            await results.fetchall(),
+            timestamp=timestamp,
+            latency=latency,
+        )
+
+
+class MetricResult(NamedTuple):
+    """A result for a metric from a query."""
+
+    metric: str
+    value: Any
+    labels: dict[str, str]
+
+
+class MetricResults(NamedTuple):
+    """Collection of metric results for a query."""
+
+    results: list[MetricResult]
+    timestamp: float | None = None
+    latency: float | None = None
+
+
+class Query:
+    """Query definition and configuration."""
+
+    def __init__(
+        self,
+        name: str,
+        databases: list[str],
+        metrics: list[QueryMetric],
+        sql: str,
+        parameters: dict[str, Any] | None = None,
+        timeout: QueryTimeout | None = None,
+        interval: int | None = None,
+        schedule: str | None = None,
+        config_name: str | None = None,
+    ):
+        self.name = name
+        self.databases = databases
+        self.metrics = metrics
+        self.sql = sql
+        self.parameters = parameters or {}
+        self.timeout = timeout
+        self.interval = interval
+        self.schedule = schedule
+        self.config_name = config_name or name
+        self._check_schedule()
+        self._check_query_parameters()
+
+    @property
+    def timed(self) -> bool:
+        """Whether the query is run periodically via interval or schedule."""
+        return bool(self.interval or self.schedule)
+
+    def labels(self) -> frozenset[str]:
+        """Resturn all labels for metrics in the query."""
+        return frozenset(chain(*(metric.labels for metric in self.metrics)))
+
+    def results(self, query_results: QueryResults) -> MetricResults:
+        """Return MetricResults from a query."""
+        if not query_results.rows:
+            return MetricResults([])
+
+        result_keys = sorted(query_results.keys)
+        labels = self.labels()
+        metrics = [metric.name for metric in self.metrics]
+        expected_keys = sorted(set(metrics) | labels)
+        if len(expected_keys) != len(result_keys):
+            raise InvalidResultCount(len(expected_keys), len(result_keys))
+        if result_keys != expected_keys:
+            raise InvalidResultColumnNames(expected_keys, result_keys)
+        results = []
+        for row in query_results.rows:
+            values = dict(zip(query_results.keys, row))
+            for metric in self.metrics:
+                metric_result = MetricResult(
+                    metric.name,
+                    values[metric.name],
+                    {label: values[label] for label in metric.labels},
+                )
+                results.append(metric_result)
+
+        return MetricResults(
+            results,
+            timestamp=query_results.timestamp,
+            latency=query_results.latency,
+        )
+
+    def _check_schedule(self):
+        if self.interval and self.schedule:
+            raise InvalidQuerySchedule(
+                self.name, "both interval and schedule specified"
+            )
+        if self.schedule and not croniter.is_valid(self.schedule):
+            raise InvalidQuerySchedule(self.name, "invalid schedule format")
+
+    def _check_query_parameters(self):
+        expr = text(self.sql)
+        query_params = set(expr.compile().params)
+        if set(self.parameters) != query_params:
+            raise InvalidQueryParameters(self.name)
+
+
+class DataBase:
+    """A database to perform Queries."""
+
+    _engine: AsyncioEngine
+    _conn: AsyncConnection | None = None
+    _pending_queries: int = 0
+
+    def __init__(
+        self,
+        config,
+        logger: logging.Logger = logging.getLogger(),
+    ):
+        self.config = config
+        self.logger = logger
+        self._connect_lock = asyncio.Lock()
+        self._engine = create_db_engine(
+            self.config.dsn,
+            strategy=ASYNCIO_STRATEGY,
+            execution_options={"autocommit": self.config.autocommit},
+        )
+
+        self._setup_query_latency_tracking()
+
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.close()
+
+    @property
+    def connected(self) -> bool:
+        """Whether the database is connected."""
+        return self._conn is not None
+
+    async def connect(self):
+        """Connect to the database."""
+        async with self._connect_lock:
+            if self.connected:
+                return
+
+            try:
+                self._conn = await self._engine.connect()
+            except Exception as error:
+                raise self._db_error(error, exc_class=DataBaseConnectError)
+
+            self.logger.debug(f'connected to database "{self.config.name}"')
+            for sql in self.config.connect_sql:
+                try:
+                    await self.execute_sql(sql)
+                except Exception as error:
+                    await self._close()
+                    raise self._db_error(
+                        f'failed executing query "{sql}": {error}',
+                        exc_class=DataBaseQueryError,
+                    )
+
+    async def close(self):
+        """Close the database connection."""
+        async with self._connect_lock:
+            if not self.connected:
+                return
+            await self._close()
+
+    async def execute(self, query: Query) -> MetricResults:
+        """Execute a query."""
+        await self.connect()
+        self.logger.debug(
+            f'running query "{query.name}" on database "{self.config.name}"'
+        )
+        self._pending_queries += 1
+        self._conn: AsyncConnection
+        try:
+            result = await self._execute_query(query)
+            return query.results(await QueryResults.from_results(result))
+        except asyncio.TimeoutError:
+            raise self._query_timeout_error(
+                query.name, cast(QueryTimeout, query.timeout)
+            )
+        except Exception as error:
+            raise self._query_db_error(
+                query.name, error, fatal=isinstance(error, FATAL_ERRORS)
+            )
+        finally:
+            assert self._pending_queries >= 0, "pending queries is negative"
+            self._pending_queries -= 1
+            if not self.config.keep_connected and not self._pending_queries:
+                await self.close()
+
+    async def execute_sql(
+        self,
+        sql: str,
+        parameters: dict[str, Any] | None = None,
+        timeout: QueryTimeout | None = None,
+    ) -> AsyncResultProxy:
+        """Execute a raw SQL query."""
+        if parameters is None:
+            parameters = {}
+        self._conn: AsyncConnection
+        return await asyncio.wait_for(
+            self._conn.execute(text(sql), parameters),
+            timeout=timeout,
+        )
+
+    async def _execute_query(self, query: Query) -> AsyncResultProxy:
+        """Execute a query."""
+        return await self.execute_sql(
+            query.sql, parameters=query.parameters, timeout=query.timeout
+        )
+
+    async def _close(self):
+        # ensure the connection with the DB is actually closed
+        self._conn.sync_connection.detach()
+        await self._conn.close()
+        self._conn = None
+        self._pending_queries = 0
+        self.logger.debug(f'disconnected from database "{self.config.name}"')
+
+    def _setup_query_latency_tracking(self):
+        engine = self._engine.sync_engine
+
+        @event.listens_for(engine, "before_cursor_execute")
+        def before_cursor_execute(
+            conn, cursor, statement, parameters, context, executemany
+        ):
+            conn.info["query_start_time"] = perf_counter()
+
+        @event.listens_for(engine, "after_cursor_execute")
+        def after_cursor_execute(
+            conn, cursor, statement, parameters, context, executemany
+        ):
+            conn.info["query_latency"] = perf_counter() - conn.info.pop(
+                "query_start_time"
+            )
+
+    def _query_db_error(
+        self,
+        query_name: str,
+        error: str | Exception,
+        fatal: bool = False,
+    ) -> DataBaseError:
+        """Create and log a DataBaseError for a failed query."""
+        message = self._error_message(error)
+        self.logger.error(
+            f'query "{query_name}" on database "{self.config.name}" failed: '
+            + message
+        )
+        _, _, traceback = sys.exc_info()
+        self.logger.debug("".join(format_tb(traceback)))
+        return DataBaseQueryError(message, fatal=fatal)
+
+    def _query_timeout_error(
+        self, query_name: str, timeout: QueryTimeout
+    ) -> QueryTimeoutExpired:
+        error = QueryTimeoutExpired(query_name, timeout)
+        self.logger.warning(str(error))
+        raise error
+
+    def _db_error(
+        self,
+        error: str | Exception,
+        exc_class: type[DataBaseError] = DataBaseError,
+        fatal: bool = False,
+    ) -> DataBaseError:
+        """Create and log a DataBaseError."""
+        message = self._error_message(error)
+        self.logger.error(
+            f'error from database "{self.config.name}": {message}'
+        )
+        return exc_class(message, fatal=fatal)
+
+    def _error_message(self, error: str | Exception) -> str:
+        """Return a message from an error."""
+        message = str(error).strip()
+        if not message and isinstance(error, Exception):
+            message = error.__class__.__name__
+        return message
