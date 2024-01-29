6466ed4d63e768f42a687571ffddbad4c3046c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:21 +0000

    Add package grateful-logging/0.0.6/grateful_logging-0.0.6.tar.gz

---
+++ packages/grateful-logging/grateful_logging-0.0.6.tar.gz/grateful_logging-0.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+with open("VERSION", "r") as f:
+    VERSION = f.read().strip()
+
+
+DESCIPTION = (
+    "modern and easy-to-use logging configuration library "
+    "inspired by the tutorial by mCoding"
+)
+
+
+with open("requirements.txt", "r") as requirements_file:
+    REQUIREMENTS = requirements_file.readlines()
+
+
+setup(
+    name="grateful_logging",
+    version=VERSION,
+    author="axdjuraev",
+    author_email="<axdjuraev@gmail.com>",
+    include_package_data=True,
+    description=DESCIPTION,
+    packages=[*find_packages(), "grateful_logging/assets"],
+    install_requires=REQUIREMENTS,
+)
