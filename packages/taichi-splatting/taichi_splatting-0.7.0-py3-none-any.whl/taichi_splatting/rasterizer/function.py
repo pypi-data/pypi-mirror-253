6c9ea100316c253534c9086406a640f2547f8a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:12 +0000

    Add package taichi-splatting/0.7.0/taichi_splatting-0.7.0-py3-none-any.whl

---
+++ packages/taichi-splatting/taichi_splatting-0.7.0-py3-none-any.whl/taichi_splatting/rasterizer/function.py
@@ -0,0 +1,141 @@
+
+from functools import cache
+from taichi_splatting.misc.autograd import restore_grad
+from taichi_splatting.mapper.tile_mapper import map_to_tiles
+# from taichi_splatting.mapper.segmented_tile_mapper import map_to_tiles
+
+
+from .forward import RasterConfig, forward_kernel
+from .backward import backward_kernel
+
+from numbers import Integral
+from beartype.typing import Tuple
+
+import torch
+from beartype import beartype
+
+@cache
+def render_function(config:RasterConfig,
+                    points_requires_grad:bool,
+                    features_requires_grad:bool, 
+                    feature_size:int):
+  
+    
+  forward = forward_kernel(config, feature_size=feature_size)
+  backward = backward_kernel(config, points_requires_grad,
+                             features_requires_grad, feature_size)
+
+  class _module_function(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, gaussians: torch.Tensor, features: torch.Tensor,
+                overlap_to_point: torch.Tensor, tile_overlap_ranges: torch.Tensor,
+                image_size: Tuple[Integral, Integral]
+                ) -> torch.Tensor:
+        
+      shape = (image_size[1], image_size[0])
+      image_feature = torch.empty((*shape, features.shape[1]),
+                                  dtype=torch.float32, device=features.device)
+      image_alpha = torch.empty(shape, dtype=torch.float32, device=features.device)
+      image_last_valid = torch.empty(shape, dtype=torch.int32, device=features.device)
+
+      forward(gaussians, features, 
+        tile_overlap_ranges, overlap_to_point,
+        image_feature, image_alpha, image_last_valid)
+
+      # Non differentiable parameters
+      ctx.overlap_to_point = overlap_to_point
+      ctx.tile_overlap_ranges = tile_overlap_ranges
+      ctx.image_last_valid = image_last_valid
+      ctx.image_alpha = image_alpha
+      ctx.image_size = image_size
+
+      ctx.mark_non_differentiable(image_alpha, image_last_valid)
+      ctx.save_for_backward(gaussians, features, image_feature)
+            
+      return image_feature, image_alpha
+
+    @staticmethod
+    def backward(ctx, grad_image_feature:torch.Tensor, grad_alpha:torch.Tensor):
+        gaussians, features, image_feature = ctx.saved_tensors
+
+        grad_gaussians = torch.zeros_like(gaussians)
+        grad_features = torch.zeros_like(features)
+  
+        with restore_grad(grad_gaussians, grad_features):
+
+          backward(gaussians, features, 
+            ctx.tile_overlap_ranges, ctx.overlap_to_point,
+            image_feature, ctx.image_alpha, ctx.image_last_valid,
+            grad_image_feature.contiguous(),
+            grad_gaussians, grad_features)
+
+          return grad_gaussians, grad_features, None, None, None, None
+  return _module_function
+
+
+
+
+
+@beartype
+def rasterize_with_tiles(gaussians2d: torch.Tensor, features: torch.Tensor,
+              overlap_to_point: torch.Tensor, tile_overlap_ranges: torch.Tensor,
+              image_size: Tuple[Integral, Integral], config: RasterConfig
+              ) -> Tuple[torch.Tensor, torch.Tensor]:
+  """
+  Rasterize an image given 2d gaussians, features and tile overlap information.
+  Consider using rasterize instead to also compute tile overlap information.
+
+  Parameters:
+      gaussians2d: (N, 6)  packed gaussians, N is the number of gaussians
+      features: (N, F)   features, F is the number of features
+
+      tile_overlap_ranges: (TH * TW, 2) M is the number of tiles, 
+        maps tile index to range of overlap indices (0..K]
+      overlap_to_point: (K, )  K is the number of overlaps, 
+        maps overlap index to point index (0..N]
+
+      image_size: (2, ) tuple of ints, (width, height)
+      config: Config - configuration parameters for rasterization
+
+    Returns:
+      image: (H, W, F) torch tensor, where H, W are the image height and width, F is the number of features
+      alpha: (H, W) torch tensor, where H, W are the image height and width
+  """
+  _module_function = render_function(config, gaussians2d.requires_grad,
+                                      features.requires_grad, features.shape[1])
+
+  return _module_function.apply(gaussians2d, features, 
+          overlap_to_point, tile_overlap_ranges, 
+          image_size)
+
+
+def rasterize(gaussians2d:torch.Tensor, encoded_depths:torch.Tensor, 
+                          features:torch.Tensor, image_size:Tuple[Integral, Integral],
+                          config:RasterConfig):
+    
+    
+  """
+  Rasterize an image given 2d gaussians, features. 
+
+  Parameters:
+      gaussians2d: (N, 6)  packed gaussians, N is the number of gaussians
+      encoded_depths: (N )  encoded depths, N is the number of gaussians
+      features: (N, F)   features, F is the number of features
+
+      image_size: (2, ) tuple of ints, (width, height)
+      config: Config - configuration parameters for rasterization
+
+    Returns:
+      image: (H, W, F) torch tensor, where H, W are the image height and width, F is the number of features
+      alpha: (H, W) torch tensor, where H, W are the image height and width
+  """
+
+  # render with padding to tile_size, later crop back to original size
+  overlap_to_point, tile_overlap_ranges = map_to_tiles(gaussians2d, encoded_depths, 
+    image_size=image_size, config=config)
+  
+  image, alpha = rasterize_with_tiles(gaussians2d, features, 
+    tile_overlap_ranges=tile_overlap_ranges.view(-1, 2), overlap_to_point=overlap_to_point,
+    image_size=image_size, config=config)
+
+  return image, alpha 
---
+++ packages/taichi-splatting/taichi_splatting-0.7.0-py3-none-any.whl/taichi_splatting/scripts/parameter_class.py
@@ -0,0 +1,107 @@
+import copy
+from functools import cached_property
+from beartype.typing import Callable
+import torch.optim as optim
+import torch
+
+
+
+class ParameterClass():
+  """
+  Maintains a group of mixed parameters/non parameters in a TensorClass,
+  as well as optimizer state synchronized with the parameters.
+
+  Note - call ParameterClass.create to create a ParameterClass instead 
+  of the constructor directly.
+
+  Supports filtering with python indexing and appending parameters.
+
+  Parameters:
+    tensors: TensorClass
+    param_groups: list of parameter dicts, see torch.optim for details 
+    param_state: dict of optimizer state to insert into the optimizer
+  """
+
+  def __init__(self, tensors, param_groups, param_state=None):
+    self.tensors = tensors
+    self.optimizer = optim.Adam(param_groups, fused=True)
+
+    if param_state is not None:
+      for k, v in param_state.items():
+        self.optimizer.state[self.tensors[k]] = v
+
+
+  @staticmethod
+  def create(tensors, learning_rates, base_lr=1.0):
+    param_dict = as_parameters(tensors, learning_rates.keys())
+    param_groups = [
+      dict(params=[param_dict[name]], lr=lr * base_lr, name=name)
+        for name, lr in learning_rates.items()
+    ]
+
+    return ParameterClass(tensors, param_groups)
+
+
+  def zero_grad(self):
+    self.optimizer.zero_grad()
+
+
+  def step(self):
+    self.optimizer.step()
+
+
+  def __getattr__(self, name):
+    return getattr(self.tensors, name)
+
+  @cached_property
+  def _optimized_keys(self):
+    return {group["name"] for group in self.optimizer.param_groups}
+    
+  def _updated_state(self, f:Callable, state_keys = ("exp_avg", "exp_avg_sq")):
+
+    def modify_state(state):
+      return {k : f(v) if k in state_keys else state[k]
+                for k, v in state.items()}
+
+    return {k:modify_state(self.optimizer.state[param])
+              for k, param in self.tensors.items()
+                if param in self.optimizer.state}
+  
+
+  def _updated_parameters(self, tensors):
+    tensors = as_parameters(tensors, self.optimized_keys)
+    updated_groups = [ replace_dict(group, params=[tensors[group["name"]]])
+      for group in self.optimizer.param_groups]
+    
+    return tensors, updated_groups
+
+  def __index__(self, idx):
+    tensors, updated_groups = self.updated_parameters(self.tensors[idx])
+    state = self._updated_state(lambda x: x[idx])
+    return ParameterClass(tensors, updated_groups, state)
+  
+  def append(self, tensors):
+    n = tensors.batch_size[0]
+
+    tensors, updated_groups = self._updated_parameters(torch.cat([self.tensors, tensors]))
+    state = self._updated_state(lambda x: torch.cat(
+      [x, x.new_zeros(n)])
+    )
+    
+    return ParameterClass(tensors, updated_groups, state)
+
+
+
+def as_parameters(tensors, keys):
+    param_dict = {k: torch.nn.Parameter(x, requires_grad=True) 
+                        for k, x in tensors.items()
+                        if k in keys}
+
+    cls = type(tensors)
+    return cls.from_dict(param_dict) 
+
+
+def replace_dict(d, **kwargs):
+  d = copy(d)
+  d.update(kwargs)
+  return d
