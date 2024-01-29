116d5e70b2f82c39696f6c087468d7c9599985bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:05:02 +0000

    Add package langadaptprompt/2.22/langadaptprompt-2.22-py3-none-any.whl

---
+++ packages/langadaptprompt/langadaptprompt-2.22-py3-none-any.whl/langadaptprompt/constants.py
@@ -0,0 +1,12 @@
+PACK_ORDERING = {'Alpha': 0,
+                 'Beta': 1,
+                 'Delta': 2,
+                 'Epsilon': 3,
+                 'Eta': 4,
+                 'Gamma': 5,
+                 'Iota': 6,
+                 'Kappa': 7,
+                 'Lambda': 8,
+                 'Theta': 9,
+                 'Zeta': 10}
+
