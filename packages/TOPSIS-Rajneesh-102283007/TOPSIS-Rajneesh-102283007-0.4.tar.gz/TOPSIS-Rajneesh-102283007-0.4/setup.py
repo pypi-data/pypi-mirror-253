a66e9f711a15dad108d7ab778aec1bfe375c118a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:12 +0000

    Add package TOPSIS-Rajneesh-102283007/0.4/TOPSIS-Rajneesh-102283007-0.4.tar.gz

---
+++ packages/TOPSIS-Rajneesh-102283007/TOPSIS-Rajneesh-102283007-0.4.tar.gz/TOPSIS-Rajneesh-102283007-0.4/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+def readme():
+    with open('README.md') as file:
+        README = file.read()
+    return README
+
+setuptools.setup(
+    name="TOPSIS-Rajneesh-102283007",
+    version="0.4",
+    description="A Python package for implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Rajneesh Bansal",
+    author_email="rajneeshb231@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    packages=["TOPSIS-Rajneesh-102283007"],
+    include_package_data=True,
+    install_requires=[                      'numpy',
+                      'pandas',
+     ],
+     entry_points={
+        "console_scripts": [
+            "topsis=src.topsis:main",
+        ]
+     },
+)
