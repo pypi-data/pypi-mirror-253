d6815c59fcbc6e2d8da75392f8f95e51bba2ba9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:46 +0000

    Add package mhy-api/0.0.3/mhy_api-0.0.3-py3-none-any.whl

---
+++ packages/mhy-api/mhy_api-0.0.3-py3-none-any.whl/mhy_api/test.py
@@ -0,0 +1,10 @@
+import genshin
+import bbs
+
+#c=bbs.login('15036350290','yuchu2580ONE')
+#a=genshin("_MHYUUID=299cb2ee-5df9-48f5-8d2e-87353c7bf766; _MHYUUID=299cb2ee-5df9-48f5-8d2e-87353c7bf766; DEVICEFP_SEED_ID=9ce50c191ae773bd; DEVICEFP_SEED_TIME=1706275827978; DEVICEFP=38d7f563bca8a; _ga=GA1.1.685214197.1706275832; MIHOYO_LOGIN_PLATFORM_LIFECYCLE_ID=0e54ba2ab1; MIHOYO_LOGIN_PLATFORM_SWITCH_STATUS={%22bll8iq97cem8%22:{%22pwd_login_tab%22:true%2C%22password_reset_entry%22:true%2C%22qr_login%22:true%2C%22sms_login_tab%22:true}}; account_mid_v2=0g8kucqt6i_mhy; account_id_v2=261455194; cookie_token=awS61pfWnDEpJxzEmtMCylt8rqCPaHOwtkVIfn28; account_id=261455194; ltmid_v2=0g8kucqt6i_mhy; ltuid_v2=261455194; ltoken=EAwxNscAhfWUFAnYcESd4gmFrd1RNp6b6zW73jMF; ltuid=261455194; _ga_KS4J8TXSHQ=GS1.1.1706361148.5.1.1706363497.0.0.0;stoken=jQuRRLYwQJ1fBf0nSLUcB6Gd0dwjhXGEHp1Y7MGZ;mid=0g8kucqt6i_mhy",261455194)
+b=bbs.game_data("_MHYUUID=299cb2ee-5df9-48f5-8d2e-87353c7bf766; _MHYUUID=299cb2ee-5df9-48f5-8d2e-87353c7bf766; DEVICEFP_SEED_ID=9ce50c191ae773bd; DEVICEFP_SEED_TIME=1706275827978; DEVICEFP=38d7f563bca8a; _ga=GA1.1.685214197.1706275832; MIHOYO_LOGIN_PLATFORM_LIFECYCLE_ID=0e54ba2ab1; MIHOYO_LOGIN_PLATFORM_SWITCH_STATUS={%22bll8iq97cem8%22:{%22pwd_login_tab%22:true%2C%22password_reset_entry%22:true%2C%22qr_login%22:true%2C%22sms_login_tab%22:true}}; account_mid_v2=0g8kucqt6i_mhy; account_id_v2=261455194; cookie_token=awS61pfWnDEpJxzEmtMCylt8rqCPaHOwtkVIfn28; account_id=261455194; ltmid_v2=0g8kucqt6i_mhy; ltuid_v2=261455194; ltoken=EAwxNscAhfWUFAnYcESd4gmFrd1RNp6b6zW73jMF; ltuid=261455194; _ga_KS4J8TXSHQ=GS1.1.1706361148.5.1.1706363497.0.0.0;stoken=jQuRRLYwQJ1fBf0nSLUcB6Gd0dwjhXGEHp1Y7MGZ;mid=0g8kucqt6i_mhy")
+
+#print(c)
+print(b)
+
