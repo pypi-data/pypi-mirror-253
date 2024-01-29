8d87b5d0f8c242b4b28caf334d969cdb01fb24eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:43 +0000

    Add package mesc/0.2.0/mesc-0.2.0-py3-none-any.whl

---
+++ packages/mesc/mesc-0.2.0-py3-none-any.whl/mesc/__init__.py
@@ -0,0 +1,27 @@
+"""MESC is the Multiple Endpoint Shared Configuration Standard"""
+
+from .interface import (
+    is_mesc_enabled,
+    get_default_endpoint,
+    get_endpoint_by_name,
+    get_endpoint_by_network,
+    get_endpoint_by_query,
+    find_endpoints,
+    get_global_metadata,
+)
+from .types import Endpoint, Profile, RpcConfig
+
+__version__ = '0.2.0'
+
+__all__ = (
+    'Endpoint',
+    'Profile',
+    'RpcConfig',
+    'is_mesc_enabled',
+    'get_default_endpoint',
+    'get_endpoint_by_name',
+    'get_endpoint_by_network',
+    'get_endpoint_by_query',
+    'find_endpoints',
+    'get_global_metadata',
+)
