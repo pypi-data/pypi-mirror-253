b8c482e54a3f35a597df96fed741a6290be67213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:42 +0000

    Add package puyuan-modelhub/1.1.4/puyuan_modelhub-1.1.4.tar.gz

---
+++ packages/puyuan-modelhub/puyuan_modelhub-1.1.4.tar.gz/puyuan_modelhub-1.1.4/modelhub/common/types/encoder.py
@@ -0,0 +1,18 @@
+from typing_extensions import TypedDict, NotRequired
+from typing import Any, List
+from .generation import BaseOutput
+
+
+class HfCrossEncoderParams(TypedDict):
+    batch_size: NotRequired[int]
+    show_progress_bar: NotRequired[bool]
+    num_workers: NotRequired[int]
+    activation_fct: NotRequired[Any]
+    apply_softmax: NotRequired[bool]
+    convert_to_numpy: NotRequired[bool]
+    convert_to_tensor: NotRequired[bool]
+
+class CrossEncoderOutput(BaseOutput):
+    scores: List[float]
+
+CrossEncoderParams = HfCrossEncoderParams
