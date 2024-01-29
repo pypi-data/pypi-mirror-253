44fad978b763579164f6e1fd6170f495597c514b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:05:02 +0000

    Add package aiographite/0.2.0/aiographite-0.2.0.tar.gz

---
+++ packages/aiographite/aiographite-0.2.0.tar.gz/aiographite-0.2.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+README_PATH = os.path.join(base, "README.rst")
+
+install_requires = []
+
+tests_require = []
+
+setup(name='aiographite',
+      version='0.2.0',
+      description='',
+      long_description=open(README_PATH).read(),
+      author='Yun Xu',
+      author_email='yunx@zillowgroup.com',
+      url='',
+      packages=find_packages(),
+      install_requires=install_requires,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Topic :: System :: Software Distribution',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+      ]
+)
