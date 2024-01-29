1fc1cb9ed1594509426b3240bc5c2a335cb76096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 16:04:00 +0000

    Add package simpleloggy/1.0/simpleloggy-1.0.tar.gz

---
+++ packages/simpleloggy/simpleloggy-1.0.tar.gz/simpleloggy-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='simpleloggy',
+      version='1.0',
+      description='Simple logger for your Python program.',
+      packages=['simpleloggy'],
+      author_email='holinimofficial@outlook.com',
+      zip_safe=False)
