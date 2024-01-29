04497b7c448805cf1c870c89f9cf3115e02e0ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:05:01 +0000

    Add package InnoFileManager/0.1/InnoFileManager-0.1.tar.gz

---
+++ packages/InnoFileManager/InnoFileManager-0.1.tar.gz/InnoFileManager-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(\
+    name='InnoFileManager',
+
+    version='0.1',
+
+    url='https://github.com/AmZakirov/FileManager',
+
+    license='',
+
+    author='AmZakirov',
+
+    author_email='am.zakirov@innopolis.university',
+
+    description='Easy way to manage files',
+
+    packages=find_packages(exclude=['tests']),
+
+    long_description=open('README.md').read(),
+
+    zip_safe=False)
