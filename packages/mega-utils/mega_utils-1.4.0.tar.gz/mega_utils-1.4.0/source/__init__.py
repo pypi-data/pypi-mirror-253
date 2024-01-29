ff37bcdc2a66764197062d87107c0896bcb56a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 05:04:21 +0000

    Add package mega-utils/1.4.0/mega_utils-1.4.0.tar.gz

---
+++ packages/mega-utils/mega_utils-1.4.0.tar.gz/mega_utils-1.4.0/source/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module defines some utility functions for working with the Mega cloud
+storage service.
+"""
+
+# Local imports.
+from .utils import mega_log_in, mega_log_out, mega_sync, mega_get, mega_get_dir
