96c0c119b46a8d0ca8a745f08995e08b040f6e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:34 +0000

    Add package dict-collection/1.0.3/dict_collection-1.0.3.tar.gz

---
+++ packages/dict-collection/dict_collection-1.0.3.tar.gz/dict_collection-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.3'
+DESCRIPTION = 'Utility data structure'
+LONG_DESCRIPTION = 'Utility data structure, which extends built-in `dict` data structure.'
+
+# Setting up
+setup(
+    name='dict_collection',
+    version=VERSION,
+    author='spwn02',
+    author_email='spwn.contact@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['dict_collection', 'dict', 'collection', 'dict', 'spwn02'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
