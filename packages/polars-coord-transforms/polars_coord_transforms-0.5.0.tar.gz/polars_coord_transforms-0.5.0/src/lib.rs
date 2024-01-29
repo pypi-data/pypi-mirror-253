248bf5206e0b723e5e657903179a1a7bfae6120d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:00 +0000

    Add package polars-coord-transforms/0.5.0/polars_coord_transforms-0.5.0.tar.gz

---
+++ packages/polars-coord-transforms/polars_coord_transforms-0.5.0.tar.gz/polars_coord_transforms-0.5.0/src/lib.rs
@@ -0,0 +1,11 @@
+mod s2_functions;
+mod coord_transforms;
+mod distance;
+mod expressions;
+
+#[cfg(target_os = "linux")]
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+#[cfg(target_os = "linux")]
+static ALLOC: Jemalloc = Jemalloc; 
