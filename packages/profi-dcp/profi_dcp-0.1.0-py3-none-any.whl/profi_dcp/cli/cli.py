c3ceca2460dc1e3cb4ab415a109c2163c530fc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:32 +0000

    Add package profi-dcp/0.1.0/profi_dcp-0.1.0-py3-none-any.whl

---
+++ packages/profi-dcp/profi_dcp-0.1.0-py3-none-any.whl/profi_dcp/cli/cli.py
@@ -0,0 +1,39 @@
+"""CLI Tool that distributes subcommands"""
+
+import argparse
+import logging
+from profi_dcp.cli.set_ip import add_set_ip_parser
+from profi_dcp.utils.logging import Logging
+
+
+def main():
+    """Parses command line arguments and calls corresponding subcommand program."""
+    # Bus agnostic options
+    parser = argparse.ArgumentParser()
+
+    # Bus specific options
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="increase output verbosity"
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="subcommand",
+        required=True,
+        title="subcommands",
+        help="Subcommand that should be called",
+    )
+
+    # Options for position
+    add_set_ip_parser(subparsers)
+
+    args = parser.parse_args()
+    if args.verbose:
+        Logging(logging.DEBUG)
+    else:
+        Logging(logging.WARNING)
+
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
