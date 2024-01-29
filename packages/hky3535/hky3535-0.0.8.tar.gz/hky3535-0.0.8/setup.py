ee849e1fb331dc708aa4ab2d79b59566aa7e260c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:25 +0000

    Add package hky3535/0.0.8/hky3535-0.0.8.tar.gz

---
+++ packages/hky3535/hky3535-0.0.8.tar.gz/hky3535-0.0.8/setup.py
@@ -0,0 +1,25 @@
+"""hekaiyue 何恺悦 2024-01-26"""
+import os 
+from setuptools import setup, find_packages
+
+setup(
+	name = "hky3535",
+	version = "0.0.8",
+    author ="hky3535",
+    author_email = "hky3535@163.com",
+	url = 'https://github.com/hky3535/hky3535.git',
+    long_description_content_type="text/markdown",
+	long_description = open('README.md',encoding="utf-8").read(),
+    install_requires=[
+        "opencv-python", 
+        "numpy", 
+        "pillow"
+    ], 
+	packages = find_packages(),
+ 	license = 'Apache',
+   	classifiers = [
+       'License :: OSI Approved :: Apache Software License'
+    ],
+    package_data={'': ['*.ttf']}, 
+    include_package_data=True
+)
