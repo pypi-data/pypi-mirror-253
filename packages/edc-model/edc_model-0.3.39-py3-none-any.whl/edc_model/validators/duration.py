624b5ac82b5926aef0b19312641906563bd84485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:43 +0000

    Add package edc-model/0.3.39/edc_model-0.3.39-py3-none-any.whl

---
+++ packages/edc-model/edc_model-0.3.39-py3-none-any.whl/edc_model/validators/duration.py
@@ -0,0 +1,29 @@
+from django.core.validators import RegexValidator
+
+from ..utils import dh_pattern, ymd_pattern
+
+# expect 1h20m, 11h5m, etc
+dh_validator = RegexValidator(
+    dh_pattern,
+    message=(
+        "Invalid format. Expected combinations of days and hours (dh): "
+        "Something like 3d2h, 7d, 12h, etc. No spaces allowed."
+    ),
+)
+
+hm_validator = RegexValidator(
+    r"^([0-9]{1,3}h([0-5]?[0-9]m)?)$",
+    message="Invalid format. Expected something like 1h20m, 11h5m, etc",
+)
+
+ymd_validator = RegexValidator(
+    ymd_pattern,
+    message=(
+        "Invalid format. Expected combinations of years and months (ym): "
+        "4y, 3y5m, 1y0m, 6m or days (d): 7d, 0d.  No spaces allowed."
+    ),
+)
+
+hm_validator2 = RegexValidator(
+    r"^([0-9]{1,3}:[0-5][0-9])$", message="Enter a valid time in hour:minutes format"
+)
