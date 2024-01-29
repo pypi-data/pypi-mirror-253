4ffa65f0a1fbf62f1b3b7e308263bbc99b19f22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:14 +0000

    Add package python-lottery/0.4/python-lottery-0.4.tar.gz

---
+++ packages/python-lottery/python-lottery-0.4.tar.gz/python-lottery-0.4/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : nickdecodes
+@Email   : nickdecodes@163.com
+@Usage   :
+@FileName: setup.py
+@DateTime: 2024/1/28 18:50
+@SoftWare: 
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-lottery',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[
+        'selenium>=4.11.2',
+        'webdriver_manager>=4.0.0',
+        'pandas>=2.0.3',
+        'numpy>=1.24.3',
+        'statsmodels>=0.14.0',
+        'Pillow>=9.5.0',
+        'scikit-learn>=1.3.2',
+        'pmdarima>=2.0.4',
+        'twine'
+    ],
+)
---
+++ packages/python-lottery/python-lottery-0.4.tar.gz/python-lottery-0.4/tests/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : zhengdongqi
+@Email   : nickdecodes@163.com
+@Usage   :
+@FileName: test.py
+@DateTime: 2024/1/28 20:13
+@SoftWare: 
+"""
