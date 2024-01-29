68ade3a1dac61bee1a48ca6dacbf884c84ac5129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:36 +0000

    Add package distribution-function/0.1/distribution_function-0.1.tar.gz

---
+++ packages/distribution-function/distribution_function-0.1.tar.gz/distribution_function-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'distribution_function',
+      version = '0.1',
+      description = 'Gaussian distributions',
+      packages = ['distribution_function'],
+      author = 'Dawit Teklay',
+      author_email = 'dandebrhan2@gmail.com',
+      zip_safe=False)
