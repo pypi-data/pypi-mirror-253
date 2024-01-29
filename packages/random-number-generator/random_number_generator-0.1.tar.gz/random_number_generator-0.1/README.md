69532b65eb29a7bfcbc0e1575de4cc0c8fdcb0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:33 +0000

    Add package random-number-generator/0.1/random_number_generator-0.1.tar.gz

---
+++ packages/random-number-generator/random_number_generator-0.1.tar.gz/random_number_generator-0.1/README.md
@@ -0,0 +1,10 @@
+# Random Number Generator in Setsbharat
+
+This package provides a random number generator that generates random numbers in setsbharat.
+
+## Installation
+
+You can install this package using pip:
+
+```bash
+pip install random_number_generator
---
+++ packages/random-number-generator/random_number_generator-0.1.tar.gz/random_number_generator-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="random_number_generator",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Abhishek Kumar",
+    author_email="abhikumar30122001@gmail.com",
+    description="A random number generator in setsbharat",
+    long_description="A random number generator that generates random numbers in setsbharat.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
