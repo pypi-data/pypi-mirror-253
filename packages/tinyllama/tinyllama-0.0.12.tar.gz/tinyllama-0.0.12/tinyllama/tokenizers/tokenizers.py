67569141abbfbc9f17df00a56f5daf1727f24029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 05:04:16 +0000

    Add package tinyllama/0.0.12/tinyllama-0.0.12.tar.gz

---
+++ packages/tinyllama/tinyllama-0.0.12.tar.gz/tinyllama-0.0.12/tinyllama/tokenizers/tokenizers.py
@@ -0,0 +1,19 @@
+import torch
+from torch import Tensor
+
+
+class CharacterTokenizer:
+    def __init__(self):
+        self.vocab = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !?.,:;'\"\nʼ"
+        self.encode = {char: tok for tok, char in enumerate(self.vocab)}
+        self.decode = {tok: char for tok, char in enumerate(self.encode)}
+
+    def tokenize(self, string: str):
+        return torch.tensor([self.encode[i] for i in string], dtype=torch.long)
+
+    def untokenize(self, tokens: Tensor):
+        return "".join([self.decode[i] for i in tokens.tolist()])
+
+    def add_eos_tokens(self, eos_token: str = "|"):
+        self.eos_token = eos_token
+        self.vocab += eos_token
