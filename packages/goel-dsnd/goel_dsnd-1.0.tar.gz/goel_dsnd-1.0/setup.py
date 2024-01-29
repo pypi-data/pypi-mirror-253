1637cea613f2712da6c08470271b7d1486eb2f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 16:04:00 +0000

    Add package goel-dsnd/1.0/goel_dsnd-1.0.tar.gz

---
+++ packages/goel-dsnd/goel_dsnd-1.0.tar.gz/goel_dsnd-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='goel_dsnd',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['goel_dsnd'],
+      author = 'Test Test',
+      zip_safe=False)
