680295efd242c9f2679cc4fbed419670fd5e5f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:36 +0000

    Add package dataclass-mage/0.24.1/dataclass_mage-0.24.1-py3-none-any.whl

---
+++ packages/dataclass-mage/dataclass_mage-0.24.1-py3-none-any.whl/dataclass_mage/__version__.py
@@ -0,0 +1,16 @@
+"""
+Dataclass Mage - a set of wizarding tools for interacting with `dataclasses`
+"""
+
+__title__ = ""
+__description__ = (
+    "Marshal dataclasses to/from JSON. Use field properties "
+    "with initial values. Construct a dataclass schema with "
+    "JSON input."
+)
+__url__ = "https://github.com/Frequency0/dataclass-wizard"
+__version__ = "0.24.1"
+__author__ = "Steven Hogue"
+__author_email__ = ""
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2024 Steven Hogue"
