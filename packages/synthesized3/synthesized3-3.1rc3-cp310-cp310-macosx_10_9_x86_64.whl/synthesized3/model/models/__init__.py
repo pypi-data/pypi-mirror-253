773baf8b6a00b70eeeb937dbbb81bc052b65a5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:13 +0000

    Add package synthesized3/3.1rc3/synthesized3-3.1rc3-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/synthesized3/synthesized3-3.1rc3-cp310-cp310-macosx_10_9_x86_64.whl/synthesized3/model/models/__init__.py
@@ -0,0 +1,14 @@
+"""Synthesized models."""
+from .base_models import (
+    DeepTableModelSchema,
+    EnumerationModelSchema,
+    ModelSchema,
+    SamplingModelSchema,
+)
+
+__all__ = [
+    "DeepTableModelSchema",
+    "SamplingModelSchema",
+    "EnumerationModelSchema",
+    "ModelSchema",
+]
