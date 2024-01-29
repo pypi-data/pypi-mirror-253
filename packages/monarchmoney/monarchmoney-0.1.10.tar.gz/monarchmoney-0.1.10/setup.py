2914ee74c42e6c59ba50766dd3f015d464cc2818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 05:04:04 +0000

    Add package monarchmoney/0.1.10/monarchmoney-0.1.10.tar.gz

---
+++ packages/monarchmoney/monarchmoney-0.1.10.tar.gz/monarchmoney-0.1.10/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+install_requires = ["aiohttp>=3.8.4", "gql>=3.4", "oathtool>=2.3.1"]
+
+setup(
+    name="monarchmoney",
+    version="0.1.10",
+    description="Monarch Money API for Python",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hammem/monarchmoney",
+    author="hammem",
+    author_email="hammem@users.noreply.github.com",
+    license="MIT",
+    keywords="monarch money, financial, money, personal finance",
+    install_requires=install_requires,
+    packages=["monarchmoney"],
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Office/Business :: Financial",
+    ],
+)
