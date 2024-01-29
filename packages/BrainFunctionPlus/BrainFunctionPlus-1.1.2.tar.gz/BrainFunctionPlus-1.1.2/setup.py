85f45890ce5bfd4f361ee22788a88b1485ff31bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:34 +0000

    Add package BrainFunctionPlus/1.1.2/BrainFunctionPlus-1.1.2.tar.gz

---
+++ packages/BrainFunctionPlus/BrainFunctionPlus-1.1.2.tar.gz/BrainFunctionPlus-1.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='BrainFunctionPlus',
+version='1.1.2',
+author='CreateSource',
+author_email='abnormalhare822@gmail.com',
+description='BrainFunction++ is an addon and fixup of BrainFunction, which is based on BrainFuck.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
