4ed4984c62f368faf08718f837ba3a50604da486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:30 +0000

    Add package typelog/0.2.3/typelog-0.2.3.tar.gz

---
+++ packages/typelog/typelog-0.2.3.tar.gz/typelog-0.2.3/typelog/types.py
@@ -0,0 +1,11 @@
+from typing import Any, Callable, Dict
+
+Serialazable = Any
+
+LogAttrs = Dict[str, Serialazable]
+LogType = Callable[[LogAttrs], None]
+
+
+LibName = str
+LogLevel = int
+RootLogLevel = LogLevel
