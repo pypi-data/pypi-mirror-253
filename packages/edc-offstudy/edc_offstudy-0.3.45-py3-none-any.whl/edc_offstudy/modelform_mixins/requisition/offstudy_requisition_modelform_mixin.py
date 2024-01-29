2d9b8c1d96a8cf7acedb37e569aba1b3538db236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:41 +0000

    Add package edc-offstudy/0.3.45/edc_offstudy-0.3.45-py3-none-any.whl

---
+++ packages/edc-offstudy/edc_offstudy-0.3.45-py3-none-any.whl/edc_offstudy/modelform_mixins/requisition/offstudy_requisition_modelform_mixin.py
@@ -0,0 +1,8 @@
+from ..crf import OffstudyCrfModelFormMixin
+
+
+class OffstudyRequisitionModelFormMixin(OffstudyCrfModelFormMixin):
+
+    """ModelForm mixin for Requisition Models."""
+
+    report_datetime_field_attr = "requisition_datetime"
