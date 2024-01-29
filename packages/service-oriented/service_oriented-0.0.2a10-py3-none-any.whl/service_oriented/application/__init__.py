089cefc72a30531af4fc5754720504fc0c01f04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:10 +0000

    Add package service-oriented/0.0.2a10/service_oriented-0.0.2a10-py3-none-any.whl

---
+++ packages/service-oriented/service_oriented-0.0.2a10-py3-none-any.whl/service_oriented/application/__init__.py
@@ -0,0 +1,8 @@
+from service_oriented.application.base_application import BaseApplication
+from service_oriented.application.entry_point_spec import EntryPointSpec
+
+
+__all__ = [
+    "BaseApplication",
+    "EntryPointSpec",
+]
