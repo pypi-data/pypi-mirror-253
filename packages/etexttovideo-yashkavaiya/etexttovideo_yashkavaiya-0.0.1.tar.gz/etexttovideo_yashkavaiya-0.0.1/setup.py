a5eb5abbcf8641f94885c16fe7173a334e70c612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:40 +0000

    Add package etexttovideo-yashkavaiya/0.0.1/etexttovideo_yashkavaiya-0.0.1.tar.gz

---
+++ packages/etexttovideo-yashkavaiya/etexttovideo_yashkavaiya-0.0.1.tar.gz/etexttovideo_yashkavaiya-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your-package-name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'Pillow',
+        'textwrap3',
+        'gtts',
+        'moviepy',
+    ],
+)
