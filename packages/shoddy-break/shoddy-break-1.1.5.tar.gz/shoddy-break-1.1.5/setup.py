eee2d94470742bcdecb2534c4a9178e550d72c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:19 +0000

    Add package shoddy-break/1.1.5/shoddy-break-1.1.5.tar.gz

---
+++ packages/shoddy-break/shoddy-break-1.1.5.tar.gz/shoddy-break-1.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shoddy-break',
+    version='1.1.5',
+    package_dir={'':'shoddy-break'},
+    install_requires=[
+        'pygame',
+    ],
+    author='FrostDream',
+    author_email='frostdream3k@gmail.com',
+    description='A pygame based art animation library',
+    url='https://github.com/frost-dream/pygame-arts',
+)
