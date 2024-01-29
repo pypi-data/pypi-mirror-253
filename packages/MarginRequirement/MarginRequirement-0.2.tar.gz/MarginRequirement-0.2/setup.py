6897196c05d2f13db967aeefd429150a4e22d343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:47 +0000

    Add package MarginRequirement/0.2/MarginRequirement-0.2.tar.gz

---
+++ packages/MarginRequirement/MarginRequirement-0.2.tar.gz/MarginRequirement-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MarginRequirement",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=['requests==2.31.0', ],
+    license='MIT',
+)
