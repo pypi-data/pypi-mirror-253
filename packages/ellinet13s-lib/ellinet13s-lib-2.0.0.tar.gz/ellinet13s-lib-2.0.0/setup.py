ceb9d25258d68fd28525ad19f28ace5e368d3d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:23 +0000

    Add package ellinet13s-lib/2.0.0/ellinet13s-lib-2.0.0.tar.gz

---
+++ packages/ellinet13s-lib/ellinet13s-lib-2.0.0.tar.gz/ellinet13s-lib-2.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ellinet13s-lib',
+    version='2.0.0',
+    author='ElliNet13',
+    author_email='ellinet13@googlegroups.com',
+    description='A library made by ElliNet13',
+    long_description='This library was made by ElliNet13 and will stay like that.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/ElliNet13/ellinet13s-lib',
+    packages=['ellinet13s-lib'],
+    classifiers=[
+        
+    ],
+    install_requires=[
+        'wheel'
+    ],
+)
