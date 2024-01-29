1f2122460f184d770564fa7e9d6047249a22be2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:09 +0000

    Add package python-lottery/0.6/python-lottery-0.6.tar.gz

---
+++ packages/python-lottery/python-lottery-0.6.tar.gz/python-lottery-0.6/README.md
@@ -0,0 +1,8 @@
+# python-lottery
+python-lottery
+
+```bash
+python setup.py sdist bdist_wheel # build package
+twine upload dist/* # upload package
+```
+
---
+++ packages/python-lottery/python-lottery-0.6.tar.gz/python-lottery-0.6/lottery/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author  : zhengdongqi
+@Email   : dongqi.zheng@mxplayer.in
+@Usage   :
+@FileName: __init__.py.py
+@DateTime: 2024/1/28 18:49
+@SoftWare: 
+"""
+
+from .util import IOUtil, ModelUtil, SpiderUtil, CalculateUtil, AnalyzeUtil
+from .daletou import Daletou
+
+__all__ = ['IOUtil', 'ModelUtil', 'SpiderUtil', 'CalculateUtil', 'AnalyzeUtil', 'Daletou']
