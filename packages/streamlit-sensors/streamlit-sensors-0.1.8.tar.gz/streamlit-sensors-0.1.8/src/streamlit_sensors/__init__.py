b2af8ba40cfaad28746db51dc1a94d715e9ce269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 14:04:21 +0000

    Add package streamlit-sensors/0.1.8/streamlit-sensors-0.1.8.tar.gz

---
+++ packages/streamlit-sensors/streamlit-sensors-0.1.8.tar.gz/streamlit-sensors-0.1.8/src/streamlit_sensors/__init__.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import Optional
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+# Tell streamlit that there is a component called streamlit_sensors,
+# and that the code to display that component is in the "frontend" folder
+frontend_dir = (Path(__file__).parent / "frontend").absolute()
+_component_func = components.declare_component(
+	"streamlit_sensors", path=str(frontend_dir)
+)
+
+# Create the python function that will be called
+def streamlit_sensors(
+    key: Optional[str] = None,
+):
+    """
+    Add a descriptive docstring
+    """
+    component_value = _component_func(
+        key=key,
+    )
+
+    return component_value
+
+
+def main():
+    st.write("## Example")
+
+
+    value = streamlit_sensors()
+    if value is not None:
+        st.write(value)
+
+        image = st.camera_input("Capture Traffic Sign")
+        if image is not None:
+            st.image(image, caption="Captured Traffic Sign", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
