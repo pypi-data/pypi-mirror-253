148daeb120eedd0fe52c261566dc91825593f674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:40 +0000

    Add package edc-lab/0.3.56/edc_lab-0.3.56-py3-none-any.whl

---
+++ packages/edc-lab/edc_lab-0.3.56-py3-none-any.whl/edc_lab/stubs.py
@@ -0,0 +1,11 @@
+from typing import Protocol
+
+
+class AliquotTypeStub(Protocol):
+    derivatives: list
+    name: str
+    alpha_code: str
+    numeric_code: str
+
+    def add_derivatives(self: "AliquotTypeStub") -> None:
+        ...
