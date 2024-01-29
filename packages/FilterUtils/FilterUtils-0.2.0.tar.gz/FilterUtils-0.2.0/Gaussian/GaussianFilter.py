926bf73cbced3b567f8e0597021bc033028359c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:10 +0000

    Add package FilterUtils/0.2.0/FilterUtils-0.2.0.tar.gz

---
+++ packages/FilterUtils/FilterUtils-0.2.0.tar.gz/FilterUtils-0.2.0/Gaussian/GaussianFilter.py
@@ -0,0 +1,34 @@
+import warnings
+from typing import List, Optional, Tuple, Union
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+
+def gaussian_filter(input: Tensor, win: Tensor) -> Tensor:
+    r""" Blur input with 1-D kernel
+    Args:
+        input (torch.Tensor): a batch of tensors to be blurred
+        window (torch.Tensor): 1-D gauss kernel
+    Returns:
+        torch.Tensor: blurred tensors
+    """
+    assert all([ws == 1 for ws in win.shape[1:-1]]), win.shape
+    if len(input.shape) == 4:
+        conv = F.conv2d
+    elif len(input.shape) == 5:
+        conv = F.conv3d
+    else:
+        raise NotImplementedError(input.shape)
+
+    C = input.shape[1]
+    out = input
+    for i, s in enumerate(input.shape[2:]):
+        if s >= win.shape[-1]:
+            out = conv(out, weight=win.transpose(2 + i, -1), stride=1, padding=0, groups=C)
+        else:
+            warnings.warn(
+                f"Skipping Gaussian Smoothing at dimension 2+{i} for input: {input.shape} and win size: {win.shape[-1]}"
+            )
+
+    return out
---
+++ packages/FilterUtils/FilterUtils-0.2.0.tar.gz/FilterUtils-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FilterUtils',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [],
+    },
+    author='Jarvis',
+    author_email='Jarvis@ldsb.com',
+    description='A Python library for various filters',
+    long_description='A Python library providing implementations of Bessel, Butterworth, Cauer, Chebyshev, and Gaussian filters.',
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/your_filter_library',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
