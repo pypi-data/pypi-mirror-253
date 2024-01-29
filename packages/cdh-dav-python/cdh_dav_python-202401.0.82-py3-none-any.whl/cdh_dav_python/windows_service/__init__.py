9d6c285a527580838548488fa3027b1db6399cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:05:12 +0000

    Add package cdh-dav-python/202401.0.82/cdh_dav_python-202401.0.82-py3-none-any.whl

---
+++ packages/cdh-dav-python/cdh_dav_python-202401.0.82-py3-none-any.whl/cdh_dav_python/windows_service/__init__.py
@@ -0,0 +1,22 @@
+# Import from sibling directory ..\databricks_service
+from ..cdc_admin_service import environment_tracing
+from ..cdc_admin_service import environment_logging
+import sys
+import os
+
+OS_NAME = os.name
+
+sys.path.append("..")
+if OS_NAME.lower() == "nt":
+    print("windows_service: windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..\\..")))
+else:
+    print("windows_service: non windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../../..")))
+
+
+__all__ = ["windows_crential"]
