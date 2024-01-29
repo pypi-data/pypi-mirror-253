e989c7338cce1921e03676dcb0810ab9936b0fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:44 +0000

    Add package pygpt-net/2.0.124/pygpt-net-2.0.124.tar.gz

---
+++ packages/pygpt-net/pygpt-net-2.0.124.tar.gz/pygpt-net-2.0.124/src/pygpt_net/ui/widget/textarea/notepad.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.28 12:00:00                  #
+# ================================================== #
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QTextEdit, QMenu, QWidget, QVBoxLayout
+
+from pygpt_net.ui.widget.element.labels import HelpLabel
+from pygpt_net.utils import trans
+
+
+class NotepadWidget(QWidget):
+    def __init__(self, window=None):
+        """
+        Notepad
+
+        :param window: main window
+        """
+        super(NotepadWidget, self).__init__(window)
+        self.window = window
+        self.id = 1
+        self.textarea = NotepadOutput(self.window)
+        self.window.ui.nodes['tip.output.tab.notepad'] = HelpLabel(trans('tip.output.tab.notepad'), self.window)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.textarea)
+        layout.addWidget(self.window.ui.nodes['tip.output.tab.notepad'])
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+    def setText(self, text):
+        self.textarea.setText(text)
+
+    def toPlainText(self):
+        return self.textarea.toPlainText()
+
+
+class NotepadOutput(QTextEdit):
+    def __init__(self, window=None):
+        """
+        Notepad
+
+        :param window: main window
+        """
+        super(NotepadOutput, self).__init__(window)
+        self.window = window
+        self.setAcceptRichText(False)
+        self.setStyleSheet(self.window.controller.theme.style('font.chat.output'))
+        self.textChanged.connect(
+            lambda: self.window.controller.notepad.save(self.id))
+        self.value = self.window.core.config.data['font_size']
+        self.max_font_size = 42
+        self.min_font_size = 8
+        self.id = 1
+
+    def contextMenuEvent(self, event):
+        menu = self.createStandardContextMenu()
+        selected_text = self.textCursor().selectedText()
+        if selected_text:
+            # audio read
+            action = menu.addAction(trans('text.context_menu.audio.read'))
+            action.triggered.connect(self.audio_read_selection)
+
+            # copy to
+            copy_to_menu = QMenu(trans('text.context_menu.copy_to'), self)
+
+            # input
+            action = copy_to_menu.addAction(trans('text.context_menu.copy_to.input'))
+            action.triggered.connect(
+                lambda: self.window.controller.chat.common.append_to_input(selected_text))
+
+            # notepad
+            num_notepads = self.window.controller.notepad.get_num_notepads()
+            if num_notepads > 0:
+                for i in range(1, num_notepads + 1):
+                    if i == self.id:
+                        continue
+                    name = self.window.controller.notepad.get_notepad_name(i)
+                    action = copy_to_menu.addAction(name)
+                    action.triggered.connect(lambda checked=False, i=i:
+                                             self.window.controller.notepad.append_text(selected_text, i))
+
+            # calendar
+            action = copy_to_menu.addAction(trans('text.context_menu.copy_to.calendar'))
+            action.triggered.connect(
+                lambda: self.window.controller.calendar.note.append_text(selected_text))
+
+            menu.addMenu(copy_to_menu)
+
+            # save as (selected)
+            action = QAction(QIcon.fromTheme("document-save"), trans('action.save_as'), self)
+            action.triggered.connect(
+                lambda: self.window.controller.chat.common.save_text(selected_text))
+            menu.addAction(action)
+        else:
+            # save as (all)
+            action = QAction(QIcon.fromTheme("document-save"), trans('action.save_as'), self)
+            action.triggered.connect(
+                lambda: self.window.controller.chat.common.save_text(self.toPlainText()))
+            menu.addAction(action)
+
+        menu.exec_(event.globalPos())
+
+    def audio_read_selection(self):
+        """
+        Read selected text (audio)
+        """
+        self.window.controller.audio.read_text(self.textCursor().selectedText())
+
+    def wheelEvent(self, event):
+        """
+        Wheel event: set font size
+
+        :param event: Event
+        """
+        if event.modifiers() & Qt.ControlModifier:
+            if event.angleDelta().y() > 0:
+                if self.value < self.max_font_size:
+                    self.value += 1
+            else:
+                if self.value > self.min_font_size:
+                    self.value -= 1
+
+            self.window.core.config.data['font_size'] = self.value
+            self.window.core.config.save()
+            option = self.window.controller.settings.editor.get_option('font_size')
+            option['value'] = self.value
+            self.window.controller.config.apply('config', 'font_size', option)
+            self.window.controller.ui.update_font_size()
+            event.accept()
+        else:
+            super(NotepadOutput, self).wheelEvent(event)
