4d04467b3009f1ac51ae45893b21ac067c9575a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:30 +0000

    Add package PhasorMath/0.0.1/PhasorMath-0.0.1.tar.gz

---
+++ packages/PhasorMath/PhasorMath-0.0.1.tar.gz/PhasorMath-0.0.1/app/Phasor/__init__.py
@@ -0,0 +1,14 @@
+from .src.phasor import (
+    PhasorPD,
+    PhasorPR,
+    PhasorRe
+)
+
+from .src.plot import (
+    Plot
+)
+
+from .src.sc import (
+    SCtoU,
+    UtoSC
+)
---
+++ packages/PhasorMath/PhasorMath-0.0.1.tar.gz/PhasorMath-0.0.1/app/Phasor/src/__init__.py
@@ -0,0 +1,3 @@
+from .phasor import PhasorPD, PhasorPR, PhasorRe, toDeg, toRad, toRec
+from .plot import Plot
+from .sc import UtoSC, SCtoU
