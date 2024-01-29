783fb8d70fa89dcb750c6b0ab6c95bc9f4983e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 05:04:12 +0000

    Add package gobi-probability/1.0/gobi_probability-1.0.tar.gz

---
+++ packages/gobi-probability/gobi_probability-1.0.tar.gz/gobi_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gobi_probability',
+      version='1.0',
+      description='Gaussian distributions and binomial distributions',
+      packages=['gobi_probability'],
+      author='Sambili Tonny',
+      author_email='synnotlee@gmail.com',
+      zip_safe=False)
