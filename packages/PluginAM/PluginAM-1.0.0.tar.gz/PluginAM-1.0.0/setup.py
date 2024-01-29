19395e4f93f60ed015a9b4d68eb6b17e586cf32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:52 +0000

    Add package PluginAM/1.0.0/PluginAM-1.0.0.tar.gz

---
+++ packages/PluginAM/PluginAM-1.0.0.tar.gz/PluginAM-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+name='PluginAM',
+version='1.0.0',
+packages=find_packages(),
+author='John Doe',
+author_email='John356@wje.com',
+description=''
+)
