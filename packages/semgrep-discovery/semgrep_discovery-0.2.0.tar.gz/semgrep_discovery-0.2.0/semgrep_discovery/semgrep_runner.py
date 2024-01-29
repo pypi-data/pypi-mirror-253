014e0b91943f360286be75340e60e8d1c1533035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 05:04:05 +0000

    Add package semgrep-discovery/0.2.0/semgrep_discovery-0.2.0.tar.gz

---
+++ packages/semgrep-discovery/semgrep_discovery-0.2.0.tar.gz/semgrep_discovery-0.2.0/semgrep_discovery/semgrep_runner.py
@@ -0,0 +1,124 @@
+import logging
+from typing import List
+from dataclasses import dataclass
+from pathlib import Path
+
+import os
+
+import subprocess
+import json
+
+# Датакласс каждой получаемой из апи строки
+@dataclass
+class SearchObject:
+    path: str
+    line: int
+    object_type: str
+    object: str
+    fields: List[str]
+    sensitive: bool
+    
+
+
+# Класс чтения данных из апи
+class SemgrepRunner:
+
+    def __init__(self, workdir: str, langs: str, objects: str, keywords: str):
+        self.workdir = Path(workdir)
+        self.workdir_len = len(str(self.workdir))
+        self.keywords = keywords.split(',')
+        self.langs = langs.split(',')
+        self.objects = objects.split(',')
+
+        self.rulesdir = Path(os.path.dirname(__file__), '/rules')
+
+        self.logger = logging.getLogger(__name__)
+        
+
+    def find_objects(self) -> List[SearchObject]:
+
+        objects = []
+
+        rules_list = []
+
+        for lang in self.langs:
+            for object in self.objects:
+
+                if Path(self.rulesdir, lang, object + '.yaml').is_file():
+
+                    rule_file = str(Path(self.rulesdir, lang, object + '.yaml'))
+                    rules_list.append(rule_file)
+                    
+                    self.logger.info(f'Add rule {rule_file} for scan')
+
+        for rule in rules_list:
+
+            self.logger.info(f'Run scan {self.workdir} with rule {rule}')
+
+            result = subprocess.run(
+                ["semgrep", "scan", "--config"] + rule +  [ self.workdir, "--json", "--metrics=off"],
+                capture_output=True,
+                text=True
+            )
+
+            if result.returncode != 0:
+                self.logger.error("Semgrep encountered an error:")
+                self.logger.error(result.stderr)
+                return objects
+
+            semgrep_data = json.loads(result.stdout)
+
+            objects_dict = {}
+
+            for finding in semgrep_data['results']:
+                
+                full_path = finding.get('path')
+                path = full_path[self.workdir_len + 1:]
+
+                full_rule = finding.get('check_id')
+                object_type = full_rule[6:]
+
+                object = finding.get('extra').get('metavars').get('$OBJECT').get('abstract_content')
+                field = finding.get('extra').get('metavars').get('$FIELD', {}).get('abstract_content')
+                method = finding.get('extra').get('metavars').get('$METHOD', {}).get('abstract_content')
+                line = finding.get('extra').get('metavars').get('$OBJECT').get('start').get('line')
+
+                if path not in objects_dict:
+                    self.logger.info(path)
+                    objects_dict[path] = {}
+
+                if object not in objects_dict[path]:
+                    self.logger.info('   ' + object)
+                    objects_dict[path][object] = { 'object_type': object_type,
+                                                   'line': line,
+                                                   'sensitive': False,
+                                                   'fields': []}
+                    
+                if field and field not in objects_dict[path][object]['fields']:
+                    self.logger.info('        ' + field)
+                    objects_dict[path][object]['fields'].append(field)
+
+                if method and method not in objects_dict[path][object]['fields']:
+                    self.logger.info('        ' + field)
+                    objects_dict[path][object]['fields'].append(method)
+
+                for kw in self.keywords:
+                    if kw in str(object).lower() or kw in str(field).lower():
+                        objects_dict[path][object]['sensitive'] = True
+
+            for path_key, path_dict in objects_dict.items():
+                for object_key, object_data in path_dict.items():
+                    objects.append(
+                        SearchObject(
+                            path=path_key,
+                            line=object_data['line'],
+                            object_type=object_data['object_type'],
+                            object=object_key,
+                            fields=object_data['fields'],
+                            sensitive=object_data['sensitive'],
+                        )
+                    )
+
+        self.logger.info(f"Got {len(objects)} objects")
+
+        return objects
