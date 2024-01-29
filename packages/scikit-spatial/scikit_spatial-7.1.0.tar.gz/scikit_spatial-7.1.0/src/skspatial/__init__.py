5e1e4b232d35c9add56bdeb3b986220ee62a0809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:09 +0000

    Add package scikit-spatial/7.1.0/scikit_spatial-7.1.0.tar.gz

---
+++ packages/scikit-spatial/scikit_spatial-7.1.0.tar.gz/scikit_spatial-7.1.0/src/skspatial/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for scikit-spatial."""
+
+import importlib.metadata as importlib_metadata  # type: ignore
+
+__author__ = "Andrew Hynes"
+__email__ = "andrewjhynes@gmail.com"
+__version__ = importlib_metadata.version("scikit-spatial")
