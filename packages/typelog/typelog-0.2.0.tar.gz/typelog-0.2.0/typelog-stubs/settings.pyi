7da7dced2da1eba178323dca3b17a2cf7acee3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:18 +0000

    Add package typelog/0.2.0/typelog-0.2.0.tar.gz

---
+++ packages/typelog/typelog-0.2.0.tar.gz/typelog-0.2.0/typelog-stubs/settings.pyi
@@ -0,0 +1,7 @@
+LOG_LEVEL: str
+LOG_JSON: bool
+add_thread: bool
+add_process: bool
+add_level: bool
+add_filepath: bool
+add_time: bool
---
+++ packages/typelog/typelog-0.2.0.tar.gz/typelog-0.2.0/typelog-stubs/types.pyi
@@ -0,0 +1,8 @@
+from _typeshed import Incomplete
+from typing import Callable
+
+LogAttrs: Incomplete
+LogType = Callable[[LogAttrs], None]
+LibName: Incomplete
+LogLevel: Incomplete
+RootLogLevel: Incomplete
