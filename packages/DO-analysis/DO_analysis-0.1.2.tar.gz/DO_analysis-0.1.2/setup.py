d5c2ae2bdcfad5533db1af9760855bffe9b1a10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:04:47 +0000

    Add package DO-analysis/0.1.2/DO_analysis-0.1.2.tar.gz

---
+++ packages/DO-analysis/DO_analysis-0.1.2.tar.gz/DO_analysis-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='DO_analysis',
+    version='0.1.2',
+    description='A simple Python library for calculating Dissimilarity Overlap Curve and alpha/beta diversity',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Valeriia Ivanova',
+    packages=find_packages(include=['DO_analysis']),
+    license="MIT",
+    download_url='https://github.com/IvanovaVA/DOC_analysis/archive/refs/tags/0.1.0.tar.gz',
+)
