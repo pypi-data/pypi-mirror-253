105d98031190d3eb1e3de9ef0da64c6d0e1756d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 15:04:52 +0000

    Add package topsis-manvendra-102103799/0.1/topsis-manvendra-102103799-0.1.tar.gz

---
+++ packages/topsis-manvendra-102103799/topsis-manvendra-102103799-0.1.tar.gz/topsis-manvendra-102103799-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='topsis-manvendra-102103799',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "pandas","numpy"
+    ],
+    author='Manvendra Raj Singh',
+    author_email='manvendrasingh8802@gmail.com',
+    description='Topsis Implementation Package',
+    long_description=open('README.md', encoding='utf-8').read(),
+
+    long_description_content_type='text/markdown',
+    
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
