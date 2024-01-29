8def6a26b70bdd6cde94b9aa8dd896b9e8a2bafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 03:05:00 +0000

    Add package apple-ocr/1.0.7/apple-ocr-1.0.7.tar.gz

---
+++ packages/apple-ocr/apple-ocr-1.0.7.tar.gz/apple-ocr-1.0.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apple-ocr",
+    version="1.0.7",
+    license="Apache License 2.0",
+    author="Louis Brulé Naudet",
+    author_email="louisbrulenaudet@icloud.com",
+    description="An OCR (Optical Character Recognition) utility for text extraction from images.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/louisbrulenaudet/apple-ocr",
+    project_urls={"Repository": "https://github.com/louisbrulenaudet/apple-ocr"},
+    keywords="OCR, image-recognition, text-extraction, clustering, Apple Vision, NLP, LLM, data",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(where="apple_ocr"),
+    install_requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "Pillow",
+        "scikit-learn",
+        "plotly",
+        "pyobjc",
+    ],
+)
