c7bb8be8d879504b2d234e0fce4553e9f6cb68b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:05:32 +0000

    Add package configwidgets/0.0.2/configwidgets-0.0.2-py3-none-any.whl

---
+++ packages/configwidgets/configwidgets-0.0.2-py3-none-any.whl/configwidgets/__init__.py
@@ -0,0 +1,8 @@
+#
+__version__ = "0.0.2"
+
+
+from .checkbox import ConfigCheckBox
+from .spinbox import ConfigSpinBox, ConfigDoubleSpinBox
+from .lineedit import ConfigLineEdit
+from .datetime import ConfigDateEdit, ConfigTimeEdit, ConfigDateTimeEdit
