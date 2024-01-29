0a41be58e0a5b8a324dfbbd4624c9c9708f1cd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-29 04:04:28 +0000

    Add package traccar-api/0.1.0/traccar_api-0.1.0.tar.gz

---
+++ packages/traccar-api/traccar_api-0.1.0.tar.gz/traccar_api-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='traccar_api',
+    version='0.1.0',
+    description='Python client for interacting with Traccar GPS tracking API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='LiGuru',
+    author_email='services.nvlahovski@gmail.com',
+    url='https://github.com/scrt-nvlahovski/traccar_api',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
