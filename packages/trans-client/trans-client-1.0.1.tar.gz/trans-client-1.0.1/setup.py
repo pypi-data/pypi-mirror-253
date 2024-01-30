ee2fe84efa80a3498c69157e14d368558db79ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:49 +0000

    Add package trans-client/1.0.1/trans-client-1.0.1.tar.gz

---
+++ packages/trans-client/trans-client-1.0.1.tar.gz/trans-client-1.0.1/setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='trans-client',
+    version='1.0.1',
+    py_modules=['tclient'],
+    description='A data exchange script based on ssh',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='fetch150zy',
+    author_email='zhewei@stu.xidian.edu.cn',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'tcl=tclient:main',
+        ],
+    },
+    python_requires='>=3.8',
+)
