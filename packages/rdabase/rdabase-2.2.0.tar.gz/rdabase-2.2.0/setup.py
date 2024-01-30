760e0f57f51ce0efc7edc7e77c63bfcfb9763ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:27 +0000

    Add package rdabase/2.2.0/rdabase-2.2.0.tar.gz

---
+++ packages/rdabase/rdabase-2.2.0.tar.gz/rdabase-2.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdabase",
+    version="2.2.0",
+    description="Redistricting analytics data",
+    url="https://github.com/rdatools/rdabase",
+    author="alecramsay",
+    author_email="a73cram5ay@gmail.com",
+    license="MIT",
+    packages=[
+        "rdabase",
+    ],
+    install_requires=[
+        "fiona",
+        "geopandas",
+        "libpysal",
+        "shapely",
+    ],
+    zip_safe=False,
+)
