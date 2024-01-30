ec862bc9b1d6732a6b6d010eedde7e4343e29430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:05:13 +0000

    Add package bisheng-langchain/0.2.2.4/bisheng_langchain-0.2.2.4-py3-none-any.whl

---
+++ packages/bisheng-langchain/bisheng_langchain-0.2.2.4-py3-none-any.whl/bisheng_langchain/chat_models/__init__.py
@@ -0,0 +1,13 @@
+from .host_llm import CustomLLMChat, HostBaichuanChat, HostChatGLM, HostLlama2Chat, HostQwenChat
+from .minimax import ChatMinimaxAI
+from .proxy_llm import ProxyChatLLM
+from .qwen import ChatQWen
+from .wenxin import ChatWenxin
+from .xunfeiai import ChatXunfeiAI
+from .zhipuai import ChatZhipuAI
+from .sensetime import SenseChat 
+
+__all__ = [
+    'ProxyChatLLM', 'ChatMinimaxAI', 'ChatWenxin', 'ChatZhipuAI', 'ChatXunfeiAI', 'HostChatGLM',
+    'HostBaichuanChat', 'HostLlama2Chat', 'HostQwenChat', 'CustomLLMChat', 'ChatQWen', 'SenseChat'
+]
