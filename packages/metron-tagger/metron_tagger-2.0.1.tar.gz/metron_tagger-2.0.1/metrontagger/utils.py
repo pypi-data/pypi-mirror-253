869b50e7d7eba9fbb65677bcca792962709ce7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:38 +0000

    Add package metron-tagger/2.0.1/metron_tagger-2.0.1.tar.gz

---
+++ packages/metron-tagger/metron_tagger-2.0.1.tar.gz/metron_tagger-2.0.1/metrontagger/utils.py
@@ -0,0 +1,50 @@
+"""Some miscellaneous functions"""
+from pathlib import Path
+from urllib.parse import quote_plus
+
+from darkseid.filename import FileNameParser
+
+
+def cleanup_string(path_name: str | None) -> str | None:
+    """
+    Function to remove some characters that don't play nicely on Windows machines filesystem
+    """
+    if path_name is None:
+        return None
+
+    if isinstance(path_name, int):
+        path_name = str(path_name)
+
+    path_name = path_name.replace("/", "-")
+    path_name = path_name.replace(" :", " -")
+    path_name = path_name.replace(": ", " - ")
+    path_name = path_name.replace(":", "-")
+    return path_name.replace("?", "")
+
+
+def create_query_params(filename: Path) -> dict[str, str]:
+    """Function to create a diction of values based on the provided filename"""
+    fnp = FileNameParser()
+    fnp.parse_filename(filename)
+
+    # Remove hyphen when searching for series name
+    fixed_txt: str = fnp.series.replace(" - ", " ")
+    series_word_list: list[str] = fixed_txt.split()
+    series_string: str = " ".join(series_word_list).strip()
+
+    # If there isn't an issue number, let's assume it's "1".
+    number: str = quote_plus(fnp.issue.encode("utf-8")) if fnp.issue else "1"
+    # Handle issues with #½
+    if number == "0.5":
+        number = "½"
+
+    params = {
+        "series_name": series_string,
+        "number": number,
+    }
+    if fnp.volume:
+        params["series_volume"] = fnp.volume
+    if fnp.year:
+        params["cover_year"] = fnp.year
+
+    return params
---
+++ packages/metron-tagger/metron_tagger-2.0.1.tar.gz/metron_tagger-2.0.1/tests/test_utils.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+
+import pytest
+
+from metrontagger.utils import cleanup_string, create_query_params
+
+
+def test_dict(tmp_path: Path) -> None:
+    series = "Aquaman"
+    number = "9"
+    volume = "1"
+    year = "1999"
+    # Make the tmp file
+    comic = tmp_path / f"{series} v{volume} #{number} ({year}).cbz"
+
+    result = create_query_params(comic)
+    expected = {
+        "series_name": f"{series}",
+        "series_volume": f"{volume}",
+        "number": f"{number}",
+        "cover_year": f"{year}",
+    }
+    assert result == expected
+
+
+def test_dict_with_title_hyphon(tmp_path: Path) -> None:
+    series = "Batman - Superman"
+    number = "5"
+    volume = "1"
+    year = "2013"
+    # Make the tmp file
+    comic = tmp_path / f"{series} v{volume} #{number} ({year}).cbz"
+
+    result = create_query_params(comic)
+    expected = {
+        "series_name": "Batman Superman",
+        "series_volume": f"{volume}",
+        "number": f"{number}",
+        "cover_year": f"{year}",
+    }
+    assert result == expected
+
+
+def test_query_dict_without_issue_number(tmp_path: Path) -> None:
+    series = "Batman"
+    year = "1990"
+    volume = "2"
+    # Make the tmp file
+    comic = tmp_path / f"{series} v{volume} ({year}).cbz"
+
+    result = create_query_params(comic)
+    expected = {
+        "series_name": f"{series}",
+        "series_volume": f"{volume}",
+        "number": "1",
+        "cover_year": f"{year}",
+    }
+    assert result == expected
+
+
+test_strings = [
+    pytest.param("Hashtag: Danger (2019)", "Cleanup colon space", "Hashtag - Danger (2019)"),
+    pytest.param("Hashtag :Danger (2019)", "Cleanup space colon", "Hashtag -Danger (2019)"),
+    pytest.param("Hashtag:Danger (2019)", "Cleanup colon", "Hashtag-Danger (2019)"),
+    pytest.param("Hack/Slash (2019)", "Cleanup backslash", "Hack-Slash (2019)"),
+    pytest.param("What If? (2019)", "Cleanup question mark", "What If (2019)"),
+]
+
+
+@pytest.mark.parametrize(("string", "reason", "expected"), test_strings)
+def test_string_cleanup(string: str, reason: str, expected: str) -> None:  # noqa: ARG001
+    assert cleanup_string(string) == expected
