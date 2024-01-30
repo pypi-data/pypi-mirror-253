21d5f78b2a1204a7e0102453f58e3e989b35c4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:04:59 +0000

    Add package unit-python-sdk/0.30.0/unit-python-sdk-0.30.0.tar.gz

---
+++ packages/unit-python-sdk/unit-python-sdk-0.30.0.tar.gz/unit-python-sdk-0.30.0/unit/utils/__init__.py
@@ -0,0 +1,13 @@
+from typing import List, Dict
+
+from unit.models import Relationship
+
+
+def create_relationship(type: str, id: str, relation: str = None):
+    relation = relation or type
+    return {relation: Relationship(type, id)}
+
+
+def create_deposit_account_relationship(id: str, relation: str):
+    return {relation: Relationship("depositAccount", id)}
+
