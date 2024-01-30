fabbca4ee55bfcc635a3f8bba373c35425baefe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:05:09 +0000

    Add package flamethrower/0.2.5/flamethrower-0.2.5.tar.gz

---
+++ packages/flamethrower/flamethrower-0.2.5.tar.gz/flamethrower-0.2.5/src/flamethrower/utils/zsh_history.py
@@ -0,0 +1,21 @@
+import flamethrower.config.constants as config
+
+def get_last_user_cmd() -> str:
+    with open(config.get_zsh_history_path()) as f:
+        history = f.read()
+        if not history:
+            return ''
+        
+        history = history.split('\n')
+        last_index = -1
+        last_command = history[last_index]
+        
+        while last_command == '':
+            last_index -= 1
+            last_command = history[last_index]
+
+        return last_command
+    
+def update_zsh_history(query: str) -> None:
+    with open(config.get_zsh_history_path(), 'a') as f:
+        f.write(query + '\n')
