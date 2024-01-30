66fc810f79713724a957459b69f5ab5067370c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:13 +0000

    Add package typelog/0.2.8/typelog-0.2.8.tar.gz

---
+++ packages/typelog/typelog-0.2.8.tar.gz/typelog-0.2.8/typelog-stubs/utils.pyi
@@ -0,0 +1,19 @@
+from . import settings as settings, types as types
+from _typeshed import Incomplete
+
+class LogConfig:
+    lib_name: Incomplete
+    log_level: Incomplete
+    def __init__(self, lib_name: types.LibName, log_level: types.LogLevel) -> None: ...
+
+class Loggers:
+    root_log_level: Incomplete
+    log_configs: Incomplete
+    turn_json: Incomplete
+    add_thread: Incomplete
+    add_process: Incomplete
+    add_level: Incomplete
+    add_filepath: Incomplete
+    add_time: Incomplete
+    def __init__(self, root_log_level: types.RootLogLevel, *log_configs: LogConfig, turn_json: bool = ..., add_thread: bool = ..., add_process: bool = ..., add_level: bool = ..., add_filepath: bool = ..., add_time: bool = ...) -> None: ...
+    def configure(self) -> None: ...
---
+++ packages/typelog/typelog-0.2.8.tar.gz/typelog-0.2.8/typelog/__init__.py
@@ -0,0 +1,3 @@
+from .logcore import get_logger
+from .logtypes import *
+from .utils import LogConfig, Loggers
