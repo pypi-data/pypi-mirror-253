22fc333c5459b5cf86c878ce5d5889c8299f34b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:01 +0000

    Add package flap-lite/0.6.17/flap-lite-0.6.17.tar.gz

---
+++ packages/flap-lite/flap-lite-0.6.17.tar.gz/flap-lite-0.6.17/src/flap/api/__init__.py
@@ -0,0 +1,9 @@
+"""
+Top level API for database setup and address matching
+
+This module will be made available to module level
+"""
+
+from .database_setup import create_database
+from .match import match
+from .score import score
