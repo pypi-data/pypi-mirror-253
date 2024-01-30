e357d16bfaea12fa0f1a4ec53a0fa4cd43eb661e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:42 +0000

    Add package rws-knmi-lib/1.0.1/rws_knmi_lib-1.0.1-py3-none-any.whl

---
+++ packages/rws-knmi-lib/rws_knmi_lib-1.0.1-py3-none-any.whl/rws_knmi_lib/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Documentation about RWS KNMI Lib"""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "RWS Datalab"
+__email__ = "datalab.codebase@rws.nl"
+__version__ = "1.0.1"
