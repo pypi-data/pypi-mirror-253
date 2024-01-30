5676c8d279259637ff35ccd11c7030cded9f3496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:23 +0000

    Add package scdiffeq/0.0.54rc0/scdiffeq-0.0.54rc0.tar.gz

---
+++ packages/scdiffeq/scdiffeq-0.0.54rc0.tar.gz/scdiffeq-0.0.54rc0/scdiffeq/core/lightning_models/base/_base_lightning_diffeq.py
@@ -0,0 +1,144 @@
+
+
+# -- import packages: ----------------------------------------------------------
+import lightning
+import torchsde
+from abc import abstractmethod
+import ABCParse
+
+# -- import local dependencies: ------------------------------------------------
+from ._batch_processor import BatchProcessor
+from ._sinkhorn_divergence import SinkhornDivergence
+
+from ... import utils
+
+
+from typing import Optional
+
+# -- DiffEq class: -------------------------------------------------------------
+class BaseLightningDiffEq(lightning.LightningModule):
+    """BaseLightningDiffEq"""
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        
+    def _update_lit_diffeq_hparams(self, model_params):
+        for key, val in self.hparams.items():
+            if key in model_params.keys():
+                if val != model_params[key]:
+                    self.hparams.update({key: model_params[key]})
+        
+    # -- setup: ----------------------------------------------------------------
+    def _configure_lightning_model(self, kwargs):
+        
+        """Assumes no pre-train - i.e., a single optimizer, scheduler"""
+        
+        optimizer = self.hparams['train_optimizer']
+        scheduler = self.hparams['train_scheduler']
+
+        self._optimizers = [optimizer(self.parameters(), lr=self.hparams['train_lr'])]
+        self._schedulers = [
+            scheduler(
+                optimizer=self._optimizers[0],
+                step_size=self.hparams['train_step_size']),
+        ]
+        sinkhorn_kwargs = ABCParse.function_kwargs(func = SinkhornDivergence, kwargs = kwargs)
+        self.sinkhorn_divergence = SinkhornDivergence(**sinkhorn_kwargs)
+        self.process_batch = BatchProcessor
+        self.COMPLETED_EPOCHS = 0
+
+    def _configure_torch_modules(self, func, kwargs):
+        
+        kwargs['state_size'] = self.hparams['latent_dim']
+        self.DiffEq = func(**utils.function_kwargs(func, kwargs))
+        
+    @property
+    def PRETRAIN(self):
+        return False
+    
+    # -- IVP-solving: ---------------------------------------------------------
+    @property
+    def _INTEGRATOR(self):
+        if self.hparams["adjoint"]:
+            return torchsde.sdeint_adjoint
+        return torchsde.sdeint
+
+    def integrate(self, Z0, t, dt, logqp, **kwargs):
+        return self._INTEGRATOR(
+            sde=self.DiffEq,
+            y0=Z0,
+            ts=t,
+            dt=dt,
+            logqp=logqp,
+            **kwargs,
+        )
+
+    # -- sinkhorn loss: -------------------------------------------------------
+    def compute_sinkhorn_divergence(self, X, X_hat, W, W_hat):
+        return self.sinkhorn_divergence(
+            W_hat.contiguous(), X_hat.contiguous(), W.contiguous(), X.contiguous(), 
+        ).requires_grad_()
+
+
+    # -- custom steps: -------------------------------------------------------------
+    @abstractmethod
+    def forward(self, Z0, t, **kwargs):
+        """most likely over-written in another class"""
+        ...
+
+    @abstractmethod
+    def step(self, batch, batch_idx, stage=None):
+        print("WARNING: The base (empty) step is being called from `_base_lightning_diffeq.py`")
+        ...
+
+    # -- LightningModule methods: ----------------------------------------------
+    def training_step(self, batch, batch_idx, *args, **kwargs):
+        return self.step(batch, batch_idx, stage="training")
+
+    def validation_step(self, batch, batch_idx=None, *args, **kwargs):
+        return self.step(batch, batch_idx, stage="validation")
+
+    def test_step(self, batch, batch_idx=None, *args, **kwargs):
+        return self.step(batch, batch_idx, stage="test")
+
+    def predict_step(self, batch, batch_idx=None, *args, **kwargs):
+        return self.step(batch, batch_idx, stage="predict")
+
+    def configure_optimizers(self):
+        return self._optimizers, self._schedulers
+    
+    def __repr__(self):
+        return "LightningDiffEq"
+    
+    def _configure_name(self, name: Optional[str] = None, delim: Optional[str] = ".", loading_existing: bool = False):
+        """ """
+                
+        if loading_existing or name is None:
+            return self.__repr__()
+        else:
+            return f"{self.__repr__()}{delim}{name}"
+    
+    
+# -- moved to log callback: ---
+#     def log_sinkhorn_divergence(self, sinkhorn_loss, t, stage):
+#         for i in range(len(t)):
+#             _t = round(t[i].item(), 3)
+#             msg = f"sinkhorn_{_t}_{stage}"
+#             val = sinkhorn_loss[i]
+#             self.log(msg, val)
+
+#         return sinkhorn_loss.sum()
+    
+#     def log_lr(self):
+                
+#         if not isinstance(self.optimizers(), list):
+#             lr = self.optimizers().optimizer.state_dict()["param_groups"][0]["lr"]
+#             self.log("opt_param_group_lr", lr)
+#         else:
+#             for i, opt in enumerate(self.optimizers()):
+#                 for j, pg in enumerate(opt.optimizer.state_dict()["param_groups"]):
+#                     self.log(f"opt_{i}_param_group_{j}_lr", pg["lr"])
+
+#     def log_total_epochs(self):
+#         """Train model N times --> N"""
+#         self.log("total_epochs", self.COMPLETED_EPOCHS)
+        
---
+++ packages/scdiffeq/scdiffeq-0.0.54rc0.tar.gz/scdiffeq-0.0.54rc0/scdiffeq/core/lightning_models/base/_batch_processor.py
@@ -0,0 +1,119 @@
+
+# -- import packages: ---------------------------------------------------------
+import ABCParse
+import torch
+import numpy as np
+
+
+# -- set typing: --------------------------------------------------------------
+from typing import Union, List
+
+
+class BatchRepresentation:
+    def __init__(self, batch):
+
+        self.header = "🍪 LitDataBatch"
+        self.X = f"- X: {list(batch.X.shape)}"
+        self.W = f"- W: {list(batch.W.shape)}"
+        self.W_hat = f"- W_hat: {list(batch.W_hat.shape)}"
+        if not batch.F_idx is None:
+            self.F_idx = f"- F_idx: {list(batch.F_idx.shape)}"
+        else:
+            self.F_idx = ""
+
+    def __call__(self):
+        return "\n".join([self.header, self.X, self.W, self.W_hat, self.F_idx])
+
+
+class BatchProcessor(ABCParse.ABCParse):
+    """
+    Batch Items:
+    ------------
+    0. t - time. always required
+    1. X - data. tensor of size: [batch_size, len(t), n_dim]. always required.
+    2. W - weight. tensor of size: [batch_size, len(t), 1]
+    3. F_idx - fate_idx. tensor of size: [batch_Size, len(t), 1]
+    """
+
+    def __init__(self, batch: List[torch.Tensor], batch_idx: int) -> None:
+
+        self.__parse__(locals(), private=["batch"])
+
+    def _sum_normalize(
+        self, X: Union[torch.Tensor, np.ndarray], sample_axis=1
+    ) -> Union[torch.Tensor, np.ndarray]:
+        """
+        Parameters:
+        -----------
+        X
+            torch.Tensor or nump.array
+
+        sample_axis
+            default: 1
+            type: int
+
+        Returns:
+        --------
+        return X_
+        """
+        return X / X.sum(sample_axis)[:, None]
+
+    @property
+    def device(self):
+        return self._batch[0].device
+
+    @property
+    def n_batch_items(self) -> int:
+        return len(self._batch)
+
+    @property
+    def batch_size(self) -> int:
+        return self._batch[0].shape[0]
+
+    @property
+    def t(self) -> torch.Tensor:
+        return self._batch[0].unique()
+
+    @property
+    def X(self) -> torch.Tensor:
+        return self._batch[1].transpose(1, 0).contiguous()
+
+    @property
+    def X0(self) -> torch.Tensor:
+        return self.X[0]
+
+    @property
+    def W_hat(self) -> torch.Tensor:
+        if not hasattr(self, "_W_hat"):
+            if self.n_batch_items >= 3:
+                W = self._batch[2].transpose(1, 0)
+                for i in range(1, len(self.t)):
+                    W[i] = torch.exp((self.t[i] - self.t[0]) * W[0])
+                W[0] = torch.ones_like(W[0])
+                self._W_hat = self._sum_normalize(W, sample_axis=1).contiguous()
+            else:
+                self._W_hat = self._sum_normalize(
+                    torch.ones([len(self.t), self.batch_size, 1], device=self.device)
+                ).contiguous()
+        return self._W_hat
+
+    @property
+    def W(self) -> torch.Tensor:
+        return self._sum_normalize(torch.ones_like(self.W_hat)).contiguous()
+
+    @property
+    def F_idx(self):
+        if self.n_batch_items >= 4:
+            return (
+                self._batch[3]
+                .transpose(1, 0)[0]
+                .detach()
+                .cpu()
+                .numpy()
+                .flatten()
+                .astype(int)
+                .astype(str)
+            )
+
+    def __repr__(self) -> str:
+        return BatchRepresentation(self)()
