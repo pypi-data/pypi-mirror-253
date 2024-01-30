7ec15e47101d0e56b450513bc81624abb06fd67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:04:43 +0000

    Add package unix-socket-server/0.0.5/unix_socket_server-0.0.5-py3-none-any.whl

---
+++ packages/unix-socket-server/unix_socket_server-0.0.5-py3-none-any.whl/unix_socket_server/server/handlers/stream/_stream.py
@@ -0,0 +1,20 @@
+import logging, socket
+from socketserver import BaseRequestHandler
+from unix_socket_server.server.response import Response
+
+class StreamServerHandler(BaseRequestHandler):
+     RESPONSE : Response
+     def handle(self):
+        try:
+            if self.RESPONSE.is_chunked:
+                for chunk in self.RESPONSE.chunked_response:
+                    self.request.send(chunk)
+            else:
+                self.request.sendall(self.RESPONSE.response)
+        except Exception as exc:
+            logging.error(exc)
+        try:
+            self.request.shutdown(socket.SHUT_WR)
+            self.request.close()
+        except OSError as exc:
+            logging.error(exc)
