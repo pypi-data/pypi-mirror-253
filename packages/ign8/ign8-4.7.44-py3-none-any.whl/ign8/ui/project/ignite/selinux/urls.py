6efa431d1b2b39ce7c8543306a8c57d315102245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:04:32 +0000

    Add package ign8/4.7.44/ign8-4.7.44-py3-none-any.whl

---
+++ packages/ign8/ign8-4.7.44-py3-none-any.whl/ign8/ui/project/ignite/selinux/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path, include
+from .views import selinux_list, selinux_event_list , UploadSelinuxDataView, UploadSElinuxEventView, message_list
+from .views import SetroubleshootEntry_list, SetroubleshootEntry_host, SetroubleshootEntryAPIview, messageAPIview, suggestionAPIview
+from .views import selinuxAPIview, SetroubleshootEntry_list_full, host_message
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path('', selinux_list, name='selinux_list'),
+    path('messages/', message_list, name='selinux_messages_list'),
+    path('messages/<pk>', message_list, name='selinux_messages_by_host_list'),
+    path('selinux_event_list/', selinux_event_list, name='selinux_event_list'),
+    path('upload_selinux_data/', UploadSelinuxDataView, name='upload_selinux_data'),
+    path('upload_selinux_event/', UploadSElinuxEventView, name='upload_selinux_event'),
+    path('api/setroubleshoot/upload/', SetroubleshootEntryAPIview, name='upload_setroubleshoot_entry'),
+    path('api/message/upload/', messageAPIview, name='upload_message'),
+    path('api/suggestion/upload/', suggestionAPIview, name='upload_suggestion'),
+    path('SetroubleshootEntry/<str:hostname>/', SetroubleshootEntry_host, name='SetroubleshootEntry_host'),
+    path('SetroubleshootEntry_list/', SetroubleshootEntry_list, name='SetroubleshootEntry_list'),
+    path('SetroubleshootEntry_list_full/', SetroubleshootEntry_list, name='SetroubleshootEntry_list'),
+]
