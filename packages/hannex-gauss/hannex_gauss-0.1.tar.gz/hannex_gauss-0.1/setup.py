fa605f44e4c724aee4db6fba8ab57eb0ee099170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:04:28 +0000

    Add package hannex-gauss/0.1/hannex_gauss-0.1.tar.gz

---
+++ packages/hannex-gauss/hannex_gauss-0.1.tar.gz/hannex_gauss-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hannex_gauss',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hannex_gauss'],
+      zip_safe=False)
