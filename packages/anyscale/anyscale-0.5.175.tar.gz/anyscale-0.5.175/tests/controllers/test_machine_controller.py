c105ac5740dda63ff153fbfdc1d357235206b769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:29 +0000

    Add package anyscale/0.5.175/anyscale-0.5.175.tar.gz

---
+++ packages/anyscale/anyscale-0.5.175.tar.gz/anyscale-0.5.175/tests/controllers/test_machine_controller.py
@@ -0,0 +1,36 @@
+from unittest.mock import Mock
+
+import pytest
+
+from anyscale.client.openapi_client.models import (
+    ListMachinesResponse,
+    MachineAllocationState,
+    MachineConnectionState,
+    MachineInfo,
+)
+from frontend.cli.anyscale.controllers.machine_controller import MachineController
+
+
+@pytest.mark.parametrize("cloud_id", ["cld_123"])
+def test_list_machines(mock_auth_api_client, cloud_id: str) -> None:
+    response = ListMachinesResponse(
+        machines=[
+            MachineInfo(
+                machine_id="m-123",
+                hostname="dummy",
+                machine_shape="lambda-a100-80g",
+                connection_state=MachineConnectionState.CONNECTED,
+                allocation_state=MachineAllocationState.AVAILABLE,
+            )
+        ]
+    )
+
+    api_response = Mock()
+    api_response.result = response
+
+    machine_controller = MachineController()
+    machine_controller.api_client.list_machines_api_v2_machines_get = Mock(
+        return_value=api_response
+    )
+    output = machine_controller.list_machines(cloud_id=cloud_id, cloud=None)
+    assert output == response
