d4b90527ce7ea7332f5a61494468cee4bdd23247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:05:10 +0000

    Add package weaviate-client/4.4rc0/weaviate-client-4.4rc0.tar.gz

---
+++ packages/weaviate-client/weaviate-client-4.4rc0.tar.gz/weaviate-client-4.4rc0/mock_tests/test_collection.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+import pytest
+from pytest_httpserver import HTTPServer
+
+import weaviate
+from mock_tests.conftest import MOCK_SERVER_URL, MOCK_PORT, MOCK_IP
+
+from weaviate.exceptions import UnexpectedStatusCodeError
+
+
+@pytest.mark.skip(reason="Fails with gRPC not enabled error")
+def test_warning_old_weaviate(recwarn: pytest.WarningsRecorder, ready_mock: HTTPServer) -> None:
+    ready_mock.expect_request("/v1/meta").respond_with_json({"version": "1.21.0"})
+    ready_mock.expect_request("/v1/objects").respond_with_json({})
+
+    client = weaviate.WeaviateClient(MOCK_SERVER_URL)
+    client.collections.get("Class").data.insert(
+        {
+            "date": datetime.now(),
+        }
+    )
+
+    assert len(recwarn) == 1
+    w = recwarn.pop()
+    assert issubclass(w.category, UserWarning)
+    assert str(w.message).startswith("Con002")
+
+
+def test_status_code_exception(weaviate_mock: HTTPServer) -> None:
+    weaviate_mock.expect_request("/v1/schema/Test").respond_with_json(response_json={}, status=403)
+
+    client = weaviate.connect_to_local(port=MOCK_PORT, host=MOCK_IP, skip_init_checks=True)
+    collection = client.collections.get("Test")
+    with pytest.raises(UnexpectedStatusCodeError) as e:
+        collection.config.get()
+    assert e.value.status_code == 403
+    weaviate_mock.check_assertions()
