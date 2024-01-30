fe2ee7efde5e5bbcf4bd02ec66708c7fd40783e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:04:57 +0000

    Add package easyMirai-websocket/1.0.39a0/easyMirai-websocket-1.0.39a0.tar.gz

---
+++ packages/easyMirai-websocket/easyMirai-websocket-1.0.39a0.tar.gz/easyMirai-websocket-1.0.39a0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import easyMirais
+
+setup(
+    name="easyMirai-websocket",
+    version=easyMirais.__version__(),
+    author="HexMikuMax & ExMikuPro",
+    author_email="sfnco-miku@outlook.com",
+    python_requires=">=3.6.0",
+    packages=find_packages(),
+    license="GPLv3",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
