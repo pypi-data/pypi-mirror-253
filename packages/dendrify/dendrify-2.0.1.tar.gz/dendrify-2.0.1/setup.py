574a1fa2cb42c7798c2d2dfcc03af65ddb89d3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:05:03 +0000

    Add package dendrify/2.0.1/dendrify-2.0.1.tar.gz

---
+++ packages/dendrify/dendrify-2.0.1.tar.gz/dendrify-2.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+VERSION = '2.0.1'
+DESCRIPTION = 'A package for adding dendrites to SNNs'
+with open("README.rst") as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="dendrify",
+    version=VERSION,
+    author="Michalis Pagkalos",
+    author_email="<mpagkalos93@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['brian2==2.5.4'],
+    keywords=['python', 'brian2', 'dendrites', 'SNNs', 'network models'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ]
+)
