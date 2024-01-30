aebc32e7901b26548a3fd46948e48ea995d5723e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:40 +0000

    Add package proteinTools/2.2.4/proteinTools-2.2.4.tar.gz

---
+++ packages/proteinTools/proteinTools-2.2.4.tar.gz/proteinTools-2.2.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name = "proteinTools",
+    version = "2.2.4",
+    author = "Christian de Frondeville",
+    description = "Lightweight, object-oriented backend bioinformatics package which simplifies interacting with proteins.",
+    long_description = long_description,
+    packages = ["proteinTools"],
+    install_requires=['pandas','urllib3','mygene', 'pubchempy', 'stringdb', 'chembl-webresource-client']
+)
