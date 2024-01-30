ec78c5ee6941cce2600c03d5beccfbff097638ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:06:20 +0000

    Add package airbyte-source-pokeapi/0.2.0.dev202401291710/airbyte-source-pokeapi-0.2.0.dev202401291710.tar.gz

---
+++ packages/airbyte-source-pokeapi/airbyte-source-pokeapi-0.2.0.dev202401291710.tar.gz/airbyte-source-pokeapi-0.2.0.dev202401291710/setup.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk",
+]
+
+TEST_REQUIREMENTS = [
+    "requests-mock~=1.9.3",
+    "pytest~=6.2",
+    "pytest-mock~=3.6.1",
+]
+
+setup(
+    description="Source implementation for Pokeapi.",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
