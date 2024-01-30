d26cc56701b72f03fa7307b23e9003bd92d6c0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 06:05:08 +0000

    Add package xia-pattern-xia/0.0.2/xia_pattern_xia-0.0.2-cp311-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-pattern-xia/xia_pattern_xia-0.0.2-cp311-none-macosx_11_0_x86_64.whl/xia_pattern_xia/__init__.py
@@ -0,0 +1,9 @@
+from xia_pattern_xia.templates import XiaPatternTemplate
+from xia_pattern_xia.model_pattern import XiaModelPattern
+
+__all__ = [
+    "XiaPatternTemplate",
+    "XiaModelPattern"
+]
+
+__version__ = "0.0.2"
