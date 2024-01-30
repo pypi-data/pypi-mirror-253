4667319375b2fdfb0ea2048b968dfcaeed8eb400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:44 +0000

    Add package reverse-diagrams/1.0.4/reverse_diagrams-1.0.4.tar.gz

---
+++ packages/reverse-diagrams/reverse_diagrams-1.0.4.tar.gz/reverse_diagrams-1.0.4/src/banner/banner.py
@@ -0,0 +1,33 @@
+from colorama import Fore
+
+
+banner = """Reverse Diagrams 
+ #####   ######  #    #  ######  #####    ####   ######
+ #    #  #       #    #  #       #    #  #       #
+ #    #  #####   #    #  #####   #    #   ####   #####
+ #####   #       #    #  #       #####        #  #
+ #   #   #        #  #   #       #   #   #    #  #
+ #    #  ######    ##    ######  #    #   ####   ######
+
+
+ #####      #      ##     ####   #####     ##    #    #   ####
+ #    #     #     #  #   #    #  #    #   #  #   ##  ##  #
+ #    #     #    #    #  #       #    #  #    #  # ## #   ####
+ #    #     #    ######  #  ###  #####   ######  #    #       #
+ #    #     #    #    #  #    #  #   #   #    #  #    #  #    #
+ #####      #    #    #   ####   #    #  #    #  #    #   ####
+"""
+
+
+def get_version(version):
+    """
+    Get package version.
+
+    :param version:
+    :return:
+    """
+    print(Fore.BLUE + banner)
+
+    print(version + Fore.RESET)
+
+    return version
