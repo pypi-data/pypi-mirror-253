8f40d462295c8bf50a3836f55d4645b3770818d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:02 +0000

    Add package lckytools/0.0.10/lckytools-0.0.10-py3-none-any.whl

---
+++ packages/lckytools/lckytools-0.0.10-py3-none-any.whl/lckytools/sql/schema.py
@@ -0,0 +1,23 @@
+def create_schema(catalog_name, schema_name, ignore_if_exist=False):
+    if ignore_if_exist:
+        return f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.{schema_name};"
+    else:
+        return f"CREATE SCHEMA {catalog_name}.{schema_name};"
+
+
+def drop_schema(catalog_name, schema_name, ignore_if_not_exist=False, cascade=False):
+    if ignore_if_not_exist:
+        sql = f"DROP SCHEMA IF EXISTS {catalog_name}.{schema_name}"
+    else:
+        sql = f"DROP SCHEMA {catalog_name}.{schema_name}"
+
+    if cascade:
+        sql += " CASCADE"
+    else:
+        sql += " RESTRICT"
+
+    return f"{sql};"
+
+
+def show_schemas(catalog_name):
+    return f"SHOW SCHEMAS FROM '{catalog_name}';"
