e2d0a9da5cc6862974b2ef67bc311d220556750c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:41 +0000

    Add package fluidsim/0.8.0rc0/fluidsim-0.8.0rc0.tar.gz

---
+++ packages/fluidsim/fluidsim-0.8.0rc0.tar.gz/fluidsim-0.8.0rc0/old/basilisk/__init__.py
@@ -0,0 +1,17 @@
+"""Basilisk solvers (:mod:`fluidsim.base.basilisk`)
+===================================================
+A reference implementation to use 2D solvers in `Basilisk <http://basilisk.fr>`__ via
+FluidSim.
+
+Provides:
+
+.. autosummary::
+   :toctree:
+
+    solver
+    state
+    operators
+    time_stepping
+    output
+
+"""
