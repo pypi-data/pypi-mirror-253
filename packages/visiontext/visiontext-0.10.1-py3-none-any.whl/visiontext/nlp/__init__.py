cae61eff5302b2bac611d852632524f08edc8b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:57 +0000

    Add package visiontext/0.10.1/visiontext-0.10.1-py3-none-any.whl

---
+++ packages/visiontext/visiontext-0.10.1-py3-none-any.whl/visiontext/nlp/__init__.py
@@ -0,0 +1,10 @@
+from .nltktools import (
+    rebuild_from_words_and_connectors,
+    tokenize_words_and_connectors,
+    tokenize_sentences_and_connectors,
+    apply_nltk_tokenizer,
+    ensure_setup_nltk,
+)
+from .regextools import preprocess_text_simple
+from .spacytools import maybe_download_spacy_model, get_or_maybe_download_spacy_model
+from .lemmatizer import get_lemmatizer
