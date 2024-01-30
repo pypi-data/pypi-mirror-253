3c6f24e7d967ca385fec2fca9cecca81f84c6002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:29 +0000

    Add package vuegraf/1.7.0/vuegraf-1.7.0.tar.gz

---
+++ packages/vuegraf/vuegraf-1.7.0.tar.gz/vuegraf-1.7.0/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+base_dir = os.path.dirname(__file__)
+setup(
+    name='vuegraf',
+    version='1.7.0',
+    author='Jason Ertel',
+    url='https://github.com/jertel/vuegraf',
+    description='Populate metrics from your Emporia Vue energy monitoring devices into an InfluxDB',
+    setup_requires='setuptools',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': ['vuegraf=vuegraf.vuegraf']
+    },
+    packages=find_packages(),
+    install_requires=[
+        'influxdb>=5.3.1',
+        'influxdb_client>=1.33.0',
+        'pyemvue>=0.16.0',
+        'argparse>= 1.4.0'
+    ]
+)
