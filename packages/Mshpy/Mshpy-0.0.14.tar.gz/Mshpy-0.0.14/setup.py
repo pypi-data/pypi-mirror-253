56d8ed5029b205777a00104942a63c34594f48de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 15:06:52 +0000

    Add package Mshpy/0.0.14/Mshpy-0.0.14.tar.gz

---
+++ packages/Mshpy/Mshpy-0.0.14.tar.gz/Mshpy-0.0.14/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+DESCRIPTION = 'Simplified Earth magnetosheath model'
+
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION =  (this_directory / "README.md").read_text()
+
+setup(
+    name='Mshpy',
+    version='0.0.14',
+    packages=find_packages(),
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'sympy'
+    ],
+)
