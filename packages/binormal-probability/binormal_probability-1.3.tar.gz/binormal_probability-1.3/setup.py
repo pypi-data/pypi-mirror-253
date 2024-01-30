c0b27a049035ea34565c7b751eac06cda7f1eb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:19 +0000

    Add package binormal-probability/1.3/binormal_probability-1.3.tar.gz

---
+++ packages/binormal-probability/binormal_probability-1.3.tar.gz/binormal_probability-1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binormal_probability',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['binormal_probability'],
+      zip_safe=False)
