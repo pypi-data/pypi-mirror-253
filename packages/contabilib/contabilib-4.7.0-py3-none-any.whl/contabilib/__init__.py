6050011829846f3116d4c680022e85e8b22c12c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 15:06:34 +0000

    Add package contabilib/4.7.0/contabilib-4.7.0-py3-none-any.whl

---
+++ packages/contabilib/contabilib-4.7.0-py3-none-any.whl/contabilib/__init__.py
@@ -0,0 +1,8 @@
+from .calcular_rescisao import rescisao_aposentadoria, rescisao_demissao, rescisao_falecimento, rescisao_justa_causa, rescisao_sem_justa_causa
+from .calcular_balanco import calcularbalanco
+from .exportar_pdf import gerar_pdf_balanco, gerar_pdf_rescisao
+from .grafico import gerargraficorescisao, gerargraficopatrimonio
+
+ 
+
+
