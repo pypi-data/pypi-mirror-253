bc9b1cd0128219abe0f11867de7f3a6817e7184b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 15:06:15 +0000

    Add package configmate/0.1.7a2/configmate-0.1.7a2-py3-none-any.whl

---
+++ packages/configmate/configmate-0.1.7a2-py3-none-any.whl/configmate/base/types.py
@@ -0,0 +1,23 @@
+import os
+import pathlib
+from typing import Mapping, Protocol, Sequence, TypeVar, Union
+
+T = TypeVar("T")
+
+FilePathTypes = (str, os.PathLike, pathlib.Path)
+FilePath = Union[str, os.PathLike, pathlib.Path]
+CliArgs = Sequence[str]
+NestedDict = Mapping[str, Union[T, "NestedDict[T]"]]
+
+# fmt: off
+class Unset: 'Used to mark a value that will be set.'
+class Infer: 'Marks a value that is inferred during runtime.'
+class Sentinel: 'Used to catch exhausted iterators.'
+SENTINEL = Sentinel()
+# fmt: on
+
+
+class HasDescription(Protocol):
+    @classmethod
+    def describe(cls) -> str:
+        ...
