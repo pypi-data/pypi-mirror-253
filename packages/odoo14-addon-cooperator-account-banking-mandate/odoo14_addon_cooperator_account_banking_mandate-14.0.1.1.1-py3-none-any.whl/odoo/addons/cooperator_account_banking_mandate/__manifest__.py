6c3f2ba869babbea510f32b147a9362eb36950e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:18 +0000

    Add package odoo14-addon-cooperator-account-banking-mandate/14.0.1.1.1/odoo14_addon_cooperator_account_banking_mandate-14.0.1.1.1-py3-none-any.whl

---
+++ packages/odoo14-addon-cooperator-account-banking-mandate/odoo14_addon_cooperator_account_banking_mandate-14.0.1.1.1-py3-none-any.whl/odoo/addons/cooperator_account_banking_mandate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Cooperator Account Banking Mandate",
+    "version": "14.0.1.1.1",
+    "license": "AGPL-3",
+    "summary": """
+        This module adds mandate selection to cooperator subscription request.""",
+    "author": "Som IT Cooperatiu SCCL",
+    "category": "Banking addons",
+    "website": "https://coopdevs.org",
+    "depends": [
+        "cooperator",
+        "cooperator_account_payment",
+        "account_banking_mandate",
+        "account_banking_sepa_direct_debit",
+    ],
+    "data": [
+        "views/subscription_request_views.xml",
+    ],
+    "installable": True,
+}
