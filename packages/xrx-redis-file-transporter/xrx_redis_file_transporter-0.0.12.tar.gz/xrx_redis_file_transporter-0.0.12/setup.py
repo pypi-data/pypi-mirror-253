ae515fcf212dfb057c74b667027a44b852918ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:04:32 +0000

    Add package xrx-redis-file-transporter/0.0.12/xrx_redis_file_transporter-0.0.12.tar.gz

---
+++ packages/xrx-redis-file-transporter/xrx_redis_file_transporter-0.0.12.tar.gz/xrx_redis_file_transporter-0.0.12/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xrx_redis_file_transporter",
+    version="0.0.12",
+    description="A redis file transport package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
