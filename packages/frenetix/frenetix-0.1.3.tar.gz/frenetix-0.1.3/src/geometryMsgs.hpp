cc4c474616bfd669986df027ff7f35e56023fe95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:05:00 +0000

    Add package frenetix/0.1.3/frenetix-0.1.3.tar.gz

---
+++ packages/frenetix/frenetix-0.1.3.tar.gz/frenetix-0.1.3/src/geometryMsgs.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stddef.h>
+#include <Eigen/Core>
+#include <iostream>
+#include <vector>
+
+struct PoseWithCovariance
+{
+    PoseWithCovariance();
+    PoseWithCovariance(
+        const Eigen::Vector3d& position,
+        const Eigen::Vector4d& orientation,
+        const Eigen::Matrix<double,6,6>& covariance
+    );
+
+    Eigen::Vector3d position;
+    Eigen::Vector4d orientation;
+
+    Eigen::Matrix<double,6,6> covariance;
+
+    friend std::ostream& operator<<(std::ostream& os, const PoseWithCovariance& pose);
+};
+
+
+struct PredictedObject
+{
+    PredictedObject(size_t length);
+    PredictedObject(
+        int object_id,
+        std::vector<PoseWithCovariance> predictedPath
+    );
+
+
+
+
+    int object_id;
+    std::vector<PoseWithCovariance> predictedPath;
+
+    friend std::ostream& operator<<(std::ostream& os, const PredictedObject& obj);
+};
