3bd4f8d9e75bac7f1ff82391240ccafaffaaa48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 15:06:44 +0000

    Add package AyugeSpiderTools/3.9.5/ayugespidertools-3.9.5.tar.gz

---
+++ packages/AyugeSpiderTools/ayugespidertools-3.9.5.tar.gz/ayugespidertools-3.9.5/ayugespidertools/scraper/pipelines/mysql/fantasy.py
@@ -0,0 +1,9 @@
+from ayugespidertools.scraper.pipelines.mysql import AyuMysqlPipeline
+
+__all__ = ["AyuFtyMysqlPipeline"]
+
+
+class AyuFtyMysqlPipeline(AyuMysqlPipeline):
+    """Mysql 存储场景的 scrapy pipeline 扩展"""
+
+    ...
