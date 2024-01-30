1ba8d5ff869e90a4ab3e9679182fcb2499a6fb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:05:51 +0000

    Add package applipy-http/2.2.1/applipy_http-2.2.1.tar.gz

---
+++ packages/applipy-http/applipy_http-2.2.1.tar.gz/applipy_http-2.2.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from distutils.util import convert_path
+from os import path
+
+ns = {}
+ver_path = convert_path('applipy_http/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), ns)
+version = ns['__version__']
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='applipy_http',
+    url='https://gitlab.com/applipy/applipy_http',
+    project_urls={
+        'Source': 'https://gitlab.com/applipy/applipy_http',
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Library for building http services using applipy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Apache 2.0',
+    author='Alessio Linares',
+    author_email='mail@alessio.cc',
+    version=version,
+    packages=find_packages(exclude=['doc', 'tests']),
+    data_files=[],
+    python_requires='>=3.8',
+    install_requires=[
+        'applipy>=2.0.0,<3.0.0',
+        'aiohttp~=3.8',
+        'aiohttp_cors~=0.7',
+    ],
+    scripts=[],
+)
