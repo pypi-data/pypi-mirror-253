ed5fe96c470b950c8af4b30ae8f88f18a14c5c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:21 +0000

    Add package supermeter-autowrapt/1.1.9/supermeter-autowrapt-1.1.9.tar.gz

---
+++ packages/supermeter-autowrapt/supermeter-autowrapt-1.1.9.tar.gz/supermeter-autowrapt-1.1.9/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup
+
+# In newer debian-based systems, there's a dist-packages directory
+# where user packages get installed (and not site-packages), so we
+# want to write our .pth file there as well.
+def get_lib_dirs():
+    dirs = []
+    try:
+        from distutils.sysconfig import get_python_lib
+        dirs.append(get_python_lib(prefix=''))
+    except ImportError:
+        pass
+    import sysconfig
+    purelib = sysconfig.get_paths().get('purelib', '')
+    datalib = sysconfig.get_paths().get('data', '')
+    if purelib and datalib and purelib.startswith(datalib):
+        dirs.append(purelib[len(datalib) + 1:])
+    return set(dirs)
+
+
+def load_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), "rt") as f:
+        return f.read()
+
+
+setup_kwargs = dict(
+    name = 'supermeter-autowrapt',
+    version = '1.1.9',
+    description = 'Boostrap mechanism for monkey patches for supermeter.',
+    author = 'SuperTenant Ltd.',
+    author_email = 'info@supertenant.com',
+    license = 'BSD',
+    url = 'https://github.com/supertenant/supermeter-autowrapt',
+    packages = ['supermeter-autowrapt'],
+    package_dir = {'supermeter-autowrapt': 'src'},
+    package_data = {},
+    data_files = [(d, ['supermeter-init.pth']) for d in get_lib_dirs()],
+    entry_points = {},
+    install_requires = ['wrapt>=1.10.4'],
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
+    platforms=['any']
+)
+
+setup(**setup_kwargs)
