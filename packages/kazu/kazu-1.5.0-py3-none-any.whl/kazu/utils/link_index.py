658f3f3d26341749564bec2282363496644a863a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:28 +0000

    Add package kazu/1.5.0/kazu-1.5.0-py3-none-any.whl

---
+++ packages/kazu/kazu-1.5.0-py3-none-any.whl/kazu/utils/link_index.py
@@ -0,0 +1,141 @@
+import logging
+import hashlib
+from typing import Optional
+from collections.abc import Iterable
+
+import numpy
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from kazu.utils.caching import kazu_disk_cache
+from kazu.data.data import SynonymTerm, SynonymTermWithMetrics
+from kazu.database.in_memory_db import (
+    MetadataDatabase,
+    SynonymDatabase,
+    NormalisedSynonymStr,
+)
+from kazu.language.string_similarity_scorers import BooleanStringSimilarityScorer
+from kazu.ontology_preprocessing.base import (
+    OntologyParser,
+)
+from kazu.utils.string_normalizer import StringNormalizer
+from kazu.utils.utils import create_char_ngrams
+
+logger = logging.getLogger(__name__)
+
+
+class DictionaryIndex:
+    """The dictionary index looks for SynonymTerms via a char ngram search between the
+    normalised version of the query string and the term_norm of all SynonymTerms
+    associated with the provided OntologyParser."""
+
+    def __init__(
+        self,
+        parser: OntologyParser,
+        boolean_scorers: Optional[list[BooleanStringSimilarityScorer]] = None,
+    ):
+        """
+
+        :param parser:
+        :param boolean_scorers: precision can be increased by applying boolean checks on the returned result, for
+            instance, checking that all integers are represented, that any noun modifiers are present etc
+        """
+
+        parser.populate_databases(force=False)
+        self.synonym_db: SynonymDatabase = SynonymDatabase()
+        self.synonyms_for_parser = self.synonym_db.get_all(parser.name)
+        self.entity_class = parser.entity_class
+        self.parser_name = parser.name
+        self.metadata_db: MetadataDatabase = MetadataDatabase()
+        self.namespace = self.__class__.__name__
+        self.boolean_scorers = boolean_scorers
+        self.normalized_synonyms: list[str] = []
+        self.synonym_list: list[SynonymTerm] = []
+        # we sort here to ensure that the order of self.synonym_list matches the
+        # order of the synonyms in the cached vectorized from _build_index_cache
+        # if present, without relying on the order in the SynonymDatabase being the
+        # same.
+        for norm_syn, syn_term in sorted(self.synonyms_for_parser.items()):
+            self.normalized_synonyms.append(norm_syn)
+            self.synonym_list.append(syn_term)
+        self.vectorizer, self.tf_idf_matrix = self.build_index_cache()
+
+    def apply_boolean_scorers(self, reference_term: str, query_term: str) -> bool:
+
+        if self.boolean_scorers is not None:
+            return all(
+                scorer(reference_term=reference_term, query_term=query_term)
+                for scorer in self.boolean_scorers
+            )
+        else:
+            return True
+
+    def search(self, query: str, top_n: int = 15) -> Iterable[SynonymTermWithMetrics]:
+        """Search the index with a query string.
+
+        .. note::
+           This method will only return results with search scores above 0.
+           As a result, it will return fewer than ``top_n`` results when there are not
+           ``top_n`` :class:`~.SynonymTerm`\\ s in the index that score about 0 for the given
+           query.
+
+        :param query: term to search
+        :param top_n: max number of results
+        :return:
+        """
+
+        match_norm = StringNormalizer.normalize(query, entity_class=self.entity_class)
+        exact_match_term = self.synonyms_for_parser.get(match_norm)
+        if exact_match_term is not None:
+            term_with_metrics = SynonymTermWithMetrics.from_synonym_term(
+                exact_match_term, search_score=100.0, bool_score=True, exact_match=True
+            )
+            yield term_with_metrics
+
+        else:
+            # benchmarking suggests converting to dense is faster than using the
+            # csr_matrix version. Strange...
+            query_arr = self.vectorizer.transform([match_norm]).todense()
+            # minus to negate, so arg sort works in correct order
+            score_matrix = np.squeeze(-np.asarray(self.tf_idf_matrix.dot(query_arr.T)))
+            neighbours = score_matrix.argsort()[:top_n]
+            # don't use torch for this - it's slow
+            # query = torch.FloatTensor(query)
+            # score_matrix = self.tf_idf_matrix_torch.matmul(query.T)
+            # score_matrix = torch.squeeze(score_matrix.T)
+            # neighbours = torch.argsort(score_matrix, descending=True)[:top_n]
+
+            distances = score_matrix[neighbours]
+            distances = 100 * -distances
+            for neighbour, score in zip(neighbours, distances):
+                if score > 0.0:
+                    # get by index
+                    term = self.synonym_list[neighbour]
+                    if self.apply_boolean_scorers(
+                        reference_term=match_norm, query_term=term.term_norm
+                    ):
+                        term_with_metrics = SynonymTermWithMetrics.from_synonym_term(
+                            term, search_score=score, bool_score=True, exact_match=False
+                        )
+                        yield term_with_metrics
+                    else:
+                        logger.debug("filtered term %s as failed boolean checks", term)
+                else:
+                    logger.debug("score is 0.0")
+
+    @kazu_disk_cache.memoize(ignore={0, 1})
+    def _build_index_cache(
+        self, synonyms_for_parser: Iterable[NormalisedSynonymStr], _cache_key: bytes
+    ) -> tuple[TfidfVectorizer, numpy.ndarray]:
+        logger.info("building TfidfVectorizer for %s", self.parser_name)
+        vectorizer = TfidfVectorizer(min_df=1, analyzer=create_char_ngrams, lowercase=False)
+        tf_idf_matrix = vectorizer.fit_transform(synonyms_for_parser)
+        return vectorizer, tf_idf_matrix
+
+    def build_index_cache(self) -> tuple[TfidfVectorizer, numpy.ndarray]:
+        """Build the cache for the index."""
+
+        h = hashlib.new("sha1", usedforsecurity=False)
+        for norm_syn in self.normalized_synonyms:
+            h.update(norm_syn.encode(encoding="utf-8"))
+
+        return self._build_index_cache(self.normalized_synonyms, h.digest())
---
+++ packages/kazu/kazu-1.5.0-py3-none-any.whl/kazu/utils/utils.py
@@ -0,0 +1,199 @@
+import logging
+from collections.abc import Iterable, Sequence
+from pathlib import Path
+from typing import Union, overload, Any
+
+from kazu.data.data import (
+    Document,
+    Entity,
+    Section,
+    CuratedTerm,
+    CuratedTermBehaviour,
+    MentionConfidence,
+    SynonymTerm,
+)
+from kazu.utils.string_normalizer import StringNormalizer
+from transformers import BatchEncoding, PreTrainedTokenizerBase
+from transformers.file_utils import PaddingStrategy
+from transformers.tokenization_utils_base import TruncationStrategy
+
+logger = logging.getLogger(__name__)
+
+
+def extract_term_strings_from_synonym_terms(synonym_terms: set[SynonymTerm]) -> set[str]:
+    return {term_str for syn_term in synonym_terms for term_str in syn_term.terms}
+
+
+def string_to_putative_curation(term_string: str, entity_class: str) -> CuratedTerm:
+    """Create a :class:`.CuratedTerm` from a string.
+
+    Uses some simple heuristics to suggest basic rules for how
+    this term should be handled by Kazu - You will probably want to
+    change these in some cases, depending on your use case.
+
+    This should only be used with original ontology terms (not generated).
+
+    :param term_string: becomes the :attr:`~.CuratedTerm.curated_synonym` of the result
+    :param entity_class: used to determine whether the string is symbolic or not, and
+        thus its default case sensitivity behaviour
+    :return:
+    """
+
+    is_symbolic = StringNormalizer.classify_symbolic(term_string, entity_class)
+    conf = MentionConfidence.POSSIBLE if is_symbolic else MentionConfidence.PROBABLE
+    return CuratedTerm(
+        curated_synonym=term_string,
+        mention_confidence=conf,
+        case_sensitive=is_symbolic,
+        behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+        source_term=None,
+    )
+
+
+def find_document_from_entity(docs: list[Document], entity: Entity) -> Document:
+    """For a given entity and a list of docs, find the doc the entity belongs to.
+
+    :param docs:
+    :param entity:
+    :return:
+    """
+    for doc in docs:
+        if entity in doc.get_entities():
+            return doc
+    raise RuntimeError(f"Error! Entity {entity} is not attached to a document")
+
+
+def documents_to_id_section_map(docs: list[Document]) -> dict[int, Section]:
+    """Return a map of documents, indexed by order of sections.
+
+    :param docs:
+    :return:
+    """
+    result = {}
+    i = 0
+    for doc in docs:
+        for section in doc.sections:
+            result[i] = section
+            i += 1
+    return result
+
+
+def documents_to_document_section_batch_encodings_map(
+    docs: list[Document],
+    tokenizer: PreTrainedTokenizerBase,
+    stride: int = 128,
+    max_length: int = 512,
+) -> tuple[BatchEncoding, dict[int, Section]]:
+    """Convert documents into a BatchEncoding. Also returns a list of <int + section>
+    for the resulting encoding.
+
+    :param docs:
+    :param tokenizer:
+    :param stride:
+    :param max_length:
+    :return:
+    """
+    id_section_map = documents_to_id_section_map(docs)
+    strings = [section.text for section in id_section_map.values()]
+
+    batch_encodings = tokenizer(
+        strings,
+        stride=stride,
+        max_length=max_length,
+        truncation=TruncationStrategy.LONGEST_FIRST,
+        return_overflowing_tokens=True,
+        padding=PaddingStrategy.MAX_LENGTH,
+    )
+    return batch_encodings, id_section_map
+
+
+def get_match_entity_class_hash(ent: Entity) -> int:
+    return hash(
+        (
+            ent.match,
+            ent.entity_class,
+        )
+    )
+
+
+PathLike = Union[str, Path]
+
+
+def as_path(p: PathLike) -> Path:
+    return p if isinstance(p, Path) else Path(p)
+
+
+class EntityClassFilter:
+    """A condition that returns True if a document has any entities that match the class
+    of the required_entity_classes."""
+
+    def __init__(self, required_entity_classes: Iterable[str]):
+        """
+
+        :param required_entity_classes: list of str, specifying entity classes to assess
+        """
+        self.required_entities = set(required_entity_classes)
+
+    def __call__(self, document: Document) -> bool:
+        return any(
+            (entity.entity_class in self.required_entities for entity in document.get_entities())
+        )
+
+
+@overload
+def _create_ngrams_iter(tokens: str, n: int) -> Iterable[str]:
+    pass
+
+
+@overload
+def _create_ngrams_iter(tokens: Sequence[str], n: int) -> Iterable[Sequence[str]]:
+    pass
+
+
+def _create_ngrams_iter(tokens, n=2):
+    """Yields ngrams of the input as a sequence of strings.
+
+    Tokens can be a single string where each token is a character, or an Iterable of
+    words. If tokens is a str
+    """
+    num_tokens = len(tokens)
+    for i in range(num_tokens):
+        ngram_end_index = i + n
+        if ngram_end_index > num_tokens:
+            # ngram would extend beyond end of parts
+            # it's ok for it to be the same number though as otherwise
+            # we don't get the final word of ngrams at the right of parts
+            break
+        yield tokens[i : i + n]
+
+
+def create_char_ngrams(s: str, n: int = 2) -> list[str]:
+    """Return list of char ngrams as a string."""
+    return list(_create_ngrams_iter(s, n))
+
+
+def create_word_ngrams(s: str, n: int = 2) -> list[str]:
+    """Return list of word ngrams as a single space-separated string."""
+    words = s.split(" ")
+    ngrams_iter = _create_ngrams_iter(words, n)
+    return [" ".join(ngram) for ngram in ngrams_iter]
+
+
+class Singleton(type):
+    _instances: dict[type, Any] = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+    @staticmethod
+    def clear_all():
+        logger.warning(
+            "When clearing singletons, check that instances of classes with metaclass=Singleton are "
+            "not used as class fields, as this will cause unexpected behaviour. Also note that any existing "
+            "classes that use a Singleton will need to be re-instantiated to work correctly!. Much pain "
+            "can be avoided by observing this simple principle! Some people consider Singletons an "
+            "anti-pattern, and this is a reason why!!!"
+        )
+        Singleton._instances.clear()
