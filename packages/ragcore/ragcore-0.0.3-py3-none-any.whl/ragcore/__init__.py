b631df246392afa9b9ac50922f171095dee2d860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:38 +0000

    Add package ragcore/0.0.3/ragcore-0.0.3-py3-none-any.whl

---
+++ packages/ragcore/ragcore-0.0.3-py3-none-any.whl/ragcore/__init__.py
@@ -0,0 +1,7 @@
+from .app import RAGCore
+
+__package_name__ = "ragcore"
+__project_name__ = "RAG Core"
+__author__ = "David Kirchhoff"
+__email__ = "david.kirchhoff@mail.utoronto.ca"
+__version__ = "0.0.2"
