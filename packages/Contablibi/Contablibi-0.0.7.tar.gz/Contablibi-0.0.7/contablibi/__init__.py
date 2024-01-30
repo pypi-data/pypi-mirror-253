f44637513dbdadf3fe4dff94ee1afab6cd561c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:25 +0000

    Add package Contablibi/0.0.7/Contablibi-0.0.7.tar.gz

---
+++ packages/Contablibi/Contablibi-0.0.7.tar.gz/Contablibi-0.0.7/contablibi/__init__.py
@@ -0,0 +1,8 @@
+from contablibi.calcular_rescisao import calcular_rescisao
+from contablibi.calcular_balanco import calcular_balanco
+from contablibi.exportar_pdf import exportar_pdf
+from contablibi.grafico import grafico
+
+ 
+
+
