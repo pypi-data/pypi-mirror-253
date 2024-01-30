179e36dc285ac495e334ac11cd212532c800d59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 07:04:45 +0000

    Add package azure-appconfiguration-provider/1.1.0/azure-appconfiguration-provider-1.1.0.tar.gz

---
+++ packages/azure-appconfiguration-provider/azure-appconfiguration-provider-1.1.0.tar.gz/azure-appconfiguration-provider-1.1.0/azure/appconfiguration/provider/_constants.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+FEATURE_MANAGEMENT_KEY = "FeatureManagementFeatureFlags"
+FEATURE_FLAG_PREFIX = ".appconfig.featureflag/"
+
+EMPTY_LABEL = "\0"
+
+REQUEST_TRACING_DISABLED_ENVIRONMENT_VARIABLE = "AZURE_APP_CONFIGURATION_TRACING_DISABLED"
+AzureFunctionEnvironmentVariable = "FUNCTIONS_EXTENSION_VERSION"
+AzureWebAppEnvironmentVariable = "WEBSITE_SITE_NAME"
+ContainerAppEnvironmentVariable = "CONTAINER_APP_NAME"
+KubernetesEnvironmentVariable = "KUBERNETES_PORT"
+ServiceFabricEnvironmentVariable = "Fabric_NodeName"  # cspell:disable-line
---
+++ packages/azure-appconfiguration-provider/azure-appconfiguration-provider-1.1.0.tar.gz/azure-appconfiguration-provider-1.1.0/tests/conftest.py
@@ -0,0 +1,39 @@
+import os
+from devtools_testutils import (
+    add_general_regex_sanitizer,
+    add_general_string_sanitizer,
+    add_oauth_response_sanitizer,
+    set_custom_default_matcher,
+)
+import pytest
+
+# autouse=True will trigger this fixture on each pytest run, even if it's not explicitly used by a test method
+
+
+@pytest.fixture(scope="session", autouse=True)
+def add_sanitizers(test_proxy):
+    add_general_regex_sanitizer(
+        value="https://fake-endpoint.azconfig.io",
+        regex=os.environ.get("APPCONFIGURATION_ENDPOINT_STRING", "https://fake-endpoint.azconfig.io"),
+    )
+    add_general_regex_sanitizer(
+        value="fake-connection-string",
+        regex=os.environ.get("APPCONFIGURATION_CONNECTION_STRING", "fake-connection-string"),
+    )
+    add_general_regex_sanitizer(
+        value="fake-client-id", regex=os.environ.get("APPCONFIGURATION_CLIENT_ID", "fake-client-id")
+    )
+    add_general_regex_sanitizer(
+        value="fake-client-secret", regex=os.environ.get("APPCONFIGURATION_CLIENT_SECRET", "fake-client-secret")
+    )
+    add_general_regex_sanitizer(
+        value="fake-tenant-id", regex=os.environ.get("APPCONFIGURATION_TENANT_ID", "fake-tenant-id")
+    )
+    add_general_string_sanitizer(
+        value="https://fake-key-vault.vault.azure.net/",
+        target=os.environ.get("APPCONFIGURATION_KEY_VAULT_REFERENCE", "https://fake-key-vault.vault.azure.net/"),
+    )
+
+    add_general_regex_sanitizer(value="api-version=1970-01-01", regex="api-version=.+")
+    set_custom_default_matcher(ignored_headers="x-ms-content-sha256, Accept", excluded_headers="Content-Length")
+    add_oauth_response_sanitizer()
