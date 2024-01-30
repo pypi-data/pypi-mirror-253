cabc4dc97c6a89102c1260c799db0cc7406fe492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 06:04:45 +0000

    Add package scikit-lego/0.7.4/scikit-lego-0.7.4.tar.gz

---
+++ packages/scikit-lego/scikit-lego-0.7.4.tar.gz/scikit-lego-0.7.4/sklego/meta/__init__.py
@@ -0,0 +1,29 @@
+__all__ = [
+    "ConfusionBalancer",
+    "DecayEstimator",
+    "EstimatorTransformer",
+    "GroupedEstimator",
+    "GroupedPredictor",
+    "GroupedTransformer",
+    "OrdinalClassifier",
+    "OutlierRemover",
+    "SubjectiveClassifier",
+    "Thresholder",
+    "RegressionOutlierDetector",
+    "OutlierClassifier",
+    "ZeroInflatedRegressor",
+]
+
+from sklego.meta.confusion_balancer import ConfusionBalancer
+from sklego.meta.decay_estimator import DecayEstimator
+from sklego.meta.estimator_transformer import EstimatorTransformer
+from sklego.meta.grouped_estimator import GroupedEstimator
+from sklego.meta.grouped_predictor import GroupedPredictor
+from sklego.meta.grouped_transformer import GroupedTransformer
+from sklego.meta.ordinal_classification import OrdinalClassifier
+from sklego.meta.outlier_classifier import OutlierClassifier
+from sklego.meta.outlier_remover import OutlierRemover
+from sklego.meta.regression_outlier_detector import RegressionOutlierDetector
+from sklego.meta.subjective_classifier import SubjectiveClassifier
+from sklego.meta.thresholder import Thresholder
+from sklego.meta.zero_inflated_regressor import ZeroInflatedRegressor
