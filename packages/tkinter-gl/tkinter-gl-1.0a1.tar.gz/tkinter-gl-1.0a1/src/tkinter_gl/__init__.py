ee16df7dd36f75a5bcd60f2575aa8be2e93900c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:47 +0000

    Add package tkinter-gl/1.0a1/tkinter-gl-1.0a1.tar.gz

---
+++ packages/tkinter-gl/tkinter-gl-1.0a1.tar.gz/tkinter-gl-1.0a1/src/tkinter_gl/__init__.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import tkinter
+__version__ = '1.0a1'
+
+class GLViewBase(tkinter.Widget, tkinter.Misc):
+    """
+    A Tk widget which provides an OpenGL rendering surface.
+    """
+    # Set to "legacy" (default, for OpenGL 2.1), "3_2", or "4_1"
+    profile = ''
+
+    def __init__(self, parent, cnf={}, **kw):
+        self.parent = parent
+        if sys.platform in ('linux', 'win32'):
+            # Make sure the parent has been mapped.
+            parent.update()
+        pkg_dir = os.path.join(__path__[0], 'tk', sys.platform,)
+        if not os.path.exists(pkg_dir):
+            raise RuntimeError('TkGL package directory "%s" is missing.' % pkg_dir)
+        parent.tk.call('lappend', 'auto_path', pkg_dir)
+        parent.tk.call('package', 'require', 'Tkgl')
+        if self.profile:
+            kw['profile'] = self.profile
+        tkinter.Widget.__init__(self, parent, 'tkgl', cnf, kw)
+        try:
+            self.draw()
+        except:
+            # OpenGL may not be initialized.  Wait a bit.
+            pass
+        self.after(50, self.draw)
+
+    def gl_version(self):
+        return self.tk.call(self._w, 'glversion')
+
+    def make_current(self):
+        self.tk.call(self._w, 'makecurrent')
+
+    def swap_buffers(self):
+        self.tk.call(self._w, 'swapbuffers')
+
+    def draw(self, event=None):
+        """Draw the scene.
+
+        Subclasses override this method to make GL calls.
+        """
+        pass
---
+++ packages/tkinter-gl/tkinter-gl-1.0a1.tar.gz/tkinter-gl-1.0a1/test/clear.py
@@ -0,0 +1,32 @@
+import tkinter
+from tkinter_gl import GLViewBase
+from OpenGL import GL
+
+class GLView(GLViewBase):
+    def __init__(self, parent, cnf={}, **kw):
+        GLViewBase.__init__(self, parent, cnf, **kw)
+        self.bind('<Enter>', lambda event: self.draw(color='blue'))
+        self.bind('<Leave>', lambda event: self.draw(color='purple'))
+        
+    def draw(self, color=None):
+        if color is None:
+            if (0 <= self.winfo_pointerx() < self.winfo_width() and
+                0 <= self.winfo_pointery() < self.winfo_height()):
+                color = 'blue'
+            else:
+                color = 'purple'
+        self.make_current()
+        if color == 'blue':
+            GL.glClearColor(0.0, 0, 1.0, 1.0)
+        elif color == 'purple':
+            GL.glClearColor(1.0, 0, 1.0, 1.0)
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+        self.swap_buffers()
+        
+root = tkinter.Tk()
+surface = GLView(root)
+print("Using OpenGL", surface.gl_version())
+surface.pack(expand=True, fill='both')
+root.mainloop()
+
+    
