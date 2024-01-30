87ee99beaacbd70dc2751ec87b04087a7b4bd496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:25 +0000

    Add package person-generate/0.2.0/person_generate-0.2.0.tar.gz

---
+++ packages/person-generate/person_generate-0.2.0.tar.gz/person_generate-0.2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+
+VERSION = "0.2.0"
+DESCRIPTION = "Pacote de geração de dados de pessoas"
+LONG_DESCRIPTION = "Pacote de geração de dados de pessoas, tais como nome, cpf, data de nascimento, peso, altura, telefone, cidade e estado. Para mais informações acesse: https://github.com/lannyily/person-generate"
+
+#setting up
+
+setup(
+    # the name must match the folder name
+    name="person_generate",
+    version = VERSION,
+    author = "Erlanny",
+    author_email = "erlannyrodrigues@gmail.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    install_requires = ["numpy"],#adiciona bibliotecas adicionais
+    keywords = ['python', 'person', 'generate', 'brasil'],
+    classifiers = ["Development Status :: 3 - Alpha",]
+)
