e22ad3f125294ec39068126f44d385b6e91469a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 06:04:52 +0000

    Add package circuitpython-tool/0.4.0/circuitpython_tool-0.4.0.tar.gz

---
+++ packages/circuitpython-tool/circuitpython_tool-0.4.0.tar.gz/circuitpython_tool-0.4.0/src/circuitpython_tool/device.py
@@ -0,0 +1,32 @@
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Device:
+    """A CircuitPython composite USB device."""
+
+    vendor: str
+    model: str
+    serial: str
+
+    # Path to partition device.
+    partition_path: Path | None = None
+
+    # Path to serial device.
+    serial_path: Path | None = None
+
+    def get_mountpoint(self) -> Path | None:
+        """Find mountpoint. Returns None if not mounted."""
+        raise NotImplementedError()
+
+    def mount_if_needed(self) -> Path:
+        """Mounts the partition device if needed, and returns the mountpoint."""
+        raise NotImplementedError
+
+    def unmount_if_needed(self) -> None:
+        """Unmounts the partition device if needed."""
+        raise NotImplementedError
---
+++ packages/circuitpython-tool/circuitpython_tool-0.4.0.tar.gz/circuitpython_tool-0.4.0/src/circuitpython_tool/dirs.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+from click import get_app_dir
+
+app_dir = Path(get_app_dir("circuitpython-tool"))
