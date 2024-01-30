06d3cd35a4b1628686b5fddbc8deb35abded68e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:05:14 +0000

    Add package my-telebot/0.2/my-telebot-0.2.tar.gz

---
+++ packages/my-telebot/my-telebot-0.2.tar.gz/my-telebot-0.2/my_telebot/__init__.py
@@ -0,0 +1,1 @@
+# my_telebot/my_telebot/__init__.py
---
+++ packages/my-telebot/my-telebot-0.2.tar.gz/my-telebot-0.2/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+# setup.py
+setup(
+    name='my-telebot',
+    version='0.2',  # Update the version number
+    packages=['my_telebot'],
+    install_requires=[
+        'python-telegram-bot',
+    ],
+)
