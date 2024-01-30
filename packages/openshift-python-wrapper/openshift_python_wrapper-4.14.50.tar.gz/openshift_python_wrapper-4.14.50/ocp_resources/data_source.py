8f959bec198b4976d92639c602417904d03be053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:05:25 +0000

    Add package openshift-python-wrapper/4.14.50/openshift_python_wrapper-4.14.50.tar.gz

---
+++ packages/openshift-python-wrapper/openshift_python_wrapper-4.14.50.tar.gz/openshift_python_wrapper-4.14.50/ocp_resources/data_source.py
@@ -0,0 +1,59 @@
+from openshift.dynamic.exceptions import ResourceNotFoundError
+
+from ocp_resources.constants import TIMEOUT_4MINUTES
+from ocp_resources.persistent_volume_claim import PersistentVolumeClaim
+from ocp_resources.resource import NamespacedResource
+
+
+class DataSource(NamespacedResource):
+    api_group = NamespacedResource.ApiGroup.CDI_KUBEVIRT_IO
+
+    def __init__(
+        self,
+        name=None,
+        namespace=None,
+        client=None,
+        source=None,
+        teardown=True,
+        yaml_file=None,
+        delete_timeout=TIMEOUT_4MINUTES,
+        **kwargs,
+    ):
+        super().__init__(
+            name=name,
+            namespace=namespace,
+            client=client,
+            teardown=teardown,
+            yaml_file=yaml_file,
+            delete_timeout=delete_timeout,
+            **kwargs,
+        )
+        self.source = source
+
+    def to_dict(self):
+        super().to_dict()
+        if not self.yaml_file:
+            self.res.update(
+                {
+                    "spec": {
+                        "source": self.source,
+                    },
+                }
+            )
+
+    @property
+    def pvc(self):
+        data_source_pvc = self.instance.spec.source.pvc
+        pvc_name = data_source_pvc.name
+        pvc_namespace = data_source_pvc.namespace
+        try:
+            return PersistentVolumeClaim(
+                client=self.client,
+                name=pvc_name,
+                namespace=pvc_namespace,
+            )
+        except ResourceNotFoundError:
+            self.logger.warning(
+                f"dataSource {self.name} is pointing to a non-existing PVC, name:"
+                f" {pvc_name}, namespace: {pvc_namespace}"
+            )
