299d135644480b381c261ba0a82aec068277ac75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:04:58 +0000

    Add package cgg/0.0.3/cgg-0.0.3.tar.gz

---
+++ packages/cgg/cgg-0.0.3.tar.gz/cgg-0.0.3/src/cgg/__init__.py
@@ -0,0 +1,20 @@
+
+from .__main__ import __version__
+
+# __version__="0.0.3"
+# def __init__():
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+#     parser.add_argument('tag', help='tag(s) available: c, cpp, g, gpp; or menu')
+#     args = parser.parse_args()
+#     if args.tag=="c":
+#         from gguf_connector import c
+#     elif args.tag=="cpp":
+#         from gguf_connector import cpp
+#     elif args.tag=="g":
+#         from gguf_connector import g
+#     elif args.tag=="gpp":
+#         from gguf_connector import gpp
+#     elif args.tag=="menu":
+#         from gguf_connector import menu
---
+++ packages/cgg/cgg-0.0.3.tar.gz/cgg-0.0.3/src/cgg/__main__.py
@@ -0,0 +1,50 @@
+# !/usr/bin/env python3
+
+__version__="0.0.3"
+
+import argparse
+import urllib.request
+from os.path import basename
+
+def clone_file(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+        filename = basename(url)
+        with open(filename, 'wb') as file:
+            file.write(content)
+        print(f"File cloned successfully and saved as '{filename}' in the current directory.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def __main__():
+    parser = argparse.ArgumentParser(description="Execute different functions based on command-line arguments.")
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand", help="Choose a subcommand")
+
+    # Subparser for the 'clone + URL' command
+    clone_parser = subparsers.add_parser('clone', help='Download a GGUF file/model from URL')
+    clone_parser.add_argument('url', type=str, help='URL to download from')
+
+    # Subparser for the 'menu/cpp/c/gpp/g' command
+    subparsers.add_parser('menu', help='Connector selection list')
+    subparsers.add_parser('cpp', help='cpp connector')
+    subparsers.add_parser('c', help='c connector')
+    subparsers.add_parser('gpp', help='gpp connector')
+    subparsers.add_parser('g', help='g connector')
+
+    args = parser.parse_args()
+
+    if args.subcommand == 'clone':
+        clone_file(args.url)
+    elif args.subcommand == 'menu':
+        from gguf_connector import menu
+    elif args.subcommand == 'cpp':
+        from gguf_connector import cpp
+    elif args.subcommand == 'c':
+        from gguf_connector import c
+    elif args.subcommand == 'gpp':
+        from gguf_connector import gpp
+    elif args.subcommand == 'g':
+        from gguf_connector import g
