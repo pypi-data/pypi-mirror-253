e26b5182e81e0acb2f408627d79a652f046d0808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:44 +0000

    Add package torchgating/0.1.3b0/torchgating-0.1.3b0.tar.gz

---
+++ packages/torchgating/torchgating-0.1.3b0.tar.gz/torchgating-0.1.3b0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torchgating",
+    version="0.1.3-beta",
+    author="Asaf Zorea",
+    author_email="zoreasaf@gmail.com",
+    description="A PyTorch-based implementation of Spectral Gating, an algorithm for denoising audio signals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/noisereduce/TorchSpectralGating",
+    packages=setuptools.find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*", "tests.*"]
+    ),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["torchgating = torchgating.run:main"]},
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "soundfile",
+        "torch",
+    ],
+)
