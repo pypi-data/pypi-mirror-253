a3c0b784c8b5a114388dfce85b486b313e56a0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:43 +0000

    Add package pydmsp/0.1.6/pydmsp-0.1.6-py3-none-any.whl

---
+++ packages/pydmsp/pydmsp-0.1.6-py3-none-any.whl/pydmsp/__init__.py
@@ -0,0 +1,5 @@
+from pydmsp._from_gz import unzip
+from pydmsp._make_dataset import make_xr_dataset
+from pydmsp._transform_dataset import make_transform_dataset
+import pydmsp._dmspexeptions
+import pydmsp._tables_E, pydmsp._tables_G
