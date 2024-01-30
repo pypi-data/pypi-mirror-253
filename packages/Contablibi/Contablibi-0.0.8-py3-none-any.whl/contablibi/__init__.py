54106358694be4ee187e4e081c34373138a71361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:53 +0000

    Add package Contablibi/0.0.8/Contablibi-0.0.8-py3-none-any.whl

---
+++ packages/Contablibi/Contablibi-0.0.8-py3-none-any.whl/contablibi/__init__.py
@@ -0,0 +1,8 @@
+from .calcular_rescisao import rescisao_aposentadoria, rescisao_demissao, rescisao_falecimento, rescisao_justa_causa, rescisao_sem_justa_causa
+from .calcular_balanco import calcular_balanco
+from .exportar_pdf import exportar_pdf
+from .grafico import grafico
+
+ 
+
+
