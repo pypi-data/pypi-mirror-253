10ff2c58bdd2d69b07cc30fdbdd09dde0372738a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:58 +0000

    Add package djangoldp-tzcld/3.0.8/djangoldp_tzcld-3.0.8-py3-none-any.whl

---
+++ packages/djangoldp-tzcld/djangoldp_tzcld-3.0.8-py3-none-any.whl/djangoldp_tzcld/views.py
@@ -0,0 +1,17 @@
+from django.db.models import Q
+from djangoldp.views import LDPViewSet
+from djangoldp_community.models import Community
+
+class MyTerritoriesView(LDPViewSet):
+    model = Community
+    parent_model = Community
+    def get_queryset(self):
+        user = self.request.user
+        return super().get_queryset().filter(Q(tzcld_profile__regions__referents=user) | Q(admins__user=user)).distinct()
+
+class MemberOfCommunitiesView(LDPViewSet):
+    model = Community
+    parent_model = Community
+    def get_queryset(self):
+        user = self.request.user
+        return super().get_queryset().filter(Q(members__in=user.communities.filter(is_admin=True)) | Q(members__in=user.communities.filter(is_admin=False))).distinct()
