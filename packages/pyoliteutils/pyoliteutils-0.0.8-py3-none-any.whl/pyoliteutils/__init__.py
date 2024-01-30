a3f775a9d5e7e195fc98d250ec5c7f0eed0a1069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 17:05:03 +0000

    Add package pyoliteutils/0.0.8/pyoliteutils-0.0.8-py3-none-any.whl

---
+++ packages/pyoliteutils/pyoliteutils-0.0.8-py3-none-any.whl/pyoliteutils/__init__.py
@@ -0,0 +1,8 @@
+await micropip.install(["matplotlib", "pandas"])
+
+from .pyoliteutils import load_file_into_in_mem_filesystem
+from .mermaid import mm
+from .lessonsurvey import lessonsurvey
+
+from . import _version
+__version__ = _version.get_versions()['version']
