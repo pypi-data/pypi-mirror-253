3a71b981f961552a688ce523b301d0fbe3879c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:01 +0000

    Add package test1-H4upackage/6.6.6/test1_H4upackage-6.6.6-py3-none-any.whl

---
+++ packages/test1-H4upackage/test1_H4upackage-6.6.6-py3-none-any.whl/Hack4u/__init__.py
@@ -0,0 +1,6 @@
+# the init file contains the executed code when a library is called,
+# on this case when you execute the "from hack4u import *"
+from .courses import *
+#iport all the funcations of the module courses by default
+from .utils import *
+#makes the same with the utils file
