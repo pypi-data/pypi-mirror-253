f8924a19b0f8490653be5de633e613b30a389041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:16 +0000

    Add package vdk-sqlite/0.1.1156222304/vdk-sqlite-0.1.1156222304.tar.gz

---
+++ packages/vdk-sqlite/vdk-sqlite-0.1.1156222304.tar.gz/vdk-sqlite-0.1.1156222304/setup.py
@@ -0,0 +1,19 @@
+# Copyright 2021-2024 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import setuptools
+
+"""
+Builds a package with the help of setuptools in order for this package to be imported in other projects
+"""
+
+__version__ = "0.1.1156222304"
+
+setuptools.setup(
+    name="vdk-sqlite",
+    version=__version__,
+    install_requires=["vdk-core", "tabulate"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    include_package_data=True,
+    entry_points={"vdk.plugin.run": ["vdk-sqlite = vdk.plugin.sqlite.sqlite_plugin"]},
+)
