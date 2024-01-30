3cf31cc812ef9e19909bc7ec9b3571c1945732c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 15:06:34 +0000

    Add package contabilib/4.7.0/contabilib-4.7.0.tar.gz

---
+++ packages/contabilib/contabilib-4.7.0.tar.gz/contabilib-4.7.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+
+# lê o conteúdo do arquivo README.md
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+REQUIRED_PACKAGES = [
+    'numpy',
+    'matplotlib',
+    'fpdf',  # Especificando a versão exata do FPDF
+    'seaborn',
+]
+
+setup(
+    name="contabilib",
+    version="4.7.0",
+    author="crisly, erik",
+    author_email="crislymaria21@gmail.com",
+    description="Biblioteca para cálculos contábeis.",
+    long_description=long_description,
+    # especifica que o long_description está em Markdown
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,  # adiciona bibliotecas adicionais
+    keywords=['python', 'contablidade'],
+    classifiers=["Development Status :: 3 - Alpha",]
+)
