56dd04af4fec7585203fea28b896ba991c87a3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:24 +0000

    Add package trailblazer/20.2.5/trailblazer-20.2.5-py3-none-any.whl

---
+++ packages/trailblazer/trailblazer-20.2.5-py3-none-any.whl/trailblazer/server/app.py
@@ -0,0 +1,55 @@
+import logging
+import os
+
+from flask import Flask
+from flask_cors import CORS
+from flask_reverse_proxy import FlaskReverseProxied
+from sqlalchemy.orm import scoped_session
+
+from trailblazer.server import api, ext
+from trailblazer.services import AnalysisService, JobService
+from trailblazer.store.database import get_session
+from trailblazer.store.store import Store
+
+app = Flask(__name__)
+
+LOG = logging.getLogger(__name__)
+
+SECRET_KEY = "unsafe!!!"
+TEMPLATES_AUTO_RELOAD = True
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
+SQLALCHEMY_POOL_RECYCLE = os.environ.get("SQLALCHEMY_POOL_RECYCLE", 7200)
+SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("FLASK_DEBUG", False)
+
+app.config.from_object(__name__)
+
+# register blueprints
+app.register_blueprint(api.blueprint)
+
+# services
+store = Store()
+analysis_service = AnalysisService(store)
+job_service = JobService(store)
+
+# configure extensions
+FlaskReverseProxied(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
+ext.store.init_app(app)
+
+app.extensions["analysis_service"] = analysis_service
+app.extensions["job_service"] = job_service
+
+
+@app.route("/")
+def index():
+    return "Welcome to Trailblazer REST API"
+
+
+@app.teardown_appcontext
+def teardown_session(_):
+    """
+    Remove the database session to ensure database resources are released when a
+    request has been processed.
+    """
+    session: scoped_session = get_session()
+    session.remove()
