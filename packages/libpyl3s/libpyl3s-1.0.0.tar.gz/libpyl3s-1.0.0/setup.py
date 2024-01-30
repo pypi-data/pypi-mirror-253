916ab2ffac725cf5e6ec22b4281bd4f4a3b6414d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:57 +0000

    Add package libpyl3s/1.0.0/libpyl3s-1.0.0.tar.gz

---
+++ packages/libpyl3s/libpyl3s-1.0.0.tar.gz/libpyl3s-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+DESCRIPTION = "Pacote de teste"
+LONG_DESCRIPTION = "Biblioteca Python para manipulações de arquivos."
+
+
+REQUIRED_PACKAGES = [
+    'PyPDF2',
+    'fpdf'
+]
+
+setup(
+    # the name must match the folder name
+    name="libpyl3s",
+    version=VERSION,
+    author="PedroVitor",
+    author_email="pedro.passos@ufpi.edu.br",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,  # adiciona bibliotecas adicionais
+    keywords=['python', 'files', 'Pyl3s', 'Pyl3sLib'],
+
+)
