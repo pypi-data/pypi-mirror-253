f76280405aa9097957f079eab5695a9d297549bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 06:04:36 +0000

    Add package ship-ai-module/0.1.0/ship-ai-module-0.1.0.tar.gz

---
+++ packages/ship-ai-module/ship-ai-module-0.1.0.tar.gz/ship-ai-module-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ship-ai-module',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.26.0'
+    ],
+)
