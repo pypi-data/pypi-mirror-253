305bda8cb486576b0fbd0e581944850c9db8115d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:04:42 +0000

    Add package sqlalchemy-declarative-extensions/0.6.8/sqlalchemy_declarative_extensions-0.6.8-py3-none-any.whl

---
+++ packages/sqlalchemy-declarative-extensions/sqlalchemy_declarative_extensions-0.6.8-py3-none-any.whl/sqlalchemy_declarative_extensions/dialects/mysql/schema.py
@@ -0,0 +1,44 @@
+from sqlalchemy import bindparam, column, table
+
+from sqlalchemy_declarative_extensions.sqlalchemy import select
+
+views = table(
+    "views",
+    column("table_schema"),
+    column("table_name"),
+    column("view_definition"),
+    schema="INFORMATION_SCHEMA",
+)
+
+tables = table(
+    "tables",
+    column("table_schema"),
+    column("table_name"),
+    schema="information_schema",
+)
+
+schemata = table(
+    "schemata",
+    column("schema_name"),
+    schema="information_schema",
+)
+
+views_query = (
+    select(
+        views.c.table_schema.label("schema"),
+        views.c.table_name.label("name"),
+        views.c.view_definition.label("definition"),
+    )
+    .where(views.c.view_definition.isnot(None))
+    .where(views.c.table_schema.notin_(["sys"]))
+)
+
+table_exists_query = (
+    select(tables)
+    .where(tables.c.table_schema == bindparam("schema"))
+    .where(tables.c.table_name == bindparam("name"))
+)
+
+schema_exists_query = select(schemata).where(
+    schemata.c.schema_name == bindparam("schema")
+)
