40865e9592a14dbf8b1508d1e2d182b660a67494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:47 +0000

    Add package nwon_django_toolbox/0.5.18/nwon_django_toolbox-0.5.18-py3-none-any.whl

---
+++ packages/nwon_django_toolbox/nwon_django_toolbox-0.5.18-py3-none-any.whl/nwon_django_toolbox/views/__init__.py
@@ -0,0 +1,13 @@
+from nwon_django_toolbox.views.create_and_return_in_view import (
+    create_and_return_in_view,
+)
+from nwon_django_toolbox.views.redirect_to_s3_url import redirect_to_s3_url
+from nwon_django_toolbox.views.update_and_return_in_view import (
+    update_and_return_in_view,
+)
+
+__ALL__ = [
+    "create_and_return_in_view",
+    "redirect_to_s3_url",
+    "update_and_return_in_view",
+]
