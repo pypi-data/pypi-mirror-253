924e5b69ec099feac62afb59489348085512ec26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 16:05:08 +0000

    Add package kapitan/0.33.0/kapitan-0.33.0-py3-none-any.whl

---
+++ packages/kapitan/kapitan-0.33.0-py3-none-any.whl/kapitan/reclass/run_tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of reclass (http://github.com/madduck/reclass)
+#
+# Copyright © 2007–13 martin f. krafft <madduck@madduck.net>
+# Released under the terms of the Artistic Licence 2.0
+#
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+tests = unittest.TestLoader().discover('reclass')
+unittest.TextTestRunner(verbosity=1).run(tests)
