2641eead2e9dc005b2972125be504322c291e08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:06 +0000

    Add package basedpyright/1.1.0/basedpyright-1.1.0.tar.gz

---
+++ packages/basedpyright/basedpyright-1.1.0.tar.gz/basedpyright-1.1.0/pdm_build.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from json import loads
+from pathlib import Path
+from shutil import copyfile, copytree
+from typing import TYPE_CHECKING
+
+# https://github.com/samwillis/nodejs-pypi/pull/23
+if TYPE_CHECKING:
+    # https://github.com/astral-sh/ruff/issues/9528
+    from subprocess import run  # noqa: S404
+else:
+    from nodejs.npm import run
+
+if not Path("node_modules").exists():
+    _ = run(["ci"], check=True)
+_ = run(["run", "build:cli:dev"], check=True)
+
+npm_package_dir = Path("packages/pyright")
+pypi_package_dir = Path("basedpyright")
+
+copytree(npm_package_dir / "dist", pypi_package_dir / "dist", dirs_exist_ok=True)
+for script_path in loads((npm_package_dir / "package.json").read_text())[
+    "bin"
+].values():
+    copyfile(npm_package_dir / script_path, pypi_package_dir / script_path)
