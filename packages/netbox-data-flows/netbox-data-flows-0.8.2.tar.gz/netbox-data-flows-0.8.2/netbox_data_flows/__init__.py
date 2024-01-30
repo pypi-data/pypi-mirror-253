f59bf78762e6c1e22b65cff7b4eeb4e6bd2216d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 06:05:16 +0000

    Add package netbox-data-flows/0.8.2/netbox-data-flows-0.8.2.tar.gz

---
+++ packages/netbox-data-flows/netbox-data-flows-0.8.2.tar.gz/netbox-data-flows-0.8.2/netbox_data_flows/__init__.py
@@ -0,0 +1,26 @@
+from extras.plugins import PluginConfig
+
+__version__ = "0.8.2"
+
+
+class DataFlowsConfig(PluginConfig):
+    name = "netbox_data_flows"
+    verbose_name = "Data Flows"
+    description = (
+        "NetBox plugin to document data flows between "
+        "systems and applications."
+    )
+    version = __version__
+    base_url = "data-flows"
+    author = "Thomas Fargeix"
+    required_settings = []
+    default_settings = {}
+    min_version = "3.6.0"
+
+    def ready(self):
+        from . import signals  # noqa: F401
+
+        super().ready()
+
+
+config = DataFlowsConfig
