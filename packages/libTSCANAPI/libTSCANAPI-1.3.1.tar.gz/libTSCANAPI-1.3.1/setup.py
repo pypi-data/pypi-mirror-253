f26ad8ec383c313379d91d53f613969a68ac67ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 15:06:42 +0000

    Add package libTSCANAPI/1.3.1/libTSCANAPI-1.3.1.tar.gz

---
+++ packages/libTSCANAPI/libTSCANAPI-1.3.1.tar.gz/libTSCANAPI-1.3.1/setup.py
@@ -0,0 +1,48 @@
+'''
+Author: seven 865762826@qq.com
+Date: 2023-03-24 09:26:29
+LastEditors: seven 865762826@qq.com
+LastEditTime: 2023-11-24 11:46:54
+FilePath: \VSCode_Pro\Python_Pro\TSMasterApi\setup.py
+'''
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r",encoding="utf-8") as f:
+  long_description = f.read()
+
+# 
+setup(name='libTSCANAPI',  # 包名
+      version='1.3.1',  # 版本号
+      description='Use TSMaster hardware',
+      long_description=long_description,
+      author='seven',
+      author_email='865762826@qq.com',
+      install_requires=[],
+      license='MIT License',
+      packages=find_packages(),
+      package_data={
+        'libTSCANAPI': ['./libTSCANAPI/linux/libTSCANApiOnLinux.so','./libTSCANAPI/linux/libTSH.so','./libTSCANAPI/windows/x64/libTSCAN.dll','./libTSCANAPI/windows/x64/libTSH.dll','./libTSCANAPI/windows/x86/libTSCAN.dll','./libTSCANAPI/windows/x86/libTSH.dll','./libTSCANAPI/windows/x86/binlog.dll','./libTSCANAPI/windows/x86/libLog.dll'],
+      },
+      # data_files=[
+      #   ('./Lib/site-packages/libTSCANAPI/linux', ['./libTSCANAPI/linux/libTSCANApiOnLinux.so','./libTSCANAPI/linux/libTSH.so']),
+      #   ('./Lib/site-packages/libTSCANAPI/windows/x64', ['./libTSCANAPI/windows/x64/libTSCAN.dll','./libTSCANAPI/windows/x64/libTSH.dll']),
+      #   ('./Lib/site-packages/libTSCANAPI/windows/x86', ['./libTSCANAPI/windows/x86/libTSCAN.dll','./libTSCANAPI/windows/x86/libTSH.dll','./libTSCANAPI/windows/x86/binlog.dll','./libTSCANAPI/windows/x86/libLog.dll']),
+      #         ],
+      platforms=["Any"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
