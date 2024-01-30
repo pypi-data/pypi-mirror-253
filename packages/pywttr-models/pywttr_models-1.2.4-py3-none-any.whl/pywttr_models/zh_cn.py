ef6e32525a1e966aa27de2dee791685880e4509b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:05:00 +0000

    Add package pywttr-models/1.2.4/pywttr_models-1.2.4-py3-none-any.whl

---
+++ packages/pywttr-models/pywttr_models-1.2.4-py3-none-any.whl/pywttr_models/zh_cn.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import List
+
+from . import base
+from ._pydantic import Field
+
+
+class CurrentConditionItem(base.CurrentConditionItem):
+    lang_zh_cn: List[base.LangItem] = Field(alias="lang_zh-cn")
+
+
+class HourlyItem(base.HourlyItem):
+    lang_zh_cn: List[base.LangItem] = Field(alias="lang_zh-cn")
+
+
+class WeatherItem(base.WeatherItem):
+    hourly: List[HourlyItem]
+
+
+class Model(base.Model):
+    current_condition: List[CurrentConditionItem]
+    weather: List[WeatherItem]
