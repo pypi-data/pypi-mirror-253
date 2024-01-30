e4be6a71f64241576209a90e5b9b772cab494629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 05:04:57 +0000

    Add package azol/0.1.1a0/azol-0.1.1a0-py3-none-any.whl

---
+++ packages/azol/azol-0.1.1a0-py3-none-any.whl/azol/credentials/__init__.py
@@ -0,0 +1,12 @@
+"""A module containing all credential types"""
+from azol.credentials.access_token import AccessToken
+from azol.credentials.service_principal import ServicePrincipal
+from azol.credentials.application_object import ApplicationObject
+from azol.credentials.user import User
+
+__all__ = [
+    "AccessToken",
+    "ServicePrincipal",
+    "ApplicationObject",
+    "User"
+]
