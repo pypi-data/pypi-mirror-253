874292221ee5178794d128c62adb8995fb2c7d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 06:04:53 +0000

    Add package tpower/0.1.6/tpower-0.1.6.tar.gz

---
+++ packages/tpower/tpower-0.1.6.tar.gz/tpower-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tpower',
+    version='0.1.6',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Python package by T-Power.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Matías Galetovic Streeter',
+    author_email='matias.galetovic@toesca.com',
+    url='https://github.com/toesca-dev/tpower/',
+    install_requires=[
+        'requests', 'pandas', 'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
