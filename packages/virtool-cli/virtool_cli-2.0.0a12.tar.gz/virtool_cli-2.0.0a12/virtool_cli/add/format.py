623d75fde51e6d7d1c266fb2272f369480bbc8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:13 +0000

    Add package virtool-cli/2.0.0a12/virtool_cli-2.0.0a12.tar.gz

---
+++ packages/virtool-cli/virtool_cli-2.0.0a12.tar.gz/virtool_cli-2.0.0a12/virtool_cli/add/format.py
@@ -0,0 +1,26 @@
+from Bio import SeqRecord
+
+from virtool_cli.add.helpers import find_taxon_id
+from virtool_cli.utils.format import check_source_type, format_sequence, get_qualifiers
+from virtool_cli.utils.ncbi import fetch_isolate_metadata
+
+
+async def format_record(data: SeqRecord) -> dict:
+    seq_qualifiers = get_qualifiers(data.features)
+
+    return {
+        **format_sequence(record=data, qualifiers=seq_qualifiers),
+        "isolate": await format_isolate_metadata(qualifiers=seq_qualifiers),
+    }
+
+
+async def format_isolate_metadata(qualifiers) -> dict:
+    if isolate_type := check_source_type(qualifiers):
+        # Isolate metadata contained in qualifiers
+        return {
+            "source_name": qualifiers.get(isolate_type)[0],
+            "source_type": isolate_type,
+        }
+
+    # Extract isolate metadata from NCBI Taxonomy docsum.
+    return await fetch_isolate_metadata(find_taxon_id(qualifiers["db_xref"]))
