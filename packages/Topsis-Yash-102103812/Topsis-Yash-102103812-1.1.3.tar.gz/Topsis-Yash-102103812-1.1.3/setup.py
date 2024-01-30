77170b6d5963a86dd3c952eef461ac6a54989f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 03:04:20 +0000

    Add package Topsis-Yash-102103812/1.1.3/Topsis-Yash-102103812-1.1.3.tar.gz

---
+++ packages/Topsis-Yash-102103812/Topsis-Yash-102103812-1.1.3.tar.gz/Topsis-Yash-102103812-1.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name="Topsis-Yash-102103812",
+version="1.1.3",
+description="A simple Python package for math operations and geometry calculations",
+packages=find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires=">=3.6",
+)
