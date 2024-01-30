e5ab1ef26373aa69dc43a98304cac85a1922ff63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 14:05:09 +0000

    Add package nriapp/1.2.6/nriapp-1.2.6.tar.gz

---
+++ packages/nriapp/nriapp-1.2.6.tar.gz/nriapp-1.2.6/setup.py
@@ -0,0 +1,36 @@
+
+from setuptools import setup, find_packages
+from glob import glob
+
+#with open('requirements.txt') as f:
+#    requirements = f.read().splitlines()
+
+
+setup(
+   name='nriapp',
+   version='1.2.06',
+   description='This is an internal tool for querying MS 365 Defender using MSGraph and MS Sentinels API with NRI\n'
+               'Added capability for MDR portal',
+    license='MIT',
+   author='Llallum Victoria',
+   author_email='llallumvictoria@gmail.com',
+#   packages=find_packages('src'),
+   package_dir = {'':'src/nriapp'},
+    
+   packages=['.','config', 'core', 'helper'],  #same as name
+#   packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+   data_files=[('.', ['src/nriapp/changelog.txt']),
+               ('./session', glob('./src/session/*.*')),
+               ('./logs', glob('./src/logs/*.*')),
+               ('./output', glob('./src/output/*.*'))
+              ],
+   #setup_requires=['session', 'logs', 'output'],
+   include_package_data=True,
+   install_requires=[],
+#   install_requires=requirements, #external packages as dependencies,
+   entry_points = '''
+        [console_scripts]
+        nriapp=nriapp:main
+    '''
+
+)
