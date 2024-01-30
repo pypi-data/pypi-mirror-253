f666128c9630edfde8d4a97385ee67882faf7f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-30 04:04:31 +0000

    Add package flygym/0.2.5/flygym-0.2.5.tar.gz

---
+++ packages/flygym/flygym-0.2.5.tar.gz/flygym-0.2.5/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="flygym",
+    version="0.2.5",
+    author="Neuroengineering Laboratory, EPFL",
+    author_email="sibo.wang@epfl.ch",
+    description="Gym environments for NeuroMechFly in various physics simulators",
+    packages=find_packages(),
+    package_data={"flygym": ["data/*"], "flygym.mujoco": ["mujoco/config.yaml"]},
+    include_package_data=True,
+    python_requires=">=3.8,<3.12",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=[
+        "gymnasium",
+        "numpy",
+        "scipy",
+        "pyyaml",
+        "jupyter",
+        "mediapy",
+        "imageio",
+        "imageio[pyav]",
+        "imageio[ffmpeg]",
+        "tqdm",
+    ],
+    extras_require={
+        "mujoco": ["mujoco>=2.1.2", "dm_control", "numba", "opencv-python"],
+        "pybullet": ["pybullet"],
+        "dev": [
+            "sphinx",
+            "furo",
+            "numpydoc",
+            "pytest",
+            "ruff",
+            "black==23.3.0",
+            "black[jupyter]",
+            "shapely",
+            "rasterio",
+            "requests",
+        ],
+        "tutorials": ["networkx"],
+    },
+    url="https://neuromechfly.org/",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
