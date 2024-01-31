383a4b04c88e1d06c9e827110f747f2bd16f8124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:05:57 +0000

    Add package mkdocs-ja-ruby/1.0.0/mkdocs-ja-ruby-1.0.0.tar.gz

---
+++ packages/mkdocs-ja-ruby/mkdocs-ja-ruby-1.0.0.tar.gz/mkdocs-ja-ruby-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mkdocs-ja-ruby",
+    version="1.0.0",
+    description="Just for fun. A ruby plugin for mkdocs",
+    url="https://ashitemaru.github.io",
+    author="Ashitemaru",
+    author_email="ashitemaru.holder@gmail.com",
+    packages=find_packages(),
+    entry_points={
+        "mkdocs.plugins": [
+            "mkdocs-ja-ruby=mkdocs_ja_ruby.plugin:ExtendedMarkdown",
+        ]
+    }
+)
