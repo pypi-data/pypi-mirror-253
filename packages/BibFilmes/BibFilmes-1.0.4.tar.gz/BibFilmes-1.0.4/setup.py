71052e69ef86f7ac53bc1fc8fc8c97b78f9e273a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:06:47 +0000

    Add package BibFilmes/1.0.4/BibFilmes-1.0.4.tar.gz

---
+++ packages/BibFilmes/BibFilmes-1.0.4.tar.gz/BibFilmes-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as arq:
+    readme = arq.read()
+
+setup(
+    name='BibFilmes',
+    version='1.0.4',
+    license='MIT License',
+    author='Luiz Felipe',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='luiz.nogueira@ufpi.edu.br',
+    keywords='BibFilmes',
+    description='',
+    packages=['BibFilmes'],
+    install_requires=['requests', 'pandas', 'pyarrow'],
+)
