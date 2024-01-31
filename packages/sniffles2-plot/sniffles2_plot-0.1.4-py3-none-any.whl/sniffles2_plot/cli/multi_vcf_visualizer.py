969f683cb4196332624220b6538e1ab7a97065f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 06:04:53 +0000

    Add package sniffles2-plot/0.1.4/sniffles2_plot-0.1.4-py3-none-any.whl

---
+++ packages/sniffles2-plot/sniffles2_plot-0.1.4-py3-none-any.whl/sniffles2_plot/cli/multi_vcf_visualizer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 15 10:17:54 2023
+
+@author: HGSC Farhang Jaryani
+"""
+
+from sniffles2_plot.chart_generator import (
+    GenomeChartDataGenerator,
+    SizeDistribution,
+    Sv_sites_per_genome,
+    VariantCount,
+)
+
+
+def generate_multi_vcf_charts(input_vcf_file, output_path):
+    """Multi VCF plot generator"""
+    SV_site = Sv_sites_per_genome(input_vcf_file, output_path)
+    Genome_chart_data_generator = GenomeChartDataGenerator(input_vcf_file, output_path)
+    S_D_obj = SizeDistribution(input_vcf_file, output_path)
+    V_C_obj = VariantCount(input_vcf_file, output_path)
+
+    SV_site.sv_sites_per_genome()
+    V_C_obj.variant_count_chart_generator()
+    Genome_chart_data_generator.allele_frequency_chart_generator()
+    Genome_chart_data_generator.samples_sv_numbers()
+    Genome_chart_data_generator.heat_map_generator()
