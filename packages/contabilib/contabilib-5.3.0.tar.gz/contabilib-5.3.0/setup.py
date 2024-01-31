e3187aeb3cc7ce1515f32d28bc44deec933423be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:06:48 +0000

    Add package contabilib/5.3.0/contabilib-5.3.0.tar.gz

---
+++ packages/contabilib/contabilib-5.3.0.tar.gz/contabilib-5.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+
+# lê o conteúdo do arquivo README.md
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
+setup(
+    name="contabilib",
+    version="5.3.0",
+    author="crisly, erik",
+    author_email="crislymaria21@gmail.com",
+    description="Biblioteca para cálculos contábeis.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'fpdf',
+    ],  # adiciona bibliotecas adicionais
+    keywords=['python', 'contablidade'],
+    classifiers=["Development Status :: 3 - Alpha",]
+)
