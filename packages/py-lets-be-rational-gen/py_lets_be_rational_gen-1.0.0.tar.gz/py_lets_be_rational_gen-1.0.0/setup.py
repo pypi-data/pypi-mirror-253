2da994b534efd6d3de1ecbf9f2ed73cf8e2047aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:08:47 +0000

    Add package py-lets-be-rational-gen/1.0.0/py_lets_be_rational_gen-1.0.0.tar.gz

---
+++ packages/py-lets-be-rational-gen/py_lets_be_rational_gen-1.0.0.tar.gz/py_lets_be_rational_gen-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+    name='py_lets_be_rational_gen',
+    version='1.0.0',
+    url='',
+    license='MIT',
+    maintainer='',
+    maintainer_email='',
+    long_description=open('README.rst').read(),
+    description='Pure python implementation of Peter Jaeckel\'s LetsBeRational.',
+    install_requires = [
+        'numpy'
+    ]
+)
