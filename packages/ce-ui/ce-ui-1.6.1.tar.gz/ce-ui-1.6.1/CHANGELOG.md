33e17fa5cc056c37fa1383cc6213e1a117693a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 06:05:12 +0000

    Add package ce-ui/1.6.1/ce-ui-1.6.1.tar.gz

---
+++ packages/ce-ui/ce-ui-1.6.1.tar.gz/ce-ui-1.6.1/CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog for plugin *ce-ui*
+
+## 1.6.1 (not yet released)
+
+- MAINT: Adding gitignore
+
+## 1.6.0 (2024-01-22)
+
+- ENH: Select search order
+- ENH: Report creation date
+
+## 1.5.1 (2024-01-21)
+
+- ENH: Added alert for failing uploads
+- BUG: Fixed search (#45)
+- BUG: Fixed display of alerts
+- MAINT: Nicer placeholders while loading dataset
+
+## 1.5.0 (2024-01-20)
+
+- MAINT: Split user interface module from main TopoBank
+- MAINT: Fix Boostrap style on close button of modal windows
+- MAINT: Reorganized layout of left control buttons
+- MAINT: Enforcing PEP-8 style
---
+++ packages/ce-ui/ce-ui-1.6.1.tar.gz/ce-ui-1.6.1/ce_ui/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from topobank.fixtures import handle_usage_statistics, sync_analysis_functions, test_analysis_function  # noqa: F401
+from topobank.manager.tests.utils import two_topos, user_three_topographies_three_surfaces_three_tags, \
+    UserFactory  # noqa: F401
+from topobank.organizations.tests.utils import OrganizationFactory
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def user_with_plugin():
+    org_name = "Test Organization"
+    org = OrganizationFactory(name=org_name, plugins_available="ce_ui")
+    user = UserFactory()
+    user.groups.add(org.group)
+    return user
