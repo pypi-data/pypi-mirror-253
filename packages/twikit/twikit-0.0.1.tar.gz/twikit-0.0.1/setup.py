78cd1e3202e14ec349b36f13d5187f1c88110008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:06:58 +0000

    Add package twikit/0.0.1/twikit-0.0.1.tar.gz

---
+++ packages/twikit/twikit-0.0.1.tar.gz/twikit-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open('readme.MD') as f:
+    long_description = f.read()
+
+setup(
+    name='twikit',
+    version='0.0.1',
+    install_requires=['httpx'],
+    description='Twitter api wrapper for python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
