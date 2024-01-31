1d2d36f64da690567e53f12e91187f01e0950600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:07:56 +0000

    Add package darwin-py/0.8.57/darwin_py-0.8.57-py3-none-any.whl

---
+++ packages/darwin-py/darwin_py-0.8.57-py3-none-any.whl/darwin/exporter/formats/__init__.py
@@ -0,0 +1,18 @@
+from typing import List
+
+# When updating this file, please also update the docs:
+# https://docs.v7labs.com/docs/convert-1
+supported_formats: List[str] = [
+    "coco",
+    "cvat",
+    "dataloop",
+    "darwin_1.0",
+    "instance_mask",
+    "pascalvoc",
+    "semantic_mask",
+    "semantic_mask_grey",
+    "semantic_mask_index",
+    "yolo",
+    "yolo_segmented",
+    "nifti",
+]
