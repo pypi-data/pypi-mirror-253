afcfb98ce20edfe4d31ae4458f9b952c37ad4786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:05:38 +0000

    Add package django-arcgis-address/1.2.5/django_arcgis_address-1.2.5-py3-none-any.whl

---
+++ packages/django-arcgis-address/django_arcgis_address-1.2.5-py3-none-any.whl/address/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class AddressConfig(AppConfig):
+    """
+    Define config for the member app so that we can hook in signals.
+    """
+
+    name = "address"
