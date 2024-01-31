d59e8a2faf38951199ba9edbc6f01385985a6aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:05:35 +0000

    Add package flask-pagerouter/0.1.2/flask-pagerouter-0.1.2.tar.gz

---
+++ packages/flask-pagerouter/flask-pagerouter-0.1.2.tar.gz/flask-pagerouter-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='flask-pagerouter',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+    ],
+    author='Etienne DTS',
+    description='A lightweight Python library for page routing in Flask applications.',
+    license='MIT',
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
