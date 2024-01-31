1eab394efb7c7be9b6f3ffd2a5a3464b47123471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:07:04 +0000

    Add package ign8/4.7.72/ign8-4.7.72.tar.gz

---
+++ packages/ign8/ign8-4.7.72.tar.gz/ign8-4.7.72/src/ign8/ui/project/ignite/main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import maindata, service, project
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework import viewsets
+
+
+@csrf_exempt
+def mainview(request):
+    maindata_entries = maindata.objects.all()
+    maindata_services = service.objects.all()
+    maindata_projects = project.objects.all()
+    return render(request, 'main.html', {'maindata_entries': maindata_entries, 'title': 'IGN8', 'maindata_services': maindata_services , 'maindata_projects': maindata_projects})
+
+@csrf_exempt
+def projectdetail(request, name=None):
+
+    if name:
+        myproject = project.objects.get(name=name)
+        return render(request, 'projectdetail.html', {'projects': myproject, 'title': 'Project Detail'})    
+    else:
+        myprojects = project.objects.all()
+        return render(request, 'projects.html', {'projects': myprojects, 'title': 'Project list'})    
