93418bd04abb0fe457a3034c0d73d4169b9f6e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:05:23 +0000

    Add package Supermarq/0.5.8/Supermarq-0.5.8.tar.gz

---
+++ packages/Supermarq/Supermarq-0.5.8.tar.gz/Supermarq-0.5.8/supermarq/benchmark.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import abc
+from collections.abc import Sequence
+from typing import Any
+
+import cirq
+
+
+class Benchmark:
+    """Class representing a quantum benchmark application.
+
+    Concrete subclasses must implement the abstract methods ``circuit()`` and
+    ``score()``.
+
+    Each instantiation of a `Benchmark` object represents a single, fully defined
+    benchmark application. All the relevant parameters for a benchmark should
+    be passed in upon creation, and will be used to generate the correct circuit
+    and compute the final score.
+    """
+
+    @abc.abstractmethod
+    def circuit(self) -> cirq.Circuit | Sequence[cirq.Circuit]:
+        """Returns the quantum circuit corresponding to the current benchmark parameters."""
+
+    @abc.abstractmethod
+    def score(self, counts: Any) -> float:
+        """Returns a normalized [0,1] score reflecting device performance.
+
+        Args:
+            counts: A dictionary containing the measurement counts from execution.
+        """
