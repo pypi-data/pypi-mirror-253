fb26f707546ee6d9ff19c90b4c7a17f49392a77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:06:13 +0000

    Add package lyytools/1.5/lyytools-1.5.tar.gz

---
+++ packages/lyytools/lyytools-1.5.tar.gz/lyytools-1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyytools',
+    version='1.5',
+    author='lyy',
+    author_email='',
+    description='tools for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
