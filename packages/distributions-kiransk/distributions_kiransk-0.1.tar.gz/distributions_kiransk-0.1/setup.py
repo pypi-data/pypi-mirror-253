5e8ef8f372ebb285a631225f9de110165a8ad7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:05:49 +0000

    Add package distributions-kiransk/0.1/distributions_kiransk-0.1.tar.gz

---
+++ packages/distributions-kiransk/distributions_kiransk-0.1.tar.gz/distributions_kiransk-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_kiransk',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_kiransk'],
+      author = 'Sai Kiran Tanikella',
+      author_email = 'saikirantanikella@gmail.com',
+      zip_safe=False)
