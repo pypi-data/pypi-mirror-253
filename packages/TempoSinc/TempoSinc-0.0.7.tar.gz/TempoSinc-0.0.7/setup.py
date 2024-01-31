c1e9d9636144a626f974404ca72c6dfe47c30ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:07:28 +0000

    Add package TempoSinc/0.0.7/TempoSinc-0.0.7.tar.gz

---
+++ packages/TempoSinc/TempoSinc-0.0.7.tar.gz/TempoSinc-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='TempoSinc',
+    version='0.0.7',
+    license='MIT License',
+    author='RaiLeal/JoseMateus',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='temposinc.gerencia@gmail.com',
+    keywords='TempoSinc',
+    description=u'Previsão de tempo',
+    packages=['TempoSinc'],
+    install_requires=['requests', 'pandas', 'geopy', 'matplotlib'],)
