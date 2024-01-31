fb06a90c15f956cd5327f12101e014688ef6e7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:06:19 +0000

    Add package pygwalker/0.4.5a3/pygwalker-0.4.5a3-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.4.5a3-py3-none-any.whl/pygwalker/templates/dist/interfaces/index.d.ts
@@ -0,0 +1,47 @@
+import type { IRow, IMutField } from '@kanaries/graphic-walker/dist/interfaces';
+import type { IDarkMode, IThemeKey, IComputationFunction } from '@kanaries/graphic-walker/dist/interfaces';
+export interface IAppProps {
+    hideDataSourceConfig: boolean;
+    fieldkeyGuard: boolean;
+    themeKey: IThemeKey;
+    dark: IDarkMode;
+    dataSource: IRow[];
+    rawFields: IMutField[];
+    id: string;
+    dataSourceProps: IDataSourceProps;
+    version?: string;
+    hashcode?: string;
+    visSpec: any;
+    userConfig?: IUserConfig;
+    env?: string;
+    needLoadDatas?: boolean;
+    specType: string;
+    showCloudTool: boolean;
+    needInitChart: boolean;
+    useKernelCalc: boolean;
+    useSaveTool: boolean;
+    parseDslType: "server" | "client";
+    communicationUrl: string;
+    gwMode: "explore" | "renderer";
+    needLoadLastSpec: boolean;
+    extraConfig?: any;
+    fieldMetas: any;
+    isExportDataFrame: boolean;
+}
+export interface IDataSourceProps {
+    tunnelId: string;
+    dataSourceId: string;
+}
+export interface IUserConfig {
+    [key: string]: any;
+    privacy: 'events' | 'update-only' | 'offline';
+}
+export interface IGraphicRendererProps {
+    themeKey: IThemeKey;
+    dark: IDarkMode;
+    dataSource?: IRow[];
+    rawFields: IMutField[];
+    charts: any;
+    useKernelCalc: boolean;
+    computation?: IComputationFunction;
+}
