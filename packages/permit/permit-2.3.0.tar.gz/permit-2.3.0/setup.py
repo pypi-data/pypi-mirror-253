39a0c71d4d24f8ff79902c005490fd16e7f62f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:05:47 +0000

    Add package permit/2.3.0/permit-2.3.0.tar.gz

---
+++ packages/permit/permit-2.3.0.tar.gz/permit-2.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+def get_requirements(env=""):
+    if env:
+        env = "-{}".format(env)
+    with open("requirements{}.txt".format(env)) as fp:
+        return [x.strip() for x in fp.read().split("\n") if not x.startswith("#")]
+
+
+setup(
+    name="permit",
+    version="2.3.0",
+    packages=find_packages(),
+    author="Asaf Cohen",
+    author_email="asaf@permit.io",
+    license="Apache 2.0",
+    python_requires=">=3.8",
+    description="Permit.io python sdk",
+    install_requires=get_requirements(),
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
