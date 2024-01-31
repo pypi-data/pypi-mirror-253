2bc96c1596a1f32a56ef0df0034da19da5580d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:07:51 +0000

    Add package aiosu/2.1.9/aiosu-2.1.9-py3-none-any.whl

---
+++ packages/aiosu/aiosu-2.1.9-py3-none-any.whl/aiosu/exceptions.py
@@ -0,0 +1,31 @@
+"""
+This module contains custom exception types.
+"""
+from __future__ import annotations
+
+__all__ = ("APIException",)
+
+
+class APIException(Exception):
+    """API Exception Class
+
+    :param status: status code from the API
+    :type status: int
+    :param message: error message returned, defaults to ""
+    :type message: str, optional
+    """
+
+    def __init__(self, status: int, message: str = "") -> None:
+        super().__init__(message)
+        self.status = status
+
+
+class InvalidClientRequestedError(Exception):
+    """Invalid Client Requested Error
+
+    :param message: error message, defaults to ""
+    :type message: str, optional
+    """
+
+    def __init__(self, message: str = "") -> None:
+        super().__init__(message)
