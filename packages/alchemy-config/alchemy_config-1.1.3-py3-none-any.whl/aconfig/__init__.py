671e6630f32e3b91811e31312d5373fab7f05fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:05:19 +0000

    Add package alchemy-config/1.1.3/alchemy_config-1.1.3-py3-none-any.whl

---
+++ packages/alchemy-config/alchemy_config-1.1.3-py3-none-any.whl/aconfig/__init__.py
@@ -0,0 +1,11 @@
+#*****************************************************************#
+# (C) Copyright IBM Corporation 2020.                             #
+#                                                                 #
+# The source code for this program is not published or otherwise  #
+# divested of its trade secrets, irrespective of what has been    #
+# deposited with the U.S. Copyright Office.                       #
+#*****************************************************************#
+'''Enable calls to desired components of package.
+'''
+
+from .aconfig import AttributeAccessDict, Config, ImmutableAttributeAccessDict, ImmutableConfig
