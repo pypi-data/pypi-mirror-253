91a12de3b37e9bd81082f1faf2bc253452469f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:08:26 +0000

    Add package SearchForThis/0.1.2/SearchForThis-0.1.2.tar.gz

---
+++ packages/SearchForThis/SearchForThis-0.1.2.tar.gz/SearchForThis-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+import codecs
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'descricao.md'), encoding='utf-8') as fh:
+    readme = '\n' + fh.read()
+ 
+VERSION = '0.1.2'
+DESCRIPTION = 'Projeto final da disciplina de Programação Orientada a Objetos II'
+
+setup(
+    name = 'SearchForThis',
+    version = VERSION,
+    author = 'liedson',
+    author_email = 'fabricoliedson@gamil.com',
+    description = DESCRIPTION,
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    packages = ['SearchForThis'],
+    python_requires = '>=3.6',
+    install_requires = ['googlesearch-python==1.2.3', 'webbrowser'],
+
+    keywords = ['python', 'web', 'search'],
+    classifiers = [
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+    ]
+ 
+)
+
+# pypi-AgEIcHlwaS5vcmcCJGM1MjY4MWQyLWFhYmEtNDA5ZS05ZDNjLTQxNWY0Mzc3OTM1NwACKlszLCIxZWNkZDA1Yi0zM2VlLTQ4ZDAtOWIxZS05ZjM0NjRkYjFmYjEiXQAABiBCi99wIevAE2UyLF9i1S1QmY3bIn_V4lJyh5XJnnXwvQ
