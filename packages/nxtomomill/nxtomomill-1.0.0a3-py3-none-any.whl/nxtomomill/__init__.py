130d74e2ac9f46ff649ceb59c6bfeff84bd887ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:08:50 +0000

    Add package nxtomomill/1.0.0a3/nxtomomill-1.0.0a3-py3-none-any.whl

---
+++ packages/nxtomomill/nxtomomill-1.0.0a3-py3-none-any.whl/nxtomomill/__init__.py
@@ -0,0 +1,4 @@
+"""a set of applications and tools around the `NXtomo <https://manual.nexusformat.org/classes/applications/NXtomo.html>`_ format defined by the `NeXus community <https://manual.nexusformat.org/index.html>`_"""
+from .version import version as __version
+
+__version__ = __version
