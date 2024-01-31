4269f59721773dfeb19fbee1ca18527fe8f5bbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:06:01 +0000

    Add package mkdocs-ja-ruby/0.0.2/mkdocs-ja-ruby-0.0.2.tar.gz

---
+++ packages/mkdocs-ja-ruby/mkdocs-ja-ruby-0.0.2.tar.gz/mkdocs-ja-ruby-0.0.2/mkdocs_ja_ruby/plugin.py
@@ -0,0 +1,11 @@
+import re
+from mkdocs.plugins import BasePlugin
+
+class ExtendedMarkdown(BasePlugin):
+    def __init__(self):
+        self.raw_pattern = re.compile(r"\[@ ([^\[\]]+) ([^\(\)]+)\]")
+        self.ruby_pattern = r"<ruby>\1<rp>(</rp><rt>\2</rt><rp>)</rp></ruby>"
+
+    def on_page_markdown(self, markdown, page, config, files):
+        markdown = re.sub(self.raw_pattern, self.ruby_pattern, markdown)
+        return markdown
