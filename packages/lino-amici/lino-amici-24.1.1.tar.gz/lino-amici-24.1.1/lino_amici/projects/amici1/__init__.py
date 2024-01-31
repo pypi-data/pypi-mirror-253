62c672624f9f6a6d0f7296ef6c6230b78796ee13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:05:34 +0000

    Add package lino-amici/24.1.1/lino-amici-24.1.1.tar.gz

---
+++ packages/lino-amici/lino-amici-24.1.1.tar.gz/lino-amici-24.1.1/lino_amici/projects/amici1/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 Luc Saffre
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+"""Shows Lino Amici as a family management tool.
+
+.. autosummary::
+   :toctree:
+
+   settings
+
+"""
+
