001294e821599000f7cdcbb4f95753fdaa233c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 03:07:48 +0000

    Add package finter/0.0.39/finter-0.0.39-py3-none-any.whl

---
+++ packages/finter/finter-0.0.39-py3-none-any.whl/finter/framework_model/__init__.py
@@ -0,0 +1,7 @@
+from finter.framework_model.cm_loader import ContentModelLoader
+from finter.framework_model.alpha import BaseAlpha
+from finter.framework_model.portfolio import BasePortfolio
+from finter.framework_model.alpha_loader import AlphaPositionLoader
+from finter.framework_model.simulation import adj_stat_container_helper
+from finter.framework_model.calendar import iter_trading_days
+from finter.framework_model.universe import get_universe_list
