bd99f9fdc0848c731fc09e706bf5ff4baeb52b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:06:39 +0000

    Add package nvidia-dali-cuda110/1.34.0/nvidia_dali_cuda110-1.34.0-12152783-py3-none-manylinux2014_aarch64.whl

---
+++ packages/nvidia-dali-cuda110/nvidia_dali_cuda110-1.34.0-12152783-py3-none-manylinux2014_aarch64.whl/nvidia/dali/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2017-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# we need to have that defined and accessible before any other DALI import
+__version__ = '1.34.0'
+__cuda_version__ = int('11.8.89'.replace('.', ''))
+__git_sha__ = 'a695a4920b4797442287e3feacbc45369c3ae693'
+
+from . import ops
+from . import pipeline
+from . import tensors
+from . import fn
+from . import data_node
+from . import math
+from . import tfrecord
+from . import types
+from . import plugin_manager
+from . import sysconfig
+from .pipeline import Pipeline, pipeline_def
+from .data_node import newaxis
