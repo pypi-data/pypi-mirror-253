4d10a2bf089ffac283125cc9a9d3dd064382fab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 05:05:13 +0000

    Add package matematikk/0.0.18/matematikk-0.0.18-py3-none-any.whl

---
+++ packages/matematikk/matematikk-0.0.18-py3-none-any.whl/matematikk/__init__.py
@@ -0,0 +1,21 @@
+# from kode import superløs
+# from . import kode
+# from matematikk import kode
+
+from .superlos_def import (
+    superløs)
+from .superlos_def import (
+    Eq,
+    Reals,
+    core,
+    Symbol,
+    ConditionSet,
+    FiniteSet,
+    Intersection,
+    solve,
+    solveset,
+    nsolve)
+
+from .reggis_def import (
+    reggis)
+
