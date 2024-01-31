3c46fc239f931ffaf8e255482b2b72bdcc53217f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:05:47 +0000

    Add package aiotieba/3.7.6/aiotieba-3.7.6-pp38-pypy38_pp73-macosx_10_9_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.7.6-pp38-pypy38_pp73-macosx_10_9_x86_64.whl/aiotieba/api/add_bawu_blacklist/_api.py
@@ -0,0 +1,35 @@
+import sys
+
+import yarl
+
+from ...const import WEB_BASE_HOST
+from ...core import HttpCore
+from ...exception import TiebaServerError
+from ...helper import log_success, parse_json
+
+
+def parse_body(body: bytes) -> None:
+    res_json = parse_json(body)
+    if code := res_json['errno']:
+        raise TiebaServerError(code, res_json['errmsg'])
+
+
+async def request(http_core: HttpCore, fname: str, user_id: int) -> bool:
+    data = [
+        ('tbs', http_core.account._tbs),
+        ('user_id', user_id),
+        ('word', fname),
+        ('ie', 'utf-8'),
+    ]
+
+    request = http_core.pack_web_form_request(
+        yarl.URL.build(scheme="http", host=WEB_BASE_HOST, path="/bawu2/platform/addBlack"), data
+    )
+
+    __log__ = f"fname={fname} user_id={user_id}"
+
+    body = await http_core.net_core.send_request(request, read_bufsize=2 * 1024)
+    parse_body(body)
+
+    log_success(sys._getframe(1), __log__)
+    return True
---
+++ packages/aiotieba/aiotieba-3.7.6-pp38-pypy38_pp73-macosx_10_9_x86_64.whl/aiotieba/api/good/_api.py
@@ -0,0 +1,38 @@
+import sys
+
+import yarl
+
+from ...const import APP_BASE_HOST, APP_SECURE_SCHEME
+from ...core import HttpCore
+from ...exception import TiebaServerError
+from ...helper import log_success, parse_json
+
+
+def parse_body(body: bytes) -> None:
+    res_json = parse_json(body)
+    if code := int(res_json['error_code']):
+        raise TiebaServerError(code, res_json['error_msg'])
+
+
+async def request(http_core: HttpCore, fname: str, fid: int, tid: int, cid: int) -> bool:
+    data = [
+        ('BDUSS', http_core.account._BDUSS),
+        ('cid', cid),
+        ('fid', fid),
+        ('ntn', 'set'),
+        ('tbs', http_core.account._tbs),
+        ('word', fname),
+        ('z', tid),
+    ]
+
+    request = http_core.pack_form_request(
+        yarl.URL.build(scheme=APP_SECURE_SCHEME, host=APP_BASE_HOST, path="/c/c/bawu/commitgood"), data
+    )
+
+    __log__ = f"fname={fname} tid={tid} cid={cid}"
+
+    body = await http_core.net_core.send_request(request, read_bufsize=1024)
+    parse_body(body)
+
+    log_success(sys._getframe(1), __log__)
+    return True
