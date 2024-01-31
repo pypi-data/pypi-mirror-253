4df4b7ea8450e6c2e5094955e7070902b1284a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-31 04:05:46 +0000

    Add package aiotieba/3.7.6/aiotieba-3.7.6-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.7.6-cp312-cp312-macosx_10_9_x86_64.whl/aiotieba/api/get_bawu_blacklist/_api.py
@@ -0,0 +1,29 @@
+import bs4
+import yarl
+
+from ...const import WEB_BASE_HOST
+from ...core import HttpCore
+from ._classdef import BawuBlacklistUsers
+
+
+def parse_body(body: bytes) -> BawuBlacklistUsers:
+    soup = bs4.BeautifulSoup(body, 'lxml')
+    bawu_blacklist_users = BawuBlacklistUsers(soup)
+
+    return bawu_blacklist_users
+
+
+async def request(http_core: HttpCore, fname: str, pn: int) -> BawuBlacklistUsers:
+    params = [
+        ('word', fname),
+        ('pn', pn),
+    ]
+
+    request = http_core.pack_web_get_request(
+        yarl.URL.build(scheme="https", host=WEB_BASE_HOST, path="/bawu2/platform/listBlackUser"), params
+    )
+
+    __log__ = "fname={fname}"  # noqa: F841
+
+    body = await http_core.net_core.send_request(request, read_bufsize=64 * 1024)
+    return parse_body(body)
